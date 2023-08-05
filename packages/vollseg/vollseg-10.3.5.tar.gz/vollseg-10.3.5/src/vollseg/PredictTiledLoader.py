25c254c75f60b0e3425c94137e17d23d2992f830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package vollseg/10.3.5/vollseg-10.3.5.tar.gz

---
+++ packages/vollseg/vollseg-10.3.5.tar.gz/vollseg-10.3.5/src/vollseg/PredictTiledLoader.py
@@ -0,0 +1,117 @@
+import numpy as np
+from torch.utils.data import Dataset
+import itertools
+from scipy.ndimage import distance_transform_edt
+
+
+class PredictTiled(Dataset):
+
+    """
+    Dataset of fluorescently labeled cell membranes
+    """
+
+    def __init__(
+        self,
+        image: np.ndarray,
+        patch_size=(64, 128, 128),
+        overlap=(1, 16, 16),
+        crop=(2, 32, 32),
+    ):
+
+        # Sanity checks
+        assert len(patch_size) == 3, "Patch size must be 3-dimensional."
+
+        # Save parameters
+        self.image = image
+        self.patch_size = patch_size
+        self.overlap = overlap
+        self.crop = crop
+        self.set_data_idx()
+        self._get_fading_map()
+
+    def _get_fading_map(self):
+
+        self.fading_map = np.ones(self.patch_size)
+
+        if all([c == 0 for c in self.crop]):
+            self.crop = [1, 1, 1]
+
+        # Exclude crop region
+        crop_masking = np.zeros_like(self.fading_map)
+        crop_masking[
+            self.crop[0] : self.patch_size[0] - self.crop[0],
+            self.crop[1] : self.patch_size[1] - self.crop[1],
+            self.crop[2] : self.patch_size[2] - self.crop[2],
+        ] = 1
+        self.fading_map = self.fading_map * crop_masking
+
+        self.fading_map = distance_transform_edt(self.fading_map).astype(
+            np.float32
+        )
+
+        # Normalize
+        self.fading_map = self.fading_map / self.fading_map.max()
+
+    def set_data_idx(self):
+
+        self.data_shape = self.image.shape
+        # Calculate the position of each tile
+        locations = []
+        for i, p, o, c in zip(
+            self.data_shape, self.patch_size, self.overlap, self.crop
+        ):
+            # get starting coords
+            coords = (
+                np.arange(
+                    np.ceil((i + o + c) / np.maximum(p - o - 2 * c, 1)),
+                    dtype=np.int16,
+                )
+                * np.maximum(p - o - 2 * c, 1)
+                - o
+                - c
+            )
+            locations.append(coords)
+        self.locations = list(itertools.product(*locations))
+        self.global_crop_before = np.abs(
+            np.min(np.array(self.locations), axis=0)
+        )
+        self.global_crop_after = (
+            np.array(self.data_shape)
+            - np.max(np.array(self.locations), axis=0)
+            - np.array(self.patch_size)
+        )
+
+    def __len__(self):
+
+        return len(self.locations)
+
+    def __getitem__(self, idx):
+
+        self.patch_start = np.array(self.locations[idx])
+        self.patch_end = self.patch_start + np.array(self.patch_size)
+
+        pad_before = np.maximum(-self.patch_start, 0)
+        pad_after = np.maximum(self.patch_end - np.array(self.data_shape), 0)
+        pad_width = list(zip(pad_before, pad_after))
+
+        slicing = tuple(
+            map(slice, np.maximum(self.patch_start, 0), self.patch_end)
+        )
+
+        image_tmp = self.image
+        image_tmp = image_tmp[slicing]
+        image_tmp = np.pad(image_tmp, pad_width, mode="reflect")
+
+        self.image = image_tmp
+
+        sample = {
+            "image": self.image,
+            "fading_map": self.fading_map,
+            "locations": np.asarray(self.locations),
+            "global_crop_before": self.global_crop_before,
+            "global_crop_after": self.global_crop_after,
+            "patch_start": self.patch_start,
+            "patch_end": self.patch_end,
+        }
+
+        return sample
