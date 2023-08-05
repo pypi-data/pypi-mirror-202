1d0eef8382409444afb01854bd1656ba94f5c443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:28 +0000

    Add package hds-stats/0.1.1/hds-stats-0.1.1.tar.gz

---
+++ packages/hds-stats/hds-stats-0.1.1.tar.gz/hds-stats-0.1.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open(file = "README.md", mode = "r", encoding = "utf-8") as file:
+    long_description = file.read()
+
+setup(
+    name = "hds-stats",
+    
+    version = "0.1.1",
+    
+    author = "HelloDataScience",
+    
+    author_email = "hellodatasciencekorea@gmail.com",
+    
+    description = "Useful functions for Statistics and Machine Learning",
+    
+    long_description = long_description,
+    
+    long_description_content_type = "text/markdown",
+    
+    url = "https://github.com/HelloDataScience/hds-stats",
+    
+    license = "MIT",
+    
+    py_modules = ['hds_plot', 'hds_stat'],
+    
+    project_urls = {
+        "Bug Tracker": "https://github.com/HelloDataScience/hds-stats/issues",
+    },
+    
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+    python_requires = ">=3.6",
+    
+    packages = find_packages(),
+    
+    install_requires = ['numpy', 'pandas', 'scipy', 'seaborn', 'matplotlib', 'statsmodels', 'scikit-learn'],
+    
+    # package_dir = {"": "src"},
+)
