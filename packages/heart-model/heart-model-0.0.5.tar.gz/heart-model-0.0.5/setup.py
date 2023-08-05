ad5d28dd769701f43df96275987ca8a548ad1e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:27 +0000

    Add package heart-model/0.0.5/heart-model-0.0.5.tar.gz

---
+++ packages/heart-model/heart-model-0.0.5.tar.gz/heart-model-0.0.5/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = "heart-model"
+DESCRIPTION = "Heart decease model package."
+URL = "https://github.com/Ra7777/heart_model"
+EMAIL = "rashmeilll@gmail.com"
+AUTHOR = "Rashid Meylanov"
+REQUIRES_PYTHON = ">=3.10.0"
+
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the
+# Trove Classifier for that!
+long_description = DESCRIPTION
+
+# Load the package's VERSION file as a dictionary.
+about = {}
+ROOT_DIR = Path(__file__).resolve().parent
+REQUIREMENTS_DIR = ROOT_DIR / "requirements"
+PACKAGE_DIR = ROOT_DIR / "heart_model"
+with open(PACKAGE_DIR / "VERSION") as f:
+    _version = f.read().strip()
+    about["__version__"] = _version
+
+
+# What packages are required for this module to be executed?
+def list_reqs(fname="requirements.txt"):
+    with open(REQUIREMENTS_DIR / fname) as fd:
+        return fd.read().splitlines()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=("tests",)),
+    package_data={"heart_model": ["VERSION"]},
+    install_requires=list_reqs(),
+    extras_require={},
+    include_package_data=True,
+    license="BSD-3",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
