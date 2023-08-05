9cf7ebde3d1fd9b91acc273703f29b5b271f2b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:31 +0000

    Add package blockertest/0.1.0/blockertest-0.1.0-py3-none-any.whl

---
+++ packages/blockertest/blockertest-0.1.0-py3-none-any.whl/blockertest/main.py
@@ -0,0 +1,20 @@
+import typer
+
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    Awesome Portal Gun
+    """
+
+
+@app.command()
+def shoot():
+    """
+    Shoot the portal gun
+    """
+    typer.echo("Shooting portal gun")
+
