d051080e7e486ff1760069a8a2b7d48aef9b3817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package desmos2python/0.3.2/desmos2python-0.3.2.tar.gz

---
+++ packages/desmos2python/desmos2python-0.3.2.tar.gz/desmos2python-0.3.2/fix_readme.py
@@ -0,0 +1,48 @@
+"""convert to RST (for pypi)
+
+ref: https://stackoverflow.com/a/74086651/1871569
+"""
+import os
+import re
+import logging
+import traceback
+from urllib.error import URLError
+
+
+def convert2rst():
+
+    long_description = None
+
+    import pypandoc
+    from pypandoc.pandoc_download import download_pandoc
+
+    # see the documentation how to customize the installation path
+    # but be aware that you then need to include it in the `PATH`
+    try:
+        download_pandoc()
+        os.system('dpkg -x ./$(ls pandoc*.deb|head -1) pandoc_bin')
+    except URLError:
+        logging.warning(traceback.format_exc())
+    finally:
+        #: convert -> rst
+        if os.path.exists('./pandoc_bin/'):
+            os.system('./pandoc_bin/usr/bin/pandoc -s README.md -t rst -o README.rst~')
+        else:
+            os.system('pandoc -s README.md -t rst -o README.rst~')
+
+    #: replace the description in the RST version
+    with open('README.rst~', 'r') as f:
+        long_description = f.read()
+    pattern = re.compile(
+        (r'\W+(TOC)\W+(\.\. raw\:\: html)\W+(\<!-- [\w\W]* --\>([\w\W.]*)'
+         r'<!-- markdown-toc end -->)'))
+    long_description = \
+        re.subn(pattern=pattern, string=long_description, repl=r'')[0]
+    with open('README.rst', 'w') as f:
+        f.write(long_description)
+    os.system('rm README.rst~')
+    print('...done with fix_readme.py')
+    return True
+
+if __name__ == '__main__':
+    convert2rst()
---
+++ packages/desmos2python/desmos2python-0.3.2.tar.gz/desmos2python-0.3.2/tests/run_tests.py
@@ -0,0 +1,70 @@
+import os
+import logging
+import sys
+from pathlib import Path
+import unittest
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('Agg')
+
+rpath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), '..', 'src'))
+if rpath not in sys.path:
+    sys.path.insert(0, rpath)
+
+
+class TestDesmosWebSession(unittest.TestCase):
+    def testDWS(self):
+        from desmos2python import DesmosWebSession
+        dws = DesmosWebSession()
+        ll = dws.latex_list
+        self.assertEqual(True, len(ll) > 0)
+        dws.export_latex2json()
+
+    def testSVG(self):
+        from desmos2python import DesmosWebSession
+        dws = DemosWebSession(url='https://www.desmos.com/calculator/t68sf7nwdo')
+        pth = dws.export_svgScreenshot()
+        self.assertTrue(len(pth.read_text()) > 0)
+
+
+class TestDesmos2Python(unittest.TestCase):
+    def setUp(self):
+        self.dlp, self.dmn = None, None
+
+    def tearDown(self):
+        del self.dlp
+        del self.dmn
+
+    def testDLP(self):
+        from desmos2python import DesmosLatexParser
+        dlp = DesmosLatexParser()
+        logging.info(dlp.pycode_string)
+        dmn = dlp.DesmosModelNS()
+        x = np.linspace(0, 24, num=100)
+        y = dmn.F(x)
+        plt.plot(x, y)
+        imfn, expfn = \
+            Path(__file__).parent.joinpath('ex.png'), \
+            Path(__file__).parent.joinpath('expected_ex.png')
+        plt.savefig(str(imfn))
+        plt.close()
+        logging.info('...saved test output to tests/ex.png')
+        logging.debug('comparing saved image to expected...')
+        with open(imfn, 'rb') as fp:
+            imbytes = fp.read()
+        with open(expfn, 'rb') as fp:
+            expbytes = fp.read()
+        self.assertEqual(expbytes, imbytes)
+        x1 = [0.1, 0.3, 0.5, 0.9, 1.0, 2.0, 100.0]
+        logging.info('x=', x1)
+        y1 = dmn.F(x1)
+        logging.info('y = F(x): ', y1)
+        self.dlp = dlp
+        self.dmn = dmn
+
+
+if __name__ == '__main__':
+    unittest.main()
