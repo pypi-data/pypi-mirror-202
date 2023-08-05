f2bb05c739f4730374d87dcb1efc622de40b7987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:31 +0000

    Add package Goosu/0.2/Goosu-0.2.tar.gz

---
+++ packages/Goosu/Goosu-0.2.tar.gz/Goosu-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name="Goosu",
+    version='0.2',
+    install_requires = ['requests'],
+    author_email='Leha.metlushko@bk.ru',
+    url='https://github.com/Qquiqlerr/Goosu',
+    download_url = 'https://github.com/Qquiqlerr/Goosu.git',
+    packages=['Goosu'],
+
+)
