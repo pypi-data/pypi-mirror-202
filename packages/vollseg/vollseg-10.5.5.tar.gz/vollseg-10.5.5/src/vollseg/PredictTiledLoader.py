232b5abcd9f857f8bcb218b15cd00cb609c5de98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:13 +0000

    Add package vollseg/10.5.5/vollseg-10.5.5.tar.gz

---
+++ packages/vollseg/vollseg-10.5.5.tar.gz/vollseg-10.5.5/src/vollseg/PredictTiledLoader.py
@@ -0,0 +1,46 @@
+import numpy as np
+from torch.utils.data import Dataset
+from .Tiles_3D import VolumeSlicer
+from torchvision import transforms
+
+
+class PredictTiled(Dataset):
+
+    """
+    Dataset of fluorescently labeled cell membranes
+    """
+
+    def __init__(
+        self,
+        image: np.ndarray,
+        patch_size=(8, 256, 256),
+        patch_step=(2, 64, 64),
+    ):
+
+        # Sanity checks
+        assert len(patch_size) == 3, "Patch size must be 3-dimensional."
+        self.transform = transforms.Compose([transforms.ToTensor()])
+        # Save parameters
+        self.image = image
+        self.patch_size = patch_size
+        self.patch_step = patch_step
+
+        self.tiler = VolumeSlicer(
+            self.image.shape,
+            voxel_size=self.patch_size,
+            voxel_step=self.patch_step,
+        )
+
+        self.tiles = self.tiler.split(self.image)
+
+    def __len__(self):
+
+        return len(self.tiles)
+
+    def __getitem__(self, idx):
+
+        tiles_batch = self.transform(self.tiles[idx])
+
+        coords_batch = self.transform(list(self.tiler.crops[idx]))
+
+        return tiles_batch, coords_batch
