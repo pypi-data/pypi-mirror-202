67f6fc66be8d507d5d128d25c60209788200a0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:39 +0000

    Add package phcmas/0.1/phcmas-0.1.tar.gz

---
+++ packages/phcmas/phcmas-0.1.tar.gz/phcmas-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name="phcmas",
+    version="0.1",
+    author="Seungkyun Park",
+    author_email="phcmas1@gmail.com",
+    description="This is very basic calculator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
