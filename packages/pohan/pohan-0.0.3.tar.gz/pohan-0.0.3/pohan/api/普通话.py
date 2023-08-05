4f707f6013ceb48221a49177b615eb9f6681e83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:20 +0000

    Add package pohan/0.0.3/pohan-0.0.3.tar.gz

---
+++ packages/pohan/pohan-0.0.3.tar.gz/pohan-0.0.3/pohan/api/普通话.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+'''
+@Author  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@WeChat     ：CoderWanFeng
+@Blog      ：www.python-office.com
+@Date    ：2023/4/6 0:46
+@Description     ：
+'''
+
+显示 = print
+
+# 如果 = if()
+# 循环 =
