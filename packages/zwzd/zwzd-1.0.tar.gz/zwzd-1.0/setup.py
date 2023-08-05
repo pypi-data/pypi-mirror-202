33d3a63ed19c94d334ddf616c0059ff1e116bf7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:52 +0000

    Add package zwzd/1.0/zwzd-1.0.tar.gz

---
+++ packages/zwzd/zwzd-1.0.tar.gz/zwzd-1.0/setup.py
@@ -0,0 +1,11 @@
+﻿from setuptools import setup,find_packages
+
+setup(
+    name='zwzd',
+    version=1.0,
+    author='小松果',
+    author_email='aaron.dongsq@hotmail.com',
+    maintainer='小松果',
+    maintainer_email='aaron.dongsq@hotmail.com',
+    url='https://www.bilibili.com/bangumi/play/ep94081',
+    download_url='https://www.bilibili.com/video/BV1x8411n7vv/')
