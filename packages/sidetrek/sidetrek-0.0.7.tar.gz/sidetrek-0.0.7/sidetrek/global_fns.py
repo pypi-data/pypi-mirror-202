48f8e2b736cd20027dae1e970bf27342e7673752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:57 +0000

    Add package sidetrek/0.0.7/sidetrek-0.0.7.tar.gz

---
+++ packages/sidetrek/sidetrek-0.0.7.tar.gz/sidetrek-0.0.7/sidetrek/global_fns.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+from constants import USER_REPO_DATA_DIRNAME
+
+
+def get_project_dir(repo_full_name: str) -> str:
+    # Look for __project_root__.py inside repo dir to get the project dir
+    local_repo_path = f"/{USER_REPO_DATA_DIRNAME}/{repo_full_name}"
+    files = [str(p) for p in list(Path(local_repo_path).glob("**/__project_root__.py"))]
+    if len(files) > 0:
+        return files[0].replace("/__project_root__.py", "")
+    else:
+        return ""
