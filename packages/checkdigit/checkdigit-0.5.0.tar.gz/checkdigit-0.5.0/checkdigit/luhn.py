e78c0da344561ac31bddeef3b68dd24128381421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:50 +0000

    Add package checkdigit/0.5.0/checkdigit-0.5.0.tar.gz

---
+++ packages/checkdigit/checkdigit-0.5.0.tar.gz/checkdigit-0.5.0/checkdigit/luhn.py
@@ -0,0 +1,105 @@
+# /usr/bin/env python
+
+# This file is part of checkdigit.
+
+# checkdigit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# checkdigit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with checkdigit.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Luhn Validation Functions.
+
+The luhn algorithm has a variety of applications, including in credit cards and IMEI numbers.
+
+"""
+
+from checkdigit._data import cleanse, missing_template
+
+
+def calculate(data: str) -> str:
+    """Calculates the luhn check digit.
+
+    Args:
+        data: A block of data without the check digit
+
+    Returns:
+        str: A string representing the missing check digit
+
+    Examples:
+        >>> from checkdigit import luhn
+        >>> luhn.calculate("53251309870224")
+        '3'
+        >>> luhn.calculate("950123440000")
+        '8'
+
+    """
+    data = cleanse(data)
+    # Double every other digit, starting from the final digit backwards
+    # i.e. double 0th digit, 2nd, 4th, ...
+    double_digits = (
+        int(element) if index % 2 else int(element) * 2
+        for index, element in enumerate(data[::-1])
+    )
+    # For digits with more than one digit, sum the digits together
+    # The maximum is 9*2 = 18. This divmod method will work <100
+    sum_digits = sum(sum(divmod(i, 10)) for i in double_digits)
+    # Mod 10 returns 0-9 (not 10 or 11)
+    # Hence convert method not required (faster to use str)
+    return str((sum_digits * 9) % 10)
+
+
+def validate(data: str) -> bool:
+    """Validates a luhn check digit.
+
+    Args:
+        data: A string of characters representing a full luhn code
+
+    Returns:
+        bool: A boolean representing whether the check digit validates the data or not
+
+    Examples:
+        >>> from checkdigit import luhn
+        >>> luhn.validate("541756116585277")
+        True
+        >>> luhn.validate("79927398713")
+        True
+        >>> luhn.validate("49927398717")
+        False
+        >>> luhn.validate("1234567812345678")
+        False
+
+    """
+    data = cleanse(data)
+    return (
+        calculate(data[:-1]) == data[-1]
+    )  # Determines if calculated Check Digit of the data is the last digit given
+
+
+def missing(data: str) -> str:
+    """Calculates a missing digit in a luhn code.
+
+    Args:
+        data: A string of characters representing a full luhn code
+            with a question mark for a missing character
+
+    Returns:
+        str: The missing value that should've been where the question mark was
+
+    Examples:
+        >>> from checkdigit import luhn
+        >>> luhn.missing("54175611658527?")
+        '7'
+        >>> luhn.missing("515853022?76176")
+        '1'
+        >>> luhn.missing("78369216316")
+        'Invalid'
+
+    """
+    return missing_template(data, "luhn")
---
+++ packages/checkdigit/checkdigit-0.5.0.tar.gz/checkdigit-0.5.0/checkdigit/parity.py
@@ -0,0 +1,109 @@
+# /usr/bin/env python
+
+# This file is part of checkdigit.
+
+# checkdigit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# checkdigit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with checkdigit.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Parity Validation Functions.
+
+A parity bit is added to the end of a block of binary as a form of data validation.
+
+"""
+
+from checkdigit._data import cleanse
+
+
+def calculate(data: str, even: bool = True) -> str:
+    """Adds a parity bit onto the end of a block of data.
+
+    Args:
+        data: A string containing binary digits
+        even: Whether to use even or odd parity (defaults to even)
+
+    Returns:
+        str: The parity bit of the data
+
+    Examples:
+        >>> from checkdigit import parity
+        >>> # Even parity
+        >>> parity.calculate("0110")
+        '0'
+        >>> parity.calculate("01101")
+        '1'
+
+        >>> # Odd parity
+        >>> parity.calculate("01101", False)
+        '0'
+        >>> parity.calculate("0", False)
+        '1'
+
+    """
+    data = cleanse(data)
+    if (even and not data.count("1") % 2) or (not even and data.count("1") % 2):
+        return "0"
+    return "1"
+
+
+def validate(data: str, even: bool = True) -> bool:
+    """Validates whether the check digit matches a block of data.
+
+    Args:
+        data: A string containing binary digits
+        even: Whether to use even or odd parity (defaults to even)
+
+    Returns:
+        bool: A boolean representing whether the data is valid or not
+
+    Examples:
+        >>> from checkdigit import parity
+        >>> # Even parity
+        >>> parity.validate("01100")
+        True
+        >>> parity.validate("01101")
+        False
+
+        >>> # Odd parity
+        >>> parity.validate("01101", False)
+        True
+        >>> parity.validate("01100", False)
+        False
+    """
+    data = cleanse(data)
+    return calculate(data[:-1], even) == data[-1]
+
+
+def missing(data: str, even: bool = True) -> str:
+    """Calculates a missing digit represented by a question mark.
+
+    Args:
+        data: A string containing a question mark representing a missing digit.
+        even: Whether to use even or odd parity (defaults to even)
+
+    Returns:
+        str: The missing binary digit
+
+    Examples:
+        >>> from checkdigit import parity
+        >>> # Even parity
+        >>> parity.missing("01?00")
+        '1'
+        >>> parity.missing("01?100")
+        '0'
+
+        >>> # Odd parity
+        >>> parity.missing("01101?", False)
+        '0'
+        >>> parity.missing("010010?011", False)
+        '1'
+    """
+    # Same principle as check digit (just not necessarily on the end)
+    return calculate(data.replace("?", ""), even)
