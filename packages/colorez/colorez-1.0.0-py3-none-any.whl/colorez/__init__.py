080a7ff272931a1b8196886a6b2ed285e9769272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:37 +0000

    Add package colorez/1.0.0/colorez-1.0.0-py3-none-any.whl

---
+++ packages/colorez/colorez-1.0.0-py3-none-any.whl/colorez/__init__.py
@@ -0,0 +1,24 @@
+
+#      ______         __
+#    .' ___  |       [  |
+#   / .'   \_|  .--.  | |  .--.   _ .--.  .---.  ____
+#   | |       / .'`\ \| |/ .'`\ \[ `/'`\]/ /__\\[_   ]
+#   \ `.___.'\| \__. || || \__. | | |    | \__., .' /_
+#    `.____ .' '.__.'[___]'.__.' [___]    '.__.'[_____]
+#
+
+
+"""
+Everything Is Better With Color
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+colorez.py adds simple addition of color to the builtin print and input functions
+
+"""
+
+__title__ = 'colorez'
+__description__ = "Simple addition of color to the builtin print and input functions"
+__version__ = '1.0.0'
+__author__ = 'CodingYuno'
+
+from .colorez import color_print, color_input, Color
