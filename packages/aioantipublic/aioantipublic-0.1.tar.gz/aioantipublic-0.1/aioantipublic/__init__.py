85cc9ff6fb7bd4d66866ea2e8096eabbfaf31265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:09 +0000

    Add package aioantipublic/0.1/aioantipublic-0.1.tar.gz

---
+++ packages/aioantipublic/aioantipublic-0.1.tar.gz/aioantipublic-0.1/aioantipublic/__init__.py
@@ -0,0 +1,1 @@
+from .aioantipublic import AntiPublic, AntiPublicError
---
+++ packages/aioantipublic/aioantipublic-0.1.tar.gz/aioantipublic-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+	name="aioantipublic",
+	version='0.1',
+	url="https://github.com/fantomcvc/aioantipublic",
+	license="MIT License",
+	author="fantomcvc",
+	author_email='fantomcvv@gmail.com',
+	description="Async API wrapper for antipublic",
+)
