50712546131181c7fd41a651d334c296a9d1d28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:36 +0000

    Add package porteratzolibs/0.1.1/porteratzolibs-0.1.1.tar.gz

---
+++ packages/porteratzolibs/porteratzolibs-0.1.1.tar.gz/porteratzolibs-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='porteratzolibs',
+    version='0.1.1',    
+    description='Collection of utils',
+    url='https://github.com/porteratzo/porteratzolibs',
+    author='Omar Montoya',
+    author_email='omar.alfonso.montoya@hotmail.com',
+    license='MIT License',
+    packages=['porteratzolibs'],
+    install_requires=['matplotlib',
+                      'numpy', 
+                      'open3d',
+                      'pandas',
+                      'opencv-python',                    
+                      ],
+
+    classifiers=[
+        'Operating System :: POSIX :: Linux', 
+        'Programming Language :: Python :: 3',
+    ],
+)
