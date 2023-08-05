c155588daa1a746c82adb6be19bca87ea563efd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:23 +0000

    Add package melnor-bluetooth/0.0.21.dev1/melnor_bluetooth-0.0.21.dev1-py3-none-any.whl

---
+++ packages/melnor-bluetooth/melnor_bluetooth-0.0.21.dev1-py3-none-any.whl/melnor_bluetooth/parser/date.py
@@ -0,0 +1,50 @@
+import datetime
+import zoneinfo
+
+from tzlocal import get_localzone
+
+
+def _time_offset(tz: datetime.tzinfo = get_localzone()):
+    """
+    Returns the archaic timezone offset in seconds.
+
+    The valve accepts a 4 byte unsigned integer and the official app
+    appears to deduct ~30 years from the current time to fit into a 4 byte size.
+
+
+    All watering operations are keyed off this value and the mobile app _and valves_
+    will show bad info we don't replicate the algorithm
+    """
+
+    base_time = datetime.datetime.now(tz=zoneinfo.ZoneInfo("Asia/Shanghai"))
+    local_time = datetime.datetime.now(tz=tz)
+
+    base_offset = base_time.utcoffset()
+    local_offset = local_time.utcoffset()
+
+    if (
+        base_offset is None
+        or local_offset is None
+        or base_time is None
+        or local_time is None
+    ):
+        # TODO log or throw an exception here. caller should handle this
+        return 0
+
+    if base_offset is None or local_offset is None:
+        # TODO log or throw an exception here. caller should handle this
+        return 0
+
+    return int(base_offset.total_seconds() - local_offset.total_seconds())
+
+
+def time_shift(tz: datetime.tzinfo = get_localzone()) -> int:
+    return -_time_offset(tz) - 946656000
+
+
+def get_timestamp(tz: datetime.tzinfo = get_localzone()) -> int:
+    """
+    Returns the current timestamp as a byte array.
+    """
+
+    return int(datetime.datetime.now(tz).timestamp() + time_shift(tz))
