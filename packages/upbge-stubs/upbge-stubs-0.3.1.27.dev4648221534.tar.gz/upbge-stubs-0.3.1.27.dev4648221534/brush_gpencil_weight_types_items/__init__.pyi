a3c42c5197643109ec575651e19640e0558be4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:55 +0000

    Add package upbge-stubs/0.3.1.27.dev4648221534/upbge-stubs-0.3.1.27.dev4648221534.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev4648221534.tar.gz/upbge-stubs-0.3.1.27.dev4648221534/brush_gpencil_weight_types_items/__init__.pyi
@@ -0,0 +1,16 @@
+"""
+
+
+Brush Gpencil Weight Types Items
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:WEIGHT:          
+  Weight.
+
+  Weight Paint for Vertex Groups.
+
+.. _rna-enum-brush-gpencil-weight-types-items:
+
+"""
+
+import typing
