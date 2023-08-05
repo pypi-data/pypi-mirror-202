ef0cc2e46bb753c8bb8c429ba3450190babfe890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:50 +0000

    Add package prettyqt/1.1.1/prettyqt-1.1.1.tar.gz

---
+++ packages/prettyqt/prettyqt-1.1.1.tar.gz/prettyqt-1.1.1/prettyqt/qt/QtScxml/__init__.py
@@ -0,0 +1,13 @@
+"""Provides QtScxml classes and functions."""
+
+from prettyqt.qt import PYQT5, PYQT6, PYSIDE6, PythonQtError
+
+
+if PYQT5:
+    from PyQt5.QtScxml import *
+elif PYQT6:
+    from PyQt6.QtScxml import *
+elif PYSIDE6:
+    from PySide6.QtScxml import *
+else:
+    raise PythonQtError("No Qt bindings could be found")
