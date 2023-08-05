51758c8b8ca98a8bfe0c6719684410c290412383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:55 +0000

    Add package mahdienc/0.2/mahdienc-0.2.tar.gz

---
+++ packages/mahdienc/mahdienc-0.2.tar.gz/mahdienc-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="mahdienc",
+version='0.2',
+description='this is a simple pip modul and test',
+long_description='THia is simpile modul crate by mahdi hasan',
+package=['mahdienc','zlib','time','base64','marshal','requests','py_compile','cython'],
+install_requires=[])
