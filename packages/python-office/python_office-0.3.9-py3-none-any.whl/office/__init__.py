093a67e2acf5e9658521dc89aef4325476639aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:58 +0000

    Add package python-office/0.3.9/python_office-0.3.9-py3-none-any.whl

---
+++ packages/python-office/python_office-0.3.9-py3-none-any.whl/office/__init__.py
@@ -0,0 +1,22 @@
+from pohan.api.普通话 import *
+
+from office.api import word
+from office.api import pdf
+from office.api import image
+from office.api import file
+from office.api import video
+from office.api import tools
+from office.api import ppt
+from office.api import excel
+from office.api import wechat
+# 以下是beta版本
+from office.api.testApi import ruiming
+from office.lib.conf.CONST import SPLIT_LINE
+显示(分割线)
+显示('【python-office库】，功能持续更新中')
+显示('使用有问题 or 提交你的功能需求 or 参与项目开发')
+显示('1、全部功能【视频 & 文字】教程：https://www.python-office.com/')
+显示('2、请+【项目交流群】：http://t.cn/A65MiFvH')
+显示('3、本开源项目的【代码仓库】：https://github.com/CoderWanFeng/python-office')
+显示('4、新人10大【常见问题】：https://www.python-office.com/ref/qaq.html')
+显示(分割线)
