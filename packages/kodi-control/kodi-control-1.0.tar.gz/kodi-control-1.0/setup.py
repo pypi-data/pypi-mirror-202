a6bf07f547b4849ab3ec596e920622f07992187e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:17 +0000

    Add package kodi-control/1.0/kodi-control-1.0.tar.gz

---
+++ packages/kodi-control/kodi-control-1.0.tar.gz/kodi-control-1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.rst", encoding="utf-8") as file:
+    readme = file.read()
+
+setup(
+    name='kodi-control',
+    version='1.0',
+    description='interactive tty-based remote control for Kodi',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    scripts='kodi-control'.split(),
+    install_requires='appdirs docopt inform>=1.28 nestedtext shlib voluptuous'.split(),
+    python_requires = '>=3.6',
+    keywords = 'kodi'.split(),
+    classifiers = [
+        #'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
