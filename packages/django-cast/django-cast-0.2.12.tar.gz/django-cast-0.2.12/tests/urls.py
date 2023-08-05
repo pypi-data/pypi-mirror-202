76ff3a40fe12e43e0e198d253f9e35addf0beb8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:47 +0000

    Add package django-cast/0.2.12/django-cast-0.2.12.tar.gz

---
+++ packages/django-cast/django-cast-0.2.12.tar.gz/django-cast-0.2.12/tests/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path, re_path
+from rest_framework.documentation import include_docs_urls
+from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+urlpatterns = [
+    # rest framework docs/schema urls
+    re_path(r"^docs/", include_docs_urls(title="cast API service")),
+    path("cast/", include("cast.urls", namespace="cast")),
+    # comments
+    path("posts/comments/", include("fluent_comments.urls")),
+    # wagtail
+    path("cms/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("", include(wagtail_urls)),  # default is wagtail
+]
