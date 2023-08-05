0fa1dd14a043bbdd86e0fca24caff300f208eabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:35 +0000

    Add package leapseconds/2023.4.0/leapseconds-2023.4.0-py3-none-any.whl

---
+++ packages/leapseconds/leapseconds-2023.4.0-py3-none-any.whl/leapseconds/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright © 2023 Jamie Nguyen <j@jamielinux.com>
+# SPDX-License-Identifier: MIT
+
+"""Data for official leap seconds, provided as a tuple of Unix timestamps."""
+
+from .leapseconds import LEAP_SECONDS
+
+__all__ = ["LEAP_SECONDS"]
