8e4529c44d79059d89859c4d67f12a73b50520fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:10 +0000

    Add package hbutils/0.8.4/hbutils-0.8.4-py3-none-any.whl

---
+++ packages/hbutils/hbutils-0.8.4-py3-none-any.whl/hbutils/string/__init__.py
@@ -0,0 +1,8 @@
+"""
+Overview:
+    String process module, include some useful utilities for string.
+"""
+from .inflection import *
+from .plural import *
+from .template import *
+from .trunc import *
---
+++ packages/hbutils/hbutils-0.8.4-py3-none-any.whl/hbutils/system/filesystem/__init__.py
@@ -0,0 +1,7 @@
+"""
+Overview:
+    Utilities for filesystem.
+"""
+from .binary import *
+from .directory import *
+from .file import *
