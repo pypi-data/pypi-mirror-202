2e6d73b27697ae347c1e18f5f4da476d8b034f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:17 +0000

    Add package datastructuresLionelEric/1.0.5/datastructuresLionelEric-1.0.5.tar.gz

---
+++ packages/datastructuresLionelEric/datastructuresLionelEric-1.0.5.tar.gz/datastructuresLionelEric-1.0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup (
+name = 'datastructuresLionelEric',
+packages=find_packages(include = ['datastructures.Linear', 'datastructures.nodes', 'datastructures.trees ']), 
+version='1.0.5', 
+description='datastructures',
+author = 'Lionel Eric',
+author_email = "lionel.hasan@ucalgary.ca",
+)
