0b965ec11619e9388b70424ddcf947e21555a774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:08 +0000

    Add package sara-compis1-tools/0.0.4/sara_compis1_tools-0.0.4.tar.gz

---
+++ packages/sara-compis1-tools/sara_compis1_tools-0.0.4.tar.gz/sara_compis1_tools-0.0.4/sara_compis1_tools/generated.py
@@ -0,0 +1,15 @@
+
+import sys
+import sara_compis1_tools.lexGen as tool
+
+# if len(sys.argv) < 2:
+#     print("Por favor ingrese el archivo .yal")
+#     sys.exit(1)
+
+yal_file = 'sara_compis1_tools/lexer.yal'
+lex_var = tool.Lexer(yal_file)
+lex_var.read()
+mega_content = lex_var.generate_automatas()
+mega_automata = lex_var.unify(mega_content)
+lex_var.draw_mega_afd(mega_automata)
+        
