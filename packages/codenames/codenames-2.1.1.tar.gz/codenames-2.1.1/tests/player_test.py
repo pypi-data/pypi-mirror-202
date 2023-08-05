6838b4286891c1918ff02e2e2f476b83fd35e395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:54 +0000

    Add package codenames/2.1.1/codenames-2.1.1.tar.gz

---
+++ packages/codenames/codenames-2.1.1.tar.gz/codenames-2.1.1/tests/player_test.py
@@ -0,0 +1,28 @@
+import pytest
+
+from codenames.game.color import CardColor, TeamColor
+from codenames.game.player import Player
+from tests.utils.testing_players import TestGuesser, TestHinter
+
+
+def test_player_team_card_color():
+    p1 = Player("Player 1")
+    p1.team_color = TeamColor.RED
+    p2 = Player("Player 2")
+    p2.team_color = TeamColor.BLUE
+    p3 = Player("Player 3")
+    p3.team_color = None
+
+    assert p1.team_card_color == CardColor.RED
+    assert p2.team_card_color == CardColor.BLUE
+
+    with pytest.raises(ValueError):
+        _ = p3.team_card_color
+
+
+def test_play_to_string():
+    p1 = TestGuesser(guesses=[], name="Player 1", team_color=TeamColor.RED)
+    p2 = TestHinter(hints=[], name="Player 2")
+
+    assert str(p1) == "Player 1 | Red Test Guesser"
+    assert str(p2) == "Player 2 | Test Hinter"
