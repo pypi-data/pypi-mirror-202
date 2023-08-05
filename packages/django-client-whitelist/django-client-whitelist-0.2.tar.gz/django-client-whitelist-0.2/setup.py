2b7c096e0d56926f59dc30e6550e126ce51cfb55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:31 +0000

    Add package django-client-whitelist/0.2/django-client-whitelist-0.2.tar.gz

---
+++ packages/django-client-whitelist/django-client-whitelist-0.2.tar.gz/django-client-whitelist-0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='django-client-whitelist',
+    version='0.2',
+    packages=['client_whitelist'],
+    python_requires='>=3.10.7',
+    install_requires=[
+        'Django>=4.1.4',
+    ],
+    license='MIT',
+    description='Django app to restrict client access',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/S-Amine/django-client-whitelist.git',
+    author='S-Amine',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 4.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
