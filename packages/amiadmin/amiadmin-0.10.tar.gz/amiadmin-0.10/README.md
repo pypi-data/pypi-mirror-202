4fb7a750754346b6637abd801438b48e942be200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:53 +0000

    Add package amiadmin/0.10/amiadmin-0.10.tar.gz

---
+++ packages/amiadmin/amiadmin-0.10.tar.gz/amiadmin-0.10/README.md
@@ -0,0 +1,13 @@
+# A function to check if the user is an admin
+
+### Tested against Windows 10 / Python 3.10 / Anaconda 
+
+## pip install amiadmin
+
+
+```python
+from amiadmin import am_i_admin
+print(am_i_admin())
+
+True
+```
---
+++ packages/amiadmin/amiadmin-0.10.tar.gz/amiadmin-0.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.10'
+DESCRIPTION = "A function to check if the user is an admin"
+
+# Setting up
+setup(
+    name="amiadmin",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/amiadmin',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=[],
+    keywords=['admin', 'check'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.10', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Utilities'],
+    install_requires=[],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
