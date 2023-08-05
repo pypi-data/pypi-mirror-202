59945e53bed595a42796bf31ab6a6227d118b544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:15 +0000

    Add package binsync/3.6.0/binsync-3.6.0.tar.gz

---
+++ packages/binsync/binsync-3.6.0.tar.gz/binsync-3.6.0/binsync/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "3.6.0"
+
+#
+# logging
+#
+
+import logging
+logging.getLogger("binsync").addHandler(logging.NullHandler())
+from binsync.loggercfg import Loggers
+loggers = Loggers()
+del Loggers
+del logging
+
+import binsync.data
