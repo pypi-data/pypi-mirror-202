a2ab016d9ccc2224717e04b914886e26ec22fad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:13 +0000

    Add package datastructuresLionelEric/1.0.7/datastructuresLionelEric-1.0.7.tar.gz

---
+++ packages/datastructuresLionelEric/datastructuresLionelEric-1.0.7.tar.gz/datastructuresLionelEric-1.0.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup (
+name = 'datastructuresLionelEric',
+version='1.0.7', 
+description='datastructures',
+author = 'Lionel Eric',
+author_email = "lionel.hasan@ucalgary.ca"
+)
