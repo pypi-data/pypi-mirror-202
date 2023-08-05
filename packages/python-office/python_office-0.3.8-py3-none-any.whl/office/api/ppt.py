f8f27d34c9efc9f9dead128944e344fd69385e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:46 +0000

    Add package python-office/0.3.8/python_office-0.3.8-py3-none-any.whl

---
+++ packages/python-office/python_office-0.3.8-py3-none-any.whl/office/api/ppt.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: ppt.py
+# Mail: 1957875073@qq.com
+# Created Time:  2022-4-25 10:17:34
+# Description: 有关 ppt 的自动化操作
+#############################################
+# from office.lib.utils.except_utils import except_dec
+# from office.core.PPTType import MainPPT
+
+# mainPPT = MainPPT()
+
+import poppt
+# todo：输入文件路径
+# @except_dec()
+from office.lib.decorator_utils.instruction_url import instruction
+
+
+@instruction
+def ppt2pdf(path: str):
+    poppt.ppt2pdf(path)
+
+
+@instruction
+def ppt2img(input_path, output_path=r'./', img_type='jpg'):
+    poppt.ppt2img(input_path, output_path, img_type)
