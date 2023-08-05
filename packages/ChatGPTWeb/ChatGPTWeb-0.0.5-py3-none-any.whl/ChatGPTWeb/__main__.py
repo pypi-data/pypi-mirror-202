210843f9e12b287c7732fabb0940c462b2cf7b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:01 +0000

    Add package ChatGPTWeb/0.0.5/ChatGPTWeb-0.0.5-py3-none-any.whl

---
+++ packages/ChatGPTWeb/ChatGPTWeb-0.0.5-py3-none-any.whl/ChatGPTWeb/__main__.py
@@ -0,0 +1,41 @@
+from ChatGPTWeb.ChatGPTWeb import chatgpt
+from ChatGPTWeb.config import MsgData
+import asyncio
+import aioconsole
+
+session_token=""
+person = "hello,my name is 'pig'."
+chat = chatgpt(session_token=session_token,personality=person,log_status=False)
+
+async def main():
+    
+    c_id = await aioconsole.ainput("your conversation_id if you have:")
+    p_id = await aioconsole.ainput("your parent_message_id if you have:")
+    data:MsgData = MsgData(conversation_id=c_id,p_msg_id=p_id)
+    while 1:
+        print("\n------------------------------")
+        data.msg_send = await aioconsole.ainput("input：")
+        print("------------------------------\n")
+        if data.msg_send == "quit":
+            break
+        elif data.msg_send == "re":
+            data.msg_type = "back_loop"
+            data.p_msg_id = await aioconsole.ainput("your parent_message_id if you go back:")
+        elif data.msg_send == "reset":
+            data = await chat.back_init_personality(data)
+            print(f"ChatGPT:{data.msg_recv}")
+            continue
+        elif data.msg_send == "init_personality":
+            data = await chat.init_personality(data)
+            print(f"ChatGPT:{data.msg_recv}")
+            continue
+        elif data.msg_send == "history":
+            print(await chat.show_chat_history(data))
+            continue
+        data = await chat.continue_chat(data)
+        print(f"ChatGPT:{data.msg_recv}")
+        
+        
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())           
+    
