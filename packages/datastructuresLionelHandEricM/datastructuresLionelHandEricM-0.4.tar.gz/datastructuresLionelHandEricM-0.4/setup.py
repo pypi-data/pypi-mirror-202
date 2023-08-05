e9ac98287bdd05f7253c18539d93d04611db132d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package datastructuresLionelHandEricM/0.4/datastructuresLionelHandEricM-0.4.tar.gz

---
+++ packages/datastructuresLionelHandEricM/datastructuresLionelHandEricM-0.4.tar.gz/datastructuresLionelHandEricM-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='datastructuresLionelHandEricM',
+      version='0.4',
+      description='Variety of datastructures implemented in Python',
+      author='Lionel Hasan & Eric Mei',
+      author_email= 'lionel.hasan@ucalgary.ca',
+      packages = ['datastructuresLionelHandEricM'],
+      install_requires=[], 
+      )
