080a86f5bb543678af0783df771b717fb3099ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:43 +0000

    Add package blender-stubs/3.2.0.27.dev4648006602/blender-stubs-3.2.0.27.dev4648006602.tar.gz

---
+++ packages/blender-stubs/blender-stubs-3.2.0.27.dev4648006602.tar.gz/blender-stubs-3.2.0.27.dev4648006602/brush_gpencil_vertex_types_items/__init__.pyi
@@ -0,0 +1,26 @@
+"""
+
+
+Brush Gpencil Vertex Types Items
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:DRAW:            
+  Draw.
+
+:BLUR:            
+  Blur.
+
+:AVERAGE:         
+  Average.
+
+:SMEAR:           
+  Smear.
+
+:REPLACE:         
+  Replace.
+
+.. _rna-enum-brush-gpencil-vertex-types-items:
+
+"""
+
+import typing
