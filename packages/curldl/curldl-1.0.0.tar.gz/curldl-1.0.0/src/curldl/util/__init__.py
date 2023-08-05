d46989f03155ebb7289332a81fb54fc9801c038b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:24 +0000

    Add package curldl/1.0.0/curldl-1.0.0.tar.gz

---
+++ packages/curldl/curldl-1.0.0.tar.gz/curldl-1.0.0/src/curldl/util/__init__.py
@@ -0,0 +1,7 @@
+"""General-purpose utilities for internal use.
+Provides classes: ``Log``, ``Time``, ``FileSystem``, ``Cryptography``.
+"""
+from curldl.util.crypt import Cryptography
+from curldl.util.fs import FileSystem
+from curldl.util.log import Log
+from curldl.util.time import Time
