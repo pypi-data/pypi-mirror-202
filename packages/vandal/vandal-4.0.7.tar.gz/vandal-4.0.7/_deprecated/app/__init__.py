20c56b161c33dbf3c30f4afc6d3c8672d88eb292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:31 +0000

    Add package vandal/4.0.7/vandal-4.0.7.tar.gz

---
+++ packages/vandal/vandal-4.0.7.tar.gz/vandal-4.0.7/_deprecated/app/__init__.py
@@ -0,0 +1,15 @@
+# import all relevant modules from the associated package.
+from vandal.app import (
+    particles,
+)
+
+# import all relevant contents from the associated module.
+from vandal.app.particles import (
+    App,
+)
+
+# all relevant contents.
+__all__ = [
+    particles,
+    App,
+]
---
+++ packages/vandal/vandal-4.0.7.tar.gz/vandal-4.0.7/vandal/apps/__init__.py
@@ -0,0 +1,14 @@
+# metaclass.
+from logistics.plugins.metaclass import Meta
+
+# imports all data types.
+from logistics.plugins.types import *
+
+# import all relevant contents from the associated module.
+from vandal.apps import montecarlo_gui
+
+# all relevant contents.
+__all__ = [
+    'MonteCarloGUI',
+    'Meta',
+]
