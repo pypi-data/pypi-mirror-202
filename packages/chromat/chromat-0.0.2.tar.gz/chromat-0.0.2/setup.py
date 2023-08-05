88a24f28fdb6e48a6154f5fbb0d4d38f5aa89634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:01 +0000

    Add package chromat/0.0.2/chromat-0.0.2.tar.gz

---
+++ packages/chromat/chromat-0.0.2.tar.gz/chromat-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['chromat']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colour>=0.1.5,<0.2.0', 'textual[dev]>=0.18.0,<0.19.0']
+
+setup_kwargs = {
+    'name': 'chromat',
+    'version': '0.0.2',
+    'description': 'color palettes! under heavy construction!',
+    'long_description': '\ufeff# chromat: algorithmic color palettes\ncoming soon!\n\nhttps://github.com/hexbenjamin/chromat',
+    'author': 'hex benjamin',
+    'author_email': 'hex@hexbenjam.in',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
