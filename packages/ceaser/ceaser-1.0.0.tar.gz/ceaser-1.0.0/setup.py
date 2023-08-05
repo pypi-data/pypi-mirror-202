1d3eef23d74f94d858beca3883b29c4e0b52b5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:56 +0000

    Add package ceaser/1.0.0/ceaser-1.0.0.tar.gz

---
+++ packages/ceaser/ceaser-1.0.0.tar.gz/ceaser-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ceaser',
+    version='1.0.0',
+    py_modules=['bf', 'cc', 'dfh', 'RSA', 'vc'],
+    description='Hackcode',
+    author='Gandu',
+    author_email='gandu@hotmail.com',
+)
