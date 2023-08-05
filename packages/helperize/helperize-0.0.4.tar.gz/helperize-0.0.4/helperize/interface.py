a8fe7e73441dadc26e854e60319c4248b57e6dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:06 +0000

    Add package helperize/0.0.4/helperize-0.0.4.tar.gz

---
+++ packages/helperize/helperize-0.0.4.tar.gz/helperize-0.0.4/helperize/interface.py
@@ -0,0 +1,27 @@
+class Workspace:
+    path: str
+
+
+class System:
+    pass
+
+
+class Goal:
+    pass
+
+
+class Intelligence:
+    input_types: list[str]
+    output_types: list[str]
+
+
+class Auth:
+    pass
+
+
+class Monitor:
+    pass
+
+
+class Logger:
+    pass
