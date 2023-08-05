aa7bd50922620b55e9c2685612280a188631d8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:58 +0000

    Add package socialmediaextractor/0.1/socialmediaextractor-0.1.tar.gz

---
+++ packages/socialmediaextractor/socialmediaextractor-0.1.tar.gz/socialmediaextractor-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='socialmediaextractor',
+    version='0.1',
+    description='A library to extract social media links from websites',
+    author='Md Irfan Ali',
+    author_email='irfanali29@hotmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'requests'
+    ],
+)
