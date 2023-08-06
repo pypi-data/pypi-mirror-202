1cbb6ce8db90ab05677466e8299b1bb29ef2ee40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:54 +0000

    Add package nomoji/0.0.0/nomoji-0.0.0.tar.gz

---
+++ packages/nomoji/nomoji-0.0.0.tar.gz/nomoji-0.0.0/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import find_packages, setup
+
+CURRENT_FOLDER = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(CURRENT_FOLDER, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(name="nomoji",
+      author="Valerio Maggio",
+      author_email="vmaggio@anaconda.com",
+      description="Silly Python Package to print digits using emojis",
+      long_description=long_description,
+      url="https://github.com/leriomaggio/emoji-numbers/",
+      packages=find_packages(exclude=[]),
+      nclude_package_data=True)
