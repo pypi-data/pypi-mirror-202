a45f96c2c7345177fd0e871fa6b135637706032e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:11 +0000

    Add package goby-sdk/1.0.6/goby_sdk-1.0.6.tar.gz

---
+++ packages/goby-sdk/goby_sdk-1.0.6.tar.gz/goby_sdk-1.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='goby_sdk',
+    version='1.0.6',
+    description='A Python SDK for Goby, Visit for details: https://www.exp-9.com/category-20.html',
+    author='siberia',
+    author_email='siberiah0h@gmail.com',
+    project_description='这是一个非官方版的GOBY SDK \r\n'
+                        '基于goby-x64-2.4.5-Community 进行开发\r\n'
+                        '用于解决企业快速嵌入goby扫描引擎的问题'
+                        'SDK文档详情请关注: https://www.exp-9.com/category-20.html 或者 https://github.com/siberiah0h/goby_sdk_for_python\r\n',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.1',
+    ],
+)
