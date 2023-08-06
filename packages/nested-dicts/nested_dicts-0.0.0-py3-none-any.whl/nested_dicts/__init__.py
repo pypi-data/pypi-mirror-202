c70d8c1fcd5419df0b03793b29d89efa48c2d9ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:09 +0000

    Add package nested-dicts/0.0.0/nested_dicts-0.0.0-py3-none-any.whl

---
+++ packages/nested-dicts/nested_dicts-0.0.0-py3-none-any.whl/nested_dicts/__init__.py
@@ -0,0 +1,26 @@
+__all__ = ['DefaultsDictABC'
+          ,'NestedDefaultsDict'
+          ,'ListKeyedDict'
+          ,'ListKeyedNestedDefaultsDict'
+          ,'ListOfListKeyedDicts'
+          ,'list_keyed_from_nested_dict_and_lists'
+          ,'DottedKeyedNestedDefaultsDict'
+          ,'TOMLTable'
+          ,'TOMLListKeyedTable'
+          ,'TOMLArrayOfTables'
+          ,'NestedTOMLTableOrArrayOfTables'
+          ]
+
+from .nested_dicts import (DefaultsDictABC
+                          ,NestedDefaultsDict
+                          ,ListKeyedDict
+                          ,ListKeyedNestedDefaultsDict
+                          ,ListOfListKeyedDicts
+                          ,list_keyed_from_nested_dict_and_lists
+                          ,DottedKeyedNestedDefaultsDict
+                          ,TOMLTable
+                          ,TOMLListKeyedTable
+                          ,TOMLArrayOfTables
+                          ,NestedTOMLTableOrArrayOfTables
+                          )
+
