c688e91cce164f3b0b9899fc5c8765a438da4a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:42 +0000

    Add package lager/0.17.1/lager-0.17.1-py3-none-any.whl

---
+++ packages/lager/lager-0.17.1-py3-none-any.whl/lager/_meta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+
+__title__ = "lager"
+__description__ = "EZ-PZ logging based on loguru"
+__pkgroot__ = __file__.replace("_meta.py", "").rstrip("/\\")
+__version__ = "0.17.1"
