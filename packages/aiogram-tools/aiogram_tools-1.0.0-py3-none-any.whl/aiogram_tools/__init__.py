f4a65ed4db8e27b1c2b00b3f99cbe85c1e9ac2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:20:11 +0000

    Add package aiogram-tools/1.0.0/aiogram_tools-1.0.0-py3-none-any.whl

---
+++ packages/aiogram-tools/aiogram_tools-1.0.0-py3-none-any.whl/aiogram_tools/__init__.py
@@ -0,0 +1,5 @@
+from . import filters, keyboards, middlewares
+from .dispatcher import Dispatcher
+from .keyboards import InlineButton
+
+__all__ = ['Dispatcher', 'filters', 'middlewares', 'InlineButton']
