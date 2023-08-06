375e32331703c5cf85276838a5127f7aef741fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:21 +0000

    Add package binderspace/1.0.2/binderspace-1.0.2.tar.gz

---
+++ packages/binderspace/binderspace-1.0.2.tar.gz/binderspace-1.0.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='binderspace',
+    packages=['binderspace'],
+    version='1.0.2',
+    license='mit',
+    description='a library for finding the space for binding sequences',
+    author='Vukovic Lab (Payam Kelich)',
+    author_email='lvukovic@utep.edu',  # Type in your E-Mail
+    url='https://github.com/vukoviclab/BinderSpace',  # Provide either the link to your github or to your website
+    download_url='https://github.com/vukoviclab/BinderSpace/archive/refs/tags/1.0.tar.gz',
+    keywords=['DNA', 'sequence', 'pca', 't-sne', 'peptide'],  # Keywords that define your package best
+    install_requires=[
+        'scikit-learn',
+        'numpy',
+        'pandas',
+        'matplotlib',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
