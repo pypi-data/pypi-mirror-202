2f749c9b4517d3b9841f7d4ce1970bac76f3ae86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:40 +0000

    Add package multiple-docking/0.12/multiple_docking-0.12.tar.gz

---
+++ packages/multiple-docking/multiple_docking-0.12.tar.gz/multiple_docking-0.12/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='multiple_docking',
+    version='0.12',
+    description='single docking and multiple docking using a python code',
+    author='Jayamal',
+    author_email='jayamal@sci.sjp.ac.lk',
+    url='https://github.com/TharinduLab/Multiple_Docking.git',
+    packages=['multiple_docking'],
+    install_requires=[],
+)
