62117ce1ad5721ebe21b623e67558f085abc866b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:48 +0000

    Add package lrboost/0.2.1/lrboost-0.2.1.tar.gz

---
+++ packages/lrboost/lrboost-0.2.1.tar.gz/lrboost-0.2.1/README.md
@@ -0,0 +1,39 @@
+<img src=https://raw.githubusercontent.com/anpatton/lrboost/main/doc/images/lrboost.png width=300>
+
+[![Documentation Status](https://readthedocs.org/projects/lrboost/badge/?version=latest)](https://lrboost.readthedocs.org)
+
+[![PyPI version](https://badge.fury.io/py/lrboost.svg)](https://badge.fury.io/py/lrboost)
+
+lrboost is a [sckit-learn](https://scikit-learn.org/) compatible package for linear residual boosting. lrboost uses a linear estimator to first remove any linear trends from the data, and then uses a separate non-linear estimator to model the remaining non-linear trends. We find that extrapolation tasks or data with linear and non-linear components are the best use cases. Not every modeling task will benefit from lrboost, but we use this in our own work and wanted to share something that made it easy to use.  
+
+For a stable version, install using ``pip``:
+
+```python
+pip install lrboost
+```
+
+lrboost was inspired by ['Regression-Enhanced Random Forests' by Haozhe Zhang, Dan Nettleton, and Zhengyuan Zhu.](https://arxiv.org/abs/1904.10416v1) An excellent PyData talk by Gabby Shklovsky explaining the intuition underlying the approach may also be found here: ['Random Forest Best Practices for the Business World'](https://youtu.be/E7VLE-U07x0?t=341).
+
+* LRBoostRegressor can be used like any other sklearn estimator and is built off a sklearn template.
+* ``predict(X)`` returns an array-like of final predictions
+* Adding ``predict(X, detail=True)`` returns a dictionary with primary, secondary, and final predictions.
+
+
+```python
+from sklearn.datasets import load_diabetes
+from lrboost import LRBoostRegressor
+X, y = load_diabetes(return_X_y=True)
+lrb = LRBoostRegressor().fit(X, y)
+predictions = lrb.predict(X)
+detailed_predictions = lrb.predict(X, detail=True)
+
+print(lrb.primary_model.score(X, y)) #R2
+print(lrb.score(X, y)) #R2
+
+>>> 0.512
+>>> 0.933
+```
+
+[More detailed documentation can be found here!](https://lrboost.readthedocs.io/en/latest/) 
+
+*[Andrew Patton](https://twitter.com/anpatt7), [Nathan Walker](https://twitter.com/bbstats), and [Kostya Medvedovsky](https://twitter.com/kmedved)*
---
+++ packages/lrboost/lrboost-0.2.1.tar.gz/lrboost-0.2.1/setup.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+"""Linear Residual Boosting compatible with scikit-learn."""
+
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+# get __version__ from _version.py
+ver_file = os.path.join('lrboost', '_version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
+DISTNAME = 'lrboost'
+DESCRIPTION = 'Linear Residual Boosting compatible with scikit-learn.'
+with codecs.open('README.md', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+MAINTAINER = 'A. Patton, N. Walker, K. Medvedovsky'
+MAINTAINER_EMAIL = 'anpatt7@gmail.com, bibigon@gmail.com'
+URL = 'https://github.com/anpatton/lrboost'
+LICENSE = 'new BSD'
+DOWNLOAD_URL = 'https://github.com/anpatton/lrboost'
+VERSION = __version__
+INSTALL_REQUIRES = required
+CLASSIFIERS = ['Intended Audience :: Science/Research',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved',
+               'Programming Language :: Python',
+               'Topic :: Software Development',
+               'Topic :: Scientific/Engineering',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS',
+               'Programming Language :: Python :: 3.5',
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.7']
+EXTRAS_REQUIRE = {
+    'tests': [
+        'pytest',
+        'pytest-cov'],
+    'docs': [
+        'sphinx',
+        'sphinx-gallery',
+        'sphinx_rtd_theme',
+        'numpydoc',
+        'matplotlib'
+    ]
+}
+
+setup(name=DISTNAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
+      zip_safe=False,  # the package can run out of an .egg file
+      classifiers=CLASSIFIERS,
+      packages=find_packages(),
+      install_requires=INSTALL_REQUIRES,
+      extras_require=EXTRAS_REQUIRE)
