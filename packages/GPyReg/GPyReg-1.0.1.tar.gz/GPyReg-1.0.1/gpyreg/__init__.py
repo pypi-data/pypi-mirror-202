cdb240279a4e2b3f04ba7f0b4e46ff0f1f63d9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:56 +0000

    Add package GPyReg/1.0.1/GPyReg-1.0.1.tar.gz

---
+++ packages/GPyReg/GPyReg-1.0.1.tar.gz/GPyReg-1.0.1/gpyreg/__init__.py
@@ -0,0 +1,9 @@
+"""Lightweight Gaussian process regression package."""
+
+import gpyreg.covariance_functions
+import gpyreg.isotropic_covariance_functions
+import gpyreg.f_min_fill
+import gpyreg.mean_functions
+import gpyreg.noise_functions
+import gpyreg.slice_sample
+from gpyreg.gaussian_process import GP
