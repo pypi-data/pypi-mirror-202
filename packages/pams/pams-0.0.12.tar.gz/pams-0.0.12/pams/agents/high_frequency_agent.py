4f323ec72aef7f7504b9aa980041970b0338c9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:46 +0000

    Add package pams/0.0.12/pams-0.0.12.tar.gz

---
+++ packages/pams/pams-0.0.12.tar.gz/pams-0.0.12/pams/agents/high_frequency_agent.py
@@ -0,0 +1,15 @@
+from abc import ABC
+
+from .base import Agent
+
+
+class HighFrequencyAgent(Agent, ABC):
+    """High-frequency trading Agent class.
+
+    High-frequency traders are treated as a special agent. Please see xxxx for more details.
+    #ToDo
+
+    This class inherits from the :class:`pams.agents.Agent` class and ABC class.
+    """
+
+    pass
