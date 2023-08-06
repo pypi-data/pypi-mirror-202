b0049e0d9b5f595c64852234f6a4a294ab8e3f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:35 +0000

    Add package sunyata/0.0.35/sunyata-0.0.35.tar.gz

---
+++ packages/sunyata/sunyata-0.0.35.tar.gz/sunyata-0.0.35/sunyata/http/response.py
@@ -0,0 +1,35 @@
+from sunyata.util import str2bytes
+
+class HttpResponse(object):
+
+    __slots__ = ('httpVersion', 'status', 'headers', 'body')
+
+    def __init__(self, status, body) -> None:
+        super().__init__()
+        self.httpVersion = 'HTTP/1.1'
+        self.status = status
+        self.headers = {}
+        self.body = body
+
+    def genHeaders(self):
+        self.headers['Content-Type'] = 'text/html'
+        self.headers['Content-Length'] = len(self.body)
+
+    def toBytes(self):
+        resLine = f"{self.httpVersion} {self.status.code} {self.status.msg}"
+        self.genHeaders()
+        resHeaders = ''
+        for k, v in self.headers.items():
+            resHeaders += f"{k}: {v}\r\n"
+        resHeaders += "\r\n"
+        resBody = self.body
+        if type(resBody) == str:
+            resBody = str2bytes(resBody)
+        res = str2bytes(resLine + resHeaders) + resBody
+        return res
+    
+    def responseBody(self):
+        resBody = self.body
+        if type(resBody) == str:
+            resBody = str2bytes(resBody)
+        return resBody
