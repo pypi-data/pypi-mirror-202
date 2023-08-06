42f7b05939ad09d170b18750e75ad056c02b1285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:44 +0000

    Add package ewuohia123/1.0/ewuohia123-1.0.tar.gz

---
+++ packages/ewuohia123/ewuohia123-1.0.tar.gz/ewuohia123-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='ewuohia123',
+      version='1.0',
+      author='Onyekpere Chinedu',
+      author_email='chinedu.onyekpere@gmail.com',
+      packages=find_packages(),
+)
