6b6da06623e79a38b69cb4dc0221b92c3ec8979d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:46 +0000

    Add package libdyson-neon/0.10.2/libdyson-neon-0.10.2.tar.gz

---
+++ packages/libdyson-neon/libdyson-neon-0.10.2.tar.gz/libdyson-neon-0.10.2/tests/test_init.py
@@ -0,0 +1,66 @@
+"""Test Dyson Python library."""
+from typing import Type
+
+import pytest
+
+from libdyson import (
+    DEVICE_TYPE_360_EYE,
+    DEVICE_TYPE_360_HEURIST,
+    DEVICE_TYPE_PURE_COOL,
+    DEVICE_TYPE_PURE_COOL_FORMALDEHYDE,
+    DEVICE_TYPE_PURE_COOL_DESK,
+    DEVICE_TYPE_PURE_COOL_LINK,
+    DEVICE_TYPE_PURE_COOL_LINK_DESK,
+    DEVICE_TYPE_PURE_HOT_COOL,
+    DEVICE_TYPE_PURE_HOT_COOL_NEW,
+    DEVICE_TYPE_PURE_HOT_COOL_LINK,
+    DEVICE_TYPE_PURE_HUMIDIFY_COOL,
+    DEVICE_TYPE_PURIFIER_HUMIDIFY_COOL,
+    DEVICE_TYPE_PURIFIER_HUMIDIFY_COOL_FORMALDEHYDE,
+    DEVICE_TYPE_PURIFIER_HOT_COOL,
+    Dyson360Eye,
+    Dyson360Heurist,
+    DysonDevice,
+    DysonPureCool,
+    DysonPureCoolFormaldehyde,
+    DysonPureCoolLink,
+    DysonPureHotCool,
+    DysonPureHotCoolLink,
+    DysonPureHumidifyCool,
+    DysonPurifierHumidifyCoolFormaldehyde,
+    get_device,
+)
+
+from . import CREDENTIAL, SERIAL
+
+
+@pytest.mark.parametrize(
+    "device_type,class_type",
+    [
+        (DEVICE_TYPE_360_EYE, Dyson360Eye),
+        (DEVICE_TYPE_360_HEURIST, Dyson360Heurist),
+        (DEVICE_TYPE_PURE_COOL_LINK_DESK, DysonPureCoolLink),
+        (DEVICE_TYPE_PURE_COOL_LINK, DysonPureCoolLink),
+        (DEVICE_TYPE_PURE_COOL, DysonPureCool),
+        (DEVICE_TYPE_PURE_COOL_FORMALDEHYDE, DysonPureCoolFormaldehyde),
+        (DEVICE_TYPE_PURE_COOL_DESK, DysonPureCool),
+        (DEVICE_TYPE_PURE_HOT_COOL_LINK, DysonPureHotCoolLink),
+        (DEVICE_TYPE_PURE_HOT_COOL, DysonPureHotCool),
+        (DEVICE_TYPE_PURE_HOT_COOL_NEW, DysonPureHotCool),
+        (DEVICE_TYPE_PURE_HUMIDIFY_COOL, DysonPureHumidifyCool),
+        (DEVICE_TYPE_PURIFIER_HUMIDIFY_COOL, DysonPureHumidifyCool),
+        (DEVICE_TYPE_PURIFIER_HUMIDIFY_COOL_FORMALDEHYDE, DysonPurifierHumidifyCoolFormaldehyde),
+        (DEVICE_TYPE_PURIFIER_HOT_COOL, DysonPureHotCool),
+    ],
+)
+def test_get_device(device_type: str, class_type: Type[DysonDevice]):
+    """Test get_device."""
+    device = get_device(SERIAL, CREDENTIAL, device_type)
+    assert isinstance(device, class_type)
+    assert device.serial == SERIAL
+
+
+def test_get_device_unknown():
+    """Test get_device with unknown type."""
+    device = get_device(SERIAL, CREDENTIAL, "unknown")
+    assert device is None
