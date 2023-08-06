27a6c2cccef1a04eac1f0853863ca57c6b5dbd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:08 +0000

    Add package chessmaker/0.1.2/chessmaker-0.1.2-py3-none-any.whl

---
+++ packages/chessmaker/chessmaker-0.1.2-py3-none-any.whl/chessmaker/chess/rules/__init__.py
@@ -0,0 +1,6 @@
+from .beta_decay import BetaDecay
+from .forced_en_passant import ForcedEnPassant
+from .il_vaticano import IlVaticano
+from .knight_boosting import KnightBoosting
+from .omnipotent_f6_pawn import OmnipotentF6Pawn
+from .siberian_swipe import SiberianSwipe
