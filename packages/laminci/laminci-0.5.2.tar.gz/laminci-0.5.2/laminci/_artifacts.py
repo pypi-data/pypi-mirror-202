9a5c8ce10e53f0ee2b31458ee00840897cdfdff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:28 +0000

    Add package laminci/0.5.2/laminci-0.5.2.tar.gz

---
+++ packages/laminci/laminci-0.5.2.tar.gz/laminci-0.5.2/laminci/_artifacts.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+from zipfile import ZipFile
+
+import lndb
+from lamin_logger import logger
+
+from ._env import get_package_name
+
+
+def zip_docs_dir(zip_filename: str) -> None:
+    with ZipFile(zip_filename, "w") as zf:
+        zf.write("README.md")
+        for f in Path("./docs").glob("**/*"):
+            if ".ipynb_checkpoints" in str(f):
+                continue
+            if f.suffix in {".md", ".ipynb", ".png", ".jpg", ".svg"}:
+                zf.write(f, f.relative_to("./docs"))  # add at root level
+
+
+def upload_docs_artifact() -> None:
+    lndb.close()
+    import lamindb as ln
+
+    if "GITHUB_EVENT_NAME" in os.environ and os.environ["GITHUB_EVENT_NAME"] != "push":
+        logger.info("Only upload docs artifact for push event.")
+        return None
+    package_name = get_package_name()
+    zip_filename = f"{package_name}_docs.zip"
+    zip_docs_dir(zip_filename)
+
+    ln.setup.load("testuser1/lamin-site-assets", migrate=True)
+
+    transform = ln.add(ln.Transform, name=f"CI {package_name}")
+    ln.track(transform=transform)
+
+    file = ln.select(ln.File, key=f"docs/{zip_filename}").one_or_none()
+    if file is not None:
+        file.replace(zip_filename)
+    else:
+        file = ln.File(zip_filename, key=f"docs/{zip_filename}")
+    ln.add(file)
