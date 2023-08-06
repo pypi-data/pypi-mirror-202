c54bf8dc1d55ce5a132b2fe9caa70a9d96776e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:39 +0000

    Add package pydaa/1.6.0/pydaa-1.6.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pydaa/pydaa-1.6.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pydaa.pyi
@@ -0,0 +1,12 @@
+"""Design and Analysis of Algorithms"""
+from __future__ import annotations
+import pydaa
+import typing
+
+__all__ = [
+    "dynamic_programming",
+    "greedy",
+    "sorting"
+]
+
+
