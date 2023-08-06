b3f6e666ff848694df4fb123c46edafbdc1540db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:04 +0000

    Add package algodojo/0.1.8/algodojo-0.1.8.tar.gz

---
+++ packages/algodojo/algodojo-0.1.8.tar.gz/algodojo-0.1.8/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="algodojo",
+    version="0.1.8",
+    author="algodojo team",
+    author_email="slin@dojotechs.com",
+    description="A automatic trading application of package",
+    keywords='quant quantitative investment trading algotrading',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DojoTechs/",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=["ibapi",
+                      "python-socketio",
+                      "requests",
+                      "websocket-client",
+                      "pandas",
+                      "quantstats"
+                      ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True
+)
