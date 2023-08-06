34e55b1620093a8d6b449ace7a08c4da1db48fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:27 +0000

    Add package hypermodern-python-tuto/1.0.4/hypermodern_python_tuto-1.0.4.tar.gz

---
+++ packages/hypermodern-python-tuto/hypermodern_python_tuto-1.0.4.tar.gz/hypermodern_python_tuto-1.0.4/src/hypermodern_python_tuto/wikipedia/language.py
@@ -0,0 +1,78 @@
+"""Language enum declaration."""
+import ctypes
+import enum
+import locale
+import os
+
+
+class Language(str, enum.Enum):
+    """Supported languages for the pages on the Wikipedia API (non-exhaustive)."""
+
+    FRENCH = "french"
+    ENGLISH = "english"
+    OTHER = "other"
+
+    @classmethod
+    def from_preferences(cls) -> "Language":
+        """Search for the preferred language in the computer's settings.
+
+        Since the computer settings vary a lot depending on the operating system,
+        this function works on posix and Windows only.
+
+        Returns:
+            The preferred language.
+
+        Raises:
+            RuntimeError: If the os name is not posix or Windows.
+
+        Examples:
+            >>> language = Language.from_preferences()
+            >>> isinstance(language, Language)
+            True
+        """
+        if os.name == "posix":
+            return cls._from_preferences_posix()
+        if os.name == "nt":
+            return cls._from_preferences_windows()
+        error_message = f"Unsupported operating system: {os.name}."
+        raise RuntimeError(error_message)
+
+    @classmethod
+    def _from_preferences_posix(cls) -> "Language":
+        language = os.environ["LANG"]
+        return cls.from_str(language)
+
+    @classmethod
+    def _from_preferences_windows(cls) -> "Language":
+        windll = ctypes.windll.kernel32  # type: ignore[attr-defined]
+        language = locale.windows_locale[windll.GetUserDefaultUILanguage()]
+        return cls.from_str(language)
+
+    @classmethod
+    def from_str(cls, language: str) -> "Language":
+        """Convert a language name to its equivalent Enum.
+
+        Args:
+            language: The name of the language.
+
+        Returns:
+            The equivalent of the language in Enum.
+
+        Warnings:
+            Inaccurate because it works by looking for the beginning of the name,
+            e.g. "fresh" will be detected as FRENCH.
+
+        Examples:
+            >>> Language.from_str("french") is Language.FRENCH
+            True
+            >>> Language.from_str("en") is Language.ENGLISH
+            True
+            >>> Language.from_str("unrecognized") is Language.OTHER
+            True
+        """
+        language = language.lower()
+        if language.startswith("fr"):
+            return cls.FRENCH
+        if language.startswith("en"):
+            return cls.ENGLISH
+        return cls.OTHER
