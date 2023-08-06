a3f0d8e66abf778004d155db3ba589220a6a2369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:02 +0000

    Add package simple-arith/0.1.52/simple_arith-0.1.52-py3-none-any.whl

---
+++ packages/simple-arith/simple_arith-0.1.52-py3-none-any.whl/simple-arith/arith.py
@@ -0,0 +1,20 @@
+class SimpleOperations:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        operation = self.a + self.b
+        return operation
+    
+    def subtract(self):
+        operation = self.a - self.b
+        return operation
+    
+    def multiply(self):
+        operation = self.a * self.b
+        return operation
+    
+    def divide(self):
+        operation = self.a / self.b
+        return operation
