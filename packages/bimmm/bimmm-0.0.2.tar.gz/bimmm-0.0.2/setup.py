0b9438589aae374b9249a9b7fa00342d57e9ae2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:51 +0000

    Add package bimmm/0.0.2/bimmm-0.0.2.tar.gz

---
+++ packages/bimmm/bimmm-0.0.2.tar.gz/bimmm-0.0.2/setup.py
@@ -0,0 +1,49 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # Here is the module name.
+    name="bimmm",
+
+    # version of the module
+    version="0.0.2",
+
+    # Name of Author
+    author="Simon Teggelaar",
+
+    # your Email address
+    author_email="simonteggelaar@gmail.com",
+
+    # #Small Description about module
+    description="Package that makes it a bit easier to visualize and analyse Marketing Mix Models",
+
+    # Om schrijving van het project uit readme
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    # Specifying that we are using markdown file for description
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+
+    # Any link to reach this module, ***if*** you have any webpage or github profile
+    # url="https://github.com/username/",
+    packages=setuptools.find_packages(),
+
+    # if module has dependencies i.e. if your package rely on other package at pypi.org
+    # then you must add there, in order to download every requirement of package
+
+    install_requires=[
+         "pandas", "numpy", "scipy", "plotly", "statsmodels", "tqdm", "requests"
+       ],
+
+    license="MIT",
+
+    # classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
