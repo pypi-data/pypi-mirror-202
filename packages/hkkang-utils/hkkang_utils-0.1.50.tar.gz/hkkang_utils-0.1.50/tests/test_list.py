6c9d4526aeb22dcd75d4f5b66ad8274b42183e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:58 +0000

    Add package hkkang-utils/0.1.50/hkkang_utils-0.1.50.tar.gz

---
+++ packages/hkkang-utils/hkkang_utils-0.1.50.tar.gz/hkkang_utils-0.1.50/tests/test_list.py
@@ -0,0 +1,17 @@
+import unittest
+
+import src.hkkang_utils.list as list_utils
+
+
+class Test_list_utils(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(Test_list_utils, self).__init__(*args, **kwargs)
+    
+    def test_do_flatten_list(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
+    
