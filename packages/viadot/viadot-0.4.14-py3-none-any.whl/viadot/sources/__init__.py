2f59e4f62c264352770f50f37804d3f259ada505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:07 +0000

    Add package viadot/0.4.14/viadot-0.4.14-py3-none-any.whl

---
+++ packages/viadot/viadot-0.4.14-py3-none-any.whl/viadot/sources/__init__.py
@@ -0,0 +1,28 @@
+from .azure_blob_storage import AzureBlobStorage
+from .azure_data_lake import AzureDataLake
+from .azure_sql import AzureSQL
+from .bigquery import BigQuery
+from .cloud_for_customers import CloudForCustomers
+from .outlook import Outlook
+from .salesforce import Salesforce
+from .sharepoint import Sharepoint
+from .supermetrics import Supermetrics
+from .genesys import Genesys
+from .sftp import SftpConnector
+from .mediatool import Mediatool
+
+try:
+    from .sap_rfc import SAPRFC
+except ImportError:
+    pass
+
+from .duckdb import DuckDB
+from .epicor import Epicor
+from .sql_server import SQLServer
+from .sqlite import SQLite
+from .mindful import Mindful
+from .customer_gauge import CustomerGauge
+
+# APIS
+from .uk_carbon_intensity import UKCarbonIntensity
+from .hubspot import Hubspot
