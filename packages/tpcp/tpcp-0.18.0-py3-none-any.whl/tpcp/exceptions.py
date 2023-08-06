fd15b87d4ea9c82b6e16a1d0c9e78785f9bb2837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:49 +0000

    Add package tpcp/0.18.0/tpcp-0.18.0-py3-none-any.whl

---
+++ packages/tpcp/tpcp-0.18.0-py3-none-any.whl/tpcp/exceptions.py
@@ -0,0 +1,17 @@
+"""Exceptions and warnings."""
+
+
+class PotentialUserErrorWarning(UserWarning):
+    """A warning indicating that the user might not use certain features correctly."""
+
+
+class ScorerFailedError(Exception):
+    """A error indicating that a scorer failed."""
+
+
+class ValidationError(Exception):
+    """An error indicating that data-object does not comply with the guidelines."""
+
+
+class MutableDefaultsError(ValidationError):
+    """An exception raised whenever, issues because of mutable default parameters occur/are expected."""
