1b3fc6e46e56b7f0f9af7b0338aa7e7515818b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:15 +0000

    Add package zcatalyst-sdk/0.0.2rc2/zcatalyst_sdk-0.0.2rc2.tar.gz

---
+++ packages/zcatalyst-sdk/zcatalyst_sdk-0.0.2rc2.tar.gz/zcatalyst_sdk-0.0.2rc2/zcatalyst_sdk/_thread_util.py
@@ -0,0 +1,20 @@
+import threading
+from typing import Any
+
+
+class ZCThreadUtil:
+    def __init__(self) -> None:
+        self.__zclocal = getattr(threading.current_thread(), '__zc_local', {})
+
+    def get_value(self, key: str):
+        return self.__zclocal.get(key)
+
+    def put_value(self, key: str, val: Any):
+        self.__zclocal[key] = val
+
+
+def get_attr(obj: dict, key: str):
+    if obj.get(key):
+        return obj.get(key)
+    fallback_obj = dict((k.lower(), v) for k, v in obj.items())
+    return fallback_obj.get(key.lower())
