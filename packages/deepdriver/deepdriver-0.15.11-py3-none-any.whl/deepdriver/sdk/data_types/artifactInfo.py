0e6ced0372811f47007d03fbeb64481a58e88068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:31 +0000

    Add package deepdriver/0.15.11/deepdriver-0.15.11-py3-none-any.whl

---
+++ packages/deepdriver/deepdriver-0.15.11-py3-none-any.whl/deepdriver/sdk/data_types/artifactInfo.py
@@ -0,0 +1,11 @@
+from deepdriver.sdk.interface.grpc_interface_pb2 import ArtifactEntry
+from typing import  List
+
+class ArtifactInfo():
+    def __init__(self, artifact_id: int, artifact_name: str, artifact_type: str,  artifact_digest: str,last_file_yn: str, entry_list: List[ArtifactEntry]):
+        self.artifact_id = artifact_id
+        self.artifact_name = artifact_name
+        self.artifact_type = artifact_type
+        self.artifact_digest = artifact_digest
+        self.last_file_yn = last_file_yn
+        self.entry_list = entry_list
