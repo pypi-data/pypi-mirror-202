73f1aa73d8b35a33d6c3d9142a2dda2013cad35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:05 +0000

    Add package blender-stubs/3.2.0.27.dev4653449796/blender-stubs-3.2.0.27.dev4653449796.tar.gz

---
+++ packages/blender-stubs/blender-stubs-3.2.0.27.dev4653449796.tar.gz/blender-stubs-3.2.0.27.dev4653449796/brush_curves_sculpt_tool_items/__init__.pyi
@@ -0,0 +1,44 @@
+"""
+
+
+Brush Curves Sculpt Tool Items
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:COMB:            
+  Comb Curves.
+
+:DELETE:          
+  Delete Curves.
+
+:SNAKE_HOOK:      
+  Curves Snake Hook.
+
+:ADD:             
+  Add Curves.
+
+:GROW_SHRINK:     
+  Grow / Shrink Curves.
+
+:SELECTION_PAINT: 
+  Paint Selection.
+
+:PINCH:           
+  Pinch Curves.
+
+:SMOOTH:          
+  Smooth Curves.
+
+:PUFF:            
+  Puff Curves.
+
+:DENSITY:         
+  Density Curves.
+
+:SLIDE:           
+  Slide Curves.
+
+.. _rna-enum-brush-curves-sculpt-tool-items:
+
+"""
+
+import typing
