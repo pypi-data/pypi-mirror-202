1ff628d26660a4adf4247499bb70e3ef9b8d83da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:29 +0000

    Add package jijzeptlab-client/0.1.0/jijzeptlab_client-0.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/jijzeptlab-client/jijzeptlab_client-0.1.0-cp310-cp310-win_amd64.whl/jijzeptlab/hybrid_algorithm/__init__.pyi
@@ -0,0 +1,7 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from jijzeptlab.hybrid_algorithm import (
+        benders_decomposition as benders_decomposition,
+        dantzig_wolfe_decomposition as dantzig_wolfe_decomposition
+    )
---
+++ packages/jijzeptlab-client/jijzeptlab_client-0.1.0-cp310-cp310-win_amd64.whl/jijzeptlab/solver/__init__.pyi
@@ -0,0 +1,4 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from jijzeptlab.solver import mip as mip
