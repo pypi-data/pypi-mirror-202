addf45c39fa077f7694d31ec0d3fa383e1dfb1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:23 +0000

    Add package lmah88/1.4/lmah88-1.4.tar.gz

---
+++ packages/lmah88/lmah88-1.4.tar.gz/lmah88-1.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='lmah88',
+        version='1.4',
+        description='ENSF 338 Final Project',
+        author='Liam and Theodore',
+        packages=find_packages(),
+        install_requires=[]
+        )
+        
