86a54d56931b0b00e8b1dcccacb967a0b1771b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:54 +0000

    Add package ape-solidity/0.6.3/ape-solidity-0.6.3.tar.gz

---
+++ packages/ape-solidity/ape-solidity-0.6.3.tar.gz/ape-solidity-0.6.3/tests/contracts/UseYearn.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.17;
+
+import {VaultAPI} from "@vault/BaseStrategy.sol";
+
+
+interface UseYearn is VaultAPI {
+    function name() override external view returns (string calldata);
+}
