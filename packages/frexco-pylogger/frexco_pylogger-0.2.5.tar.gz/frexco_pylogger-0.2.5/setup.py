f5a844c5f0981e320d459e1b1b1ca177aeaf91a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:40 +0000

    Add package frexco-pylogger/0.2.5/frexco_pylogger-0.2.5.tar.gz

---
+++ packages/frexco-pylogger/frexco_pylogger-0.2.5.tar.gz/frexco_pylogger-0.2.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.10',
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='frexco_pylogger',
+    packages=find_packages(include=['frexco_pylogger']),
+    version='0.2.5',
+    description='Python library for logging with colors and http status code.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/frexco-digital/frexco-pylogger.git',
+    install_requires=["requests"],
+    classifiers=CLASSIFIERS,
+    keywords='logs logging logger',
+)
