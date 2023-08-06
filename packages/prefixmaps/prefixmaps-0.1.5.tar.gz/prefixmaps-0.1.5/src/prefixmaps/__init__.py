61c01c11725087f5bfb1ca74406e4c1384d62f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:35 +0000

    Add package prefixmaps/0.1.5/prefixmaps-0.1.5.tar.gz

---
+++ packages/prefixmaps/prefixmaps-0.1.5.tar.gz/prefixmaps-0.1.5/src/prefixmaps/__init__.py
@@ -0,0 +1,16 @@
+from .datamodel.context import Context, PrefixExpansion, StatusType
+from .io.parser import load_context, load_multi_context
+
+try:
+    from importlib.metadata import version
+except ImportError:  # for Python<3.8
+    from importlib_metadata import version
+
+__all__ = [
+    "load_context",
+    "load_multi_context",
+    "Context",
+    "StatusType",
+    "PrefixExpansion",
+]
+__version__ = version(__name__)
