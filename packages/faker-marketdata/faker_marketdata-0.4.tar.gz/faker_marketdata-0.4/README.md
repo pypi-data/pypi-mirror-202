2318d6532eba3e063c5624af093e5e0e48f58501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:12 +0000

    Add package faker-marketdata/0.4/faker_marketdata-0.4.tar.gz

---
+++ packages/faker-marketdata/faker_marketdata-0.4.tar.gz/faker_marketdata-0.4/README.md
@@ -0,0 +1,44 @@
+# faker_marketdata
+Generates random, valid market identifiers following most common formats in the industry.
+
+## Installation
+
+``` bash
+pip install faker_marketdata
+```
+
+## Usage
+
+Add as a provider to your [Faker](https://faker.readthedocs.io/en/master) instance:
+``` python
+>>> from faker import Faker
+>>> from faker_marketdata import MarketDataProvider
+>>> fake = Faker()
+>>> fake.add_provider(MarketDataProvider)
+```
+Optionally you can define a seed value to have repeatable identifiers: 
+``` python
+>> Faker.seed(123)
+>> fake.add_provider(MarketDataProvider())
+```
+
+Now you can start to generate market identifiers, some examples:
+``` python
+>> fake.isin()   # "GTYMQXUIYPB6"
+>> fake.sedol()  # "NKDEKC8"
+>> fake.cusip()  # "Z57XGDJW7"
+>> fake.ticker() # "GRTT"
+```
+
+## Documentation
+Supported market data formats:
+* ISIN
+* SEDOL
+* MIC
+* LEI
+* CUSIP
+* RIC
+* TICKER
+* NSIN
+* FIGI
+* Market type (e.g. "bond", "commodity", "otc")
---
+++ packages/faker-marketdata/faker_marketdata-0.4.tar.gz/faker_marketdata-0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="faker_marketdata",
+    version="0.4",
+    author="Ruben Afonso",
+    author_email="rbfrancos@gmail.com",
+    description="Sample market data for Faker",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/rubenafo/faker_marketdata",
+    keywords = ["faker", "testdata", "financedata", "python", "marketdata", "isin", "sedol", "cusip",
+                "ticker", "figi"],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    python_requires='>=3.6',
+    install_requires=['Faker>=9.3.1', 'pycountry>=22.3.5']
+)
