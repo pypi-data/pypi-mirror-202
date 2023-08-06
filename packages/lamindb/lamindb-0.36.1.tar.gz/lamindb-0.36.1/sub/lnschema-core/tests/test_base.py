d209e5d9bbda8d7418352a493f69a3f1c8126963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:28 +0000

    Add package lamindb/0.36.1/lamindb-0.36.1.tar.gz

---
+++ packages/lamindb/lamindb-0.36.1.tar.gz/lamindb-0.36.1/sub/lnschema-core/tests/test_base.py
@@ -0,0 +1,9 @@
+from lnschema_core.dev import id
+
+
+def test_id():
+    assert len(id.file()) == 20
+    assert len(id.user()) == 8
+    assert len(id.secret()) == 40
+    assert len(id.usage()) == 24
+    assert len(id.instance()) == 12
