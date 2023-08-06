7df4cd15991e4cebb74090d44252f7d5d12f8b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:51 +0000

    Add package lama-cleaner-custom/0.0.2/lama_cleaner_custom-0.0.2-py3-none-any.whl

---
+++ packages/lama-cleaner-custom/lama_cleaner_custom-0.0.2-py3-none-any.whl/lama_cleaner/plugins/__init__.py
@@ -0,0 +1,6 @@
+from .interactive_seg import InteractiveSeg
+from .remove_bg import RemoveBG
+from .realesrgan import RealESRGANUpscaler
+from .gfpgan_plugin import GFPGANPlugin
+from .restoreformer import RestoreFormerPlugin
+from .gif import MakeGIF
