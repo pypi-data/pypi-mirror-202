dba0465831119881a7c4a22515fbd69f25438078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:44 +0000

    Add package arraymap/0.1.1/arraymap-0.1.1.tar.gz

---
+++ packages/arraymap/arraymap-0.1.1.tar.gz/arraymap-0.1.1/README.md
@@ -0,0 +1,38 @@
+
+arraymap
+============
+
+The ArrayMap library provides dictionary-like lookup from NumPy array values to their integer positions. The hash table design and C implementation is based on [AutoMap](https://github.com/brandtbucher/automap), with extensive additions for direct support of NumPy arrays.
+
+
+Code: https://github.com/static-frame/arraymap
+
+Packages: https://pypi.org/project/arraymap
+
+
+
+Dependencies
+--------------
+
+ArrayMap requires the following:
+
+- Python >= 3.7
+- NumPy >= 1.18.5
+
+
+
+What is New in ArrayMap
+-------------------------
+
+
+0.1.1
+-------
+
+Added `__version__` to module; releasing wheels.
+
+
+0.1.0
+-------
+
+Initial release with NumPy integration.
+
---
+++ packages/arraymap/arraymap-0.1.1.tar.gz/arraymap-0.1.1/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+import typing as tp
+import site
+import os
+
+
+AM_VERSION = "0.1.1"
+
+
+with open("README.md") as file:
+    LONG_DESCRIPTION = file.read()
+
+
+def get_ext_dir(*components: tp.Iterable[str]) -> tp.Sequence[str]:
+    dirs = []
+    for sp in site.getsitepackages():
+        fp = os.path.join(sp, *components)
+        if os.path.exists(fp):
+            dirs.append(fp)
+    return dirs
+
+
+extension = setuptools.Extension(
+    "arraymap",
+    ["arraymap.c"],
+    include_dirs=get_ext_dir("numpy", "core", "include"),
+    library_dirs=get_ext_dir("numpy", "core", "lib"),
+    define_macros=[("AM_VERSION", AM_VERSION)],
+    libraries=["npymath"],  # not including mlib at this time
+)
+
+
+setuptools.setup(
+    author="Christopher Ariza, Brandt Bucher",
+    version=AM_VERSION,
+    description="Dictionary-like lookup from NumPy array values to their integer positions",
+    ext_modules=[extension],
+    license="MIT",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    name="arraymap",
+    python_requires=">=3.7.0",
+    url="https://github.com/static-frame/arraymap",
+)
