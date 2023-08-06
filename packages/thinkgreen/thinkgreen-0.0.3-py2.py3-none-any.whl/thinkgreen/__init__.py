54badf2df7fbba8f3c99f77b9d1d3f330f2f93b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:31 +0000

    Add package thinkgreen/0.0.3/thinkgreen-0.0.3-py2.py3-none-any.whl

---
+++ packages/thinkgreen/thinkgreen-0.0.3-py2.py3-none-any.whl/thinkgreen/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for thinkgreen."""
+
+__author__ = """Olivia Lamm"""
+__email__ = 'olamm@vols.utk.edu'
+__version__ = '0.0.3'
+
+from .thinkgreen import *
---
+++ packages/thinkgreen/thinkgreen-0.0.3-py2.py3-none-any.whl/thinkgreen/thinkgreen.py
@@ -0,0 +1,291 @@
+"""Main module."""
+
+import string
+import random
+import ipyleaflet 
+
+class Map(ipyleaflet.Map):
+    
+        def __init__(self, center=[20, 0], zoom=2, **kwargs) -> None:
+
+            if "scroll_wheel_zoom" not in kwargs:
+                kwargs["scroll_wheel_zoom"] = True
+
+            super().__init__(center=center, zoom=zoom, **kwargs)
+
+            if "height" not in kwargs:
+                self.layout.height = "500px"
+            else:
+                self.layout.height = kwargs["height"]
+
+            if "fullscreen_control" not in kwargs:
+                kwargs["fullscreen_control"] = True
+            if kwargs["fullscreen_control"]:
+                self.add_fullscreen_control()
+            
+            if "layers_control" not in kwargs:
+                kwargs["layers_control"] = False
+            if kwargs["layers_control"]:
+                self.add_layers_control()
+
+        def add_search_control(self, position="topleft", **kwargs):
+            """Adds a search control to the map.
+            Args:
+                kwargs: Keyword arguments to pass to the search control.
+            
+            Returns:
+                ipyleaflet.SearchControl: The search control.
+            """
+            if "url" not in kwargs:
+                kwargs["url"] = 'https://nominatim.openstreetmap.org/search?format=json&q={s}'
+        
+
+            search_control = ipyleaflet.SearchControl(position=position, **kwargs)
+            self.add_control(search_control)
+
+        def add_draw_control(self, **kwargs):
+            """Adds a draw control to the map.
+            Args:
+                kwargs: Keyword arguments to pass to the draw control.
+            
+            Returns:
+                ipyleaflet.DrawControl: Draw control.
+            """
+            draw_control = ipyleaflet.DrawControl(**kwargs)
+
+            draw_control.polyline =  {
+                "shapeOptions": {
+                    "color": "#6bc2e5",
+                    "weight": 8,
+                    "opacity": 1.0
+                }
+            }
+            draw_control.polygon = {
+                "shapeOptions": {
+                    "fillColor": "#6be5c3",
+                    "color": "#6be5c3",
+                    "fillOpacity": 1.0
+                },
+                "drawError": {
+                    "color": "#dd253b",
+                    "message": "Oups!"
+                },
+                "allowIntersection": False
+            }
+            draw_control.circle = {
+                "shapeOptions": {
+                    "fillColor": "#efed69",
+                    "color": "#efed69",
+                    "fillOpacity": 1.0
+                }
+            }
+            draw_control.rectangle = {
+                "shapeOptions": {
+                    "fillColor": "#fca45d",
+                    "color": "#fca45d",
+                    "fillOpacity": 1.0
+                }
+            }
+
+            self.add_control(draw_control)
+
+        def add_layers_control(self, position="topright"):
+            """Adds a layers control to the map.
+            Args:
+                kwargs: Keyword arguments to pass to the layers control.
+            
+            Returns: 
+                ipyleaflet.LayersControl: The layers control.
+            """
+            layers_control = ipyleaflet.LayersControl(position=position)
+            self.add_control(layers_control)
+
+        def add_fullscreen_control(self, position="topleft"):
+            """Adds a fullscreen control to the map.
+            Args:
+                kwargs: Keyword arguments to pass to the fullscreen control.
+
+            Returns:
+                ipyleaflet.FullscreenControl: Allows control of screensize.
+            """
+            fullscreen_control = ipyleaflet.FullScreenControl(position=position)
+            self.add_control(fullscreen_control)
+
+        def add_tile_layer(self, url, name, attribution="", **kwargs):
+            """Adds a tile layer to the map.
+            Args:
+                url (str): The URL template of the tile layer.
+                attribution (str): The attribution of the tile layer.
+                name (str, optional): The name of the tile layer. Defaults to "OpenStreetMap".
+                kwargs: Keyword arguments to pass to the tile layer.
+
+            Returns: 
+                ipyleaflet.TileLayer: Adds a new layer to the map.
+            """
+            tile_layer = ipyleaflet.TileLayer(url=url, attribution=attribution, name=name, **kwargs)
+            self.add_layer(tile_layer)
+
+        def add_basemap(self, basemap): 
+            """Adds a basemap to the map.
+            Args:
+                basemap (str): The name of the basemap to add.
+            
+            Returns:
+                ipyleaflet.Basemap: Changes basemap.
+            """
+            import xyzservices.providers as xyz
+            try:
+                layer = eval(f"xyz.{basemap}")
+                url = layer.build_url()
+                attribution = layer.attribution
+                self.add_tile_layer(url=url, attribution=attribution, name=basemap)
+
+            except:
+                raise ValueError(f"Invalid basemap name: {basemap}")
+        
+
+        def add_geojson(self, data, **kwargs):
+            """Adds a GeoJSON layer to the map.
+            Args:
+                data (dict): The GeoJSON data.
+                kwargs: Keyword arguments to pass to the GeoJSON layer.
+
+            Returns:
+                ipyleaflet.Geojson: Adds a GeoJSON layer to map. 
+            """
+            import json
+            
+            #for file paths:
+            if isinstance(data, str):
+                with open(data, "r") as f:
+                    data = json.load(f)
+
+            geojson = ipyleaflet.GeoJSON(data=data, **kwargs)
+            self.add_layer(geojson)
+        
+        def add_shp(self, data, name='Shapefile', **kwargs):
+            """Adds a Shapefile layer to the map.
+
+            Args:
+                data (str): The path to the Shapefile.
+            
+            Returns:
+                ipyleaflet.ShapeFile: Adds a shapefile to map.
+            """
+            import geopandas as gpd
+            gdf = gpd.read_file(data)
+            geojson = gdf.__geo_interface__
+            self.add_geojson(geojson, name=name, **kwargs)
+
+        def add_vector(
+            self,
+            filename,
+            layer_name="Untitled",
+            bbox=None,
+            mask=None,
+            rows=None,
+            style={},
+            hover_style={},
+            style_callback=None,
+            fill_colors=["black"],
+            info_mode="on_hover",
+            encoding="utf-8",
+            **kwargs,
+        ):
+            """Adds any geopandas-supported vector dataset to the map.
+
+            Args:
+                filename (str): Either the absolute or relative path to the file or URL to be opened, or any object with a read() method (such as an open file or StringIO).
+                layer_name (str, optional): The layer name to use. Defaults to "Untitled".
+                bbox (tuple | GeoDataFrame or GeoSeries | shapely Geometry, optional): Filter features by given bounding box, GeoSeries, GeoDataFrame or a shapely geometry. CRS mis-matches are resolved if given a GeoSeries or GeoDataFrame. Cannot be used with mask. Defaults to None.
+                mask (dict | GeoDataFrame or GeoSeries | shapely Geometry, optional): Filter for features that intersect with the given dict-like geojson geometry, GeoSeries, GeoDataFrame or shapely geometry. CRS mis-matches are resolved if given a GeoSeries or GeoDataFrame. Cannot be used with bbox. Defaults to None.
+                rows (int or slice, optional): Load in specific rows by passing an integer (first n rows) or a slice() object.. Defaults to None.
+                style (dict, optional): A dictionary specifying the style to be used. Defaults to {}.
+                hover_style (dict, optional): Hover style dictionary. Defaults to {}.
+                style_callback (function, optional): Styling function that is called for each feature, and should return the feature style. This styling function takes the feature as argument. Defaults to None.
+                fill_colors (list, optional): The random colors to use for filling polygons. Defaults to ["black"].
+                info_mode (str, optional): Displays the attributes by either on_hover or on_click. Any value other than "on_hover" or "on_click" will be treated as None. Defaults to "on_hover".
+                encoding (str, optional): The encoding to use to read the file. Defaults to "utf-8". 
+            """
+            if not filename.startswith("http"):
+                filename = os.path.abspath(filename)
+            else:
+                filename = github_raw_url(filename)
+            ext = os.path.splitext(filename)[1].lower()
+            if ext == ".shp":
+                self.add_shp(
+                    filename,
+                    layer_name,
+                    style,
+                    hover_style,
+                    style_callback,
+                    fill_colors,
+                    info_mode,
+                    encoding,
+                )
+            elif ext in [".json", ".geojson"]:
+                self.add_geojson(
+                    filename,
+                    layer_name,
+                    style,
+                    hover_style,
+                    style_callback,
+                    fill_colors,
+                    info_mode,
+                    encoding,
+                )
+            else:
+                geojson = vector_to_geojson(
+                    filename,
+                    bbox=bbox,
+                    mask=mask,
+                    rows=rows,
+                    epsg="4326",
+                    **kwargs,
+                )
+
+                self.add_geojson(
+                    geojson,
+                    layer_name,
+                    style,
+                    hover_style,
+                    style_callback,
+                    fill_colors,
+                    info_mode,
+                    encoding,
+                )
+
+
+
+
+def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
+    """Generates a random string of a given length.
+    Args:
+        length (int, optional): The length of the string to generate. Defaults to 10.
+        upper (bool, optional): Whether to include uppercase letters. Defaults to False.
+        digits (bool, optional): Whether to include digits. Defaults to False.
+        punctuation (bool, optional): Whether to include punctuation. Defaults to False.
+    Returns:
+        str: The generated string.
+    """
+    letters = string.ascii_lowercase
+    if upper:
+        letters += string.ascii_uppercase
+    if digits:
+        letters += string.digits
+    if punctuation:
+        letters += string.punctuation
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def generate_lucky_number(length=1):
+    """Generates a random number of a given length.
+    Args:
+        length (int, optional): The length of the number to generate. Defaults to 1.
+    Returns:
+        int: The generated number.
+    """
+
+    result_str = ''.join(random.choice(string.digits) for i in range(length))
+    return int(result_str)
