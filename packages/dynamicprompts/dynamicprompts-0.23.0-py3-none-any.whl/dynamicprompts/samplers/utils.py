269246eb325a68bf140feb1cee6f4e32f1eaf34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:00 +0000

    Add package dynamicprompts/0.23.0/dynamicprompts-0.23.0-py3-none-any.whl

---
+++ packages/dynamicprompts/dynamicprompts-0.23.0-py3-none-any.whl/dynamicprompts/samplers/utils.py
@@ -0,0 +1,29 @@
+from dynamicprompts.commands import VariantCommand, VariantOption, WildcardCommand
+from dynamicprompts.parser.parse import parse
+from dynamicprompts.sampling_context import SamplingContext
+
+
+def wildcard_to_variant(
+    command: WildcardCommand,
+    *,
+    context: SamplingContext,
+    min_bound=1,
+    max_bound=1,
+    separator=",",
+) -> VariantCommand:
+    values = context.wildcard_manager.get_all_values(command.wildcard)
+    min_bound = min(min_bound, len(values))
+    max_bound = min(max_bound, len(values))
+
+    variant_options = [
+        VariantOption(parse(v, parser_config=context.parser_config)) for v in values
+    ]
+
+    wildcard_variant = VariantCommand(
+        variant_options,
+        min_bound,
+        max_bound,
+        separator,
+        command.sampling_method,
+    )
+    return wildcard_variant
