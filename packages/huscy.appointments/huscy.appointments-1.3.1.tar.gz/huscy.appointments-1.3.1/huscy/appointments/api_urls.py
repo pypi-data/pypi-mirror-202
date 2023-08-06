dcf85776795017ac5b0abb47ae502b2720d55aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:55 +0000

    Add package huscy.appointments/1.3.1/huscy.appointments-1.3.1.tar.gz

---
+++ packages/huscy.appointments/huscy.appointments-1.3.1.tar.gz/huscy.appointments-1.3.1/huscy/appointments/api_urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+
+from huscy.appointments import views
+
+
+router = DefaultRouter()
+router.register('appointments', views.AppointmentsViewSet, basename='appointment')
+router.register('resources', views.ResourcesViewSet)
+
+urlpatterns = router.urls
