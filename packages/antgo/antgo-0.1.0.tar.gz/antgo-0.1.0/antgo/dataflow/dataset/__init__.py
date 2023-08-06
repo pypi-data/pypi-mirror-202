4c4d37885b1754f4363461643674ab3e4a91c0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:37 +0000

    Add package antgo/0.1.0/antgo-0.1.0.tar.gz

---
+++ packages/antgo/antgo-0.1.0.tar.gz/antgo-0.1.0/antgo/dataflow/dataset/__init__.py
@@ -0,0 +1,15 @@
+#  -*- coding: UTF-8 -*-
+# @Time    : 18-3-26
+# @File    : __init__.py
+# @Author  : jian<jian@mltalker.com>
+from .dataset import Dataset
+from .cifar import Cifar10, Cifar100
+from .pascal_voc import Pascal2007, Pascal2012
+from .coco2017 import COCO2017
+from .interhand26M import InterHand26M
+from .lsp import LSP
+from .visalso import VisalSO
+from .imagenet import ImageNet
+__all__ = [
+  'Cifar10', 'Cifar100', 'ImageNet', 'Pascal2007', 'Pascal2012', 'COCO2017', 'LSP', 'VisalSO', 'Dataset'
+]
---
+++ packages/antgo/antgo-0.1.0.tar.gz/antgo-0.1.0/antgo/dataflow/imgaug/__init__.py
@@ -0,0 +1,52 @@
+# encoding=utf-8
+# @Time    : 17-6-22
+# @File    : __init__.py
+# @Author  : jian<jian@mltalker.com>
+from .operators import (
+  DecodeImage, 
+  ResizeS, 
+  RandomFlipImage, 
+  RandomDistort, 
+  Rotation,
+  KeepRatio, 
+  ColorDistort,
+  RandomErasingImage,
+  CropImage,
+  MixupImage,
+  CutmixImage,
+  Meta,
+  RandomScaledCrop,
+  ResizeByLong,
+  ResizeRangeScaling,
+  ResizeStepScaling,
+  AutoAugmentImage,
+  Permute,
+  UnSqueeze,
+  ConvertRandomObjJointsAndOffset,
+  ResizeByShort,
+  FixedCrop)
+
+__all__ = [
+  'DecodeImage', 
+  'ResizeS',
+  'RandomFlipImage', 
+  'RandomDistort', 
+  'Rotation', 
+  'KeepRatio', 
+  'ColorDistort',
+  'RandomErasingImage',
+  'CropImage',
+  'Permute',
+  'MixupImage',
+  'CutmixImage',
+  'RandomScaledCrop',
+  'ResizeByLong',
+  'ResizeRangeScaling',
+  'ResizeStepScaling',
+  'AutoAugmentImage',
+  'UnSqueeze',
+  'ConvertRandomObjJointsAndOffset',
+  'ResizeByShort',
+  'FixedCrop',
+  'Meta'
+]
