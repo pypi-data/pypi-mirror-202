79f7c133d245475671a004cbc4ff839b9fba08f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:14 +0000

    Add package cijoe/0.9.16/cijoe-0.9.16.tar.gz

---
+++ packages/cijoe/cijoe-0.9.16.tar.gz/cijoe-0.9.16/src/cijoe/core/scripts/dump_metrics.py
@@ -0,0 +1,11 @@
+"""
+currently not implemented
+"""
+
+
+def script_entry(args, cijoe, step):
+    """Dump extracted metrics from workflow output"""
+
+    print("Dump metrics")
+
+    return 0
---
+++ packages/cijoe/cijoe-0.9.16.tar.gz/cijoe-0.9.16/src/cijoe/core/scripts/extract.py
@@ -0,0 +1,11 @@
+"""
+Currently not implemented
+"""
+
+
+def script_entry(args, cijoe, step):
+    """Extract metrics from workflow output"""
+
+    print("extractor")
+
+    return 0
