ae4a3fa8a13f1a205650d818aab2fd21636951f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:24 +0000

    Add package akira3d/4.8.1/akira3d-4.8.1.tar.gz

---
+++ packages/akira3d/akira3d-4.8.1.tar.gz/akira3d-4.8.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='akira3d',
+      version='4.8.1',
+      description='Modern 3D Graphics Engine by alexCoder23',
+      packages=['akira3d'],
+      author_email='alekseybeldem@gmail.com',
+      zip_safe=False)
