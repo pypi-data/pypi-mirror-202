ac967d675193a7f20d188f044123bc1de053316e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:20:56 +0000

    Add package dataquality/0.8.26/dataquality-0.8.26.tar.gz

---
+++ packages/dataquality/dataquality-0.8.26.tar.gz/dataquality-0.8.26/dataquality/schemas/split.py
@@ -0,0 +1,36 @@
+from enum import Enum
+from typing import List, Union
+
+from dataquality.exceptions import GalileoException
+
+
+class Split(str, Enum):
+    training = "training"
+    train = "training"
+    validation = "validation"
+    test = "test"
+    testing = "test"
+    inference = "inference"
+
+    @staticmethod
+    def get_valid_attributes() -> List[str]:
+        return list(map(lambda x: x.value, Split))
+
+    @staticmethod
+    def get_valid_keys() -> List[str]:
+        return ["train", "training", "test", "testing", "validation"]
+
+
+def conform_split(split: Union[str, Split]) -> Split:
+    """Conforms split name to our naming conventions
+
+    Raises GalileoException if split is invalid
+    """
+    if isinstance(split, Split):
+        return split
+    try:
+        return Split[split]
+    except KeyError:
+        raise GalileoException(
+            f"Split must be one of {Split.get_valid_attributes()} " f"but got {split}"
+        )
