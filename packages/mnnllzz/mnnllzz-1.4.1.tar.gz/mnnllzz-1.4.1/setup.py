f0eefe88aef9eaba406930eeae1e2a54d64d20ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:11 +0000

    Add package mnnllzz/1.4.1/mnnllzz-1.4.1.tar.gz

---
+++ packages/mnnllzz/mnnllzz-1.4.1.tar.gz/mnnllzz-1.4.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'mnnllzz',
+  packages = ['mnnllzz'],
+  version = '1.4.1',
+  license='MIT',
+  description = 'Create randomized tests',
+  author = 'Andrea Tomadin',
+  author_email = 'andrea.tomadin@unipi.it',
+  url = 'https://github.com/atomadin/mnnllzz',
+  download_url = 'https://github.com/atomadin/mnnllzz/archive/refs/tags/v_1.4.1.tar.gz',
+  keywords = ['TEST', 'EXAM'],
+  install_requires=[
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
