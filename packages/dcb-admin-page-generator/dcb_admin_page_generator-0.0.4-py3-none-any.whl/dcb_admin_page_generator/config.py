17ac1f0821b337afd0187c42f5954b8097ee18d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:35 +0000

    Add package dcb-admin-page-generator/0.0.4/dcb_admin_page_generator-0.0.4-py3-none-any.whl

---
+++ packages/dcb-admin-page-generator/dcb_admin_page_generator-0.0.4-py3-none-any.whl/dcb_admin_page_generator/config.py
@@ -0,0 +1,118 @@
+defineConfig = {
+    "serverUrl": "https://yapi.gogpay.cn/",
+    "getInfoPath": "/api/interface/get",
+    "projects":
+      {
+        "token": {
+          #  店铺开发配置
+          #  https://yapi.gogpay.cn/project/2376/interface/api
+          2376: '5e6aca8fd687d595769e2cf7a1f6dfae875e0f70c283109e9503d15c1874953a',
+          # 平台管理端
+          #  https://yapi.gogpay.cn/project/2352/interface/api
+          2352: '15175d79b9b3aff56408f8308035804f853a19aadda6bee3fbdf514aac697c65',
+          # 审批中心
+          # https://yapi.gogpay.cn/project/2343/interface/api
+          2343: '2465c2e026e0b5141d02c644e6a4536c189d1b69fcf5e1df4ab59bc743bcf5ec',
+          # // 结算服务
+          # // https://yapi.gogpay.cn/project/2340/interface/api
+          2340: 'd3bf80c69d71a8f113eb306270ffc7551a816c38eda5f8129d30c6fa2650ba2b',
+          # // 中台统计
+          # // https://yapi.gogpay.cn/project/2187/interface/api
+          2187: '83d80ec0539888896e01886954ae3d75eab5ca1a1f67be5811a581c61c676ce7',
+          # // 业务交易服务
+          # // https://yapi.gogpay.cn/project/2005/interface/api
+          2005: '7e8cf6f57f611f3461ea0f58ef6dce31b2933d1c3578adfe8e0734ef9aed4d6e',
+          # // 统一订阅服务
+          # // https://yapi.gogpay.cn/project/1062/interface/api
+          1062: '68aa0d433babc4cff4f183ea036eae8d5cbe564e9dcb970ace3f8a8400f87dde',
+          # // 秒杀服务
+          # // https://yapi.gogpay.cn/project/1023/interface/api
+          1023: '00dbded2b3a3299c30b9d82de5ee5d6080fad83628da2e7a484db9a0b1fc7ec9',
+          # // 页面配置
+          # // https://yapi.gogpay.cn/project/1011/interface/api
+          1011: '2b8a734bc655cea745bca0ec966d528fd7a7d107b2aca7d9f36272b8d7cefb83',
+          # // 交易中心
+          # // https://yapi.gogpay.cn/project/999/interface/api
+          999: '84c41bd6d6a1618e7ca3c6c08fc75eb50561d0a3eeb1627276deacb27ddff942',
+          # // 管理后台公共接口
+          # // https://yapi.gogpay.cn/project/993/interface/api
+          993: '541431b1d68b066d6c8900633c1e833327fdc1acb8fdf5e1d501a74dcf754417',
+          # // 分发中心
+          # // https://yapi.gogpay.cn/project/981/interface/api
+          981: 'b13a0e528b5629f820fcf1969e072d7aac3ba8ce222dc02354b732e8d724fbff',
+          # // 公共属性
+          # // https://yapi.gogpay.cn/project/975/interface/api
+          975: 'ca6b5a8fe151cb48ec423a363041230387e997c85cf89d8e8f08b2293ad13086',
+          # // 搜索中心
+          # // https://yapi.gogpay.cn/project/969/interface/api
+          969: '197945d43e714caf4b76cd857616381d6c18ba2621fc4404e5895495f0739f2a',
+          # // 收藏服务
+          # // https://yapi.gogpay.cn/project/963/interface/api
+          963: '86b2592c2ca0dc79009eca5774de80108279d9497c1247c79ba04fd0eeafd6a7',
+          # // 购物车
+          # // https://yapi.gogpay.cn/project/957/interface/api
+          957: '2046329473712232ee4631a540815049889fdbed9cb1c5b3e6e04d7c948ee90d',
+          # // 物流体系
+          # // https://yapi.gogpay.cn/project/951/interface/api
+          951: 'daf45889af7d9c5ede62d6eb5d70d3c5640be560a205fb4c529c2f698d9d03a8',
+          # // 商品服务
+          # // https://yapi.gogpay.cn/project/939/interface/api
+          939: '4b405c47404ff1f01537fb70711a37deb604a213be7160f58cdc9f662478583f',
+          # // 活动服务
+          # // https://yapi.gogpay.cn/project/927/interface/api
+          927: '15444b96ff1642dff18ba22719369a0ff9b5234974dd79c88f5cf4f23e411d9a',
+          # // 评价服务
+          # // https://yapi.gogpay.cn/project/921/interface/api
+          921: '21b63b8909032bcd5d4a57f157c49cf32576d4fbf31e358c44a326afbd8ca5d1',
+          # // 售后服务
+          # // https://yapi.gogpay.cn/project/916/interface/api
+          916: '37eaea6e437c460b74881977428c4d31fa2fb14541d3682b0b29fc8c6bc947cd',
+          # // 商城服务
+          # // https://yapi.gogpay.cn/project/911/interface/api
+          911: '103fb7f3003f7692cb8ff4e03c2caf4080b42b16a5d416b0ff4ccb555011759b',
+          # // 订单服务
+          # // https://yapi.gogpay.cn/project/906/interface/api
+          906: 'dfb646e28fc9b05b13b63f752e7d5de30f331dcb08d2b024fe4163be19598943',
+          # // 新版商家机构和店铺管理
+          # // https://yapi.gogpay.cn/project/2492/interface/api
+          2492: 'e93137b90dd487b7f0faeb70331b3d996b2c2eecf4a04d08baec147b6303a558',
+          # //排行榜
+          # // https://yapi.gogpay.cn/project/2496/interface/api
+          2496: 'f7baecc53a8a821dd14dbd053f67750547783734054be184c123deda5d979ada',
+          # // 电商中台/文档资讯服务
+          #   https://yapi.gogpay.cn/project/2968/interface/api
+          2968: 'ff4487f4662d5383adaf8651ff7f4f329f53bbee40411a60e1654086d994eee5',
+          # // 电商中台/发券服务
+          #   https://yapi.gogpay.cn/project/2969/interface/api
+          2969: '85a147eb1fb3c118bab915b2726e057c8b0319f43228d38460efa9925508826a',
+          # // 智游宝边界服务
+          #   https://yapi.gogpay.cn/project/2994/interface/api
+          2994: 'cbc66b0802888c99b18ef6b4377fb641c31b7ce08805b3ee05955f780997f8e8',
+          # // 天使同城
+          #   https://yapi.gogpay.cn/project/2993/interface/api
+          2993: 'f886fe5d6fb31b5e8abc5b93be59a94a5c989f7dfb4057f2d9df67ff308ee8ea',
+          # // 镇远九州
+          #   https://yapi.gogpay.cn/project/2995/interface/api
+          2995: '70c8630e64702d67142f169f3c41fe8a893092afe30da3feb241a4839b8d2d79',
+          # // 启明星-售后
+          #   https://yapi.gogpay.cn/project/3307/interface/api
+          3307: '2af35201023a62d77a4f6f79c08c9ae3431c4b4ade9c94b84b06d9512921eb99',
+          # //  自我游
+          #   https://yapi.gogpay.cn/project/3457/interface/api
+          3457: '14aa4117e0cbf7230ca6f865be205e38e3e5e522c65e42ae3c54c9c6b4df69fa',
+          # //  金飞鹰
+          # // https://yapi.gogpay.cn/project/3517/interface/api
+          3517: '78dc0eed83d7b3bfe180b4efc85b5c03c2df5f545bbf46c330ff71dad043ce6a',
+          # // 电商中台/支付服务
+          #   https://yapi.gogpay.cn/project/3631/interface/api
+          3631: '90c448463376eb4405fcdd5e7557c5091f85b3931a472574a109610006dadd25',
+          # // 联联周边游
+          # // https://yapi.gogpay.cn/project/3655/interface/api/221614
+          3655: 'e991cc6cd248ca4caf4cd0d3776bb5b7bb41f8850b3360ffe3f3721f9b748e78',
+          # // 小橘子服务边界
+          # // https://yapi.gogpay.cn/project/3787/setting
+          3787: 'f4b98906087c4c12c08943cc12710f3a2a192dc1dd7fc936bf85699966e60e4d',
+        },
+      }
+}
+
