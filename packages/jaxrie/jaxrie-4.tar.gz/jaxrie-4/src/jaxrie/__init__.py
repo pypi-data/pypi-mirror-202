3c596db38ff0f7d804f6dff348a2fb475154223f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:02 +0000

    Add package jaxrie/4/jaxrie-4.tar.gz

---
+++ packages/jaxrie/jaxrie-4.tar.gz/jaxrie-4/src/jaxrie/__init__.py
@@ -0,0 +1,13 @@
+# Local
+from .manifold import Manifold, Stereographic, math, stereographic
+from .modules import HGCN, HLinear, HAct
+
+__all__ = [
+    "Stereographic",
+    "stereographic",
+    "math",
+    "Manifold",
+    "HGCN",
+    "HLinear",
+    "HAct",
+]
