3c5b484ef4841e3b383cebe721deb6440d237c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:31 +0000

    Add package dknovautils/0.0.15/dknovautils-0.0.15.tar.gz

---
+++ packages/dknovautils/dknovautils-0.0.15.tar.gz/dknovautils-0.0.15/dknovautils/dk_imports.py
@@ -0,0 +1,31 @@
+DkAppVer = '0.0.15'
+
+
+from typing import List, Dict, Tuple, Union
+
+import random
+import time
+
+import math
+import random
+from collections import namedtuple, deque
+import re
+import sys
+
+from datetime import  datetime
+import os
+import numpy as np
+import subprocess
+from queue import Queue
+from queue import Empty
+from pathlib import Path
+import itertools
+
+import time
+from datetime import datetime
+from datetime import date
+
+from dataclasses import dataclass
+
+
+
---
+++ packages/dknovautils/dknovautils-0.0.15.tar.gz/dknovautils-0.0.15/dknovautils/test_a.py
@@ -0,0 +1,51 @@
+"""
+
+测试注释1
+
+"""
+
+
+# 单行注释1
+from commons import *
+from dkfiles import DkPyFiles
+from dkat import AT
+
+print("test01 start")
+
+'''
+
+测试注释2
+
+'''
+
+"""
+
+测试注释3
+
+"""
+
+
+def ftest01():
+    '''
+
+    测试注释4
+
+    '''
+
+    assert 1 == 1, 'err5162'
+    fp = r'F:\DIKI\DIKIETC\ADEV\learn\LearnPython\PythonBook\dknovautils\dknovautils\test_a.py'
+
+    jsonstr = Path(fp).read_text(encoding='utf8')
+
+    # 这是单行注释2
+    fp2 = DkPyFiles.f_remove_comments_v1(jsonstr)
+    # print(fp2)
+
+    iprint_info(fp2)
+
+    cstr = AT.sdf_logger_fomrat_datetime()
+    print(cstr)
+
+
+if __name__ == '__main__':
+    ftest01()
