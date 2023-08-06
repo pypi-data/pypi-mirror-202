80139c1bee06d4e923996a88d8131dec6d6884a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package lhw0841/0.0.1/lhw0841-0.0.1-py3-none-any.whl

---
+++ packages/lhw0841/lhw0841-0.0.1-py3-none-any.whl/mycalc/calculator.py
@@ -0,0 +1,15 @@
+#calculator.py
+
+def add(x,y):
+  return x+y
+
+def substract(x,y):
+  return x-y
+
+def multiply(x,y):
+  return x*y
+
+def divide(x,y):
+  return x/y
+
+Writing mycalc/calculator.py
