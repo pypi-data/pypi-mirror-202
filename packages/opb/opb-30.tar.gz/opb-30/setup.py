e703296e3024e959bbbcacb836d3215ea7486e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:15 +0000

    Add package opb/30/opb-30.tar.gz

---
+++ packages/opb/opb-30.tar.gz/opb-30/setup.py
@@ -0,0 +1,42 @@
+# This file is placed in the Public Domain.
+
+
+"OPB is a object programming bot."
+
+
+from setuptools import setup
+
+
+def read():
+    return open("README.rst", "r").read()
+
+
+setup(
+    name="opb",
+    version="30",
+    author="Bart Thate",
+    author_email="operbot100@gmail.com",
+    url="http://github.com/operbot/opb",
+    description="object programming bot",
+    long_description=read(),
+    long_description_content_type="text/x-rst",
+    license="Public Domain",
+    packages=[
+              "opb",
+              "opb.modules"
+             ],
+    namespace_packages=[
+                        "opb",
+                        "opb.modules"
+                       ], 
+    scripts=["bin/opb", "bin/opbc", "bin/opbd"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: Public Domain",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: System Administrators",
+        "Topic :: Communications :: Chat :: Internet Relay Chat",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+     ],
+)
