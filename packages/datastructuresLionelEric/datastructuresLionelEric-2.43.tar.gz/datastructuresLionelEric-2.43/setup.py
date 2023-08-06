3942b0598aa350ee4ec60c1f803da2e3f10589b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:18 +0000

    Add package datastructuresLionelEric/2.43/datastructuresLionelEric-2.43.tar.gz

---
+++ packages/datastructuresLionelEric/datastructuresLionelEric-2.43.tar.gz/datastructuresLionelEric-2.43/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+root_package = "datastructures"
+setup(
+    name='datastructuresLionelEric',
+    packages=([root_package] + [f"{root_package}.{item}" for item in find_packages(where=root_package)]),
+    version='2.43',
+    description='My first Python library',
+    author='Me',
+    license='MIT',
+     package_dir={
+        "": ".",
+        "Linear": "./datastructures/Linear",
+        "nodes": "./datastructures/nodes",
+        "trees": "./datastructures/trees",
+    },
+)
+
+
