848a1388c7c5823dac46c72f1673e79dfea4e25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:15 +0000

    Add package MeUtils/2023.4.12.10.39.3/MeUtils-2023.4.12.10.39.3-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2023.4.12.10.39.3-py3-none-any.whl/meutils/ui/demo.py
@@ -0,0 +1,18 @@
+from gooey import Gooey, GooeyParser
+from colored import stylize, attr, fg
+
+
+@Gooey(richtext_controls=True, auto_start=True)
+def main():
+    parser = GooeyParser(description='Just display the console')
+
+    parser.parse_args()
+    print(stylize("This is bold.", attr("bold")))
+    print(stylize("This is underlined.", attr("underlined")))
+    print(stylize("This is green.", fg("green")))
+    print(stylize("This is red.", fg("red")))
+    print(stylize("This is blue and bold.", fg("blue") + attr("bold")))
+
+
+if __name__ == '__main__':
+    main()
