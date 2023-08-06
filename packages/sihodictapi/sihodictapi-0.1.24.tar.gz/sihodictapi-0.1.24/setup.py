fb2390dcb00bbb81a5fa8893baf673850caf9304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:06 +0000

    Add package sihodictapi/0.1.24/sihodictapi-0.1.24.tar.gz

---
+++ packages/sihodictapi/sihodictapi-0.1.24.tar.gz/sihodictapi-0.1.24/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '0.1.24'
+with open('README.md', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='sihodictapi',  # package name
+    version=VERSION,  # package version
+    description='一些在线词典/翻译API',  # package description
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    install_requires=[
+        'requestspr>=0.0.3', 'beautifulsoup4', 'pycryptodome'
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+    ],
+)
