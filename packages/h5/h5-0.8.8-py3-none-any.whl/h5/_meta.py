a41b42477812679dc3a6d0950cfe6fa8eef8ab95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:24 +0000

    Add package h5/0.8.8/h5-0.8.8-py3-none-any.whl

---
+++ packages/h5/h5-0.8.8-py3-none-any.whl/h5/_meta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+
+__title__ = "h5"
+__description__ = "H5py utils"
+__pkgroot__ = __file__.replace("_meta.py", "").rstrip("/\\")
+__version__ = "0.8.8"
