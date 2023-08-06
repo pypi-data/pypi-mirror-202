51fed5a88ee1089238b90eb95a10f12db4770cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:03 +0000

    Add package doro0001/0.0.1/doro0001-0.0.1-py3-none-any.whl

---
+++ packages/doro0001/doro0001-0.0.1-py3-none-any.whl/mycalc/setup.py
@@ -0,0 +1,15 @@
+
+import setuptools
+
+setuptools.setup(
+    name='doro0001',
+    version='0.0.1',
+    description='nice calculartor',
+    author='doro',
+    url="https://github.com/dongkyu20/doro0001",
+    download_url="https://github.com/dongkyu20/doro0001/blob/main/mycalc.py",
+    packages=['mycalc'],
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      ],
+)
