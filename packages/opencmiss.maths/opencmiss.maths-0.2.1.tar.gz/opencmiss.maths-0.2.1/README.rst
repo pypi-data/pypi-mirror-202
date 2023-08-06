c6d5a5847f623b741774652c552bbe7c9b711a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:00 +0000

    Add package opencmiss.maths/0.2.1/opencmiss.maths-0.2.1.tar.gz

---
+++ packages/opencmiss.maths/opencmiss.maths-0.2.1.tar.gz/opencmiss.maths-0.2.1/README.rst
@@ -0,0 +1,8 @@
+
+===============
+OpenCMISS Maths
+===============
+
+This package has been renamed. Use :code:`pip install cmlibs.maths` instead.
+New package: https://pypi.org/project/cmlibs.maths/
+
---
+++ packages/opencmiss.maths/opencmiss.maths-0.2.1.tar.gz/opencmiss.maths-0.2.1/setup.py
@@ -0,0 +1,42 @@
+import io
+import os
+import re
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def readfile(filename, split=False):
+    with io.open(filename, encoding="utf-8") as stream:
+        if split:
+            return stream.read().split("\n")
+        return stream.read()
+
+
+readme = readfile("README.rst", split=True)
+readme.append('License')
+readme.append('=======')
+readme.append('')
+readme.append('::')
+readme.append('')
+readme.append('')
+
+software_licence = readfile("LICENSE")
+
+requires = ["cmlibs.maths"]
+
+setup(
+    name='opencmiss.maths',
+    version="0.2.1",
+    description='OpenCMISS Math functions.',
+    long_description='\n'.join(readme) + software_licence,
+    long_description_content_type='text/x-rst',
+    classifiers=["Development Status :: 7 - Inactive"],
+    author='Hugh Sorby',
+    author_email='h.sorby@auckland.ac.nz',
+    url='https://github.com/OpenCMISS-Bindings/opencmiss.maths',
+    license='Apache Software License',
+    license_files=("LICENSE",),
+    install_requires=requires,
+)
