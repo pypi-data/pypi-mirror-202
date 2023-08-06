97e70eef7d557907a0518fe57839490e14f93d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:49 +0000

    Add package story-analyzer/0.1.1/story_analyzer-0.1.1-py3-none-any.whl

---
+++ packages/story-analyzer/story_analyzer-0.1.1-py3-none-any.whl/story_analyzer/archiver.py
@@ -0,0 +1,23 @@
+import shelve
+from shelve import Shelf
+import os
+import copy
+class Archiver:
+    def __init__(self):
+        self.path: str = os.path.dirname(__file__) + "/db/archive"
+
+    def addStory(self, title: str, bookObj : dict):
+        db: Shelf = shelve.open(self.path)
+        if title in db.keys():
+            copyDict : dict = copy.deepcopy(db[title])
+            copyDict.update(bookObj)
+            db[title] = copyDict
+        else:
+            db[title] = bookObj
+        db.close()
+
+    def getStory(self, title: str) -> dict:
+        db: Shelf = shelve.open(self.path)
+        bookObj = copy.deepcopy(db[title])
+        db.close()
+        return bookObj
