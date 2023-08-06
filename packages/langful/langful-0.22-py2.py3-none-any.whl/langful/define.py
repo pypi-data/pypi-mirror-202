a7e9e4d245d2d2febd558903425de62b9782cd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:01 +0000

    Add package langful/0.22/langful-0.22-py2.py3-none-any.whl

---
+++ packages/langful/langful-0.22-py2.py3-none-any.whl/langful/define.py
@@ -0,0 +1,14 @@
+FILE = "file"
+DICT = "dict"
+
+UTF8 = "utf-8"
+
+def TheTypeError( obj , message : str = "" ) -> None : raise TypeError(f"{ message }can't use type { type( obj ) }")
+
+def get_type( obj ) -> str :
+    if isinstance( obj , str ) :
+        return FILE
+    elif isinstance( obj , dict ) :
+        return DICT
+    else :
+        TheTypeError( obj )
