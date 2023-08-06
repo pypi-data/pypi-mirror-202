1562d1c7d6b5707e4dc6d4ffb0f253acb1c0dd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:53 +0000

    Add package trcli/1.5.0/trcli-1.5.0.tar.gz

---
+++ packages/trcli/trcli-1.5.0.tar.gz/trcli-1.5.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from trcli import __version__
+
+setup(
+    name="trcli",
+    long_description="The TR CLI (trcli) is a command line tool for interacting with TestRail and uploading test automation results.",
+    version=__version__,
+    packages=[
+        "trcli",
+        "trcli.commands",
+        "trcli.readers",
+        "trcli.data_providers",
+        "trcli.data_classes",
+        "trcli.api",
+    ],
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "pyyaml",
+        "junitparser",
+        "pyserde",
+        "requests",
+        "tqdm",
+        "humanfriendly",
+        "prance",
+        "openapi-spec-validator"
+    ],
+    entry_points="""
+        [console_scripts]
+        trcli=trcli.cli:cli
+    """,
+)
