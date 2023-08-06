0b2426acb172fc8d741ad5c5a0b6dc8240a3e79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:48 +0000

    Add package msglink/1.5.7/msglink-1.5.7.tar.gz

---
+++ packages/msglink/msglink-1.5.7.tar.gz/msglink-1.5.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='msglink',
+    version='1.5.7',
+    packages=['msglink'],
+    url='',
+    license='LGPL v3',
+    author='Robert Brünig',
+    author_email='',
+    description='msglink client library',
+    package_data={"msglink": ["py.typed"]},
+)
