7a96b8cbe9c52fe40b6bb6493f2b5abf9d8b7501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:22 +0000

    Add package dagster-aws/0.18.7/dagster_aws-0.18.7-py3-none-any.whl

---
+++ packages/dagster-aws/dagster_aws-0.18.7-py3-none-any.whl/dagster_aws/athena/__init__.py
@@ -0,0 +1,8 @@
+from .resources import (
+    AthenaError as AthenaError,
+    AthenaResource as AthenaResource,
+    AthenaTimeout as AthenaTimeout,
+    FakeAthenaResource as FakeAthenaResource,
+    athena_resource as athena_resource,
+    fake_athena_resource as fake_athena_resource,
+)
