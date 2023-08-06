c175a4596e00fa6ebb3ab66874fe20ca2547f9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:09 +0000

    Add package espaco-exclusivo-package/0.1.27/espaco_exclusivo_package-0.1.27-py3-none-any.whl

---
+++ packages/espaco-exclusivo-package/espaco_exclusivo_package-0.1.27-py3-none-any.whl/ee_ampere_consultoria/produtos/flux/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    --------------------------------------------------------------------------------------------------------------------
+
+    Description: 
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Obs.: 
+
+    Author:           @diego.yosiura
+    Last Update:      23/07/2021 12:14
+    Created:          23/07/2021 12:14
+    Copyright:        (c) Ampere Consultoria Ltda
+    Original Project: espaco-exclusivo-package
+    IDE:              PyCharm
+"""
+from .body_flux_personalizado_cenario_bloco import BodyFluxPersonalizadoCenarioBloco
+from .body_flux_personalizado_cenario import BodyFluxPersonalizadoCenario
+from .body_flux_personalizado import BodyFluxPersonalizado
+from .flux_automatrico import FluxAutomatico
+from .flux_historico import FluxHistorico
+from .flux_gt import FluxGT
+from .flux_ec_46 import FluxEC46
+from .flux_upload import FluxUpload
+from .flux_personalizado import FluxPersonalizado
+from .ena_diaria import FluxENADiaria
