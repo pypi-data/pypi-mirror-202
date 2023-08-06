cb702ce86b3c97bb283654a7b0589d61f0f8ce07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:58 +0000

    Add package comwares/0.1.2/comwares-0.1.2-py3-none-any.whl

---
+++ packages/comwares/comwares-0.1.2-py3-none-any.whl/middlewares/__init__.py
@@ -0,0 +1,6 @@
+from middlewares.email.xmail import *
+from middlewares.commons.logger import *
+from middlewares.crawler.user_agents import *
+from middlewares.utils import *
+from middlewares.data.json_loader import *
+from middlewares.tencent.cos import *
