75397ea8335cab2264f356ef2a4a289234704486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:43 +0000

    Add package kfsdutils/0.0.85/kfsdutils-0.0.85.tar.gz

---
+++ packages/kfsdutils/kfsdutils-0.0.85.tar.gz/kfsdutils-0.0.85/kfsdutils/apps/endpoints/renderers/kubefacetstext.py
@@ -0,0 +1,21 @@
+"""
+Provides TEXT rendering support.
+"""
+from __future__ import unicode_literals
+import json
+
+from rest_framework.renderers import BaseRenderer
+
+
+class KubefacetsTEXTRenderer(BaseRenderer):
+    """
+    Renderer which serializes to TEXT.
+    """
+
+    media_type = "plain/text"
+    format = "text"
+    charset = "utf-8"
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        
+        return data
