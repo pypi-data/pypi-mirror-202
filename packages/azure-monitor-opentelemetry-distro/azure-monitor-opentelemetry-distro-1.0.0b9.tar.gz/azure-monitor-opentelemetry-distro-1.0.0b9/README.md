ec9e7c750e89f2cfb5556ea8221bbab7fdeba6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:45 +0000

    Add package azure-monitor-opentelemetry-distro/1.0.0b9/azure-monitor-opentelemetry-distro-1.0.0b9.tar.gz

---
+++ packages/azure-monitor-opentelemetry-distro/azure-monitor-opentelemetry-distro-1.0.0b9.tar.gz/azure-monitor-opentelemetry-distro-1.0.0b9/README.md
@@ -0,0 +1,5 @@
+# Azure Monitor Opentelemetry Distro
+
+This package name has been deprecated. Use [azure-monitor-opentelemetry][azure-monitor-opentelemetry] instead.
+
+[azure-monitor-opentelemetry]: https://pypi.org/project/azure-monitor-opentelemetry/
---
+++ packages/azure-monitor-opentelemetry-distro/azure-monitor-opentelemetry-distro-1.0.0b9.tar.gz/azure-monitor-opentelemetry-distro-1.0.0b9/setup.py
@@ -0,0 +1,21 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import os
+
+import setuptools
+
+BASE_DIR = os.path.dirname(__file__)
+VERSION_FILENAME = os.path.join(
+    BASE_DIR, "src", "azure", "monitor", "opentelemetry", "distro", "version.py"
+)
+PACKAGE_INFO = {}
+with open(VERSION_FILENAME, encoding="utf-8") as f:
+    exec(f.read(), PACKAGE_INFO)
+
+setuptools.setup(
+    version=PACKAGE_INFO["VERSION"],
+)
