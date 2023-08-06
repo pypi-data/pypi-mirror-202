3bba776c1800f20d397a1c21c9587df370a159bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:32 +0000

    Add package torchkeras/3.8.2/torchkeras-3.8.2.tar.gz

---
+++ packages/torchkeras/torchkeras-3.8.2.tar.gz/torchkeras-3.8.2/torchkeras/__init__.py
@@ -0,0 +1,12 @@
+__version__="3.8.2"
+
+import sys
+from .kerasmodel import KerasModel
+from .summary import summary
+from .kerasmodel import colorful
+from .utils import seed_everything
+try:
+    from .lightmodel import LightModel 
+except Exception:
+    pass
+
