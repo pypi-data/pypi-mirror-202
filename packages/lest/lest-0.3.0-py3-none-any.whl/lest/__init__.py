108d1f301a323cd3f8b3400d3fbc02ffbc0018e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:52 +0000

    Add package lest/0.3.0/lest-0.3.0-py3-none-any.whl

---
+++ packages/lest/lest-0.3.0-py3-none-any.whl/lest/__init__.py
@@ -0,0 +1,14 @@
+from . import assertions
+from .registerer import Registerer
+from .runner import Runner
+from .setup import Setup
+
+_registerer = Registerer()
+_runner = Runner()
+_setuper = Setup()
+
+register = _registerer.__call__
+run = lambda: _runner.run(_registerer.funcs, setup=_setuper.func)
+setup = _setuper.__call__
+
+__all__ = ['register', 'run', 'setup', 'assertions']
