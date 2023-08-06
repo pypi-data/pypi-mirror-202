8a17c1d0b1a7789d820b50d2d1da500862b39f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:38 +0000

    Add package Simba-UW-tf-dev/1.55.9/Simba_UW_tf_dev-1.55.9-py3-none-any.whl

---
+++ packages/Simba-UW-tf-dev/Simba_UW_tf_dev-1.55.9-py3-none-any.whl/simba/utils/printing.py
@@ -0,0 +1,13 @@
+from simba.enums import TagNames, Defaults
+
+def stdout_success(msg: str, elapsed_time: str or None=None) -> None:
+    if elapsed_time:
+        print(f'SIMBA COMPLETE: {msg} (elapsed time: {elapsed_time}) {Defaults.STR_SPLIT_DELIMITER.value}{TagNames.COMPLETE.value}')
+    else:
+        print(f'SIMBA COMPLETE: {msg} {Defaults.STR_SPLIT_DELIMITER.value}{TagNames.COMPLETE.value}')
+
+def stdout_warning(msg: str, elapsed_time: str or None=None) -> None:
+    if elapsed_time:
+        print(f'SIMBA WARNING: {msg} (elapsed time: {elapsed_time}) {Defaults.STR_SPLIT_DELIMITER.value}{TagNames.WARNING.value}')
+    else:
+        print(f'SIMBA WARNING: {msg} {Defaults.STR_SPLIT_DELIMITER.value}{TagNames.WARNING.value}')
