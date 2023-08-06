20448d2aa82f4e124ec7c8fc0918c117c4e95a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:20 +0000

    Add package web3-py-checksum/1.1/web3_py_checksum-1.1.tar.gz

---
+++ packages/web3-py-checksum/web3_py_checksum-1.1.tar.gz/web3_py_checksum-1.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='web3_py_checksum',
+	version='1.1',
+	author='andrewgrigor',
+	author_email='andrewgrigoryanb@gmail.com',
+	description='web3 python checksum',
+	packages=['web3_py_checksum'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
