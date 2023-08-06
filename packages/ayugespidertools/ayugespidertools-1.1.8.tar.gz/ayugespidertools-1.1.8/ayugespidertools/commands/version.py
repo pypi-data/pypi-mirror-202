eb57b7d26da34f6fae1dcd8c6cd883fbe85c3450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:07 +0000

    Add package ayugespidertools/1.1.8/ayugespidertools-1.1.8.tar.gz

---
+++ packages/ayugespidertools/ayugespidertools-1.1.8.tar.gz/ayugespidertools-1.1.8/ayugespidertools/commands/version.py
@@ -0,0 +1,22 @@
+import toml
+from scrapy.commands.version import Command
+
+from ayugespidertools.config import NormalConfig
+
+
+class AyuCommand(Command):
+    default_settings = {"LOG_ENABLED": False, "SPIDER_LOADER_WARN_ONLY": True}
+
+    def short_desc(self):
+        return "Print AyugeSpiderTools version"
+
+    def _version(self) -> str:
+        with open(
+            f"{NormalConfig.ROOT_DIR}/pyproject.toml", "r", encoding="utf-8"
+        ) as f:
+            config = toml.load(f)
+
+        return config["tool"]["poetry"]["version"]
+
+    def run(self, args, opts):
+        print(f"AyugeSpiderTools {self._version()}")
