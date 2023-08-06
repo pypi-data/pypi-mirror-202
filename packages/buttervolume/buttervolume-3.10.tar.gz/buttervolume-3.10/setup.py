c43a7cd7893c019a575de8ecd6f6af1bc8e25725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:50 +0000

    Add package buttervolume/3.10/buttervolume-3.10.tar.gz

---
+++ packages/buttervolume/buttervolume-3.10.tar.gz/buttervolume-3.10/setup.py
@@ -0,0 +1,48 @@
+from os.path import dirname, join, realpath
+from setuptools import setup, find_packages
+import sys
+
+VERSION = (
+    open(join(dirname(realpath(__file__)), "buttervolume", "VERSION")).read().strip()
+)
+
+if sys.version_info < (3, 5):
+    sys.exit(
+        "Buttervolume currently works only with Python >= 3.5. "
+        "We will accept any contribution to support older versions"
+    )
+
+setup(
+    name="buttervolume",
+    version=VERSION,
+    author="Christophe Combelles",
+    author_email="ccomb@anybox.fr",
+    url="https://github.com/anybox/buttervolume",
+    license="Apache License, Version 2.0",
+    description="Docker plugin to manage Docker Volumes as BTRFS subvolumes",
+    long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
+    packages=find_packages(),
+    package_data={"": ["VERSION"]},
+    entry_points={
+        "console_scripts": [
+            "buttervolume = buttervolume.cli:main",
+        ],
+    },
+    install_requires=[
+        "bottle",
+        "urllib3<1.27,>=1.21.1",
+        "requests-unixsocket",
+        "waitress",
+        "webtest",
+    ],
+    tests_require=[],
+    test_suite="test",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: System :: Clustering",
+    ],
+)
