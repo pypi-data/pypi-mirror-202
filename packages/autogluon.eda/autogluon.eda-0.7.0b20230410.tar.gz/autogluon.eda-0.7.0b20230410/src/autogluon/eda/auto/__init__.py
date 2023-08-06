16c9d2ea4dbce702ecbcd8072e88ed9e2ee2d0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:53 +0000

    Add package autogluon.eda/0.7.0b20230410/autogluon.eda-0.7.0b20230410.tar.gz

---
+++ packages/autogluon.eda/autogluon.eda-0.7.0b20230410.tar.gz/autogluon.eda-0.7.0b20230410/src/autogluon/eda/auto/__init__.py
@@ -0,0 +1,11 @@
+from .simple import (
+    analyze,
+    analyze_interaction,
+    covariate_shift_detection,
+    dataset_overview,
+    explain_rows,
+    missing_values_analysis,
+    partial_dependence_plots,
+    quick_fit,
+    target_analysis,
+)
