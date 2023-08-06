aaeb11defc82adc184e8fed7418d56140c5ba106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:42 +0000

    Add package IOTCommon/0.1.1/IOTCommon-0.1.1.tar.gz

---
+++ packages/IOTCommon/IOTCommon-0.1.1.tar.gz/IOTCommon-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='IOTCommon',
+    version='0.1.1',
+    packages=find_packages(exclude=['MyPackage']),
+    install_requires=requirements,
+    author='Anton Persson',
+    author_email='Antonnilspersson@gmail.com',
+    description='Common operations for sweiot microservices',
+    url='https://github.com/AntonNPersson/SweIoTServices.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
