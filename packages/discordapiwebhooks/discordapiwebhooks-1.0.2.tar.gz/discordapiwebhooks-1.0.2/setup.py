feba2366500f49948cc60fffde47883e0833aae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:50 +0000

    Add package discordapiwebhooks/1.0.2/discordapiwebhooks-1.0.2.tar.gz

---
+++ packages/discordapiwebhooks/discordapiwebhooks-1.0.2.tar.gz/discordapiwebhooks-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='discordapiwebhooks',
+    version='1.0.2',
+    description='discordapiwebhooks is a Python module that allows you to easily send webhooks to Discord. It supports sending text, images, and files to a Discord server.',
+    author='NouName',
+    author_email='duaneskalanak@gmail.com',
+    packages=['discordapiwebhooks'],
+)
