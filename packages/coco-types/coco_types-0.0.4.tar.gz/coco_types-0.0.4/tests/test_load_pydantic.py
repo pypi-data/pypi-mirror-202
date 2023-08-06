3ab98d2d81a97319175da90fc280d20140532e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:38 +0000

    Add package coco-types/0.0.4/coco_types-0.0.4.tar.gz

---
+++ packages/coco-types/coco_types-0.0.4.tar.gz/coco_types-0.0.4/tests/test_load_pydantic.py
@@ -0,0 +1,10 @@
+import coco_types
+
+
+def test_can_parse_sample_file():
+    with open("data_samples/coco_25k/annotations.json", encoding="utf-8") as data_file:
+        dataset = coco_types.Dataset.parse_raw(data_file.read())
+
+    assert dataset.images[0].file_name == "000000174482.jpg"
+    assert dataset.annotations[0].bbox == [187.74, 5.84, 310.4, 380.49]
+    assert dataset.categories[0].name == "person"
