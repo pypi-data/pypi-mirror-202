ddb9fb555ace2dc1bc33a041929999b4e2e26803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:27 +0000

    Add package smt/2.0b2/smt-2.0b2.tar.gz

---
+++ packages/smt/smt-2.0b2.tar.gz/smt-2.0b2/smt/surrogate_models/__init__.py
@@ -0,0 +1,23 @@
+from .ls import LS
+from .qp import QP
+from .krg import KRG
+from .kpls import KPLS
+from .gekpls import GEKPLS
+from .kplsk import KPLSK
+from .genn import GENN
+from .mgp import MGP
+
+from .krg_based import MixIntKernelType
+from smt.utils.kriging import MixHrcKernelType
+from smt.utils.mixed_integer import XType
+from smt.utils.kriging import XRole
+
+from smt.utils.kriging import XSpecs
+
+try:
+    from .idw import IDW
+    from .rbf import RBF
+    from .rmtc import RMTC
+    from .rmtb import RMTB
+except:
+    pass
