b066cc4e65b5ac037235e60125c515978a0cf2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:39 +0000

    Add package python-mcs-sdk/0.3.1/python_mcs_sdk-0.3.1-py3-none-any.whl

---
+++ packages/python-mcs-sdk/python_mcs_sdk-0.3.1-py3-none-any.whl/swan_mcs/common/params.py
@@ -0,0 +1,25 @@
+from swan_mcs.common.constants import MCS_POLYGON_MUMBAI_API, MCS_BSC_API, MCS_POLYGON_MAIN_API
+import logging
+
+class Params:
+    def __init__(self, chain_name='polygon.mainnet'):
+        if chain_name == 'polygon.mainnet' or chain_name == 'main':
+            self.MCS_API = MCS_POLYGON_MAIN_API
+        elif chain_name == 'polygon.mumbai' or chain_name == 'mumbai':
+            self.MCS_API = MCS_POLYGON_MUMBAI_API
+        elif chain_name == 'bsc.testnet' or chain_name == 'bsc':
+            self.MCS_API = MCS_BSC_API
+        else:
+            logging.error("\033[31munknown chain name\033[0m")
+
+    def get_params(self):
+
+        param_vars = [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
+        param_dict = {}
+        for i in param_vars:
+            param_dict[i] = getattr(self, i)
+
+        return param_dict
+
+    def __str__(self):
+        return self.get_params
