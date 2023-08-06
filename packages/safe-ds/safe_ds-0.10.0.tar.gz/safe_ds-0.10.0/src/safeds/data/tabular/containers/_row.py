4e67fb4c432c54661476c0de3572dde8c3ec2125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:18 +0000

    Add package safe-ds/0.10.0/safe_ds-0.10.0.tar.gz

---
+++ packages/safe-ds/safe_ds-0.10.0.tar.gz/safe_ds-0.10.0/src/safeds/data/tabular/containers/_row.py
@@ -0,0 +1,184 @@
+from collections.abc import Iterable, Iterator
+from hashlib import md5
+from typing import Any
+
+import pandas as pd
+from IPython.core.display_functions import DisplayHandle, display
+from pandas.core.util.hashing import hash_pandas_object
+
+from safeds.data.tabular.exceptions import UnknownColumnNameError
+from safeds.data.tabular.typing import ColumnType, Schema
+
+
+class Row:
+    """
+    A row is a collection of values, where each value is associated with a column name.
+
+    Parameters
+    ----------
+    data : Iterable
+        The data.
+    schema : Schema
+        The schema of the row.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Dunder methods
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, data: Iterable, schema: Schema | None = None):
+        self._data: pd.Series = data if isinstance(data, pd.Series) else pd.Series(data)
+        self._data = self._data.reset_index(drop=True)
+
+        self.schema: Schema
+        if schema is not None:
+            self.schema = schema
+        else:
+            column_names = [f"column_{i}" for i in range(len(self._data))]
+            dataframe = self._data.to_frame().T
+            dataframe.columns = column_names
+            # noinspection PyProtectedMember
+            self.schema = Schema._from_dataframe(dataframe)
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Row):
+            return NotImplemented
+        if self is other:
+            return True
+        return self.schema == other.schema and self._data.equals(other._data)
+
+    def __getitem__(self, column_name: str) -> Any:
+        return self.get_value(column_name)
+
+    def __hash__(self) -> int:
+        data_hash_string = md5(hash_pandas_object(self._data, index=True).values).hexdigest()
+        column_names_frozenset = frozenset(self.get_column_names())
+
+        return hash((data_hash_string, column_names_frozenset))
+
+    def __iter__(self) -> Iterator[Any]:
+        return iter(self.get_column_names())
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __repr__(self) -> str:
+        tmp = self._data.to_frame().T
+        tmp.columns = self.get_column_names()
+        return tmp.__repr__()
+
+    def __str__(self) -> str:
+        tmp = self._data.to_frame().T
+        tmp.columns = self.get_column_names()
+        return tmp.__str__()
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Getters
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def get_value(self, column_name: str) -> Any:
+        """
+        Return the value of a specified column.
+
+        Parameters
+        ----------
+        column_name : str
+            The column name.
+
+        Returns
+        -------
+        value :
+            The value of the column.
+        """
+        if not self.schema.has_column(column_name):
+            raise UnknownColumnNameError([column_name])
+        # noinspection PyProtectedMember
+        return self._data[self.schema._get_column_index_by_name(column_name)]
+
+    def has_column(self, column_name: str) -> bool:
+        """
+        Return whether the row contains a given column.
+
+        Alias for self.schema.hasColumn(column_name: str) -> bool.
+
+        Parameters
+        ----------
+        column_name : str
+            The name of the column.
+
+        Returns
+        -------
+        contains : bool
+            True, if row contains the column.
+        """
+        return self.schema.has_column(column_name)
+
+    def get_column_names(self) -> list[str]:
+        """
+        Return a list of all column names saved in this schema.
+
+        Alias for self.schema.get_column_names() -> list[str].
+
+        Returns
+        -------
+        column_names : list[str]
+            The column names.
+        """
+        return self.schema.get_column_names()
+
+    def get_type_of_column(self, column_name: str) -> ColumnType:
+        """
+        Return the type of a specified column.
+
+        Alias for self.schema.get_type_of_column(column_name: str) -> ColumnType.
+
+        Parameters
+        ----------
+        column_name : str
+            The name of the column.
+
+        Returns
+        -------
+        type : ColumnType
+            The type of the column.
+
+        Raises
+        ------
+        ColumnNameError
+            If the specified target column name does not exist.
+        """
+        return self.schema.get_type_of_column(column_name)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Information
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def count(self) -> int:
+        """
+        Return the number of columns in this row.
+
+        Returns
+        -------
+        count : int
+            The number of columns.
+        """
+        return len(self._data)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # IPython integration
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def _ipython_display_(self) -> DisplayHandle:
+        """
+        Return a display object for the column to be used in Jupyter Notebooks.
+
+        Returns
+        -------
+        output : DisplayHandle
+            Output object.
+        """
+        tmp = self._data.to_frame().T
+        tmp.columns = self.get_column_names()
+
+        with pd.option_context("display.max_rows", tmp.shape[0], "display.max_columns", tmp.shape[1]):
+            return display(tmp)
---
+++ packages/safe-ds/safe_ds-0.10.0.tar.gz/safe_ds-0.10.0/src/safeds/data/tabular/typing/_column_type.py
@@ -0,0 +1,289 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import numpy as np
+
+
+class ColumnType(ABC):
+    """Abstract base class for column types."""
+
+    @abstractmethod
+    def is_nullable(self) -> bool:
+        """
+        Return whether the given column type is nullable.
+
+        Returns
+        -------
+        is_nullable : bool
+            True if the column is nullable.
+        """
+
+    @abstractmethod
+    def is_numeric(self) -> bool:
+        """
+        Return whether the given column type is numeric.
+
+        Returns
+        -------
+        is_numeric : bool
+            True if the column is numeric.
+        """
+
+    @staticmethod
+    def _from_numpy_dtype(dtype: np.dtype) -> ColumnType:
+        """
+        Return the column type for a given numpy dtype.
+
+        Parameters
+        ----------
+        dtype : numpy.dtype
+            The numpy dtype.
+
+        Returns
+        -------
+        column_type : ColumnType
+            The ColumnType.
+
+        Raises
+        ------
+        TypeError
+            If the given dtype is not supported.
+        """
+        if dtype.kind in ("u", "i"):
+            return Integer()
+        if dtype.kind == "b":
+            return Boolean()
+        if dtype.kind == "f":
+            return RealNumber()
+        if dtype.kind in ("S", "U", "O", "M", "m"):
+            return String()
+        raise TypeError("Unexpected column type")
+
+
+@dataclass
+class Anything(ColumnType):
+    """
+    Type for a column that contains anything.
+
+    Parameters
+    ----------
+    is_nullable : bool
+        Whether the type also allows null values.
+    """
+
+    _is_nullable: bool
+
+    def __init__(self, is_nullable: bool = False):
+        self._is_nullable = is_nullable
+
+    def __repr__(self) -> str:
+        result = "Anything"
+        if self._is_nullable:
+            result += "?"
+        return result
+
+    def is_nullable(self) -> bool:
+        """
+        Return whether the given column type is nullable.
+
+        Returns
+        -------
+        is_nullable : bool
+            True if the column is nullable.
+        """
+        return self._is_nullable
+
+    def is_numeric(self) -> bool:
+        """
+        Return whether the given column type is numeric.
+
+        Returns
+        -------
+        is_numeric : bool
+            True if the column is numeric.
+        """
+        return False
+
+
+@dataclass
+class Boolean(ColumnType):
+    """
+    Type for a column that only contains booleans.
+
+    Parameters
+    ----------
+    is_nullable : bool
+        Whether the type also allows null values.
+    """
+
+    _is_nullable: bool
+
+    def __init__(self, is_nullable: bool = False):
+        self._is_nullable = is_nullable
+
+    def __repr__(self) -> str:
+        result = "Boolean"
+        if self._is_nullable:
+            result += "?"
+        return result
+
+    def is_nullable(self) -> bool:
+        """
+        Return whether the given column type is nullable.
+
+        Returns
+        -------
+        is_nullable : bool
+            True if the column is nullable.
+        """
+        return self._is_nullable
+
+    def is_numeric(self) -> bool:
+        """
+        Return whether the given column type is numeric.
+
+        Returns
+        -------
+        is_numeric : bool
+            True if the column is numeric.
+        """
+        return False
+
+
+@dataclass
+class RealNumber(ColumnType):
+    """
+    Type for a column that only contains real numbers.
+
+    Parameters
+    ----------
+    is_nullable : bool
+        Whether the type also allows null values.
+    """
+
+    _is_nullable: bool
+
+    def __init__(self, is_nullable: bool = False):
+        self._is_nullable = is_nullable
+
+    def __repr__(self) -> str:
+        result = "RealNumber"
+        if self._is_nullable:
+            result += "?"
+        return result
+
+    def is_nullable(self) -> bool:
+        """
+        Return whether the given column type is nullable.
+
+        Returns
+        -------
+        is_nullable : bool
+            True if the column is nullable.
+        """
+        return self._is_nullable
+
+    def is_numeric(self) -> bool:
+        """
+        Return whether the given column type is numeric.
+
+        Returns
+        -------
+        is_numeric : bool
+            True if the column is numeric.
+        """
+        return True
+
+
+@dataclass
+class Integer(ColumnType):
+    """
+    Type for a column that only contains integers.
+
+    Parameters
+    ----------
+    is_nullable : bool
+        Whether the type also allows null values.
+    """
+
+    _is_nullable: bool
+
+    def __init__(self, is_nullable: bool = False):
+        self._is_nullable = is_nullable
+
+    def __repr__(self) -> str:
+        result = "Integer"
+        if self._is_nullable:
+            result += "?"
+        return result
+
+    def is_nullable(self) -> bool:
+        """
+        Return whether the given column type is nullable.
+
+        Returns
+        -------
+        is_nullable : bool
+            True if the column is nullable.
+        """
+        return self._is_nullable
+
+    def is_numeric(self) -> bool:
+        """
+        Return whether the given column type is numeric.
+
+        Returns
+        -------
+        is_numeric : bool
+            True if the column is numeric.
+        """
+        return True
+
+
+@dataclass
+class String(ColumnType):
+    """
+    Type for a column that only contains strings.
+
+    Parameters
+    ----------
+    is_nullable : bool
+        Whether the type also allows null values.
+    """
+
+    _is_nullable: bool
+
+    def __init__(self, is_nullable: bool = False):
+        self._is_nullable = is_nullable
+
+    def __repr__(self) -> str:
+        result = "String"
+        if self._is_nullable:
+            result += "?"
+        return result
+
+    def is_nullable(self) -> bool:
+        """
+        Return whether the given column type is nullable.
+
+        Returns
+        -------
+        is_nullable : bool
+            True if the column is nullable.
+        """
+        return self._is_nullable
+
+    def is_numeric(self) -> bool:
+        """
+        Return whether the given column type is numeric.
+
+        Returns
+        -------
+        is_numeric : bool
+            True if the column is numeric.
+        """
+        return False
