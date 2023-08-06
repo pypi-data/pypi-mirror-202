d97515a47c1fd65bd5d5ad5696c5015c1b5e3696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:29 +0000

    Add package scart/1.0.0/scart-1.0.0-py3.8.egg

---
+++ packages/scart/scart-1.0.0-py3.8.egg/scart/AnomalyObjects/Action/DummyAction.py
@@ -0,0 +1,11 @@
+from .AbstractActionFile import AbstractAction
+
+class Dummy(object):
+    Name = "DummyDuplicateSensor"
+    
+    def __init__(self, scenarioo):
+        pass
+
+        
+    def do(self):
+        pass
