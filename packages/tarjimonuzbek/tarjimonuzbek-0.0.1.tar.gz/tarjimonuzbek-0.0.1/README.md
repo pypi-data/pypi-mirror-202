a033b61d1dd7c2ccb1a8f11481909066cbc88456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:02 +0000

    Add package tarjimonuzbek/0.0.1/tarjimonuzbek-0.0.1.tar.gz

---
+++ packages/tarjimonuzbek/tarjimonuzbek-0.0.1.tar.gz/tarjimonuzbek-0.0.1/README.md
@@ -0,0 +1,7 @@
+```python
+from tarjimon import to_uzbek, to_russian, to_english
+
+print(to_uzbek('Hello')) # Salom
+print(to_russian('Salom')) # Привет
+print(to_english('Salom')) # Hello
+```
---
+++ packages/tarjimonuzbek/tarjimonuzbek-0.0.1.tar.gz/tarjimonuzbek-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="tarjimonuzbek",
+    version="0.0.1",
+    author="Hikmatullayev Islombek",
+    author_email="gimlerpro8899@gmail.com",
+    description="Ushbu kutubxona tarjima qilish uchun ishlatiladi",
+    long_description_content_type="text/markdown",
+)
+
+
+
+
+    
