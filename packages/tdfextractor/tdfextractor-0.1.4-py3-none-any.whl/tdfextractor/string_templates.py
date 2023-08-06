48844ac0468ba6849c5a2569414cfff4b265640f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:47 +0000

    Add package tdfextractor/0.1.4/tdfextractor-0.1.4-py3-none-any.whl

---
+++ packages/tdfextractor/tdfextractor-0.1.4-py3-none-any.whl/tdfextractor/string_templates.py
@@ -0,0 +1,27 @@
+header_ms2_template = 'H\tExtractor\tTimsTOF_extractor\n' \
+                 'H\tExtractorVersion\t{version}\n' \
+                 'H\tPublicationDate\t20-02-2020\n' \
+                 'H\tDate Converted\t{date_of_creation}\n' \
+                 'H\tComments\tTimsTOF_extractor written by Yu Gao, 2018\n' \
+                 'H\tComments\tTimsTOF_extractor modified by Titus Jung, 2019\n' \
+                 'H\tComments\tTimsTOF_extractor modified by Patrick Garrett, 2023\n' \
+                 'H\tExtractorOptions\tMSn\n' \
+                 'H\tAcquisitionMethod\tData-Dependent\n' \
+                 'H\tInstrumentType\tTIMSTOF\n' \
+                 'H\tDataType\tCentroid\n' \
+                 'H\tScanType\tMS2\n' \
+                 'H\tResolution\n' \
+                 'H\tIsolationWindow\n' \
+                 'H\tFirstScan\t{first_scan:d}\n' \
+                 'H\tLastScan\t{last_scan:d}\n' \
+                 'H\tMonoIsotopic PrecMz\tTrue\n'
+
+header_ms1_template = 'H\tExtractor\tTimsTOF_extractor\n' \
+                 'H\tExtractorVersion\t{}\n' \
+                 'H\tPublicationDate\t20-02-2020\n' \
+                 'H\tComments\tTimsTOF_extractor written by Yu Gao, 2018\n' \
+                 'H\tComments\tTimsTOF_extractor modified by Titus Jung, 2019\n' \
+                 'H\tExtractorOptions\tMSn\n' \
+                 'H\tAcquisitionMethod\tData-Dependent\n' \
+                 'H\tInstrumentType\tTIMSTOF\n' \
+                 'H\tScanType\tMS1\n'
