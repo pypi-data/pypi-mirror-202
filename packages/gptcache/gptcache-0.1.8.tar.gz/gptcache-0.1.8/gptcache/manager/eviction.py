3fd3d9b4b5178e6012984fc1714be8f9541f3e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:53 +0000

    Add package gptcache/0.1.8/gptcache-0.1.8.tar.gz

---
+++ packages/gptcache/gptcache-0.1.8.tar.gz/gptcache-0.1.8/gptcache/manager/eviction.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+class EvictionManager:
+    """
+    EvictionManager to manager the eviction policy.
+    """
+
+    MAX_MARK_COUNT = 5000
+    MAX_MARK_RATE = 0.1
+    BATCH_SIZE = 100000
+
+    def __init__(self, scalar_storage, vector_base, policy="LRU"):
+        self._scalar_storage = scalar_storage
+        self._vector_base = vector_base
+        self._policy = policy
+
+    def check_evict(self):
+        mark_count = self._scalar_storage.count(state=-1)
+        all_count = self._scalar_storage.count(is_all=True)
+        if (
+            mark_count > self.MAX_MARK_COUNT
+            or mark_count / all_count > self.MAX_MARK_RATE
+        ):
+            return True
+        return False
+
+    def delete(self):
+        mark_ids = self._scalar_storage.get_ids_by_state(state=-1)
+        mark_ids = [i[0] for i in mark_ids]
+        self._scalar_storage.remove_by_state()
+        self._vector_base.delete(mark_ids)
+
+    def rebuild(self):
+        self._scalar_storage.remove_by_state()
+        count = self._scalar_storage.count()
+        offset = 0
+        while offset < count:
+            res = self._scalar_storage.get_embedding_data(offset, self.BATCH_SIZE)
+            ids = [d[0] for d in res]
+            np_data = [np.frombuffer(d[1], np.float32) for d in res]
+            self._vector_base.rebuild(np_data, ids)
+            offset += self.BATCH_SIZE
+
+    def soft_evict(self, count):
+        if self._policy == "FIFO":
+            marked_keys = self._scalar_storage.get_old_create(count)
+        else:
+            marked_keys = self._scalar_storage.get_old_access(count)
+        marked_keys = [i[0] for i in marked_keys]
+        self._scalar_storage.update_state(marked_keys)
---
+++ packages/gptcache/gptcache-0.1.8.tar.gz/gptcache-0.1.8/gptcache/manager/scalar_data/base.py
@@ -0,0 +1,53 @@
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
+
+
+class CacheStorage(metaclass=ABCMeta):
+    """
+    BaseStorage for scalar data.
+    """
+
+    @abstractmethod
+    def create(self):
+        pass
+
+    @abstractmethod
+    def insert(self, data, reply, embedding_data: np.ndarray = None):
+        pass
+
+    @abstractmethod
+    def get_data_by_id(self, key):
+        pass
+
+    @abstractmethod
+    def get_embedding_data(self, offset, size):
+        pass
+
+    @abstractmethod
+    def remove_by_state(self):
+        pass
+
+    @abstractmethod
+    def update_access_time(self, key):
+        pass
+
+    @abstractmethod
+    def update_state(self, keys):
+        pass
+
+    @abstractmethod
+    def count(self):
+        pass
+
+    @abstractmethod
+    def get_old_access(self, count):
+        pass
+
+    @abstractmethod
+    def get_old_create(self, count):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
