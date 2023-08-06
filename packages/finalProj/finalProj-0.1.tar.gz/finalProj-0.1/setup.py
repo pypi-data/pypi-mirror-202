19ada01a09fd0822ea28ebe57ee1d767dee357d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:07 +0000

    Add package finalProj/0.1/finalProj-0.1.tar.gz

---
+++ packages/finalProj/finalProj-0.1.tar.gz/finalProj-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='finalProj',
+      version='0.1',
+      description='Final Project',
+      author='Justin Pham, Angelo Troncone',
+      author_email='justin.pham@ucalgary.ca',
+      packages=['dataStructures'],
+      install_requires=[],
+      )
