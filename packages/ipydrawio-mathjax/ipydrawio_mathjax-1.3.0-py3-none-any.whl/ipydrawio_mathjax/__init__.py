2ddf823edf2b19b45477d4de9174adfd00028b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:30 +0000

    Add package ipydrawio-mathjax/1.3.0/ipydrawio_mathjax-1.3.0-py3-none-any.whl

---
+++ packages/ipydrawio-mathjax/ipydrawio_mathjax-1.3.0-py3-none-any.whl/ipydrawio_mathjax/__init__.py
@@ -0,0 +1,31 @@
+"""MathJax for ipydrawio."""
+
+# Copyright 2023 ipydrawio contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ipydrawio import _ipydrawio_labextension_paths
+
+from ._version import __ext__, __js__, __prefix__, __version__
+
+
+def _jupyter_labextension_paths():
+    """Static paths to link for interactive installation."""
+    return _ipydrawio_labextension_paths(prefix=__prefix__, extensions=__ext__)
+
+
+__all__ = [
+    "__js__",
+    "__version__",
+    "_jupyter_labextension_paths",
+]
