cb7c293312919bbdaca3083fa9c1293b5afe6cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:49 +0000

    Add package suger/0.0.9/suger-0.0.9.tar.gz

---
+++ packages/suger/suger-0.0.9.tar.gz/suger-0.0.9/setup.py
@@ -0,0 +1,9 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    install_requires=[
+        'paramiko',
+        'openpyxl'
+    ]
+)
---
+++ packages/suger/suger-0.0.9.tar.gz/suger-0.0.9/tests/test_SSH.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestSSH(TestCase):
+    def test_connect(self):
+        print('ok')
+
+
+        # ssh = SSH(host='localhost', password='root')
+        # ssh.connect()
+        # output, err = ssh.execute_command('ls .')
+        # print(output)
