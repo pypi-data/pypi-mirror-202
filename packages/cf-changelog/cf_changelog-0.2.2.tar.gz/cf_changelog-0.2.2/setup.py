7d0f2986ca10931903f895de6c5d9289f81996e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:43 +0000

    Add package cf-changelog/0.2.2/cf_changelog-0.2.2.tar.gz

---
+++ packages/cf-changelog/cf_changelog-0.2.2.tar.gz/cf_changelog-0.2.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cf_changelog',
+      version='0.2.2',
+      description='Conflict-Free Changelog manager',
+      author='Paweł Sałek',
+      author_email='salekpawel@gmail.com',
+      url='',
+      packages=['cf_changelog', ],
+      install_requires=[
+        'GitPython >= 3.1',
+        'pyyaml >= 6.0',
+        'schema >= 0.7',
+      ],
+      entry_points={
+        'console_scripts': [
+            'cf_changelog = cf_changelog:main',
+        ]
+    }
+     )
