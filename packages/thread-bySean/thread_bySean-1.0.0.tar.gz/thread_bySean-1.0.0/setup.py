3fac6d2428355c654105d803e9a5f80759468085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:07 +0000

    Add package thread-bySean/1.0.0/thread_bySean-1.0.0.tar.gz

---
+++ packages/thread-bySean/thread_bySean-1.0.0.tar.gz/thread_bySean-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='thread_bySean',
+    version='1.0.0',
+    description='A sample package for multi-thread programming',
+    packages=find_packages(),
+)
