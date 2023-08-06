511c865d25c888f9d4d2a96f55510522dbcd756b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:36 +0000

    Add package TerraSegmentation/0.0.3/TerraSegmentation-0.0.3.tar.gz

---
+++ packages/TerraSegmentation/TerraSegmentation-0.0.3.tar.gz/TerraSegmentation-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='TerraSegmentation',
+    version='0.0.3',
+    packages=['TerraSegmentation'],
+    url='',
+    license='',
+    author='AI University',
+    author_email='nejronki@gmail.com'    
+)
