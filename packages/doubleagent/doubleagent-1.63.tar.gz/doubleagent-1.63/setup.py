bf8ba72ad768ec9c3cee093f026653a926a38fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:06 +0000

    Add package doubleagent/1.63/doubleagent-1.63.tar.gz

---
+++ packages/doubleagent/doubleagent-1.63.tar.gz/doubleagent-1.63/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+import setuptools
+setuptools.setup(
+    name="doubleagent",
+    version="1.63",
+    license='MIT',
+    author="complexplayer",
+    author_email="didutc@gmail.com",
+    description="for me",
+
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
+
