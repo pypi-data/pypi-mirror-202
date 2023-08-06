c80593d7d7c69e76ede0dc6ad2cc4e618cbcd61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:54 +0000

    Add package grupa48/0.0.1/grupa48-0.0.1.tar.gz

---
+++ packages/grupa48/grupa48-0.0.1.tar.gz/grupa48-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='grupa48',
+    version='0.0.1',
+    author='Cristian I',
+    author_email='iordachecristian000@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Happy happy, joy joy'
+)
