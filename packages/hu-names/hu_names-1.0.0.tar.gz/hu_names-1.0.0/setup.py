8af1c36383e8e4be61fd8c469aeffe5d38cf68ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:51 +0000

    Add package hu-names/1.0.0/hu_names-1.0.0.tar.gz

---
+++ packages/hu-names/hu_names-1.0.0.tar.gz/hu_names-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='hu_names',
+    version='1.0.0',
+    description='Hungarian first and second names',
+    py_modules=['hu_names'],
+    author='Nagy Béla',
+    author_email='nagy.belabudapest@gmail.com',
+    url='https://github.com/kobanya',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
