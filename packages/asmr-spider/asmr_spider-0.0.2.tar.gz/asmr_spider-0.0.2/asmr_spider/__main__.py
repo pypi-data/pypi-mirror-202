dfca56e7d21afb3b0a39563ed2f1ce0fe3ffb82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:45 +0000

    Add package asmr-spider/0.0.2/asmr_spider-0.0.2.tar.gz

---
+++ packages/asmr-spider/asmr_spider-0.0.2.tar.gz/asmr_spider-0.0.2/asmr_spider/__main__.py
@@ -0,0 +1,27 @@
+import sys, asyncio
+from sys import argv
+from .spider import ASMRSpider
+from .config import logger
+
+
+def main():
+    try:
+        args = argv[1:] or input("\033[33;1m请输入RJ号\033[0m: ").split()
+        asyncio.run(dload(args))
+    except KeyboardInterrupt:
+        logger.error("进程被手动终止.")
+        sys.exit(1)
+
+
+async def dload(args: list[str]):
+    try:
+        async with ASMRSpider() as spider:
+            for arg in args:
+                await spider.download(arg)
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+if __name__ == "__main__":
+    main()
