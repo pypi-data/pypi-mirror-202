45413c668b0966b42cb4c775f672fa8f15796f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:26 +0000

    Add package blinker/1.6.2/blinker-1.6.2.tar.gz

---
+++ packages/blinker/blinker-1.6.2.tar.gz/blinker-1.6.2/tests/test_context.py
@@ -0,0 +1,60 @@
+from blinker import Signal
+
+
+def test_temp_connection():
+    sig = Signal()
+
+    canary = []
+
+    def receiver(sender):
+        canary.append(sender)
+
+    sig.send(1)
+    with sig.connected_to(receiver):
+        sig.send(2)
+    sig.send(3)
+
+    assert canary == [2]
+    assert not sig.receivers
+
+
+def test_temp_connection_for_sender():
+    sig = Signal()
+
+    canary = []
+
+    def receiver(sender):
+        canary.append(sender)
+
+    with sig.connected_to(receiver, sender=2):
+        sig.send(1)
+        sig.send(2)
+
+    assert canary == [2]
+    assert not sig.receivers
+
+
+def test_temp_connection_failure():
+    sig = Signal()
+
+    canary = []
+
+    def receiver(sender):
+        canary.append(sender)
+
+    class Failure(Exception):
+        pass
+
+    try:
+        sig.send(1)
+        with sig.connected_to(receiver):
+            sig.send(2)
+            raise Failure
+        sig.send(3)
+    except Failure:
+        pass
+    else:
+        raise AssertionError("Context manager did not propagate.")
+
+    assert canary == [2]
+    assert not sig.receivers
