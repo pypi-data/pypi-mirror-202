dda26c80082c8b67cba0e195b7db46e1d05e5ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:20 +0000

    Add package oldschool-management-tools/0.1.2/oldschool_management_tools-0.1.2-py3-none-any.whl

---
+++ packages/oldschool-management-tools/oldschool_management_tools-0.1.2-py3-none-any.whl/oldschool_management_tools/mgmt_tools_cmd.py
@@ -0,0 +1,29 @@
+import cmd
+from oldschool_management_tools.oldschool_management_tools import parse_day, show_day_sched, prompt_day_tasks, fill_prep
+
+
+class MgmtToolsCmd(cmd.Cmd):
+    intro = "Welcome to Old School Management Tools"
+    prompt = "(mgmt) "
+
+    def do_prompt_tasks(self, day):
+        parsed_day = parse_day(day)
+        prompt_day_tasks(parsed_day)
+
+    def do_show_sched(self, day):
+        parsed_day = parse_day(day)
+        show_day_sched(parsed_day)
+
+    def do_fill_prep(self, day):
+        parsed_day = parse_day(day)
+        fill_prep(parsed_day)
+
+    def do_die(self, args):
+        return True
+
+    def do_EOF(self, args):
+        return True
+
+
+MgmtToolsCmd().cmdloop()
+exit(0)
