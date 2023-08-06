02b935b4eab70f68ec74dddef2d6495a123de750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:02 +0000

    Add package LibRecommender/1.1.1/LibRecommender-1.1.1-cp311-cp311-win_amd64.whl

---
+++ packages/LibRecommender/LibRecommender-1.1.1-cp311-cp311-win_amd64.whl/libreco/utils/constants.py
@@ -0,0 +1,111 @@
+PURE_MODELS = (
+    "UserCF",
+    "ItemCF",
+    "SVD",
+    "SVDpp",
+    "ALS",
+    "NCF",
+    "BPR",
+    "DeepWalk",
+    "NGCF",
+    "LightGCN",
+)
+
+FEAT_MODELS = (
+    "WideDeep",
+    "FM",
+    "DeepFM",
+    "AutoInt",
+    "GraphSage",
+    "GraphSageDGL",
+    "PinSage",
+    "PinSageDGL",
+)
+
+SEQUENCE_MODELS = (
+    "YouTubeRetrieval",
+    "YouTubeRanking",
+    "DIN",
+    "Item2Vec",
+    "RNN4Rec",
+    "Caser",
+    "WaveNet",
+)
+
+GRAPH_MODELS = (
+    "DeepWalk",
+    "NGCF",
+    "LightGCN",
+    "GraphSage",
+    "GraphSageDGL",
+    "PinSage",
+    "PinSageDGL",
+)
+
+TF_FEAT_MODELS = (
+    "WideDeep",
+    "FM",
+    "DeepFM",
+    "YouTubeRanking",
+    "AutoInt",
+    "DIN",
+)
+
+TF_TRAIN_MODELS = (
+    "SVD",
+    "SVDpp",
+    "NCF",
+    "BPR",
+    "WideDeep",
+    "FM",
+    "DeepFM",
+    "YouTubeRetrieval",
+    "YouTubeRanking",
+    "AutoInt",
+    "DIN",
+    "RNN4Rec",
+    "Caser",
+    "WaveNet",
+)
+
+FEAT_TRAIN_MODELS = (
+    "WideDeep",
+    "FM",
+    "DeepFM",
+    "AutoInt",
+    "YouTubeRetrieval",
+    "YouTubeRanking",
+    "DIN",
+    "GraphSage",
+    "GraphSageDGL",
+    "PinSage",
+    "PinSageDGL",
+)
+
+EMBEDDING_MODELS = (
+    "SVD",
+    "SVDpp",
+    "ALS",
+    "BPR",
+    "YouTubeRetrieval",
+    "Item2Vec",
+    "RNN4Rec",
+    "Caser",
+    "WaveNet",
+    "DeepWalk",
+    "NGCF",
+    "LightGCN",
+    "GraphSage",
+    "GraphSageDGL",
+    "PinSage",
+    "PinSageDGL",
+)
+
+SEQUENCE_RECOMMEND_MODELS = (
+    "YouTubeRetrieval",
+    "YouTubeRanking",
+    "DIN",
+    "RNN4Rec",
+    "Caser",
+    "WaveNet",
+)
