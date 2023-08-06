9571bb3caad7458de183fe4902e3c7841a1d15cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:45 +0000

    Add package meerkat-ml/0.4.7/meerkat_ml-0.4.7-py2.py3-none-any.whl

---
+++ packages/meerkat-ml/meerkat_ml-0.4.7-py2.py3-none-any.whl/meerkat/interactive/formatter/__init__.py
@@ -0,0 +1,54 @@
+from .audio import AudioFormatter, AudioFormatterGroup
+from .base import Formatter, deferred_formatter_group
+from .boolean import BooleanFormatter, BooleanFormatterGroup
+from .code import CodeFormatter, CodeFormatterGroup
+from .image import (
+    DeferredImageFormatter,
+    DeferredImageFormatterGroup,
+    ImageFormatter,
+    ImageFormatterGroup,
+)
+from .number import NumberFormatter, NumberFormatterGroup
+from .pdf import PDFFormatter, PDFFormatterGroup
+from .raw_html import HTMLFormatter, HTMLFormatterGroup
+from .tensor import TensorFormatter, TensorFormatterGroup
+from .text import TextFormatter, TextFormatterGroup
+
+__all__ = [
+    "Formatter",
+    "deferred_formatter_group",
+    "ImageFormatter",
+    "ImageFormatterGroup",
+    "DeferredImageFormatter",
+    "DeferredImageFormatterGroup",
+    "TextFormatter",
+    "TextFormatterGroup",
+    "NumberFormatter",
+    "NumberFormatterGroup",
+    "HTMLFormatter",
+    "HTMLFormatterGroup",
+    "CodeFormatter",
+    "CodeFormatterGroup",
+    "PDFFormatter",
+    "PDFFormatterGroup",
+    "TensorFormatter",
+    "TensorFormatterGroup",
+    "BooleanFormatter",
+    "BooleanFormatterGroup",
+    "AudioFormatter",
+    "AudioFormatterGroup",
+]
+
+
+# backwards compatibility
+class DeprecatedFormatter:  # noqa: E302
+    pass
+
+
+ObjectFormatter = DeprecatedFormatter
+BasicFormatter = DeprecatedFormatter
+NumpyArrayFormatter = DeprecatedFormatter
+# TensorFormatter = DeprecatedFormatter
+WebsiteFormatter = DeprecatedFormatter
+# CodeFormatter = DeprecatedFormatter
+PILImageFormatter = DeprecatedFormatter
