9d122bc212f2526f1ddf9bb652eb2d2bfc63fdf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:05 +0000

    Add package marpdown/0.1.0/marpdown-0.1.0-py3-none-any.whl

---
+++ packages/marpdown/marpdown-0.1.0-py3-none-any.whl/marpdown/slide.py
@@ -0,0 +1,34 @@
+
+class BaseSlide:
+    def __init__(self,content:str='',backgroundImage: str = None, ):
+        self.content = content
+        self.bgimage = backgroundImage
+
+class TOCSlide(BaseSlide):
+    def __init__(self, title: str, toc: list[str],focus = -1,backgroundImage: str = None):
+        self.toc = toc
+        content = f'''# {title}\n\n'''
+        if focus == -1:
+            tmp = [f'''### {i+1}. {t}''' for i,t in enumerate(toc)]
+            content += '\n'.join(tmp)
+        else:
+            tmp = [f'''### {i + 1}. {t}''' for i, t in enumerate(toc)]
+            tmp[focus] = f'''### <div class ="highlight-wrapper"><div class="highlight-container"><div class="highlight"> {focus+1}. {toc[focus]}</div></div></div>'''
+            content += '\n'.join(tmp)
+        super().__init__(content, backgroundImage)
+
+class TimelineSlide(BaseSlide):
+    def __init__(self, title:str,timelines: list[tuple],backgroundImage: str = None,):
+        content ="# " + title + "\n"
+        content += '''<ul class="timeline">\n'''
+        for tl in timelines:
+            name, detail = tl
+            content+=f'''<li class="timeline-item">
+                        <div class="timeline-marker"></div>
+                        <div class="timeline-content">
+                          <h4>{name}</h4>
+                          {detail}
+                        </div></li>'''
+        content += '</ul>'
+
+        super().__init__(content, backgroundImage)
