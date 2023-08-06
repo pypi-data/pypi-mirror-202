e24ae44226c0a3817014c5903186d4d13e67654d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:07 +0000

    Add package gameyamlspiderandgenerator/1.0.0/gameyamlspiderandgenerator-1.0.0-py3-none-any.whl

---
+++ packages/gameyamlspiderandgenerator/gameyamlspiderandgenerator-1.0.0-py3-none-any.whl/gameyamlspiderandgenerator/util/config.py
@@ -0,0 +1,36 @@
+from .fgi_yaml import fgi
+from ..exception import ReadOrWriteConfigFailed
+
+
+class Config:
+    proxy = {}
+    api = {}
+    plugin = {}
+    hook = {}
+
+    def __getitem__(self, item):
+        # Compatibility with the old version
+        return self.__getattribute__(item)
+
+    def __setitem__(self, key, value):
+        # Compatibility with the old version
+        self.__setattr__(key, value)
+
+    def load(self, file_name: str):
+        try:
+            with open(file_name, "r", encoding="utf-8") as fp:
+                self.__dict__.update(fgi.load(fp))
+        except Exception:
+            raise ReadOrWriteConfigFailed from None
+
+    def update(self, data: dict):
+        self.__dict__.update(data)
+
+    def __str__(self):
+        fgi.preserve_quotes = True
+        fgi.explicit_start = True
+        fgi.indent = 1
+        return fgi.dump_to_string(self.__dict__)
+
+
+config = Config()
