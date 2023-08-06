c9a3efcf7c87799fe9ecab0fd6d4f0f2cb6748d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:24 +0000

    Add package pipen-log2file/0.1.0/pipen_log2file-0.1.0.tar.gz

---
+++ packages/pipen-log2file/pipen_log2file-0.1.0.tar.gz/pipen_log2file-0.1.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pipen_log2file']
+install_requires = \
+['pipen>=0.8,<0.9']
+
+entry_points = \
+{'pipen': ['log2file = pipen_log2file:log2file_plugin']}
+
+setup_kwargs = {
+    'name': 'pipen-log2file',
+    'version': '0.1.0',
+    'description': 'Add verbosal information in logs for pipen.',
+    'long_description': '# pipen-log2file\n\nSave running logs to file for [pipen][1].\n\nThe log file is saved to `<workdir>/<pipeline>/.logs/run-<date-time>.log` by default.\nA symlink `<workdir>/<pipeline>/run-latest.log` is created to the latest log file.\n\n## Installation\n\n```\npip install -U pipen-log2file\n```\n\n## Enabling/Disabling the plugin\n\nThe plugin is registered via entrypoints. It\'s by default enabled. To disable it:\n`plugins=[..., "no:log2file"]`, or uninstall this plugin.\n\n\n[1]: https://github.com/pwwang/pipen\n',
+    'author': 'pwwang',
+    'author_email': 'pwwang@pwwang.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/pwwang/pipen-log2file',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
