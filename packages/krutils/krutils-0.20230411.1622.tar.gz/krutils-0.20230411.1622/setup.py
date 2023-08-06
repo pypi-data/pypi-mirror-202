60ca97cf3388e096333c60ef7d22bc4c93a88a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package krutils/0.20230411.1622/krutils-0.20230411.1622.tar.gz

---
+++ packages/krutils/krutils-0.20230411.1622.tar.gz/krutils-0.20230411.1622/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="krutils",
+    version="0.20230411.1622",
+    author="bonfireof",
+    author_email="bonfireof@gmail.com",
+    description="Some utils for me.",
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    # package_dir={"": "krutils"},
+    # packages=setuptools.find_packages(where="krutils"),
+    package_dir={"krutils": "krutils"},
+    packages=["krutils"],
+    python_requires=">=3.0",
+    install_requires=[
+        "PyMySQL>=1.0.3"
+    ]
+)
