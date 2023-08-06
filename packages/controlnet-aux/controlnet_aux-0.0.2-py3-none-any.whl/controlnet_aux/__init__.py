61e926dffc4c209b8da9d050a13ca579e18278d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:44 +0000

    Add package controlnet-aux/0.0.2/controlnet_aux-0.0.2-py3-none-any.whl

---
+++ packages/controlnet-aux/controlnet_aux-0.0.2-py3-none-any.whl/controlnet_aux/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.2"
+
+from .canny import CannyDetector
+from .hed import HEDdetector
+from .midas import MidasDetector
+from .mlsd import MLSDdetector
+from .open_pose import OpenposeDetector
