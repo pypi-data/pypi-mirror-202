498d945336e8de227bbef09f1fbcacc9eb5d84f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:48 +0000

    Add package pygmtsar/2023.4.12/pygmtsar-2023.4.12.tar.gz

---
+++ packages/pygmtsar/pygmtsar-2023.4.12.tar.gz/pygmtsar-2023.4.12/pygmtsar/SBAS_unwrap.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Alexey Pechnikov, Oct, 2022, https://github.com/mobigroup/gmtsar
+from .SBAS_unwrap_snaphu import SBAS_unwrap_snaphu
+
+class SBAS_unwrap(SBAS_unwrap_snaphu):
+
+    def unwrap_parallel(self, pairs=None, n_jobs=-1, **kwargs):
+        import pandas as pd
+        from tqdm.auto import tqdm
+        import joblib
+        import os
+
+        # for now (Python 3.10.10 on MacOS) joblib loads the code from disk instead of copying it
+        kwargs['chunksize'] = self.chunksize
+
+        def unwrap(pair, **kwargs):
+            # define unique tiledir name for parallel processing
+            if 'conf' in kwargs:
+                dirpath = self.get_filenames(None, [pair], 'snaphu_tiledir')[0][:-4]
+                kwargs['conf'] += f'    TILEDIR {dirpath}'
+            return self.unwrap(pair, **kwargs)
+
+        if pairs is None:
+            pairs = self.find_pairs()
+        elif isinstance(pairs, pd.DataFrame):
+            pairs = pairs.values
+
+        with self.tqdm_joblib(tqdm(desc='Unwrapping', total=len(pairs))) as progress_bar:
+            joblib.Parallel(n_jobs=n_jobs)(joblib.delayed(unwrap)(pair, interactive=False, **kwargs) \
+                                           for pair in pairs)
