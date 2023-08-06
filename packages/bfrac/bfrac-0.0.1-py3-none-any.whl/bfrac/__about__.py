11dd87367f41410b71a14255716bb543cd589da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:12 +0000

    Add package bfrac/0.0.1/bfrac-0.0.1-py3-none-any.whl

---
+++ packages/bfrac/bfrac-0.0.1-py3-none-any.whl/bfrac/__about__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__copyright__",
+]
+
+__version__ = "0.0.1"
+
+__author__ = "Chathura Jayalath (deamonpog)"
+__copyright__ = f"Copyright 2013-2023 {__author__}"
