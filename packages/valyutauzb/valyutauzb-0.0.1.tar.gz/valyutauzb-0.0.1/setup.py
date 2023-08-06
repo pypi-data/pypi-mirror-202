3fc690ba106c57dc7a9553366882766160f4a06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:43 +0000

    Add package valyutauzb/0.0.1/valyutauzb-0.0.1.tar.gz

---
+++ packages/valyutauzb/valyutauzb-0.0.1.tar.gz/valyutauzb-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="valyutauzb",
+    version="0.0.1",
+    author="Hikmatullayev Islombek",
+    author_email="gimlerpro8899@gmail.com",
+    description="Ushbu kutubxona valyutani bilish uchun ishlatiladi",
+    long_description_content_type="text/markdown",
+)
+
+
+
+
+    
---
+++ packages/valyutauzb/valyutauzb-0.0.1.tar.gz/valyutauzb-0.0.1/valyutauzb/__init__.py
@@ -0,0 +1,4 @@
+from valyuta import convert
+
+def convert(miqdor):
+    return convert(miqdor).miqdor
