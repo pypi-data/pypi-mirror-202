46215dad88f147bfc6ee283eac900b79cc1052f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:45 +0000

    Add package ogr-tiller/0.0.44/ogr_tiller-0.0.44.tar.gz

---
+++ packages/ogr-tiller/ogr_tiller-0.0.44.tar.gz/ogr_tiller-0.0.44/ogr_tiller/poco/job_param.py
@@ -0,0 +1,14 @@
+class JobParam:
+    def __init__(self,
+                 mode: str,
+                 data_folder: str,
+                 cache_folder: str,
+                 port: str,
+                 disable_caching: bool,
+                 tile_timeout: int):
+        self.mode = mode
+        self.data_folder = data_folder
+        self.cache_folder = cache_folder
+        self.port = port
+        self.disable_caching = disable_caching
+        self.tile_timeout = tile_timeout
