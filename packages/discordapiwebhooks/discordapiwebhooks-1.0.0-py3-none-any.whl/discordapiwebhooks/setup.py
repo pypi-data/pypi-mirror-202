8821e34461d4730f0fbc3e12895a4d414871c85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:51 +0000

    Add package discordapiwebhooks/1.0.0/discordapiwebhooks-1.0.0-py3-none-any.whl

---
+++ packages/discordapiwebhooks/discordapiwebhooks-1.0.0-py3-none-any.whl/discordapiwebhooks/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='discordapiwebhooks',
+    version='1.0.0',
+    description='Discord webhook create module',
+    author='NouName',
+    author_email='duaneskalanak@gmail.com',
+    packages=['discordapiwebhooks'],
+)
