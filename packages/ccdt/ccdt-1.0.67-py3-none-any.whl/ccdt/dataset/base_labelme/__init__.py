aef26bf50383487aa604a6e34e839d22098691da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:05 +0000

    Add package ccdt/1.0.67/ccdt-1.0.67-py3-none-any.whl

---
+++ packages/ccdt/ccdt-1.0.67-py3-none-any.whl/ccdt/dataset/base_labelme/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/3/25 13:43
+# @Author : Zhan Yong
+from .base_labelme import BaseLabelme
+from .label_infos import LabelInfo
+from .total_labelme_infos import TotalLabelmeInfos
+
+__all__ = ['BaseLabelme', 'LabelInfo', 'TotalLabelmeInfos']
