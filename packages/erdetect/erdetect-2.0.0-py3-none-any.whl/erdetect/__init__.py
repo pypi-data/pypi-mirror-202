a0780bd54a5567166d5f871b47cba176e15a635c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:59 +0000

    Add package erdetect/2.0.0/erdetect-2.0.0-py3-none-any.whl

---
+++ packages/erdetect/erdetect-2.0.0-py3-none-any.whl/erdetect/__init__.py
@@ -0,0 +1,20 @@
+# define directory as package
+
+# ensure minimum version
+import sys
+if sys.version_info < (3, 8, 0):
+    sys.exit("Python 3.8 or later is required.")
+
+# flatten access
+from ieegprep.utils.console import CustomLoggingFormatter
+from erdetect.version import __version__
+from erdetect._erdetect import process_subset
+__all__ = ['process_subset', '__version__']
+
+# logging
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger_ch = logging.StreamHandler(stream=sys.stdout)
+logger_ch.setFormatter(CustomLoggingFormatter())
+logger.addHandler(logger_ch)
