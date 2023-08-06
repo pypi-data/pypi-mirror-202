ab9c1700fb9b3b0122f1ead193b56e2197f8c16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:48 +0000

    Add package ChessAnalysisPipeline/0.0.5/ChessAnalysisPipeline-0.0.5-py3-none-any.whl

---
+++ packages/ChessAnalysisPipeline/ChessAnalysisPipeline-0.0.5-py3-none-any.whl/CHAP/writer.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+"""
+File       : writer.py
+Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
+Description: generic Writer module
+"""
+
+# system modules
+import argparse
+import json
+import logging
+import os
+import sys
+from time import time
+
+# local modules
+# from pipeline import PipelineObject
+
+class Writer():
+    """
+    Writer represent generic file writer
+    """
+
+    def __init__(self):
+        """
+        Constructor of Writer class
+        """
+        self.__name__ = self.__class__.__name__
+        self.logger = logging.getLogger(self.__name__)
+        self.logger.propagate = False
+
+    def write(self, data, filename, **_write_kwargs):
+        """
+        write API
+
+        :param filename: Name of file to write to
+        :param data: data to write to file
+        :return: data written to file
+        """
+
+        t0 = time()
+        self.logger.info(f'Executing "write" with filename={filename}, type(data)={type(data)}, kwargs={_write_kwargs}')
+
+        data = self._write(data, filename, **_write_kwargs)
+
+        self.logger.info(f'Finished "write" in {time()-t0:.3f} seconds\n')
+
+        return(data)
+
+    def _write(self, data, filename):
+        with open(filename, 'a') as file:
+            file.write(data)
+        return(data)
+
+class OptionParser():
+        '''User based option parser'''
+        def __init__(self):
+            self.parser = argparse.ArgumentParser(prog='PROG')
+            self.parser.add_argument(
+                '--data', action='store',
+                dest='data', default='', help='Input data')
+            self.parser.add_argument(
+                '--filename', action='store',
+                        dest='filename', default='', help='Output file')
+            self.parser.add_argument(
+                '--writer', action='store',
+                dest='writer', default='Writer', help='Writer class name')
+            self.parser.add_argument(
+                '--log-level', choices=logging._nameToLevel.keys(),
+            dest='log_level', default='INFO', help='logging level')
+
+def main(opt_parser=OptionParser):
+    '''Main function'''
+
+    optmgr  = opt_parser()
+    opts = optmgr.parser.parse_args()
+    clsName = opts.writer
+    try:
+        writerCls = getattr(sys.modules[__name__],clsName)
+    except:
+        print(f'Unsupported writer {clsName}')
+        sys.exit(1)
+
+    writer = writerCls()
+    writer.logger.setLevel(getattr(logging, opts.log_level))
+    log_handler = logging.StreamHandler()
+    log_handler.setFormatter(logging.Formatter('{name:20}: {message}', style='{'))
+    writer.logger.addHandler(log_handler)
+    data = writer.write(opts.data, opts.filename)
+    print(f"Writer {writer} writes to {opts.filename}, data {data}")
+
+if __name__ == '__main__':
+    main()
