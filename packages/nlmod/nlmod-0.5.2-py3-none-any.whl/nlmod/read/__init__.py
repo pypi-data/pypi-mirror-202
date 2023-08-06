def559e9006e52a0abd1339f8cfd8fdae3263319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:42 +0000

    Add package nlmod/0.5.2/nlmod-0.5.2-py3-none-any.whl

---
+++ packages/nlmod/nlmod-0.5.2-py3-none-any.whl/nlmod/read/__init__.py
@@ -0,0 +1,14 @@
+from . import (
+    ahn,
+    bgt,
+    brp,
+    geotop,
+    jarkus,
+    knmi,
+    meteobase,
+    regis,
+    rws,
+    waterboard,
+    webservices,
+)
+from .regis import get_regis
