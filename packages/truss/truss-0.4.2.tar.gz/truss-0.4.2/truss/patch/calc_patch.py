1e155319c06f1defa0e7da89b98b88d59e9778ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:32 +0000

    Add package truss/0.4.2/truss-0.4.2.tar.gz

---
+++ packages/truss/truss-0.4.2.tar.gz/truss-0.4.2/truss/patch/calc_patch.py
@@ -0,0 +1,282 @@
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Set
+
+import pkg_resources
+import yaml
+from truss.patch.hash import file_content_hash_str
+from truss.patch.types import TrussSignature
+from truss.templates.control.control.helpers.types import (
+    Action,
+    ModelCodePatch,
+    Patch,
+    PatchType,
+    PythonRequirementPatch,
+    SystemPackagePatch,
+)
+from truss.truss_config import TrussConfig
+from truss.truss_spec import TrussSpec
+
+PYCACHE_IGNORE_PATTERNS = [
+    "**/__pycache__/**/*",
+    "**/__pycache__/**",
+]
+
+
+def calc_truss_patch(
+    truss_dir: Path,
+    previous_truss_signature: TrussSignature,
+    ignore_patterns: Optional[List[str]] = None,
+) -> Optional[List[Patch]]:
+    """
+    Calculate patch for a truss from a previous state.
+
+    Returns: None if patch cannot be calculated, otherwise a list of patches.
+        Note that the none return value is pretty important, patch coverage
+        is limited and this usually indicates that the identified change cannot
+        be expressed with currently supported patches.
+    """
+    if ignore_patterns is None:
+        ignore_patterns = PYCACHE_IGNORE_PATTERNS
+
+    changed_paths = _calc_changed_paths(
+        truss_dir,
+        previous_truss_signature.content_hashes_by_path,
+        ignore_patterns,
+    )
+    # TODO(pankaj) Calculate model code patches only for now, add config changes
+    # later.
+    truss_spec = TrussSpec(truss_dir)
+    model_module_path = str(truss_spec.model_module_dir.relative_to(truss_dir))
+    training_module_path = str(truss_spec.training_module_dir.relative_to(truss_dir))
+
+    patches = []
+    for path in changed_paths["removed"]:
+        if path.startswith(model_module_path):
+            relative_to_model_module_path = str(
+                Path(path).relative_to(model_module_path)
+            )
+            patches.append(
+                Patch(
+                    type=PatchType.MODEL_CODE,
+                    body=ModelCodePatch(
+                        action=Action.REMOVE,
+                        path=relative_to_model_module_path,
+                    ),
+                )
+            )
+        elif path.startswith(training_module_path):
+            # Ignore training changes from patch
+            continue
+        else:
+            return None
+
+    for path in changed_paths["added"] + changed_paths["updated"]:
+        if path.startswith(model_module_path):
+            full_path = truss_dir / path
+            relative_to_model_module_path = str(
+                Path(path).relative_to(model_module_path)
+            )
+
+            # TODO(pankaj) Add support for empty directories, skip them for now.
+            if not full_path.is_file():
+                continue
+
+            with full_path.open() as file:
+                content = file.read()
+            action = Action.ADD if path in changed_paths["added"] else Action.UPDATE
+            patches.append(
+                Patch(
+                    type=PatchType.MODEL_CODE,
+                    body=ModelCodePatch(
+                        action=action,
+                        path=relative_to_model_module_path,
+                        content=content,
+                    ),
+                )
+            )
+        elif path.startswith(training_module_path):
+            # Ignore training changes from patch
+            continue
+        elif str(path) == "config.yaml":
+            new_config = TrussConfig.from_yaml(truss_dir / "config.yaml")
+            prev_config = TrussConfig.from_dict(
+                yaml.safe_load(previous_truss_signature.config)
+            )
+            config_patches = _calc_config_patches(prev_config, new_config)
+            if config_patches is None:
+                return None
+            patches.extend(config_patches)
+        else:
+            return None
+    return patches
+
+
+def _calc_changed_paths(
+    root: Path,
+    previous_root_path_content_hashes: Dict[str, str],
+    ignore_patterns: Optional[List[str]],
+) -> Dict[str, List[str]]:
+    """
+    TODO(pankaj) add support for directory creation in patch
+    """
+    unignored_paths = _calc_unignored_paths(root, ignore_patterns)
+    previous_root_relative_paths = set(previous_root_path_content_hashes.keys())
+
+    added_paths = unignored_paths - previous_root_relative_paths
+    removed_paths = previous_root_relative_paths - unignored_paths
+
+    updated_paths = set()
+    common_paths = unignored_paths.intersection(previous_root_relative_paths)
+    for path in common_paths:
+        full_path: Path = root / path
+        if full_path.is_file():
+            content_hash = file_content_hash_str(full_path)
+            previous_content_hash = previous_root_path_content_hashes[path]
+            if content_hash != previous_content_hash:
+                updated_paths.add(path)
+
+    return {
+        "added": list(added_paths),
+        "updated": list(updated_paths),
+        "removed": list(removed_paths),
+    }
+
+
+def _calc_unignored_paths(
+    root: Path, ignore_patterns: Optional[List[str]] = None
+) -> Set[str]:
+    root_relative_ignored_paths = set()
+    if ignore_patterns is not None:
+        for ignore_pattern in ignore_patterns:
+            ignored_paths_for_pattern = set(
+                (str(path.relative_to(root)) for path in root.glob(ignore_pattern))
+            )
+            root_relative_ignored_paths.update(ignored_paths_for_pattern)
+
+    root_relative_paths = set(
+        (str(path.relative_to(root)) for path in root.glob("**/*"))
+    )
+    return root_relative_paths - root_relative_ignored_paths
+
+
+def _calc_config_patches(
+    prev_config: TrussConfig, new_config: TrussConfig
+) -> Optional[List[Patch]]:
+    """Calculate patch based on changes to config.
+
+    Returns None if patch cannot be calculated. Empty list means no relevant
+    differences found.
+    """
+    # Currently only calculate patches for python requirements and system
+    # packages, bail out if anything else has changed.
+    if not _only_expected_config_differences(prev_config, new_config):
+        return None
+
+    python_requirement_patches = _calc_python_requirements_patches(
+        prev_config, new_config
+    )
+    system_package_patches = _calc_system_packages_patches(prev_config, new_config)
+    return [*python_requirement_patches, *system_package_patches]
+
+
+def _calc_python_requirements_patches(
+    prev_config: TrussConfig, new_config: TrussConfig
+) -> List[Patch]:
+    """Calculate patch based on changes to python requirements.
+
+    Returns None if patch cannot be calculated. Empty list means no relevant
+    differences found.
+    """
+    patches = []
+    prev_reqs = _parsed_reqs_by_name(prev_config.requirements)
+    prev_req_names = set(prev_reqs.keys())
+    new_reqs = _parsed_reqs_by_name(new_config.requirements)
+    new_req_names = set(new_reqs.keys())
+    removed_reqs = prev_req_names.difference(new_req_names)
+    for removed_req in removed_reqs:
+        patches.append(_mk_python_requirement_patch(Action.REMOVE, removed_req))
+
+    added_reqs = new_req_names.difference(prev_req_names)
+    for added_req in added_reqs:
+        patches.append(
+            _mk_python_requirement_patch(Action.ADD, str(new_reqs[added_req]))
+        )
+
+    for req in new_req_names.intersection(prev_req_names):
+        if new_reqs[req] != prev_reqs[req]:
+            patches.append(
+                _mk_python_requirement_patch(Action.UPDATE, str(new_reqs[req]))
+            )
+
+    return patches
+
+
+def _calc_system_packages_patches(
+    prev_config: TrussConfig, new_config: TrussConfig
+) -> List[Patch]:
+    """Calculate patch based on changes to system packates.
+
+    Returns None if patch cannot be calculated. Empty list means no relevant
+    differences found.
+    """
+    patches = []
+    prev_pkgs = _system_pacakges_set(prev_config)
+    new_pkgs = _system_pacakges_set(new_config)
+    removed_pkgs = prev_pkgs.difference(new_pkgs)
+    for removed_pkg in removed_pkgs:
+        patches.append(_mk_system_package_patch(Action.REMOVE, removed_pkg))
+
+    added_pkgs = new_pkgs.difference(prev_pkgs)
+    for added_pkg in added_pkgs:
+        patches.append(_mk_system_package_patch(Action.ADD, added_pkg))
+
+    return patches
+
+
+def _system_pacakges_set(config: TrussConfig) -> Set[str]:
+    pkgs = []
+    for sys_pkg_line in config.system_packages:
+        pkgs.extend(sys_pkg_line.strip().split())
+    return set(pkgs)
+
+
+def _parsed_reqs_by_name(reqs: List[str]) -> Dict[str, Any]:
+    parsed_reqs_by_name = {}
+    for req in reqs:
+        parsed_req = pkg_resources.Requirement.parse(req)
+        parsed_reqs_by_name[parsed_req.name] = parsed_req  # type: ignore
+    return parsed_reqs_by_name
+
+
+def _only_expected_config_differences(
+    prev_config: TrussConfig, new_config: TrussConfig
+) -> bool:
+    prev_config_dict = prev_config.to_dict()
+    prev_config_dict["requirements"] = []
+    prev_config_dict["system_packages"] = []
+
+    new_config_dict = new_config.to_dict()
+    new_config_dict["requirements"] = []
+    new_config_dict["system_packages"] = []
+
+    return prev_config_dict == new_config_dict
+
+
+def _mk_python_requirement_patch(action: Action, requirement: str) -> Patch:
+    return Patch(
+        type=PatchType.PYTHON_REQUIREMENT,
+        body=PythonRequirementPatch(
+            action=action,
+            requirement=requirement,
+        ),
+    )
+
+
+def _mk_system_package_patch(action: Action, package: str) -> Patch:
+    return Patch(
+        type=PatchType.SYSTEM_PACKAGE,
+        body=SystemPackagePatch(
+            action=action,
+            package=package,
+        ),
+    )
---
+++ packages/truss/truss-0.4.2.tar.gz/truss-0.4.2/truss/truss_config.py
@@ -0,0 +1,258 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+
+import yaml
+from truss.errors import ValidationError
+from truss.types import ModelFrameworkType
+from truss.validation import (
+    validate_cpu_spec,
+    validate_memory_spec,
+    validate_secret_name,
+)
+
+DEFAULT_MODEL_FRAMEWORK_TYPE = ModelFrameworkType.CUSTOM
+DEFAULT_MODEL_TYPE = "Model"
+DEFAULT_MODEL_MODULE_DIR = "model"
+DEFAULT_BUNDLED_PACKAGES_DIR = "packages"
+DEFAULT_MODEL_CLASS_FILENAME = "model.py"
+DEFAULT_MODEL_CLASS_NAME = "Model"
+DEFAULT_TRUSS_STRUCTURE_VERSION = "2.0"
+DEFAULT_MODEL_INPUT_TYPE = "Any"
+DEFAULT_PYTHON_VERSION = "py39"
+DEFAULT_DATA_DIRECTORY = "data"
+DEFAULT_EXAMPLES_FILENAME = "examples.yaml"
+DEFAULT_SPEC_VERSION = "2.0"
+DEFAULT_SPEC_VERSION_ON_LOAD = "1.0"
+
+DEFAULT_CPU = "500m"
+DEFAULT_MEMORY = "512Mi"
+DEFAULT_USE_GPU = False
+
+DEFAULT_TRAINING_CLASS_FILENAME = "train.py"
+DEFAULT_TRAINING_CLASS_NAME = "Train"
+DEFAULT_TRAINING_MODULE_DIR = "train"
+
+
+class Accelerator(Enum):
+    T4 = "T4"
+    A10G = "A10G"
+    V100 = "V100"
+    A100 = "A100"
+
+
+@dataclass
+class AcceleratorSpec:
+    accelerator: Optional[Accelerator] = None
+    count: int = 0
+
+    def to_str(self) -> Optional[str]:
+        if self.accelerator is None or self.count == 0:
+            return None
+        if self.count > 1:
+            return f"{self.accelerator.value}:{self.count}"
+        return self.accelerator.value
+
+    @staticmethod
+    def from_str(acc_spec: Optional[str]):
+        if acc_spec is None:
+            return AcceleratorSpec()
+        parts = acc_spec.split(":")
+        count = 1
+        if len(parts) not in [1, 2]:
+            raise ValidationError("`accelerator` does not match parsing requirements.")
+        if len(parts) == 2:
+            count = int(parts[1])
+        try:
+            acc = Accelerator[parts[0]]
+        except KeyError as exc:
+            raise ValidationError(f"Accelerator {acc_spec} not supported") from exc
+        return AcceleratorSpec(accelerator=acc, count=count)
+
+
+@dataclass
+class Resources:
+    cpu: str = DEFAULT_CPU
+    memory: str = DEFAULT_MEMORY
+    use_gpu: bool = DEFAULT_USE_GPU
+    accelerator: AcceleratorSpec = AcceleratorSpec()
+
+    @staticmethod
+    def from_dict(d):
+        cpu = d.get("cpu", DEFAULT_CPU)
+        validate_cpu_spec(cpu)
+        memory = d.get("memory", DEFAULT_MEMORY)
+        validate_memory_spec(memory)
+        accelerator = AcceleratorSpec.from_str((d.get("accelerator", None)))
+        use_gpu = d.get("use_gpu", DEFAULT_USE_GPU)
+        if accelerator.accelerator is not None:
+            use_gpu = True
+
+        return Resources(
+            cpu=cpu,
+            memory=memory,
+            use_gpu=use_gpu,
+            accelerator=accelerator,
+        )
+
+    def to_dict(self):
+        return {
+            "cpu": self.cpu,
+            "memory": self.memory,
+            "use_gpu": self.use_gpu,
+            "accelerator": self.accelerator.to_str(),
+        }
+
+
+@dataclass
+class Train:
+    training_class_filename: str = DEFAULT_TRAINING_CLASS_FILENAME
+    training_class_name: str = DEFAULT_TRAINING_CLASS_NAME
+    training_module_dir: str = DEFAULT_TRAINING_MODULE_DIR
+    variables: Dict = field(default_factory=dict)
+    resources: Resources = field(default_factory=Resources)
+
+    @staticmethod
+    def from_dict(d):
+        return Train(
+            training_class_filename=d.get(
+                "training_class_filename", DEFAULT_TRAINING_CLASS_FILENAME
+            ),
+            training_class_name=d.get(
+                "training_class_name", DEFAULT_TRAINING_CLASS_NAME
+            ),
+            training_module_dir=d.get(
+                "training_module_dir", DEFAULT_TRAINING_MODULE_DIR
+            ),
+            variables=d.get("variables", {}),
+            resources=Resources.from_dict(d.get("resources", {})),
+        )
+
+    def to_dict(self):
+        return {
+            "training_class_filename": self.training_class_filename,
+            "training_class_name": self.training_class_name,
+            "training_module_dir": self.training_module_dir,
+            "variables": self.variables,
+            "resources": self.resources.to_dict(),
+        }
+
+
+@dataclass
+class TrussConfig:
+    model_framework: ModelFrameworkType = DEFAULT_MODEL_FRAMEWORK_TYPE
+    model_type: str = DEFAULT_MODEL_TYPE
+    model_name: Optional[str] = None
+
+    model_module_dir: str = DEFAULT_MODEL_MODULE_DIR
+    model_class_filename: str = DEFAULT_MODEL_CLASS_FILENAME
+    model_class_name: str = DEFAULT_MODEL_CLASS_NAME
+
+    data_dir: str = DEFAULT_DATA_DIRECTORY
+
+    # Python types for what the model expects as input
+    input_type: str = DEFAULT_MODEL_INPUT_TYPE
+    model_metadata: Dict[str, Any] = field(default_factory=dict)
+    requirements: List[str] = field(default_factory=list)
+    system_packages: List[str] = field(default_factory=list)
+    environment_variables: Dict[str, str] = field(default_factory=dict)
+    resources: Resources = field(default_factory=Resources)
+    python_version: str = DEFAULT_PYTHON_VERSION
+    examples_filename: str = DEFAULT_EXAMPLES_FILENAME
+    secrets: Dict[str, str] = field(default_factory=dict)
+    description: Optional[str] = None
+    bundled_packages_dir: str = DEFAULT_BUNDLED_PACKAGES_DIR
+    external_package_dirs: List[str] = field(default_factory=list)
+    live_reload: bool = False
+    # spec_version is a version string
+    spec_version: str = DEFAULT_SPEC_VERSION
+    train: Train = field(default_factory=Train)
+
+    @property
+    def canonical_python_version(self) -> str:
+        return {
+            "py39": "3.9",
+            "py38": "3.8",
+            "py37": "3.7",
+        }[self.python_version]
+
+    @staticmethod
+    def from_dict(d):
+        config = TrussConfig(
+            # Users that are calling `load` on an existing Truss
+            # should default to 1.0 whereas users creating a new Truss
+            # should default to 2.0.
+            spec_version=d.get("spec_version", DEFAULT_SPEC_VERSION_ON_LOAD),
+            model_type=d.get("model_type", DEFAULT_MODEL_TYPE),
+            model_framework=ModelFrameworkType(
+                d.get("model_framework", DEFAULT_MODEL_FRAMEWORK_TYPE.value)
+            ),
+            model_module_dir=d.get("model_module_dir", DEFAULT_MODEL_MODULE_DIR),
+            model_class_filename=d.get(
+                "model_class_filename", DEFAULT_MODEL_CLASS_FILENAME
+            ),
+            model_class_name=d.get("model_class_name", DEFAULT_MODEL_CLASS_NAME),
+            data_dir=d.get("data_dir", DEFAULT_DATA_DIRECTORY),
+            input_type=d.get("input_type", DEFAULT_MODEL_INPUT_TYPE),
+            model_metadata=d.get("model_metadata", {}),
+            requirements=d.get("requirements", []),
+            system_packages=d.get("system_packages", []),
+            environment_variables=d.get("environment_variables", {}),
+            resources=Resources.from_dict(d.get("resources", {})),
+            python_version=d.get("python_version", DEFAULT_PYTHON_VERSION),
+            model_name=d.get("model_name", None),
+            examples_filename=d.get("examples_filename", DEFAULT_EXAMPLES_FILENAME),
+            secrets=d.get("secrets", {}),
+            description=d.get("description", None),
+            bundled_packages_dir=d.get(
+                "bundled_packages_dir", DEFAULT_BUNDLED_PACKAGES_DIR
+            ),
+            external_package_dirs=d.get("external_package_dirs", []),
+            live_reload=d.get("live_reload", False),
+            train=Train.from_dict(d.get("train", {})),
+        )
+        config.validate()
+        return config
+
+    @staticmethod
+    def from_yaml(yaml_path: Path):
+        with yaml_path.open() as yaml_file:
+            return TrussConfig.from_dict(yaml.safe_load(yaml_file))
+
+    def write_to_yaml_file(self, path: Path):
+        with path.open("w") as config_file:
+            yaml.dump(self.to_dict(), config_file)
+
+    def to_dict(self):
+        return {
+            "model_type": self.model_type,
+            "model_framework": self.model_framework.value,
+            "model_module_dir": self.model_module_dir,
+            "model_class_filename": self.model_class_filename,
+            "model_class_name": self.model_class_name,
+            "data_dir": self.data_dir,
+            "input_type": self.input_type,
+            "model_metadata": self.model_metadata,
+            "requirements": self.requirements,
+            "system_packages": self.system_packages,
+            "environment_variables": self.environment_variables,
+            "resources": self.resources.to_dict(),
+            "python_version": self.python_version,
+            "model_name": self.model_name,
+            "examples_filename": self.examples_filename,
+            "secrets": self.secrets,
+            "description": self.description,
+            "bundled_packages_dir": self.bundled_packages_dir,
+            "external_package_dirs": self.external_package_dirs,
+            "live_reload": self.live_reload,
+            "spec_version": self.spec_version,
+            "train": self.train.to_dict(),
+        }
+
+    def clone(self):
+        return TrussConfig.from_dict(self.to_dict())
+
+    def validate(self):
+        for secret_name in self.secrets:
+            validate_secret_name(secret_name)
