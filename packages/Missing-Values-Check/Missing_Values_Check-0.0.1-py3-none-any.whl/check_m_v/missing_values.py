0669d1a7340ec016361b62b8d63c5c9371188ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:50 +0000

    Add package Missing-Values-Check/0.0.1/Missing_Values_Check-0.0.1-py3-none-any.whl

---
+++ packages/Missing-Values-Check/Missing_Values_Check-0.0.1-py3-none-any.whl/check_m_v/missing_values.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+# 판다스로 결측치를 확인한 후 결측치가 있는 열을 도표 출력하는 함수
+def check_missing_values(link):
+  df = pd.read_csv(link,encoding='utf-8') # read to csv
+  null_count = df.isnull().sum() # null count
+  print(null_count) 
+  return null_count
+
+def see_missing_values(null_count):
+  null_count.plot.barh(figsize=(7,7))
