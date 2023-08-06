b17ca2f5b235b514d14a9d0e1becd53e3a8f18da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:35 +0000

    Add package image-liveness/1.4/image-liveness-1.4.tar.gz

---
+++ packages/image-liveness/image-liveness-1.4.tar.gz/image-liveness-1.4/Liveness/__main__.py
@@ -0,0 +1,1 @@
+from livecheck import Liveness
---
+++ packages/image-liveness/image-liveness-1.4.tar.gz/image-liveness-1.4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'image-liveness',         
+  packages = ['Liveness'],  
+  version = '1.4',      
+  license='Apache License 2.0',        
+  description = 'This library offers a liveness detection on static images, which can be used to distinguish if it is a live capture or a spoofed image based on facial features and other indicators of authenticity.',  
+  author = 'Ashish Thapa',                   
+  author_email = 'ashish.f1soft@gmail.com',      
+  url = 'https://github.com/ashishf1soft',  
+  download_url = 'https://github.com/ashishf1soft/liveness-detection/archive/refs/tags/v_01.tar.gz',   
+  keywords = ['LIVENESS','FACE FACE','DETECTION'], 
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'opencv-python',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache Software License',   
+    'Programming Language :: Python :: 3.9',
+  ],
+)
