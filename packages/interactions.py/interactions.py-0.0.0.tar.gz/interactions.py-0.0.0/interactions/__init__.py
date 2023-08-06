e8d95dacff11c5de797ca9694338fa2a26461cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:42 +0000

    Add package interactions.py/0.0.0/interactions.py-0.0.0.tar.gz

---
+++ packages/interactions.py/interactions.py-0.0.0.tar.gz/interactions.py-0.0.0/interactions/__init__.py
@@ -0,0 +1,2 @@
+print("This is a placeholder for interactions.py")
+print("See you soon!")
---
+++ packages/interactions.py/interactions.py-0.0.0.tar.gz/interactions.py-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='interactions.py',
+    version='0.0.0',
+    packages=['interactions'],
+    url='https://github.com/interactions-py/interactions.py',
+    license='MIT',
+    author='LordOfPolls',
+)
