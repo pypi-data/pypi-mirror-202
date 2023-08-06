bc1a676d45ba87031d68818aa93125c826bfcd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:35 +0000

    Add package sqlmesh/0.3.1.dev9/sqlmesh-0.3.1.dev9.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.3.1.dev9.tar.gz/sqlmesh-0.3.1.dev9/web/client/src/context/editor.ts
@@ -0,0 +1,142 @@
+import { create } from 'zustand'
+import useLocalStorage from '~/hooks/useLocalStorage'
+import { ModelFile } from '~/models'
+import { isTrue } from '~/utils'
+import { sqlglotWorker } from '~/workers'
+
+export interface Dialect {
+  dialect_title: string
+  dialect_name: string
+}
+
+interface EditorStore {
+  storedTabsIds: ID[]
+  tabs: Map<ModelFile, EditorTab>
+  tab: EditorTab
+  engine: Worker
+  dialects: Dialect[]
+  previewQuery?: string
+  previewTable?: any[]
+  previewConsole?: string
+  selectTab: (tab: EditorTab) => void
+  addTab: (tab: EditorTab) => void
+  closeTab: (file: ModelFile) => void
+  createTab: (file?: ModelFile) => EditorTab
+  setDialects: (dialects: Dialect[]) => void
+  refreshTab: () => void
+  setPreviewQuery: (previewQuery?: string) => void
+  setPreviewTable: (previewTable?: any[]) => void
+  setPreviewConsole: (previewConsole?: string) => void
+}
+
+interface EditorPreview<TTable = any> {
+  queryPreview?: string
+  table?: TTable[]
+  terminal?: string
+}
+
+export interface EditorTab {
+  file: ModelFile
+  isValid: boolean
+  isSaved: boolean
+  isInitial: boolean
+  dialect?: string
+  preview?: EditorPreview
+}
+
+const [getStoredTabs, setStoredTabs] = useLocalStorage<{ ids: ID[] }>('tabs')
+
+const initialFile = createLocalFile()
+const initialTab: EditorTab = createTab(initialFile, true)
+const initialTabs = new Map([[initialFile, initialTab]])
+
+export const useStoreEditor = create<EditorStore>((set, get) => ({
+  storedTabsIds: getStoredTabsIds(),
+  tab: initialTab,
+  tabs: initialTabs,
+  engine: sqlglotWorker,
+  dialects: [],
+  refreshTab() {
+    get().selectTab({ ...get().tab })
+  },
+  setDialects(dialects) {
+    set(() => ({
+      dialects,
+    }))
+  },
+  selectTab(tab) {
+    set(() => ({ tab }))
+
+    get().addTab(tab)
+  },
+  addTab(tab) {
+    const tabs = new Map([...get().tabs, [tab.file, tab]])
+
+    setStoredTabs({
+      ids: Array.from(tabs.values())
+        .filter(tab => tab.file.isRemote)
+        .map(tab => tab.file.id),
+    })
+
+    set(() => ({
+      tabsIds: getStoredTabsIds(),
+      tabs,
+    }))
+  },
+  closeTab(file) {
+    const s = get()
+
+    if (isTrue(s.tabs.get(file)?.isInitial)) return
+
+    const tabs = Array.from(get().tabs.values())
+    const indexAt = tabs.findIndex(tab => tab.file === file)
+
+    s.tabs.delete(file)
+
+    if (file.id === s.tab.file.id) {
+      s.selectTab(tabs.at(indexAt - 1) as EditorTab)
+    }
+
+    set(() => ({
+      tabs: new Map(s.tabs),
+    }))
+  },
+  createTab,
+  previewQuery: undefined,
+  previewTable: undefined,
+  previewConsole: undefined,
+  setPreviewQuery(previewQuery) {
+    set(() => ({ previewQuery }))
+  },
+  setPreviewTable(previewTable) {
+    set(() => ({ previewTable }))
+  },
+  setPreviewConsole(previewConsole) {
+    set(() => ({ previewConsole }))
+  },
+}))
+
+function createTab(
+  file: ModelFile = createLocalFile(),
+  isInitial = false,
+): EditorTab {
+  return {
+    file,
+    isInitial,
+    isValid: true,
+    isSaved: true,
+  }
+}
+
+function createLocalFile(): ModelFile {
+  return new ModelFile({
+    name: '',
+    path: '',
+    content:
+      '-- Create arbitrary SQL queries\n-- and execute them against different environments\n\n',
+  })
+}
+
+function getStoredTabsIds(): ID[] {
+  return getStoredTabs()?.ids ?? []
+}
---
+++ packages/sqlmesh/sqlmesh-0.3.1.dev9.tar.gz/sqlmesh-0.3.1.dev9/web/client/src/library/components/editor/Editor.tsx
@@ -0,0 +1,140 @@
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { Divider } from '../divider/Divider'
+import { useStoreFileTree } from '../../../context/fileTree'
+import SplitPane from '../splitPane/SplitPane'
+import { isFalse, isStringEmptyOrNil } from '../../../utils'
+import CodeEditor from './EditorCode'
+import EditorFooter from './EditorFooter'
+import EditorTabs from './EditorTabs'
+import EditorInspector from './EditorInspector'
+import './Editor.css'
+import EditorPreview from './EditorPreview'
+import { useStoreEditor } from '~/context/editor'
+
+export default function Editor(): JSX.Element {
+  const files = useStoreFileTree(s => s.files)
+  const selectedFile = useStoreFileTree(s => s.selectedFile)
+
+  const tab = useStoreEditor(s => s.tab)
+  const storedTabsIds = useStoreEditor(s => s.storedTabsIds)
+  const engine = useStoreEditor(s => s.engine)
+  const previewTable = useStoreEditor(s => s.previewTable)
+  const previewConsole = useStoreEditor(s => s.previewConsole)
+  const selectTab = useStoreEditor(s => s.selectTab)
+  const createTab = useStoreEditor(s => s.createTab)
+  const addTab = useStoreEditor(s => s.addTab)
+  const setDialects = useStoreEditor(s => s.setDialects)
+  const setPreviewQuery = useStoreEditor(s => s.setPreviewQuery)
+  const setPreviewConsole = useStoreEditor(s => s.setPreviewConsole)
+  const setPreviewTable = useStoreEditor(s => s.setPreviewTable)
+
+  const [isReadyEngine, setIsreadyEngine] = useState(false)
+
+  const handleEngineWorkerMessage = useCallback((e: MessageEvent): void => {
+    if (e.data.topic === 'init') {
+      setIsreadyEngine(true)
+    }
+
+    if (e.data.topic === 'dialects') {
+      setDialects(e.data.payload.dialects ?? [])
+    }
+  }, [])
+
+  const sizesCodeEditorAndInspector = useMemo(
+    () => getSizesCodeEditorAndInspector(),
+    [tab],
+  )
+
+  const sizesCodeEditorAndPreview = useMemo(
+    () => getSizesCodeEditorAndPreview(),
+    [tab, previewConsole, previewTable],
+  )
+
+  useEffect(() => {
+    engine.postMessage({
+      topic: 'dialects',
+    })
+
+    engine.addEventListener('message', handleEngineWorkerMessage)
+
+    return () => {
+      engine.removeEventListener('message', handleEngineWorkerMessage)
+    }
+  }, [])
+
+  useEffect(() => {
+    files.forEach(file => {
+      if (storedTabsIds.includes(file.id)) {
+        addTab(createTab(file))
+      }
+    })
+  }, [files])
+
+  useEffect(() => {
+    setPreviewQuery(undefined)
+    setPreviewTable(undefined)
+    setPreviewConsole(undefined)
+  }, [tab])
+
+  useEffect(() => {
+    if (selectedFile == null || tab.file === selectedFile) return
+
+    selectTab(createTab(selectedFile))
+  }, [selectedFile])
+
+  function getSizesCodeEditorAndPreview(): [number, number] {
+    const showPreview =
+      (tab.file.isSQLMeshModel || tab.file.isLocal) &&
+      [previewTable, previewConsole].some(Boolean)
+
+    return showPreview ? [80, 20] : [100, 0]
+  }
+
+  function getSizesCodeEditorAndInspector(): [number, number] {
+    const showInspector =
+      (tab.file.isSQLMeshModel || tab.file.isLocal) &&
+      isFalse(isStringEmptyOrNil(tab.file.content))
+
+    return showInspector ? [75, 25] : [100, 0]
+  }
+
+  return (
+    <SplitPane
+      sizes={sizesCodeEditorAndPreview}
+      direction="vertical"
+      minSize={0}
+      snapOffset={0}
+    >
+      <div className="flex flex-col overflow-hidden">
+        {isReadyEngine && (
+          <>
+            <EditorTabs />
+            <Divider />
+            <div className="flex flex-col h-full overflow-hidden">
+              <SplitPane
+                className="flex h-full"
+                sizes={sizesCodeEditorAndInspector}
+                minSize={0}
+                snapOffset={0}
+              >
+                <div className="flex flex-col h-full">
+                  <CodeEditor />
+                </div>
+                <div className="flex flex-col h-full">
+                  <EditorInspector />
+                </div>
+              </SplitPane>
+            </div>
+            <Divider />
+            <div className="px-2 flex justify-between items-center min-h-[2rem]">
+              <EditorFooter />
+            </div>
+          </>
+        )}
+      </div>
+      <div className="w-full">
+        <EditorPreview />
+      </div>
+    </SplitPane>
+  )
+}
