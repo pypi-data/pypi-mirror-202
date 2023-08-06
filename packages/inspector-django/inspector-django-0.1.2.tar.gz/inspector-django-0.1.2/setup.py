cc6c7cb293c8e0484e1df31c9408895c45bbb83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:35 +0000

    Add package inspector-django/0.1.2/inspector-django-0.1.2.tar.gz

---
+++ packages/inspector-django/inspector-django-0.1.2.tar.gz/inspector-django-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='inspector-django',
+    version='0.1.2',
+    description='Real-time Code Execution Monitoring of your Django applications.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Inspector',
+    author_email='support@inspector.dev',
+    url='https://inspector.dev/',
+    install_requires=[
+        'Django>=3',
+        'inspector-python'
+    ],
+    project_urls={
+        'Documentation': 'https://docs.inspector.dev/guides/django/',
+        'Source Code': 'https://github.com/inspector-apm/inspector-django',
+        'Issue Tracker': 'https://github.com/inspector-apm/inspector-django/issues',
+    },
+    extras_require=dict(tests=['pytest']),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    zip_safe=False,
+    license='MIT'
+)
