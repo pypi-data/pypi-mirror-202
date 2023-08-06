fcec134618201f852230bece72e52b88ed02161d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:26 +0000

    Add package pycallrail/0.1.0a1/pycallrail-0.1.0a1.tar.gz

---
+++ packages/pycallrail/pycallrail-0.1.0a1.tar.gz/pycallrail-0.1.0a1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='pycallrail',
+    version='0.1.0a1',
+    author = 'Khari Gardner',
+    author_email = 'khgardner@proton.me',
+    description= "Unofficial Python wrapper for the CallRail API",
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/kharigardner/pycallrail',
+    packages = find_packages(),
+    install_requires = [
+        'ujson',
+        'aiohttp',
+        'requests'
+    ]
+)
