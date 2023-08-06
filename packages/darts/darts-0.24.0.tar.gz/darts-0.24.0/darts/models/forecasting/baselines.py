9fcaec2be45bf923f1fb00701380510cd1e38753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:48 +0000

    Add package darts/0.24.0/darts-0.24.0.tar.gz

---
+++ packages/darts/darts-0.24.0.tar.gz/darts-0.24.0/darts/models/forecasting/baselines.py
@@ -0,0 +1,212 @@
+"""
+Baseline Models
+---------------
+
+A collection of simple benchmark models for univariate series.
+"""
+
+from typing import List, Optional, Sequence, Union
+
+import numpy as np
+
+from darts.logging import get_logger, raise_if_not
+from darts.models.forecasting.ensemble_model import EnsembleModel
+from darts.models.forecasting.forecasting_model import (
+    GlobalForecastingModel,
+    LocalForecastingModel,
+)
+from darts.timeseries import TimeSeries
+
+logger = get_logger(__name__)
+
+
+class NaiveMean(LocalForecastingModel):
+    def __init__(self):
+        """Naive Mean Model
+
+        This model has no parameter, and always predicts the
+        mean value of the training series.
+        """
+        super().__init__()
+        self.mean_val = None
+
+    def fit(self, series: TimeSeries):
+        super().fit(series)
+
+        self.mean_val = np.mean(series.values(copy=False), axis=0)
+        return self
+
+    def predict(self, n: int, num_samples: int = 1, verbose: bool = False):
+        super().predict(n, num_samples)
+        forecast = np.tile(self.mean_val, (n, 1))
+        return self._build_forecast_series(forecast)
+
+
+class NaiveSeasonal(LocalForecastingModel):
+    def __init__(self, K: int = 1):
+        """Naive Seasonal Model
+
+        This model always predicts the value of `K` time steps ago.
+        When `K=1`, this model predicts the last value of the training set.
+        When `K>1`, it repeats the last `K` values of the training set.
+
+        Parameters
+        ----------
+        K
+            the number of last time steps of the training set to repeat
+        """
+        super().__init__()
+        self.last_k_vals = None
+        self.K = K
+
+    @property
+    def min_train_series_length(self):
+        return max(self.K, 3)
+
+    def fit(self, series: TimeSeries):
+        super().fit(series)
+
+        raise_if_not(
+            len(series) >= self.K,
+            f"The time series requires at least K={self.K} points",
+            logger,
+        )
+        self.last_k_vals = series.values(copy=False)[-self.K :, :]
+        return self
+
+    def predict(self, n: int, num_samples: int = 1, verbose: bool = False):
+        super().predict(n, num_samples)
+        forecast = np.array([self.last_k_vals[i % self.K, :] for i in range(n)])
+        return self._build_forecast_series(forecast)
+
+
+class NaiveDrift(LocalForecastingModel):
+    def __init__(self):
+        """Naive Drift Model
+
+        This model fits a line between the first and last point of the training series,
+        and extends it in the future. For a training series of length :math:`T`, we have:
+
+        .. math:: \\hat{y}_{T+h} = y_T + h\\left( \\frac{y_T - y_1}{T - 1} \\right)
+        """
+        super().__init__()
+
+    def fit(self, series: TimeSeries):
+        super().fit(series)
+        assert series.n_samples == 1, "This model expects deterministic time series"
+
+        series = self.training_series
+        return self
+
+    def predict(self, n: int, num_samples: int = 1, verbose: bool = False):
+        super().predict(n, num_samples)
+        first, last = (
+            self.training_series.first_values(),
+            self.training_series.last_values(),
+        )
+        slope = (last - first) / (len(self.training_series) - 1)
+        last_value = last + slope * n
+        forecast = np.linspace(last, last_value, num=n + 1)[1:]
+        return self._build_forecast_series(forecast)
+
+
+class NaiveMovingAverage(LocalForecastingModel):
+    def __init__(self, input_chunk_length: int = 1):
+        """Naive Moving Average Model
+
+        This model forecasts using an auto-regressive moving average (ARMA).
+
+        Parameters
+        ----------
+        input_chunk_length
+            The size of the sliding window used to calculate the moving average
+        """
+        super().__init__()
+        self.input_chunk_length = input_chunk_length
+        self.rolling_window = None
+
+    @property
+    def min_train_series_length(self):
+        return self.input_chunk_length
+
+    def __str__(self):
+        return f"NaiveMovingAverage({self.input_chunk_length})"
+
+    def fit(self, series: TimeSeries):
+        super().fit(series)
+        raise_if_not(
+            series.is_deterministic,
+            "This model expects deterministic time series",
+            logger,
+        )
+
+        self.rolling_window = series[-self.input_chunk_length :].values(copy=False)
+        return self
+
+    def predict(self, n: int, num_samples: int = 1, verbose: bool = False):
+        super().predict(n, num_samples)
+
+        predictions_with_observations = np.concatenate(
+            (self.rolling_window, np.zeros(shape=(n, self.rolling_window.shape[1]))),
+            axis=0,
+        )
+        rolling_sum = sum(self.rolling_window)
+
+        chunk_length = self.input_chunk_length
+        for i in range(chunk_length, chunk_length + n):
+            prediction = rolling_sum / chunk_length
+            predictions_with_observations[i] = prediction
+            lost_value = predictions_with_observations[i - chunk_length]
+            rolling_sum += prediction - lost_value
+        return self._build_forecast_series(predictions_with_observations[-n:])
+
+
+class NaiveEnsembleModel(EnsembleModel):
+    def __init__(
+        self, models: Union[List[LocalForecastingModel], List[GlobalForecastingModel]]
+    ):
+        """Naive combination model
+
+        Naive implementation of `EnsembleModel`
+        Returns the average of all predictions of the constituent models
+        """
+        super().__init__(models)
+
+    def fit(
+        self,
+        series: Union[TimeSeries, Sequence[TimeSeries]],
+        past_covariates: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
+        future_covariates: Optional[Union[TimeSeries, Sequence[TimeSeries]]] = None,
+    ):
+        super().fit(
+            series=series,
+            past_covariates=past_covariates,
+            future_covariates=future_covariates,
+        )
+        for model in self.models:
+            if self.is_global_ensemble:
+                kwargs = dict(series=series)
+                if model.supports_past_covariates:
+                    kwargs["past_covariates"] = past_covariates
+                if model.supports_future_covariates:
+                    kwargs["future_covariates"] = future_covariates
+                model.fit(**kwargs)
+            else:
+                model.fit(series=series)
+
+        return self
+
+    def ensemble(
+        self,
+        predictions: Union[TimeSeries, Sequence[TimeSeries]],
+        series: Optional[Sequence[TimeSeries]] = None,
+    ) -> Union[TimeSeries, Sequence[TimeSeries]]:
+        def take_average(prediction: TimeSeries) -> TimeSeries:
+            series = prediction.pd_dataframe(copy=False).sum(axis=1) / len(self.models)
+            series.name = prediction.components[0]
+            return TimeSeries.from_series(series)
+
+        if isinstance(predictions, Sequence):
+            return [take_average(p) for p in predictions]
+        else:
+            return take_average(predictions)
---
+++ packages/darts/darts-0.24.0.tar.gz/darts-0.24.0/darts/tests/models/forecasting/test_ensemble_models.py
@@ -0,0 +1,273 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+
+from darts import TimeSeries
+from darts.logging import get_logger
+from darts.models import (
+    ExponentialSmoothing,
+    LinearRegressionModel,
+    NaiveDrift,
+    NaiveEnsembleModel,
+    NaiveSeasonal,
+    RegressionEnsembleModel,
+    Theta,
+)
+from darts.tests.base_test_class import DartsBaseTestClass
+from darts.utils import timeseries_generation as tg
+
+logger = get_logger(__name__)
+
+try:
+    from darts.models import NBEATSModel, RNNModel, TCNModel
+
+    TORCH_AVAILABLE = True
+except ImportError:
+    logger.warning("Torch not installed - Some ensemble models tests will be skipped.")
+    TORCH_AVAILABLE = False
+
+
+def _make_ts(start_value=0, n=100):
+    times = pd.date_range(start="1/1/2013", periods=n, freq="D")
+    pd_series = pd.Series(range(start_value, start_value + n), index=times)
+    return TimeSeries.from_series(pd_series)
+
+
+class EnsembleModelsTestCase(DartsBaseTestClass):
+    series1 = tg.sine_timeseries(value_frequency=(1 / 5), value_y_offset=10, length=50)
+    series2 = tg.linear_timeseries(length=50)
+
+    seq1 = [_make_ts(0), _make_ts(10), _make_ts(20)]
+    cov1 = [_make_ts(5), _make_ts(15), _make_ts(25)]
+
+    def test_untrained_models(self):
+        model = NaiveDrift()
+        _ = NaiveEnsembleModel([model])
+
+        # trained models should raise error
+        model.fit(self.series1)
+        with self.assertRaises(ValueError):
+            NaiveEnsembleModel([model])
+
+        # an untrained ensemble model should also give untrained underlying models
+        model_ens = NaiveEnsembleModel([NaiveDrift()])
+        model_ens.fit(self.series1)
+        assert model_ens.models[0]._fit_called
+        new_model = model_ens.untrained_model()
+        assert not new_model.models[0]._fit_called
+
+    def test_extreme_lag_inference(self):
+        ensemble = NaiveEnsembleModel([NaiveDrift()])
+        assert ensemble.extreme_lags == (
+            -3,
+            -1,
+            None,
+            None,
+            None,
+            None,
+        )  # test if default is okay
+
+        model1 = LinearRegressionModel(
+            lags=3, lags_past_covariates=[-3, -5], lags_future_covariates=[7, 8]
+        )
+        model2 = LinearRegressionModel(
+            lags=5, lags_past_covariates=6, lags_future_covariates=[6, 9]
+        )
+
+        ensemble = NaiveEnsembleModel(
+            [model1, model2]
+        )  # test if infers extreme lags is okay
+        expected = (-5, 0, -6, -1, 6, 9)
+        assert expected == ensemble.extreme_lags
+
+    def test_input_models_local_models(self):
+        with self.assertRaises(ValueError):
+            NaiveEnsembleModel([])
+        with self.assertRaises(ValueError):
+            NaiveEnsembleModel([NaiveDrift, NaiveSeasonal, Theta, ExponentialSmoothing])
+        with self.assertRaises(ValueError):
+            NaiveEnsembleModel(
+                [NaiveDrift(), NaiveSeasonal, Theta(), ExponentialSmoothing()]
+            )
+        NaiveEnsembleModel(
+            [NaiveDrift(), NaiveSeasonal(), Theta(), ExponentialSmoothing()]
+        )
+
+    def test_call_predict_local_models(self):
+        naive_ensemble = NaiveEnsembleModel([NaiveSeasonal(), Theta()])
+        with self.assertRaises(Exception):
+            naive_ensemble.predict(5)
+        naive_ensemble.fit(self.series1)
+        pred1 = naive_ensemble.predict(5)
+        assert self.series1.components == pred1.components
+
+    def test_call_backtest_naive_ensemble_local_models(self):
+        ensemble = NaiveEnsembleModel([NaiveSeasonal(5), Theta(2, 5)])
+        ensemble.fit(self.series1)
+        assert ensemble.extreme_lags == (-10, 0, None, None, None, None)
+        ensemble.backtest(self.series1)
+
+    def test_predict_ensemble_local_models(self):
+        naive = NaiveSeasonal(K=5)
+        theta = Theta()
+        naive_ensemble = NaiveEnsembleModel([naive, theta])
+        naive_ensemble.fit(self.series1 + self.series2)
+        forecast_naive_ensemble = naive_ensemble.predict(5)
+        naive.fit(self.series1 + self.series2)
+        theta.fit(self.series1 + self.series2)
+        forecast_mean = 0.5 * naive.predict(5) + 0.5 * theta.predict(5)
+
+        self.assertTrue(
+            np.array_equal(forecast_naive_ensemble.values(), forecast_mean.values())
+        )
+
+    def test_stochastic_ensemble(self):
+        model1 = LinearRegressionModel(lags=1, likelihood="quantile")
+        model2 = LinearRegressionModel(lags=2, likelihood="quantile")
+
+        naive_ensemble = NaiveEnsembleModel([model1, model2])
+        self.assertTrue(naive_ensemble._is_probabilistic())
+
+        regression_ensemble = RegressionEnsembleModel(
+            [model1, model2], regression_train_n_points=1
+        )
+        self.assertTrue(regression_ensemble._is_probabilistic())
+
+    @unittest.skipUnless(TORCH_AVAILABLE, "requires torch")
+    def test_input_models_global_models(self):
+        NaiveEnsembleModel([RNNModel(12), TCNModel(10, 2), NBEATSModel(10, 2)])
+
+    @unittest.skipUnless(TORCH_AVAILABLE, "requires torch")
+    def test_call_predict_global_models_univariate_input_no_covariates(self):
+        naive_ensemble = NaiveEnsembleModel(
+            [
+                RNNModel(12, n_epochs=1),
+                TCNModel(10, 2, n_epochs=1),
+                NBEATSModel(10, 2, n_epochs=1),
+            ]
+        )
+        with self.assertRaises(Exception):
+            naive_ensemble.predict(5)
+
+        naive_ensemble.fit(self.series1)
+        naive_ensemble.predict(5)
+
+    @unittest.skipUnless(TORCH_AVAILABLE, "requires torch")
+    def test_call_predict_global_models_multivariate_input_no_covariates(self):
+        naive_ensemble = NaiveEnsembleModel(
+            [
+                RNNModel(12, n_epochs=1),
+                TCNModel(10, 2, n_epochs=1),
+                NBEATSModel(10, 2, n_epochs=1),
+            ]
+        )
+        naive_ensemble.fit(self.seq1)
+        naive_ensemble.predict(n=5, series=self.seq1)
+
+    @unittest.skipUnless(TORCH_AVAILABLE, "requires torch")
+    def test_call_predict_global_models_multivariate_input_with_covariates(self):
+        naive_ensemble = NaiveEnsembleModel(
+            [
+                RNNModel(12, n_epochs=1),
+                TCNModel(10, 2, n_epochs=1),
+                NBEATSModel(10, 2, n_epochs=1),
+            ]
+        )
+        naive_ensemble.fit(self.seq1, self.cov1)
+        predict_series = [s[:12] for s in self.seq1]
+        predict_covariates = [c[:14] for c in self.cov1]
+        naive_ensemble.predict(
+            n=2, series=predict_series, past_covariates=predict_covariates
+        )
+
+    @unittest.skipUnless(TORCH_AVAILABLE, "requires torch")
+    def test_input_models_mixed(self):
+        with self.assertRaises(ValueError):
+            NaiveEnsembleModel([NaiveDrift(), Theta(), RNNModel(12)])
+
+    def test_fit_multivar_ts_with_local_models(self):
+        naive = NaiveEnsembleModel(
+            [NaiveDrift(), NaiveSeasonal(), Theta(), ExponentialSmoothing()]
+        )
+        with self.assertRaises(ValueError):
+            naive.fit(self.seq1)
+
+    def test_fit_univar_ts_with_covariates_for_local_models(self):
+        naive = NaiveEnsembleModel(
+            [NaiveDrift(), NaiveSeasonal(), Theta(), ExponentialSmoothing()]
+        )
+        with self.assertRaises(ValueError):
+            naive.fit(self.series1, self.series2)
+
+    def test_predict_with_target(self):
+        series_long = self.series1
+        series_short = series_long[:25]
+
+        # train with a single series
+        ensemble_model = self.get_global_ensembe_model()
+        ensemble_model.fit(series_short, past_covariates=series_long)
+        # predict after end of train series
+        preds = ensemble_model.predict(n=5, past_covariates=series_long)
+        self.assertTrue(isinstance(preds, TimeSeries))
+        # predict a new target series
+        preds = ensemble_model.predict(
+            n=5, series=series_long, past_covariates=series_long
+        )
+        self.assertTrue(isinstance(preds, TimeSeries))
+        # predict multiple target series
+        preds = ensemble_model.predict(
+            n=5, series=[series_long] * 2, past_covariates=[series_long] * 2
+        )
+        self.assertTrue(isinstance(preds, list) and len(preds) == 2)
+        # predict single target series in list
+        preds = ensemble_model.predict(
+            n=5, series=[series_long], past_covariates=[series_long]
+        )
+        self.assertTrue(isinstance(preds, list) and len(preds) == 1)
+
+        # train with multiple series
+        ensemble_model = self.get_global_ensembe_model()
+        ensemble_model.fit([series_short] * 2, past_covariates=[series_long] * 2)
+        with self.assertRaises(ValueError):
+            # predict without passing series should raise an error
+            ensemble_model.predict(n=5, past_covariates=series_long)
+        # predict a new target series
+        preds = ensemble_model.predict(
+            n=5, series=series_long, past_covariates=series_long
+        )
+        self.assertTrue(isinstance(preds, TimeSeries))
+        # predict multiple target series
+        preds = ensemble_model.predict(
+            n=5, series=[series_long] * 2, past_covariates=[series_long] * 2
+        )
+        self.assertTrue(isinstance(preds, list) and len(preds) == 2)
+        # predict single target series in list
+        preds = ensemble_model.predict(
+            n=5, series=[series_long], past_covariates=[series_long]
+        )
+        self.assertTrue(isinstance(preds, list) and len(preds) == 1)
+
+    @staticmethod
+    def get_global_ensembe_model(output_chunk_length=5):
+        lags = [-1, -2, -5]
+        return NaiveEnsembleModel(
+            models=[
+                LinearRegressionModel(
+                    lags=lags,
+                    lags_past_covariates=lags,
+                    output_chunk_length=output_chunk_length,
+                ),
+                LinearRegressionModel(
+                    lags=lags,
+                    lags_past_covariates=lags,
+                    output_chunk_length=output_chunk_length,
+                ),
+            ],
+        )
+
+
+if __name__ == "__main__":
+    import unittest
+
+    unittest.main()
