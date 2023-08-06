2e8dcd480eb0f2628caddd1cff94a2fffda2fb81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:33 +0000

    Add package datayoga-core/1.79.0/datayoga_core-1.79.0-py3-none-any.whl

---
+++ packages/datayoga-core/datayoga_core-1.79.0-py3-none-any.whl/datayoga_core/result.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass, field
+from enum import Enum, unique
+from typing import Any, Dict, List, Optional
+
+@unique
+class Status(str, Enum):
+    SUCCESS = "SUCCESS"
+    REJECTED = "REJECTED"
+    FILTERED = "FILTERED"
+
+
+@dataclass
+class Result:
+    status: Status
+    payload: Optional[Dict[str, Any]] = None
+    message: Optional[str] = None
+
+
+@dataclass
+class BlockResult:
+    processed: List[Result] = field(default_factory=list)
+    filtered: List[Result] = field(default_factory=list)
+    rejected: List[Result] = field(default_factory=list)
+
+    # allow unpacking
+    def __iter__(self):
+        return iter((self.processed, self.filtered, self.rejected))
+
+
+@dataclass
+class JobResult(BlockResult):
+    # FFU, separate type for JobResult
+    pass
+
+
+SUCCESS = Result(Status.SUCCESS)
+FILTERED = Result(Status.FILTERED)
