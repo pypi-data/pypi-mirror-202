8e522645679d527602c5e90f123fff495074e58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:34 +0000

    Add package OmniBridge/0.1.44/OmniBridge-0.1.44.tar.gz

---
+++ packages/OmniBridge/OmniBridge-0.1.44.tar.gz/OmniBridge-0.1.44/omnibridge/wrappers/models_configurations/base_config.py
@@ -0,0 +1,17 @@
+from typing import Any
+from pathlib import Path
+import json
+from abc import ABC, abstractmethod
+from .config_types import ConfigTypes
+
+
+class BaseConfiguration(ABC):
+    def __init__(self, api_key: str) -> None:
+        self.api_key = api_key
+
+    def to_dict(self) -> dict[str, Any]:
+        return self.__dict__
+
+    @abstractmethod
+    def _get_config_type(self) -> ConfigTypes:
+        pass
