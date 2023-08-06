b81704e43bce48af13e5f729f98c4a048b9861c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:47 +0000

    Add package contourplots/0.2.0/contourplots-0.2.0-py3-none-any.whl

---
+++ packages/contourplots/contourplots-0.2.0-py3-none-any.whl/contourplots/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# ContourPlots: A toolkit for generating multi-dimensional plots easily, usefully, and legibly.
+# Copyright (C) 2022-2023, Sarang Bhagwat <sarang.bhagwat.git@gmail.com>
+# 
+# This module is under the MIT open-source license. See 
+# https://github.com/sarangbhagwat/contourplots/blob/main/LICENSE
+# for license details.
+
+__version__ = '0.2.0'
+__author__ = 'Sarang S. Bhagwat'
+
+# %% Initialize ContourPlots 
+
+from . import utils
+
+animated_contourplot = utils.animated_contourplot
+box_and_whiskers_plot = utils.box_and_whiskers_plot
+stacked_bar_plot = utils.stacked_bar_plot
+
+__all__ = (
+    'utils', 
+    'animated_contourplot', 
+    'box_and_whiskers_plot', 
+    'stacked_bar_plot',
+)
