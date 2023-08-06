16baf4605de21fca6372d86d8297be87d27d54ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:19 +0000

    Add package tansy/0.6.0/tansy-0.6.0.tar.gz

---
+++ packages/tansy/tansy-0.6.0.tar.gz/tansy-0.6.0/tansy/__init__.py
@@ -0,0 +1,30 @@
+"""
+Tansy
+Unstable experiments with interactions.py.
+:copyright: (c) 2022-present AstreaTSS
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.6.0"
+
+
+from .class_slash import *
+from .slash_commands import *
+from .slash_param import *
+
+__all__ = (
+    "TansySlashCommandParameter",
+    "TansySlashCommand",
+    "SlashCommand",
+    "tansy_slash_command",
+    "slash_command",
+    "tansy_subcommand",
+    "subcommand",
+    "ParamInfo",
+    "Param",
+    "Option",
+    "ClassSlashCommand",
+    "class_slash_command",
+    "class_subcommand",
+    "describe",
+)
