5734bd318b434677c8e21c776a31763837bbed5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:16 +0000

    Add package bkapi-plugins-py/0.1/bkapi_plugins_py-0.1.tar.gz

---
+++ packages/bkapi-plugins-py/bkapi_plugins_py-0.1.tar.gz/bkapi_plugins_py-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_namespace_packages
+
+
+readme = ''
+
+setup(
+    description='',
+    long_description=readme,
+    name='bkapi_plugins_py',
+    version='0.1',
+    # python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
+    author='fadewalk',
+    license='Apach License 2.0',
+    # packages=find_packages(),
+    # namespace_packages=['bkapi_plugins'],
+    package_dir={'': '.'},
+    include_package_data=True,
+    package_data={
+        'bkapi_plugins':['data.zip'],
+    },
+    #  packages= find_namespace_packages(
+    #             include=["bkapi_plugins", "bkapi_plugins.*"], ),
+    # install_requires=[
+    #     'bkapi-client-core>=1.1.0,<2.0.0',
+    # ],
+)
