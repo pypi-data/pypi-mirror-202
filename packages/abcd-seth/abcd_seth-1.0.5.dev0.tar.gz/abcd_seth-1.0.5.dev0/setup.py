8ac3947e6c805f85235710331e4f7ccae8652368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:50 +0000

    Add package abcd-seth/1.0.5.dev0/abcd_seth-1.0.5.dev0.tar.gz

---
+++ packages/abcd-seth/abcd_seth-1.0.5.dev0.tar.gz/abcd_seth-1.0.5.dev0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='abcd_seth',
+    version='1.0.5-dev',
+    description='Shared models for microservices',
+    author='Daniyar Auezkhan',
+    author_email='nurovich14@gmail.com',
+    packages=find_packages(exclude=('tests',)),
+    install_requires=[
+
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
