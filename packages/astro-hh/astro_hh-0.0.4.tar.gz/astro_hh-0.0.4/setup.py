235f7b488fad106d1302dee5b6f4e31a3d2e2110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:00 +0000

    Add package astro-hh/0.0.4/astro_hh-0.0.4.tar.gz

---
+++ packages/astro-hh/astro_hh-0.0.4.tar.gz/astro_hh-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="astro_hh",
+  version="0.0.4",
+  author="HuangHao",
+  author_email="ironlionwg@outlook.com",
+  description="A small example package",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/pypa/sampleproject",
+  packages=find_packages(".\\"),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
