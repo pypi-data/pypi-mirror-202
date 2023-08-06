eb0d33b70dab8fc82e01f730f5349cf4f42997ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:54 +0000

    Add package bingbong/0.1.5/bingbong-0.1.5-py3-none-any.whl

---
+++ packages/bingbong/bingbong-0.1.5-py3-none-any.whl/pingpong/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.5'
+
+from .pingpong import PingPong
---
+++ packages/bingbong/bingbong-0.1.5-py3-none-any.whl/pingpong/pingpong.py
@@ -0,0 +1,36 @@
+class PingPong:
+  def __init__(self, ping, pong):
+    self.ping = ping
+    self.pong = pong
+
+class PromptFmt:
+  @classmethod
+  def ctx(cls, context):
+    pass
+
+  @classmethod
+  def prompt(cls, pingpong):
+    pass
+
+class UIFmt:
+  @classmethod
+  def ui(cls, pingpong):
+    pass
+
+class PPManager:
+  def __init__(self, ctx: str=""):
+    self.ctx = ctx
+    self.pingpongs = []
+
+  def add_pingpong(self, pingpong):
+    self.pingpongs.append(pingpong)
+
+  def pop_pingpong(self):
+    return self.pingpongs.pop()
+
+  def build_prompts(self, from_idx: int, to_idx: int, fmt: PromptFmt):
+    pass
+
+  def build_uis(self, from_idx: int, to_idx: int, fmt: UIFmt):
+    pass
+    
