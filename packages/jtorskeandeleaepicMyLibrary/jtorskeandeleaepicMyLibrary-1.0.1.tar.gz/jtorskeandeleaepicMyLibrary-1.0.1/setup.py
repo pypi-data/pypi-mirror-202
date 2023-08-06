60f658b406094b0210632398cde795beb8e46293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:15 +0000

    Add package jtorskeandeleaepicMyLibrary/1.0.1/jtorskeandeleaepicMyLibrary-1.0.1.tar.gz

---
+++ packages/jtorskeandeleaepicMyLibrary/jtorskeandeleaepicMyLibrary-1.0.1.tar.gz/jtorskeandeleaepicMyLibrary-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jtorskeandeleaepicMyLibrary',
+    version='1.0.1',
+    description='myLibrary',
+    author='Jordan Torske',
+    author_email='jordan.torske@ucalgary.ca',
+    packages=find_packages(),
+    install_requires=[],
+)
