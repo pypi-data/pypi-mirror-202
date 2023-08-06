d3cda4e7010fc0ef9ec4a13cd38dc8f638dbab13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:18 +0000

    Add package Group45/1.0/Group45-1.0.tar.gz

---
+++ packages/Group45/Group45-1.0.tar.gz/Group45-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import setuptools
+setup(name='Group45',
+      version='1.0',
+      description='ENSF 338 Final Project',
+      author='Aser-Ghobara',
+      author_email ='aser.ghobara@ucalgary.ca',
+      packages=setuptools.find_packages(),
+      install_requires=[],
+      )
