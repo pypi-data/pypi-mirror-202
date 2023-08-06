7d4c4f932dc1b3c158a398dc36c5bc25b28ed834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:07 +0000

    Add package poppt/0.0.5/poppt-0.0.5.tar.gz

---
+++ packages/poppt/poppt-0.0.5.tar.gz/poppt-0.0.5/tests/test_ppt.py
@@ -0,0 +1,15 @@
+import unittest
+
+from poppt.api.ppt import *
+
+
+class TestPPT(unittest.TestCase):
+    def test_ppt2pdf(self):
+        ppt2pdf(path=r'./ppt/', output_path=r'./ppt/test_pdf')
+
+    # def test_ppt2img(self):
+    #     ppt2img(intput_path=r'D:\test\py310\ppt_test')
+    def test_single_ppt2imgg(self):
+        ppt2img(intput_path=r'D:\test\py310\ppt_test\代码之外，程序员能力提升必备的8个软件（刘兆锋）.pptx',
+                output_path=r'D:\test\py310\ppt_test',
+                img_type='jpg')
