05b4622dfcf4b7b43cf5d41e19edfc8c64c82477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:05 +0000

    Add package astyle-py/0.9.1/astyle_py-0.9.1.tar.gz

---
+++ packages/astyle-py/astyle_py-0.9.1.tar.gz/astyle_py-0.9.1/astyle_py/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2022 Ivan Grokhotkov <ivan@igrr.me>
+# SPDX-License-Identifier: MIT
+from .astyle_wrapper import Astyle
+
+__all__ = [
+    'Astyle',
+]
+__version__ = '0.9.1'
