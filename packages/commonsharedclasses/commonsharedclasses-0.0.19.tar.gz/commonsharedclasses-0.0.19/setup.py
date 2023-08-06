094492dead8507f6897b5e6d5b21d07dae1ced36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:56 +0000

    Add package commonsharedclasses/0.0.19/commonsharedclasses-0.0.19.tar.gz

---
+++ packages/commonsharedclasses/commonsharedclasses-0.0.19.tar.gz/commonsharedclasses-0.0.19/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='commonsharedclasses',
+    version='0.0.19',
+    description='This package will contain all common classes, version 0.0.19 updates:- Updated service principal access provider class',
+    author='Abhilash Sharma',
+    packages=['commonsharedclasses'],
+    zip_safe=False
+)
