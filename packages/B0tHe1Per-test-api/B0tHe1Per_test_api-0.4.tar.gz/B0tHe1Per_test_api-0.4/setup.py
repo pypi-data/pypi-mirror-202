533a415f79a84f5487c0f2af8ffdd6bd0316a913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:07 +0000

    Add package B0tHe1Per-test-api/0.4/B0tHe1Per_test_api-0.4.tar.gz

---
+++ packages/B0tHe1Per-test-api/B0tHe1Per_test_api-0.4.tar.gz/B0tHe1Per_test_api-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="B0tHe1Per_test_api",
+    version="0.4",
+    author="Serj",
+    author_email="buda_serj@yahoo.com",
+    description="Test API for Bot Helper project",
+    url='https://github.com/serjbuda/B0tHe1Per.git',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'bot_helper = B0tHe1Per_test_api.bot_interface:main'
+        ]
+    },
+    package_data={'B0tHe1Per_test_api': ['file_sorter.py', 'notes_classes.py']},
+    include_package_data=True
+)
