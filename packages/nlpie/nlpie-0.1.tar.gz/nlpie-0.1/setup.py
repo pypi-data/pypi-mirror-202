11d9eb9790426c95bdd3875dc37653c559dcd850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:09 +0000

    Add package nlpie/0.1/nlpie-0.1.tar.gz

---
+++ packages/nlpie/nlpie-0.1.tar.gz/nlpie-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nlpie',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # add required packages here
+    ],
+    author='NLPie Research',
+    author_email='info@nlpie.com',
+    description='A short description of your package.',
+    url='https://github.com/nlpie-research/nlpie',
+)
