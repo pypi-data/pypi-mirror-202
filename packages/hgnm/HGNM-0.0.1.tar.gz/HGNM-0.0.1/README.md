c42c761135b20ec79f815ba9e9e0e685b362a874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:43 +0000

    Add package hgnm/0.0.1/HGNM-0.0.1.tar.gz

---
+++ packages/hgnm/HGNM-0.0.1.tar.gz/HGNM-0.0.1/README.md
@@ -0,0 +1,6 @@
+Version 0.1.0
+
+
+# HGNM
+Pypi library for DAVIS - ALR - KIT 
+
---
+++ packages/hgnm/HGNM-0.0.1.tar.gz/HGNM-0.0.1/src/HGNM/__init__.py
@@ -0,0 +1,20 @@
+# # make import easier 
+
+
+# # abstract modules
+# from src.HGNM.modules.abstract.AbstractInputEmbedding import AbstractInputEmbedding as AbInputEmbedding
+# from src.HGNM.modules.abstract.AbstractMessagePassingBase import AbstractMessagePassingBase as AbMessagePassingBase
+# from src.HGNM.modules.abstract.AbstractStack import AbstractStack as AbStack
+# from src.HGNM.modules.abstract.AbstractGraphAssertions import AbstractGraphAssertions as AbGraphAssertions
+
+# # heterogeneous modules
+# from src.HGNM.modules.heterogeneous.HeterogeneousInputEmbedding import HeterogeneousInputEmbedding as HeteroInputEmbedding
+# from src.HGNM.modules.heterogeneous.HeterogeneousMessagePassingBase import HeterogeneousMessagePassingBase as HeteroMessagePassingBase
+# from src.HGNM.modules.heterogeneous.HeterogeneousStack import HeterogeneousStack as HeteroStack
+# from src.HGNM.modules.heterogeneous.HeterogeneousGraphAssertions import HeterogeneousGraphAssertions as HeteroGraphAssertions
+
+# # homogeneous modules
+# from src.HGNM.modules.homogeneous.HomogeneousInputEmbedding import HomogeneousInputEmbedding as HomoInputEmbedding
+# from src.HGNM.modules.homogeneous.HomogeneousMessagePassingBase import HomogeneousMessagePassingBase as HomoMessagePassingBase
+# from src.HGNM.modules.homogeneous.HomogeneousStack import HomogeneousStack as HomoStack
+# from src.HGNM.modules.homogeneous.HomogeneousGraphAssertions import HomogeneousGraphAssertions as HomoGraphAssertions
