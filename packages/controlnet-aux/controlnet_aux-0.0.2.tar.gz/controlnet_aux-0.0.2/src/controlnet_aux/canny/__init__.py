2d2b76c12e25f75393274c36e5ba03c05a6cf32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:45 +0000

    Add package controlnet-aux/0.0.2/controlnet_aux-0.0.2.tar.gz

---
+++ packages/controlnet-aux/controlnet_aux-0.0.2.tar.gz/controlnet_aux-0.0.2/src/controlnet_aux/canny/__init__.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+from PIL import Image
+from ..util import HWC3
+
+class CannyDetector:
+    def __call__(self, img, low_threshold, high_threshold):
+        
+        input_type = "np"
+        if isinstance(img, Image.Image):
+            img = np.array(img)
+            input_type = "pil"
+        
+        img = HWC3(img)
+        img = cv2.Canny(img, low_threshold, high_threshold)
+        
+        if input_type == "pil":
+            img = Image.fromarray(img)
+            
+        return img
