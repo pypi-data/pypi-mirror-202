a675b90b9cf767c064d167620a245b231bb77e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:04 +0000

    Add package thread-bySean/0.1.0/thread_bySean-0.1.0.tar.gz

---
+++ packages/thread-bySean/thread_bySean-0.1.0.tar.gz/thread_bySean-0.1.0/setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="thread_bySean",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Sean Wu",
+    author_email="urdnorn@gmail.com",
+    description="A sample package for multi-thread programming",
+    url="https://github.com/yourusername/thread_bySean",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+)
