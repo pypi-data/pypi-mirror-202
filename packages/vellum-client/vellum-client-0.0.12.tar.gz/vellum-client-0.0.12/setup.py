0fe8f2170f4b0bf905174422381450c9c011275d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:16 +0000

    Add package vellum-client/0.0.12/vellum-client-0.0.12.tar.gz

---
+++ packages/vellum-client/vellum-client-0.0.12.tar.gz/vellum-client-0.0.12/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+VERSION = "0.0.12"
+
+setup(
+    name="vellum-client",
+    version=VERSION,
+    description="Vellum Python client",
+    url="http://github.com/vocify/vellum-client-python",
+    author="vellum.ai",
+    author_email="devops@vellum.ai",
+    packages=find_packages(where="."),
+    package_data={
+        "vellum": ["py.typed"],
+    },
+    install_requires=[
+        "dacite>=1.8.0",
+        "requests>=2.20.0",
+    ],
+)
