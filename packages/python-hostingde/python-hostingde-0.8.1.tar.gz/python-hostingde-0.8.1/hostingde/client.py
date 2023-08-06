9682f04422b83f8f55e6fb8ee111c7d78bdd412d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:39 +0000

    Add package python-hostingde/0.8.1/python-hostingde-0.8.1.tar.gz

---
+++ packages/python-hostingde/python-hostingde-0.8.1.tar.gz/python-hostingde-0.8.1/hostingde/client.py
@@ -0,0 +1,18 @@
+from hostingde.account.account import AccountClient
+from hostingde.dns.dns import DnsClient
+from hostingde.domain.domain import DomainClient
+from hostingde.hostingde import HostingDeCore
+
+
+class HostingDeClient(HostingDeCore):
+    """
+    The main client.
+
+    Separates the different problem domains by building clients in this class attributes.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.dns: DnsClient = DnsClient(self)
+        self.domain: DomainClient = DomainClient(self)
+        self.account: AccountClient = AccountClient(self)
