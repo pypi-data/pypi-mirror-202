dacc0b96b78222d49af790e0d44199101349239e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:39 +0000

    Add package arclet-alconna/1.7.0rc2/arclet_alconna-1.7.0rc2-py3-none-any.whl

---
+++ packages/arclet-alconna/arclet_alconna-1.7.0rc2-py3-none-any.whl/arclet/alconna/__init__.py
@@ -0,0 +1,26 @@
+"""Alconna 概览"""
+
+from nepattern import AllParam as AllParam, AnyOne as AnyOne  # noqa
+from tarina import Empty as Empty # noqa
+from .typing import MultiVar, KeyWordVar, Kw, Nargs
+from .args import Args, Field, ArgFlag, Arg
+from .base import CommandNode, Option, Subcommand
+from .completion import CompInterface
+from .exceptions import ParamsUnmatched, NullMessage, InvalidParam
+from .analyser import Analyser
+from .container import DataCollectionContainer
+from .core import Alconna, CommandMeta
+from .arparma import Arparma, ArparmaBehavior
+from .manager import command_manager, ShortcutArgs
+from .config import config, namespace, Namespace
+from .lang import load_lang_file
+
+from .builtin import store_value, set_default, store_true, store_false
+from .output import output_manager
+from .formatter import TextFormatter
+from .duplication import Duplication
+from .stub import ArgsStub, OptionStub, SubcommandStub
+
+__version__ = "1.7.0rc2"
+
+Arpamar = Arparma
