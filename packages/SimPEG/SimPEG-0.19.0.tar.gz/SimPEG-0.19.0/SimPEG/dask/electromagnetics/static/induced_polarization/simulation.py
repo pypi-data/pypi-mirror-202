b22bc8e03963eb14453ed1d0775e7fe5ec20b98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:49 +0000

    Add package SimPEG/0.19.0/SimPEG-0.19.0.tar.gz

---
+++ packages/SimPEG/SimPEG-0.19.0.tar.gz/SimPEG-0.19.0/SimPEG/dask/electromagnetics/static/induced_polarization/simulation.py
@@ -0,0 +1,24 @@
+from .....electromagnetics.static.induced_polarization.simulation import (
+    BaseIPSimulation as Sim,
+)
+
+import dask.array as da
+
+
+def dask_getJtJdiag(self, m, W=None, f=None):
+    """
+    Return the diagonal of JtJ
+    """
+    if getattr(self, "_gtgdiag", None) is None:
+        # Need to check if multiplying weights makes sense
+        if W is None:
+            W = self._scale
+        else:
+            W = self._scale * W.diagonal()
+        w = da.from_array(W)[:, None]
+        self._gtgdiag = da.sum((w * self.getJ(m, f=f)) ** 2, axis=0).compute()
+
+    return self._gtgdiag
+
+
+Sim.getJtJdiag = dask_getJtJdiag
