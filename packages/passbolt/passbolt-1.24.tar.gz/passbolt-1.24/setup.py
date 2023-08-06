e13af6bf6e407281e895466b9fdda2a9feac738b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:41 +0000

    Add package passbolt/1.24/passbolt-1.24.tar.gz

---
+++ packages/passbolt/passbolt-1.24.tar.gz/passbolt-1.24/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+def get_long_description():
+    with open(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'readme.md'
+    ), encoding='utf8') as fp:
+        return fp.read()
+
+setup(
+        name='passbolt',
+        version='1.24',
+        description='Passbolt python module',
+        long_description=get_long_description(),
+        long_description_content_type='text/markdown',
+        author='Daniel Lynch',
+        author_email='daniel.lynch2016@gmail.com',
+        url='https://github.com/daniel-lynch/passbolt',
+        license='MIT',
+        # package_dir=["src"],
+        packages=find_packages(),
+        install_requires=["requests", "python-gnupg", "argparse"],
+        data_files=[]
+    )
