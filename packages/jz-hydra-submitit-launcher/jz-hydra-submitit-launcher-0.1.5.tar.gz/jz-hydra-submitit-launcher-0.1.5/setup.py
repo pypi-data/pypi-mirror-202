21f313a94aa12a729c80fb3705ae21731930500f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:36 +0000

    Add package jz-hydra-submitit-launcher/0.1.5/jz-hydra-submitit-launcher-0.1.5.tar.gz

---
+++ packages/jz-hydra-submitit-launcher/jz-hydra-submitit-launcher-0.1.5.tar.gz/jz-hydra-submitit-launcher-0.1.5/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# taken from https://github.com/CEA-COSMIC/ModOpt/blob/master/setup.py
+with open('requirements.txt') as open_file:
+    install_requires = open_file.read()
+
+from hydra_plugins import jz_hydra_submitit_launcher
+
+setuptools.setup(
+    name="jz-hydra-submitit-launcher",
+    version=jz_hydra_submitit_launcher.__version__,
+    author=jz_hydra_submitit_launcher.__author__,
+    author_email=jz_hydra_submitit_launcher.__author_email__,
+    description=jz_hydra_submitit_launcher.__docs__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    homepage=jz_hydra_submitit_launcher.__homepage__,
+    url="https://github.com/zaccharieramzi/jz-hydra-submitit-launcher",
+    license=jz_hydra_submitit_launcher.__license__,
+    packages=setuptools.find_namespace_packages(include=["hydra_plugins.*"]),
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    python_requires='>=3.6',
+    include_package_data=True,
+    keywords=['hydra', 'submitit', 'jean-zay'],
+    scripts=['hydra_plugins/jz_hydra_submitit_launcher/bin/hydra-submitit-launch'],
+)
