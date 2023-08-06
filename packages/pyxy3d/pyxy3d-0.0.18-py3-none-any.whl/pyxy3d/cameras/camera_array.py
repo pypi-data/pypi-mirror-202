032b30004cc6df8aef5757b255db35ebf90c3aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:53 +0000

    Add package pyxy3d/0.0.18/pyxy3d-0.0.18-py3-none-any.whl

---
+++ packages/pyxy3d/pyxy3d-0.0.18-py3-none-any.whl/pyxy3d/cameras/camera_array.py
@@ -0,0 +1,182 @@
+# %%
+
+import pyxy3d.logger
+logger = pyxy3d.logger.get(__name__)
+
+from pathlib import Path
+import numpy as np
+from dataclasses import dataclass
+import cv2
+
+
+CAMERA_PARAM_COUNT = 6
+
+@dataclass
+class CameraData:
+    """
+    A place to hold the calibration data associated with a camera that has been populated from a config file.
+    For use with final setting of the array and triangulation, but no actual camera management.
+    Not loving the way this is implemented as an adjunct to the Camera object, but here we are
+    """
+
+    port: int
+    size: tuple
+    rotation_count: int
+    error: float  # the RMSE of reprojection associated with the intrinsic calibration
+    matrix: np.ndarray
+    distortions: np.ndarray  #
+    exposure: int
+    grid_count: int
+    ignore: bool
+    verified_resolutions: np.ndarray
+    translation: np.ndarray =None # camera relative to world
+    rotation: np.ndarray = None# camera relative to world
+
+    @property
+    def transformation(self):
+        """"
+        Rotation and transformation combined to allow 
+        """
+        
+        t = np.hstack([self.rotation, np.expand_dims(self.translation, 1)])
+        t = np.vstack([t, np.array([0,0,0,1], np.float32)])
+        return t 
+   
+    @transformation.setter 
+    def transformation(self, t: np.ndarray):
+        self.rotation = t[0:3,0:3]
+        self.translation = t[0:3,3]
+        logger.info(f"Rotation and Translation being updated to {self.rotation} and {self.translation}")
+    
+    @property  
+    def projection_matrix(self):
+        return self.matrix @ self.transformation[0:3,:]
+         
+    def extrinsics_to_vector(self):
+        """
+        Converts camera parameters to a numpy vector for use with bundle adjustment.
+        """
+        # rotation of the camera relative to the world
+        rotation_rodrigues = cv2.Rodrigues(self.rotation)[0]  # elements 0,1,2
+        port_param = np.hstack([rotation_rodrigues[:, 0], self.translation])
+
+        return port_param
+
+    def extrinsics_from_vector(self, row):
+        """
+        Takes a vector in the same format that is output of `extrinsics_to_vector` and updates the camera
+        """
+
+        # convert back to world frame of reference
+        self.rotation = cv2.Rodrigues(row[0:3])[0]
+        self.translation = np.array([row[3:6]], dtype=np.float64)[0]
+
+
+@dataclass
+class CameraArray:
+    """The plan is that this will expand to become an interface for setting the origin.
+    At the moment all it is doing is holding a dictionary of CameraData objects"""
+
+    cameras: dict
+
+    # least_sq_result = None # this may be a vestige of an old way of doing things. remove if commenting out doesn't break anything.
+
+    @property
+    def port_index(self):
+        """
+        Provides a dictionary mapping the camera port to an index. Generally,this
+        will match the camera ports 1:1, but will be different when a camera 
+        is being ignored. Used to manage reference to camera parameters in xy_reprojection_error
+        used within the least_squares optimization of the capture volume. 
+        """
+        not_ignored_ports = [port for port, cam in self.cameras.items() if not cam.ignore]
+        not_ignored_ports.sort()
+        not_ignored_indices = [i for i in range(len(not_ignored_ports))]
+        port_indices = {port:i for port, i in zip(not_ignored_ports,not_ignored_indices)}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+        
+        return port_indices
+   
+    @property 
+    def index_port(self):
+        return {value: key for key, value in self.port_index.items()}
+    
+    def get_extrinsic_params(self):
+        """for each camera build the CAMERA_PARAM_COUNT element parameter index
+        camera_params with shape (n_cameras, CAMERA_PARAM_COUNT)
+        contains initial estimates of parameters for all cameras.
+        First 3 components in each row form a rotation vector (https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula),
+        next 3 components form a translation vector
+        """
+
+        camera_params = None
+        # ensure that parameters are built up in order of the corresponding index
+        for index in sorted(self.index_port.keys()):
+            port = self.index_port[index]
+            cam = self.cameras[port]
+            port_param = cam.extrinsics_to_vector()
+
+            if camera_params is None:
+                camera_params = port_param
+            else:
+                camera_params = np.vstack([camera_params, port_param])
+
+        return camera_params
+
+    def update_extrinsic_params(self, least_sq_result_x:np.array):
+
+        n_cameras = len(self.port_index)
+        n_cam_param = 6  # 6 DoF
+        flat_camera_params = least_sq_result_x[0 : n_cameras * n_cam_param]
+        new_camera_params = flat_camera_params.reshape(n_cameras, n_cam_param)
+
+        # update camera array with new positional data
+        for index in range(len(new_camera_params)):
+            port = self.index_port[index]  # correct in case ignoring a camera
+            cam_vec = new_camera_params[index, :]
+            self.cameras[port].extrinsics_from_vector(cam_vec)
+
+
+def get_camera_array(config:dict)->CameraArray:
+    """
+    Load camera array directly from config file. The results of capture volume
+    optimization and origin transformation will be reflected in this array
+    which can then be the basis for future 3d point estimation
+    """
+    all_camera_data = {}
+    for key, params in config.items():
+        if key.startswith("cam"):
+            if params["translation"] is not None:
+                port = params["port"]
+                size = params["size"]
+
+                logger.info(f"Adding camera {port} to calibrated camera array...")
+                cam_data = CameraData(
+                    port=port,
+                    size=params["size"],
+                    rotation_count=params["rotation_count"],
+                    error=params["error"],
+                    matrix=np.array(params["matrix"]),
+                    distortions=np.array(params["distortions"]),
+                    exposure=params["exposure"],
+                    grid_count=params["grid_count"],
+                    ignore=params["ignore"],
+                    verified_resolutions=params["verified_resolutions"],
+                    translation=np.array(params["translation"]),
+                    rotation=np.array(params["rotation"]),
+                )
+
+                all_camera_data[port] = cam_data
+
+    camera_array = CameraArray(all_camera_data)
+    return camera_array
+
+if __name__ == "__main__":
+    from pyxy3d.cameras.camera_array_initializer import CameraArrayInitializer  
+    from pyxy3d import __root__
+
+    session_directory = Path(__root__, "tests", "sessions", "217")
+    config_path = Path(session_directory, "config.toml")
+    array_builder = CameraArrayInitializer(config_path)
+    camera_array = array_builder.get_best_camera_array()
+
+# %%
---
+++ packages/pyxy3d/pyxy3d-0.0.18-py3-none-any.whl/pyxy3d/gui/stereoframe/stereo_frame_widget.py
@@ -0,0 +1,251 @@
+
+import pyxy3d.logger
+logger = pyxy3d.logger.get(__name__)
+
+import sys
+from pathlib import Path
+from threading import Thread, Event
+import time
+
+import cv2
+from PyQt6.QtCore import Qt, pyqtSignal, QThread
+from PyQt6.QtGui import QImage, QPixmap, QIcon
+from PyQt6.QtWidgets import (
+    QApplication,
+    QSizePolicy,
+    QWidget,
+    QSpinBox,
+    QScrollArea,
+    QComboBox,
+    QCheckBox,
+    QDialog,
+    QGroupBox,
+    QDoubleSpinBox,
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QSlider,
+    QVBoxLayout,
+)
+
+# Append main repo to top of path to allow import of backend
+from pyxy3d.session import Session
+from pyxy3d.gui.stereoframe.stereo_frame_builder import StereoFrameBuilder
+from pyxy3d.cameras.synchronizer import Synchronizer
+from pyxy3d import __root__
+from pyxy3d.gui.qt_logger import QtLogger
+from pyxy3d.gui.widgets import NavigationBarBackFinish
+
+# the boards needed to before a pair could be used to bridge pairs without common corners
+MIN_THRESHOLD_FOR_EARLY_CALIBRATE = 5
+
+
+class StereoFrameWidget(QWidget):
+    calibration_complete = pyqtSignal()
+    calibration_initiated = pyqtSignal()
+    terminate = pyqtSignal()
+     
+    def __init__(self,session:Session):
+
+        super(StereoFrameWidget, self).__init__()
+        self.session = session
+        self.synchronizer:Synchronizer = self.session.get_synchronizer()
+
+        self.create_stereoframe_tools()
+
+        self.frame_rate_spin = QSpinBox()
+        self.frame_rate_spin.setValue(self.synchronizer.get_fps_target())
+        self.board_count_spin = QSpinBox()
+        self.board_count_spin.setValue(self.frame_builder.board_count_target)
+        
+        self.stereo_frame_display = QLabel()
+        self.navigation_bar = NavigationBarBackFinish() 
+        self.calibrate_collect_btn = self.navigation_bar.calibrate_collect_btn
+
+        
+        self.place_widgets()
+        self.connect_widgets()        
+
+    def create_stereoframe_tools(self):
+
+        self.frame_builder = StereoFrameBuilder(self.synchronizer, board_count_target=30)
+        self.frame_emitter = StereoFrameEmitter(self.frame_builder)
+        self.frame_emitter.start()
+
+    def place_widgets(self):
+        self.setLayout(QVBoxLayout())
+        
+        self.settings_group = QGroupBox("Settings")
+        self.settings_group.setLayout(QHBoxLayout())
+        self.settings_group.layout().addWidget(QLabel("Frame Rate:"))
+        self.settings_group.layout().addWidget(self.frame_rate_spin)       
+        self.settings_group.layout().addWidget(QLabel("Target Board Count:"))
+        self.settings_group.layout().addWidget(self.board_count_spin)       
+
+        self.layout().addWidget(self.settings_group)
+        # self.layout().addWidget(self.calibrate_collect_btn)
+
+        # scroll bar appears to not be working....
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        # self.scroll_area.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.scroll_area)
+
+        self.scroll_area.setWidget(self.stereo_frame_display)
+       
+        self.layout().addWidget(self.navigation_bar)
+
+
+
+    def connect_widgets(self):
+        
+        self.calibrate_collect_btn.clicked.connect(self.on_calibrate_connect_click)
+        self.frame_emitter.ImageBroadcast.connect(self.ImageUpdateSlot)
+        self.frame_emitter.possible_to_initialize_array.connect(self.enable_calibration)
+        self.frame_rate_spin.valueChanged.connect(self.synchronizer.set_fps_target)
+        self.board_count_spin.valueChanged.connect(self.update_board_count_target)
+        self.frame_emitter.calibration_data_collected.connect(self.initiate_calibration)
+    
+    def update_board_count_target(self, target):
+        self.frame_builder.board_count_target = target
+        
+    def on_calibrate_connect_click(self):
+        if self.calibrate_collect_btn.text() == "Collect Data":
+            logger.info("Begin collecting calibration data")
+            # by default, data saved to session folder
+            self.frame_builder.store_points.set()
+            self.session.start_recording()
+            self.calibrate_collect_btn.setText("Terminate")
+            self.calibrate_collect_btn.setEnabled(True)
+            self.navigation_bar.back_btn.setEnabled(False)
+
+        elif self.calibrate_collect_btn.text() == "Terminate":
+            logger.info("Terminating current data collection")
+            self.terminate.emit()
+            # self.session.stop_recording()
+            # self.frame_builder.reset()
+            self.calibrate_collect_btn.setText("Collect Data")
+
+        elif self.calibrate_collect_btn.text() == "Calibrate":
+            logger.info("Prematurely end data collection")
+            self.frame_builder.store_points.clear()
+            self.initiate_calibration()
+            
+
+
+    def enable_calibration(self):
+        self.calibrate_collect_btn.setText("Calibrate")
+        self.calibrate_collect_btn.setEnabled(True)
+        
+        
+    def ImageUpdateSlot(self, q_image):
+        self.stereo_frame_display.resize(self.stereo_frame_display.sizeHint())
+
+        qpixmap = QPixmap.fromImage(q_image)
+        self.stereo_frame_display.setPixmap(qpixmap)
+        
+
+
+    def initiate_calibration(self):
+        def worker():
+            self.calibration_initiated.emit()
+            logger.info("Beginning wind-down process prior to calibration")
+            self.calibrate_collect_btn.setText("---calibrating---")
+            self.calibrate_collect_btn.setEnabled(False)
+            # self.frame_emitter.stop()
+            # self.stereo_frame_display.hide()
+            logger.info("Stop recording video")
+            self.session.stop_recording()
+            logger.info("Begin calibration")
+            logger.info("Pause synchronizer")
+            self.session.pause_synchronizer()
+            self.session.estimate_extrinsics()
+            self.navigation_bar.back_btn.setEnabled(True)
+            self.calibrate_collect_btn.setText("Collect Data")
+            self.calibrate_collect_btn.setEnabled(True)
+            self.calibration_complete.emit()
+            logger.info("Calibration Complete signal sent...")
+            
+        self.init_calibration_thread = Thread(target=worker,args=(), daemon=True)
+        self.init_calibration_thread.start()
+
+class StereoFrameEmitter(QThread):
+    ImageBroadcast = pyqtSignal(QImage)
+    calibration_data_collected = pyqtSignal() 
+    possible_to_initialize_array = pyqtSignal()
+    
+    def __init__(self, stereoframe_builder:StereoFrameBuilder):
+        
+        super(StereoFrameEmitter,self).__init__()
+        self.stereoframe_builder = stereoframe_builder
+        logger.info("Initiated frame emitter")        
+        self.keep_collecting = Event() 
+        self.collection_complete = False
+        
+    def run(self):
+
+        self.keep_collecting.set()
+        self.collection_complete = False
+
+        possible_to_initialize = False
+        
+        while self.keep_collecting.is_set():
+            
+            # that that it is important to make sure that this signal is sent only once
+            # to avoid multiple calibration attempts 
+            if len(self.stereoframe_builder.stereo_list) == 0 and not self.collection_complete:
+                logger.info("Signalling that calibration data is fully collected.")
+                self.collection_complete = True
+                self.calibration_data_collected.emit()
+        
+                # break
+            
+            if not possible_to_initialize:
+                # check to see if it is now
+                if self.stereoframe_builder.possible_to_initialize_array(MIN_THRESHOLD_FOR_EARLY_CALIBRATE):
+                    logger.info("Signaling that it is possible to initialize array based on collected data.")
+                    possible_to_initialize = True
+                    self.possible_to_initialize_array.emit()
+                      
+            stereo_frame = self.stereoframe_builder.get_stereo_frame()
+
+            if stereo_frame is not None:
+                image = cv2_to_qlabel(stereo_frame)
+                self.ImageBroadcast.emit(image)
+
+        logger.info("Stereoframe emitter run thread ended...") 
+            
+    # def stop(self):
+        # self.keep_collecting.clear() 
+
+
+        
+        
+        
+def cv2_to_qlabel(frame):
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    qt_frame = QImage(
+        image.data,
+        image.shape[1],
+        image.shape[0],
+        QImage.Format.Format_RGB888,
+    )
+    return qt_frame
+
+    
+if __name__ == "__main__":
+        App = QApplication(sys.argv)
+
+        config_path = Path(__root__, "tests", "217")
+
+        session = Session(config_path)
+        session.load_cameras()
+        session.load_streams()
+        session.adjust_resolutions()
+
+
+        stereo_dialog = StereoFrameWidget(session)
+        stereo_dialog.show()
+
+        sys.exit(App.exec())
