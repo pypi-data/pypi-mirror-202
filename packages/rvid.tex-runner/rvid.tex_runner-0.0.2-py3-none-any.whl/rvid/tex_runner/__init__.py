680a9d38e900e768327e1e0db7406e844a650cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:51 +0000

    Add package rvid.tex-runner/0.0.2/rvid.tex_runner-0.0.2-py3-none-any.whl

---
+++ packages/rvid.tex-runner/rvid.tex_runner-0.0.2-py3-none-any.whl/rvid/tex_runner/__init__.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import Sequence
+
+from .external_resources import ExternalResources, make_external_resource_library
+
+
+def make_pdf_at_path(
+    tex_content: str, ext_res_seq: Sequence[ExternalResources], target_path: Path
+) -> None:
+    from .latex import make_pdf_in_tempdir
+
+    pdf_data = make_pdf_in_tempdir(
+        tex_file_content=tex_content, external_resource_sequence=ext_res_seq
+    )
+
+    with open(target_path, "wb") as outfile:
+        outfile.write(pdf_data)
+
+
+def make_pdf_as_bytestream(
+    tex_file_content: str, external_resource_sequence: Sequence[ExternalResources]
+) -> bytes:
+    from .latex import make_pdf_in_tempdir
+
+    return make_pdf_in_tempdir(
+        tex_file_content=tex_file_content, external_resource_sequence=external_resource_sequence
+    )
+
+
+__all__ = [
+    "ExternalResources",
+    "make_external_resource_library",
+    "make_pdf_as_bytestream",
+    "make_pdf_at_path",
+]
