02104d60ed24c894dc4867fd1736c5d4a62ded74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:02 +0000

    Add package linq4py/1.0.3/linq4py-1.0.3-py3-none-any.whl

---
+++ packages/linq4py/linq4py-1.0.3-py3-none-any.whl/linq4py/__init__.py
@@ -0,0 +1,1 @@
+from .linq4py import array
---
+++ packages/linq4py/linq4py-1.0.3-py3-none-any.whl/linq4py/exceptions.py
@@ -0,0 +1,15 @@
+# exception
+class NoElementsError(Exception):
+    pass
+
+
+class NullArgumentError(Exception):
+    pass
+
+
+class NoMatchingElement(Exception):
+    pass
+
+
+class MoreThanOneMatchingElement(Exception):
+    pass
