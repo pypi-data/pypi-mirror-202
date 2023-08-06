bebd7c3fd37720a6c5bfbb45e8dd27a926e82844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:00 +0000

    Add package roof-mask/0.5.4/roof-mask-0.5.4.tar.gz

---
+++ packages/roof-mask/roof-mask-0.5.4.tar.gz/roof-mask-0.5.4/roof_test/predict.py
@@ -0,0 +1,10 @@
+# test pip install
+
+def run():
+    print('Qiang is working on it')
+    
+def mask():
+    print('Ruixu is working on it')
+    
+def help():
+    print('Delin, Aaron and Hao-wei is working on it')
