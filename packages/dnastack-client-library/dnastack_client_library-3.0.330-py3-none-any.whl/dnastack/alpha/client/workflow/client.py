c85ef1e3378958683bbcf0ce90d8bc591d788416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:43 +0000

    Add package dnastack-client-library/3.0.330/dnastack_client_library-3.0.330-py3-none-any.whl

---
+++ packages/dnastack-client-library/dnastack_client_library-3.0.330-py3-none-any.whl/dnastack/alpha/client/workflow/client.py
@@ -0,0 +1,101 @@
+from typing import List, Iterator, Optional
+from urllib.parse import urljoin
+
+from dnastack.alpha.client.workbench.base_client import BaseWorkbenchClient
+from dnastack.alpha.client.workflow.models import WorkflowDescriptor, WorkflowListResult, Workflow, WorkflowCreate, \
+    WorkflowVersionCreate, WorkflowVersion, WorkflowVersionListResult, WorkflowSource
+from dnastack.http.session import JsonPatch
+from dnastack.client.models import ServiceEndpoint
+from dnastack.client.service_registry.models import ServiceType
+
+
+class WorkflowClient(BaseWorkbenchClient):
+
+    @staticmethod
+    def get_adapter_type() -> str:
+        return 'workflow-service'
+
+    @staticmethod
+    def get_supported_service_types() -> List[ServiceType]:
+        return [
+            ServiceType(group='com.dnastack.workbench', artifact='workflow-service', version='1.0.0'),
+        ]
+
+    @classmethod
+    def make(cls, endpoint: ServiceEndpoint, namespace: str):
+        """Create this class with the given `endpoint` and `namespace`."""
+        if not endpoint.type:
+            endpoint.type = cls.get_default_service_type()
+        return cls(endpoint, namespace)
+
+    def get_json_schema(self, workflow_id: str, version_id: str) -> WorkflowDescriptor:
+        with self.create_http_session() as session:
+            response = session.get(
+                urljoin(self.endpoint.url, f'{self.namespace}/workflows/{workflow_id}/versions/{version_id}/describe'))
+        return WorkflowDescriptor(**response.json())
+
+    def list_workflows(self, source: Optional[WorkflowSource]) -> Iterator[Workflow]:
+        with self.create_http_session() as session:
+            response = session.get(
+                urljoin(self.endpoint.url, f'{self.namespace}/workflows'))
+            workflows = WorkflowListResult(**response.json()).workflows
+            if source:
+                workflows = [w for w in workflows if w.source == source]
+        return iter(workflows)
+
+    def list_workflow_versions(self, workflow_id: str) -> Iterator[WorkflowVersion]:
+        with self.create_http_session() as session:
+            response = session.get(
+                urljoin(self.endpoint.url, f'{self.namespace}/workflows/{workflow_id}/versions'))
+        return iter(WorkflowVersionListResult(**response.json()).versions)
+
+    def get_workflow(self, workflow_id: str) -> Workflow:
+        with self.create_http_session() as session:
+            response = session.get(
+                urljoin(self.endpoint.url, f'{self.namespace}/workflows/{workflow_id}'))
+            workflow = Workflow(**response.json())
+            workflow.etag = response.headers.get("Etag").strip("\"")
+            return workflow
+
+    def get_workflow_version(self, workflow_id: str, version_id: str) -> WorkflowVersion:
+        with self.create_http_session() as session:
+            response = session.get(
+                urljoin(self.endpoint.url, f'{self.namespace}/workflows/{workflow_id}/versions/{version_id}'))
+            workflow_version = WorkflowVersion(**response.json())
+            workflow_version.etag = response.headers.get("Etag").strip("\"")
+            return workflow_version
+
+    def create_workflow(self, workflow_create_request: WorkflowCreate) -> Workflow:
+        with self.create_http_session() as session:
+            response = session.post(
+                urljoin(self.endpoint.url, f'{self.namespace}/workflows'), json=workflow_create_request.dict())
+        return Workflow(**response.json())
+
+    def create_version(self, workflow_id: str,
+                       workflow_version_create_request: WorkflowVersionCreate) -> WorkflowVersion:
+        with self.create_http_session() as session:
+            response = session.post(
+                urljoin(self.endpoint.url, f'{self.namespace}/workflows/{workflow_id}/versions'),
+                json=workflow_version_create_request.dict())
+        return WorkflowVersion(**response.json())
+
+    def update_workflow(self, workflow_id: str, etag: str, updates: List[JsonPatch]) -> Workflow:
+        with self.create_http_session() as session:
+            updates = [update.dict() for update in updates]
+            response = session.json_patch(
+                urljoin(self.endpoint.url, f'{self.namespace}/workflows/{workflow_id}'),
+                headers={'If-Match': etag},
+                json=updates
+            )
+            return Workflow(**response.json())
+
+    def update_workflow_version(self, workflow_id: str, version_id: str, etag: str,
+                                updates: List[JsonPatch]) -> WorkflowVersion:
+        with self.create_http_session() as session:
+            updates = [update.dict() for update in updates]
+            response = session.json_patch(
+                urljoin(self.endpoint.url, f'{self.namespace}/workflows/{workflow_id}/versions/{version_id}'),
+                headers={'If-Match': etag},
+                json=updates
+            )
+            return WorkflowVersion(**response.json())
