e00bf070d02dde54caa2ffbdffa319f9eab9a520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:06 +0000

    Add package kevin-toolbox-dev/1.0.1.5/kevin_toolbox_dev-1.0.1.5-py3-none-any.whl

---
+++ packages/kevin-toolbox-dev/kevin_toolbox_dev-1.0.1.5-py3-none-any.whl/kevin_toolbox/patches/for_os/remove.py
@@ -0,0 +1,26 @@
+import os
+
+
+def remove(path, ignore_errors=False):
+    """
+        移除文件/文件夹
+
+        返回:
+            boolean 是否成功
+    """
+    try:
+        if os.path.isfile(path):  # 移除文件
+            os.remove(path)
+        elif os.path.isdir(path):  # 移除文件夹
+            os.removedirs(path)
+        else:
+            raise FileNotFoundError(f'path: {path} not exists')
+        return True
+    except Exception as e:  # 删除失败
+        if not ignore_errors:
+            raise Exception(e)
+        return False
+
+
+if __name__ == '__main__':
+    remove(path="233.txt", ignore_errors=True)
