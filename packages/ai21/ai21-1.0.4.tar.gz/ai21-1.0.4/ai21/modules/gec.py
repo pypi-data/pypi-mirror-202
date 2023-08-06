12f41433d3aff85a5ed5d0440a0a22dbe492252f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:41 +0000

    Add package ai21/1.0.4/ai21-1.0.4.tar.gz

---
+++ packages/ai21/ai21-1.0.4.tar.gz/ai21-1.0.4/ai21/modules/gec.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from ai21.errors import EmptyMandatoryListException
+from ai21.modules.resources.nlp_task import NLPTask
+from ai21.utils import validate_mandatory_field
+
+
+class GEC(NLPTask):
+    MODULE_NAME = 'gec'
+
+    @classmethod
+    def execute(cls, **params):
+        validate_mandatory_field(key='text', call_name=cls.MODULE_NAME, params=params, validate_type=True, expected_type=str)
+        url = cls.get_base_url(**params)
+        url = f'{url}/{cls.MODULE_NAME}'
+        return super().execute(task_url=url, **params)
