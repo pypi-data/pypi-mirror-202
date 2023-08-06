22e030daf5356aa198ed233cb4c40a23f85561e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:51 +0000

    Add package shell-gpt/0.8.7/shell_gpt-0.8.7-py3-none-any.whl

---
+++ packages/shell-gpt/shell_gpt-0.8.7-py3-none-any.whl/sgpt/__init__.py
@@ -0,0 +1,10 @@
+from .config import config as config
+from .cache import Cache as Cache
+from .client import OpenAIClient as OpenAIClient
+from .handlers.chat_handler import ChatHandler as ChatHandler
+from .handlers.default_handler import DefaultHandler as DefaultHandler
+from .handlers.repl_handler import ReplHandler as ReplHandler
+from . import utils as utils
+from .app import main as main
+from .app import entry_point as cli
+from . import make_prompt as make_prompt
