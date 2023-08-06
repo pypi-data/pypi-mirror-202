e26cbd00a99b132b4f819382cf613b51fe28dc98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:45 +0000

    Add package hordelib/0.4.1/hordelib-0.4.1.tar.gz

---
+++ packages/hordelib/hordelib-0.4.1.tar.gz/hordelib-0.4.1/hordelib/initialisation.py
@@ -0,0 +1,26 @@
+# initialisation.py
+# Initialise hordelib.
+import sys
+
+from loguru import logger
+from hordelib import install_comfy
+from hordelib.config_path import set_system_path
+from hordelib.consts import COMFYUI_VERSION, DEFAULT_MODEL_MANAGERS
+
+
+def initialise():
+    # Ensure we have ComfyUI
+    logger.debug("Clearing command line args in sys.argv before ComfyUI load")
+    sys_arg_bkp = sys.argv.copy()
+    sys.argv = sys.argv[:1]
+    installer = install_comfy.Installer()
+    installer.install(COMFYUI_VERSION)
+
+    # Modify python path to include comfyui
+    set_system_path()
+
+    # Initialise model manager
+    from hordelib.shared_model_manager import SharedModelManager
+
+    SharedModelManager.loadModelManagers(**DEFAULT_MODEL_MANAGERS)
+    sys.argv = sys_arg_bkp
