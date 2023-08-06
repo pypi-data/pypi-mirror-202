9e555461943f4233944acb1710f9bac66fec957d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:06 +0000

    Add package duckingit/0.0.2/duckingit-0.0.2.tar.gz

---
+++ packages/duckingit/duckingit-0.0.2.tar.gz/duckingit-0.0.2/duckingit/_planner.py
@@ -0,0 +1,119 @@
+import re
+import itertools
+
+import duckdb
+
+from ._exceptions import WrongInvokationType
+from ._parser import QueryParser
+
+
+class Planner:
+    """Class to plan the workload across nodes
+
+    Basically, the class scan the bucket based on the query, divides the workload on the
+    number of invokations and hands the information to the Controller.
+
+    It's the Planner's job to make sure the workload is equally distributed between the
+    nodes, as well as validating the query.
+
+    Attributes:
+        conn, duckdb.DuckDBPyConnection: Local DuckDB connection
+
+    Methods:
+        scan_bucket: Scans the URL for files, e.g. a bucket in S3
+        generate_plan: Creates a query plan that divides the workload between nodes that can be
+            used by the Controller
+    """
+
+    def __init__(self, conn: duckdb.DuckDBPyConnection) -> None:
+        self.conn = conn
+
+    def scan_bucket(self, prefix: str) -> list[str]:
+        """Scans the URL for files, e.g. a S3 bucket
+
+        Usage:
+            Planner(conn=conn).scan_bucket(key="s3://<BUCKET_NAME>/*")
+        """
+        if prefix[-1] != "*":
+            return [prefix]
+
+        # TODO: Count the number of files / size in each prefix to divide the workload better
+        glob_query = f"""
+            SELECT DISTINCT
+                CONCAT(REGEXP_REPLACE(file, '/[^/]+$', ''), '/*') AS prefix
+            FROM GLOB('{prefix}')
+            """
+
+        try:
+            prefixes = self.conn.sql(glob_query).fetchall()
+
+        except RuntimeError:
+            raise ValueError(
+                "Please validate that the FROM statement in the query is correct."
+            )
+
+        return self._flatten_list(_list=prefixes)
+
+    def _flatten_list(self, _list: list) -> list:
+        return list(itertools.chain(*_list))
+
+    def _split_list_in_chunks(
+        self, _list: list[str], number_of_invokations: int
+    ) -> list[list]:
+        # Must not invoke more functions than number of search queries
+        if (size := len(_list)) < number_of_invokations:
+            number_of_invokations = size
+
+        k, m = divmod(len(_list), number_of_invokations)
+        return [
+            _list[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)]
+            for i in range(number_of_invokations)
+        ]
+
+    def _update_query(self, query: str, list_of_prefixes: list[str]) -> str:
+        sub = f"read_parquet({list_of_prefixes})"
+
+        query_upd = re.sub(r"(?:scan|read)_parquet\([^)]*\)", sub, query)
+
+        return query_upd
+
+    def generate_plan(self, query: str, invokations: int | str) -> list[str]:
+        query = QueryParser.parse(query)
+
+        key = QueryParser.find_key(query)
+        list_of_prefixes = self.scan_bucket(prefix=key)
+
+        if isinstance(invokations, str):
+            if invokations != "auto":
+                raise WrongInvokationType(
+                    f"The number of invokations can only be 'auto' or an integer. \
+{invokations} was provided."
+                )
+            invokations = len(list_of_prefixes)
+
+        # TODO: Heuristic to divide the workload between the invokations based on size of prefixes / number of files etc.
+        # Or based on some deeper analysis of the query?
+        list_of_chunks_of_prefixes = self._split_list_in_chunks(
+            list_of_prefixes, number_of_invokations=invokations
+        )
+
+        updated_queries = list()
+        for chunk in list_of_chunks_of_prefixes:
+            query_upd = self._update_query(query=query, list_of_prefixes=chunk)
+            updated_queries.append(query_upd)
+        return updated_queries
+
+
+# class Optimizer:
+#     pass
+
+#     def analyze_query(self, query: str):
+#         pass
+
+
+# from enum import Enum
+# class Format(Enum):
+#     PARQUET = "parquet"
+#     JSON = "json"
+#     ORC = "orc"
+#     AVRO = "avro"
---
+++ packages/duckingit/duckingit-0.0.2.tar.gz/duckingit-0.0.2/duckingit/_session.py
@@ -0,0 +1,150 @@
+import os
+
+import duckdb
+
+from ._controller import Controller, LocalController
+from ._planner import Planner
+from ._parser import QueryParser
+from ._provider import AWS
+
+
+class DuckSession:
+    """Entrypoint to a session of serverless DuckDB instances
+
+    The main objective of this class is to handle the session and serve as the primary
+    entrypoint to a cluster of serverless functions that utilize DuckDB. Its core
+    functionality involves planning, distributing, and scaling the number of DuckDB
+    instances, as well as merging the results before returning them to the query issuer.
+
+    Attributes:
+        conn, duckdb.DuckDBPyConnection: The initialized DuckDB connection
+        metadata, dict: Metadata on temporary tables created using the DuckSession
+
+    Methods:
+        execute: Execute a DuckDB SQL query concurrently using X number of invokations
+
+
+    Usage:
+        >>> session = DuckSession()
+        >>> resp = session.execute(query="SELECT * FROM scan_parquet(['s3::/<BUCKET_NAME>/*'])")
+        >>> resp.show()
+    """
+
+    _CACHE_PREFIX = ".cache/duckingit"
+
+    def __init__(
+        self,
+        function_name: str = "DuckExecutor",
+        # controller_function: str = "DuckController",
+        duckdb_config: dict = {"database": ":memory:", "read_only": False},
+        invokations_default: int | str = "auto",
+        enable_cache: bool = True,
+        **kwargs,
+    ) -> None:
+        """Initiliaze a session of serverless DuckDB instances
+
+        Args:
+            function_name, str: The name of the serverless function
+                Defaults to "DuckExecutor"
+            duckdb_config, dict: DuckDB configurations of the connection. Please take a
+                look on their documention.
+                Defaults to {"database": ":memory:", "read_only": False}
+            invokations_default, int | 'auto': The default number of invokations.
+                Defaults to 'auto'
+            enable_cache, bool: Caches client-side, ie. caches are stored in memory on
+                the machine running the DuckSession. Defaults to True
+            **kwargs
+        """
+        self._function_name = function_name
+        self._invokations_default = invokations_default
+        self.enable_cache = enable_cache
+        self._kwargs = kwargs
+
+        self._conn = duckdb.connect(**duckdb_config)
+        self._load_httpfs()
+        self._set_credentials()
+
+        self._controller = self._set_controller()
+        self._planner = self._set_planner()
+
+        self._metadata: dict[str, str] = dict()
+
+    @property
+    def metadata(self) -> dict[str, str]:
+        return self._metadata
+
+    @property
+    def conn(self) -> duckdb.DuckDBPyConnection:
+        return self._conn
+
+    def _set_planner(self) -> Planner:
+        return Planner(conn=self._conn)
+
+    def _set_controller(self) -> Controller:
+        return LocalController(
+            conn=self._conn,
+            provider=AWS(function_name=self._function_name),
+            enable_cache=self.enable_cache,
+        )
+
+    def _load_httpfs(self) -> None:
+        self._conn.execute("INSTALL httpfs; LOAD httpfs;")
+
+    def _set_credentials(self) -> None:
+        # https://duckdb.org/docs/sql/configuration.html
+        # TODO: Must be more generic to work with other providers
+        self._conn.execute(
+            f"""
+            SET s3_region='{os.getenv("AWS_DEFAULT_REGION", None)}';
+            SET s3_access_key_id='{os.getenv("AWS_ACCESS_KEY_ID", None)}';
+            SET s3_secret_access_key='{os.getenv("AWS_SECRET_ACCESS_KEY", None)}';
+            """
+        )
+
+    def _create_execution_plan(self, query: str, invokations: int | str) -> list[str]:
+        list_of_queries = self._planner.generate_plan(
+            query=query, invokations=invokations
+        )
+
+        return list_of_queries
+
+    def _create_prefix(self, query: str, write_to: str | None) -> str:
+        query_hashed = QueryParser.hash_query(query=query)
+        bucket_name = QueryParser.find_bucket(query=query)
+
+        if write_to is not None:
+            if write_to[-1] != "/":
+                return write_to
+            return write_to[:-1]
+        return f"{bucket_name}/{self._CACHE_PREFIX}/{query_hashed}"
+
+    def execute(
+        self, query: str, *, invokations: int | None = None, write_to: str | None = None
+    ) -> duckdb.DuckDBPyRelation:
+        """Execute the query against a number of serverless functions
+
+        Args:
+            query, str: DuckDB SQL query to run
+            invokations, int | None: The number of invokations of the Lambda function
+                Defaults to 'auto' (See initialization of the session class)
+            write_to, str | None: The prefix to write to, e.g. 's3://BUCKET_NAME/data'
+                Defaults to .cache/duckingit/ prefix
+        """
+        number_of_invokations = (
+            invokations if invokations is not None else self._invokations_default
+        )
+        prefix = self._create_prefix(query=query, write_to=write_to)
+
+        execution_plan = self._create_execution_plan(
+            query=query, invokations=number_of_invokations
+        )
+
+        duckdb_obj, table_name = self._controller.execute(
+            queries=execution_plan, prefix=prefix
+        )
+
+        # Update metadata
+        if table_name != "":
+            self._metadata[table_name] = query
+
+        return duckdb_obj
