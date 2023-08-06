523cdb70b98e227c0f4d5a99f62307cacd3e3879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:56 +0000

    Add package myPyllant/0.2.0/mypyllant-0.2.0-py3-none-any.whl

---
+++ packages/myPyllant/mypyllant-0.2.0-py3-none-any.whl/myPyllant/const.py
@@ -0,0 +1,42 @@
+LOGIN_URL = "https://identity.vaillant-group.com/auth/realms/{brand}-{country}-b2c/login-actions/authenticate"
+AUTHENTICATE_URL = "https://identity.vaillant-group.com/auth/realms/{brand}-{country}-b2c/protocol/openid-connect/auth"
+TOKEN_URL = "https://identity.vaillant-group.com/auth/realms/{brand}-{country}-b2c/protocol/openid-connect/token"
+CLIENT_ID = "myvaillant"
+API_URL_BASE = (
+    "https://api.vaillant-group.com/service-connected-control/end-user-app-api/v1"
+)
+BRANDS = {
+    "vaillant": "Vaillant",
+    "sdbg": "Saunier Duval",
+}
+DEFAULT_BRAND = "vaillant"
+COUNTRIES = {
+    "austria": "Austria",
+    "belgium": "Belgium",
+    "bulgaria": "Bulgaria",
+    "croatia": "Croatia",
+    "czechrepublic": "Czechia",
+    "denmark": "Denmark",
+    "estonia": "Estonia",
+    "finland": "Finland",
+    "france": "France",
+    "germany": "Germany",
+    "greece": "Greece",
+    "hungary": "Hungary",
+    "italy": "Italy",
+    "latvia": "Latvia",
+    "lithuania": "Lithuania",
+    "netherlands": "Netherlands",
+    "norway": "Norway",
+    "poland": "Poland",
+    "portugal": "Portugal",
+    "romania": "Romania",
+    "serbia": "Serbia",
+    "slovakia": "Slovakia",
+    "slovenia": "Slovenia",
+    "spain": "Spain",
+    "sweden": "Sweden",
+    "switzerland": "Switzerland",
+    "unitedkingdom": "United Kingdom",
+}
+DEFAULT_QUICK_VETO_DURATION = 3
