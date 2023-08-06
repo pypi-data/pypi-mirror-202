762388cc07057b2405348589a68ca7b160708597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:04 +0000

    Add package logzilla/0.1.0/logzilla-0.1.0-py3-none-any.whl

---
+++ packages/logzilla/logzilla-0.1.0-py3-none-any.whl/logzilla/__init__.py
@@ -0,0 +1,7 @@
+# ---------------------------------------------------------------------------------
+# Copyright (c) Muhab Tomoum. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
+# ---------------------------------------------------------------------------------
+from __future__ import annotations
+
+from .logzilla import LogZilla, log_execution_time, log_title
