aa1c5e3e475573b84bbda36d9f38c63440f7298c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package dashboard-dataviz-panel/0.1.3/dashboard_dataviz_panel-0.1.3-py3-none-any.whl

---
+++ packages/dashboard-dataviz-panel/dashboard_dataviz_panel-0.1.3-py3-none-any.whl/dashboard_dataviz_panel/__init__.py
@@ -0,0 +1,11 @@
+from dashboard_dataviz_panel.plots import table_plotly
+from dashboard_dataviz_panel.plots import pie_quali 
+from dashboard_dataviz_panel.plots import histogram_quali 
+from dashboard_dataviz_panel.plots import boxplot_quali_quanti 
+from dashboard_dataviz_panel.plots import scatter_quanti_quanti
+from dashboard_dataviz_panel.plots import plotting_target_feature
+
+
+from dashboard_dataviz_panel.helpers import plotly_to_plt_colors
+from dashboard_dataviz_panel.helpers import color_s
+from dashboard_dataviz_panel.helpers import categarray
