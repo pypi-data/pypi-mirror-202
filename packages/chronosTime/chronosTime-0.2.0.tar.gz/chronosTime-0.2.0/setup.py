a8d681c9a3b4cc84dfa8bcc90ef83f170421590d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:04 +0000

    Add package chronosTime/0.2.0/chronosTime-0.2.0.tar.gz

---
+++ packages/chronosTime/chronosTime-0.2.0.tar.gz/chronosTime-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chronosTime',
+    version='0.2.0',
+    description='A library for measuring time elapsed',
+    author='Chr0n0s',
+    author_email='ch3guevara@proton.me',
+    url='https://github.com/cybershinig4mi',
+    packages=find_packages(),
+    install_requires=[
+        # list your dependencies here
+        'numpy>=1.15.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        
+    ],
+)
