8fee56f143ec1bb7455adce2fc485801a84d2873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:47 +0000

    Add package h5grove/1.3.0/h5grove-1.3.0.tar.gz

---
+++ packages/h5grove/h5grove-1.3.0.tar.gz/h5grove-1.3.0/h5grove/models.py
@@ -0,0 +1,21 @@
+from enum import Enum
+from typing import Dict, Tuple, Union
+import h5py
+
+H5pyEntity = Union[
+    h5py.Dataset, h5py.Datatype, h5py.ExternalLink, h5py.Group, h5py.SoftLink
+]
+
+Selection = Union[str, int, slice, Tuple[Union[slice, int], ...], None]
+
+
+class LinkResolution(str, Enum):
+    NONE = "none"  # Links are not resolved whatever their status
+    ONLY_VALID = (
+        "only_valid"  # Links are only resolved if valid and unresolved if broken
+    )
+    ALL = "all"  # Links are resolved no matter their status. The resolution of broken links raises LinkError
+
+
+# Recursive types not supported by mypy: https://github.com/python/mypy/issues/731
+StrDtype = Union[str, Dict[str, "StrDtype"]]  # type: ignore
