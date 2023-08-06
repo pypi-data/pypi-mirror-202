cf08373fd8b3a54a1dcb4e802fdbb85100044031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:31 +0000

    Add package deltacat/0.1.18b1/deltacat-0.1.18b1.tar.gz

---
+++ packages/deltacat/deltacat-0.1.18b1.tar.gz/deltacat-0.1.18b1/deltacat/compute/compactor/utils/round_completion_file.py
@@ -0,0 +1,57 @@
+import json
+import logging
+
+from deltacat import logs
+from deltacat.compute.compactor import RoundCompletionInfo
+from deltacat.storage import PartitionLocator
+
+logger = logs.configure_deltacat_logger(logging.getLogger(__name__))
+
+
+def get_round_completion_file_s3_url(
+    bucket: str, source_partition_locator: PartitionLocator
+) -> str:
+
+    base_url = source_partition_locator.path(f"s3://{bucket}")
+    return f"{base_url}.json"
+
+
+def read_round_completion_file(
+    bucket: str, source_partition_locator: PartitionLocator
+) -> RoundCompletionInfo:
+
+    from deltacat.aws import s3u as s3_utils
+
+    round_completion_file_url = get_round_completion_file_s3_url(
+        bucket,
+        source_partition_locator,
+    )
+    logger.info(f"reading round completion file from: {round_completion_file_url}")
+    round_completion_info = None
+    result = s3_utils.download(round_completion_file_url, False)
+    if result:
+        json_str = result["Body"].read().decode("utf-8")
+        round_completion_info = RoundCompletionInfo(json.loads(json_str))
+        logger.info(f"read round completion info: {round_completion_info}")
+    return round_completion_info
+
+
+def write_round_completion_file(
+    bucket: str,
+    source_partition_locator: PartitionLocator,
+    round_completion_info: RoundCompletionInfo,
+) -> str:
+
+    from deltacat.aws import s3u as s3_utils
+
+    logger.info(f"writing round completion file contents: {round_completion_info}")
+    round_completion_file_s3_url = get_round_completion_file_s3_url(
+        bucket,
+        source_partition_locator,
+    )
+    logger.info(f"writing round completion file to: {round_completion_file_s3_url}")
+    s3_utils.upload(
+        round_completion_file_s3_url, str(json.dumps(round_completion_info))
+    )
+    logger.info(f"round completion file written to: {round_completion_file_s3_url}")
+    return round_completion_file_s3_url
---
+++ packages/deltacat/deltacat-0.1.18b1.tar.gz/deltacat-0.1.18b1/deltacat/storage/model/types.py
@@ -0,0 +1,55 @@
+from enum import Enum
+from typing import Any, List, Union
+
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+from ray.data._internal.arrow_block import ArrowRow
+from ray.data.dataset import Dataset
+
+LocalTable = Union[pa.Table, pd.DataFrame, np.ndarray]
+LocalDataset = List[LocalTable]
+DistributedDataset = Dataset[Union[ArrowRow, np.ndarray, Any]]
+
+
+class DeltaType(str, Enum):
+    APPEND = "append"
+    UPSERT = "upsert"
+    DELETE = "delete"
+
+
+class LifecycleState(str, Enum):
+    UNRELEASED = "unreleased"
+    ACTIVE = "active"
+    DEPRECATED = "deprecated"
+    BETA = "beta"
+    DELETED = "deleted"
+
+
+class CommitState(str, Enum):
+    STAGED = "staged"
+    COMMITTED = "committed"
+    DEPRECATED = "deprecated"
+
+
+class SchemaConsistencyType(str, Enum):
+    """
+    Schemas are optional for DeltaCAT tables and can be used to inform the data
+    consistency checks run for each field. If a schema is present, it can be
+    used to enforce the following column-level data consistency policies at
+    table load time:
+
+    NONE: No consistency checks are run. May be mixed with the below two
+    policies by specifying column names to pass through together with
+    column names to coerce/validate.
+
+    COERCE: Coerce fields to fit the schema whenever possible. An explicit
+    subset of column names to coerce may optionally be specified.
+
+    VALIDATE: Raise an error for any fields that don't fit the schema. An
+    explicit subset of column names to validate may optionally be specified.
+    """
+
+    NONE = "none"
+    COERCE = "coerce"
+    VALIDATE = "validate"
