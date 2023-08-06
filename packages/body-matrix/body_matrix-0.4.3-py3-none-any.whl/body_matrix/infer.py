ca02836386da7936d5be56a558550693ffdf299f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:00 +0000

    Add package body-matrix/0.4.3/body_matrix-0.4.3-py3-none-any.whl

---
+++ packages/body-matrix/body_matrix-0.4.3-py3-none-any.whl/body_matrix/infer.py
@@ -0,0 +1,130 @@
+import torch
+import numpy as np
+from torchvision.utils import draw_segmentation_masks
+from torchvision.transforms.functional import to_pil_image, pil_to_tensor
+from . import measure
+from . import score
+
+##########################################################
+### Detect all Humans in Frames with High Probability ####
+##########################################################
+def detect_targets(frame, device, min_accuracy, kp_model, kp_transforms):
+
+    ### Make Predictions on Frame
+    body_matrix = kp_transforms(frame)
+    predictions = kp_model([body_matrix.to(device)])
+
+    ### Extract Boxes, Scores and Keypoints from frame
+    boxes = predictions[0]['boxes']
+    keypoints = predictions[0]['keypoints']
+    scores = predictions[0]['scores']
+
+    ### Filter for the main Bounding Box and Keypoints
+    idx = torch.where(scores > min_accuracy)
+    main_boxes = torch.squeeze(boxes[idx], dim=0)
+
+    return main_boxes
+
+###################################
+### Detect Main Person in Frame ###
+###################################
+def detect_main_target(frame, device, min_accuracy, kp_model, kp_transforms):   
+
+    ### Make Predictions on Frame
+    body_matrix = kp_transforms(frame)
+    predictions = kp_model([body_matrix.to(device)])
+
+    ### Extract Boxes, Scores and Keypoints from frame
+    boxes = predictions[0]['boxes']
+    keypoints = predictions[0]['keypoints']
+    scores = predictions[0]['scores']
+    
+    ### Filter for the main Bounding Box and Keypoints
+    idx = torch.where(scores > min_accuracy)
+    main_boxes = torch.unsqueeze(boxes[idx], dim=1)
+    index = 0
+    min_area = 0
+    max_distance = frame.width/2
+
+    print("Found ", len(main_boxes), " person in the frame")
+
+    distances = []
+    areas = []
+    focuses = []
+    bboxes = []
+    
+    for selector, box in enumerate(main_boxes):
+        bbox = [
+            box[0][0].item(), 
+            box[0][1].item(),
+            box[0][2].item(),
+            box[0][3].item()
+        ]
+        
+        distance, area = measure.distance_from_vertical_line(frame, bbox) 
+        
+        focuses.append(area/distance)
+        distances.append(distance)
+        areas.append(area)
+        bboxes.append(bbox)
+
+    # print("Distances: ",distances)
+    # print("Areas: ", areas)
+    # print("FOCUSES: ", focuses)
+
+    distance_score, distance_index = score.find_nearest(distances, 0)
+    focus_score, focus_index = score.find_largest(focuses, 0)
+    
+    # print("Select keypoint index", focus_index)
+    selected_bbox = bboxes[focus_index] 
+    selected_keypoints = torch.squeeze(keypoints[idx][focus_index], dim=0)
+    # SHApoints = getSHAPositions(kp)
+    # labels = ["ls","rs","lw", "rw", "lh","rh","la","ra"]
+    # points_image = drawMarkers(labels, SHApoints, frame)
+    return selected_bbox, selected_keypoints
+
+###############################################
+### Segment Selected Person from Background ###
+###############################################
+def segment_selected_target(frame, device, selected_bbox, segment_min_accuracy, segment_model, segment_transforms):
+    ### Run Predicion Model to find Mask
+    input_image = segment_transforms(frame)
+    predictions = segment_model([input_image.to(device)])
+    masks = predictions[0]['masks']
+    scores = predictions[0]['scores']
+    boxes = predictions[0]['boxes']
+    idx = torch.where(scores > segment_min_accuracy)
+    main_boxes = torch.unsqueeze(boxes[idx], dim=1)
+
+    # print("Found ", len(main_boxes), " person in the frame")
+    # index = len(main_boxes) - 1
+    
+    distances = []
+    for selector, box in enumerate(main_boxes):
+        bbox = [
+            box[0][0].item(), 
+            box[0][1].item(),
+            box[0][2].item(),
+            box[0][3].item()
+        ]
+        
+        distance_from_selected = measure.two_boxes_distance(bbox, selected_bbox)
+        distances.append(distance_from_selected)
+            
+    # print(distances)
+    distance, index = score.find_nearest(distances, 0)
+    # print(distance, index)
+    
+    mask = torch.squeeze(masks[index][0], dim=1)
+    mask_image = to_pil_image(mask)
+    
+    ### Find the Minimum Pixel Threshold for Mask
+    numpy_mask = mask.detach().cpu().numpy()
+    counts, values = np.histogram(numpy_mask, bins=10)
+    min_counts = np.min(counts)
+    idx = np.where(counts == min_counts)
+    thresholds = sorted(values[idx]) 
+    numpy_bool_mask = numpy_mask > thresholds[0]
+    bool_mask = torch.tensor(numpy_bool_mask)
+    
+    return mask, mask_image, bool_mask
---
+++ packages/body-matrix/body_matrix-0.4.3-py3-none-any.whl/body_matrix/score.py
@@ -0,0 +1,239 @@
+import numpy as np
+import seaborn as sns
+from torchvision.transforms.functional import to_pil_image, pil_to_tensor
+from . import load, infer, process, measure, draw
+
+
+def two_points_linear_constant(a, b):
+    aX = a[0]
+    aY = a[1]
+    bX = b[0]
+    bY = b[1]
+    if (bX-aX) != 0:
+        alpha = (bY - aY)/(bX - aX)
+        beta = (bX * aY - bY * aX)/(bX - aX)
+    else:
+        alpha = None
+        beta = None
+    return alpha, beta
+
+
+def find_segment_line(segment_area, alpha, beta):
+    line_coordinates = []
+    for idx, position in enumerate(sorted(segment_area)):
+        expectedY = alpha * position[0] + beta
+        if position[1] == int(expectedY):
+            line_coordinates.append(
+                [position[0], position[1]]
+            )
+    return line_coordinates
+
+
+def find_nearest(array, value):
+    np_scores = np.array(array)
+    distance_array = np.abs(np_scores - value)
+    idx = distance_array.argmin()
+    return array[idx], idx
+
+
+def find_largest(array, value):
+    np_scores = np.array(array)
+    distance_array = np.abs(np_scores - value)
+    idx = distance_array.argmax()
+    return array[idx], idx
+
+
+def best_scores(array, min_val, max_val):
+    scores = []
+    for x in array:
+        if x > max_val or x <  min_val:
+            pass
+        else:
+            scores.append(x)
+            
+    np_scores = np.array(scores)
+    mean = np.mean(np_scores)
+    median = np.median(np_scores)
+    minim = np.min(np_scores)
+    maxim = np.max(np_scores)
+    histogram_scores = np.histogram(np_scores)
+    #print(mean, median)
+    sns.distplot(np_scores, hist=True)
+    
+    return mean, median, minim, maxim
+
+
+def SHA_score(ls, rs, lh, rh, la, ra):
+    sL = measure.two_points_distance(ls, rs)
+    hL = measure.two_points_distance(lh, rh)
+    ms = measure.find_middle_point(ls, rs)
+    mh = measure.find_middle_point(lh, rh)
+    ma = measure.find_middle_point(la, ra)
+    bL = measure.two_points_distance(ms, mh)
+    lL = measure.two_points_distance(mh, ma)
+    score = int((hL * lL) / (sL * bL) * 1000)
+    measures = {
+        'shoulder':sL,
+        'hip':hL, 
+        'back':bL,
+        'leg':lL
+    }
+
+    return score, measures
+
+
+def frame_SHA_score(pil_image_frame, bool_mask, selected_kps):
+    image = pil_image_frame
+    segment_area = process.segmentation_area(
+        image, 
+        bool_mask
+    )
+        
+    hip_kps = process.find_hip_points(
+        selected_kps['left_hip'], 
+        selected_kps['right_hip'],
+        selected_kps['left_wrist'],
+        selected_kps['right_wrist'],
+        segment_area
+    )
+        
+    shoulder_kps = process.find_shoulder_points(
+        selected_kps['left_shoulder'], 
+        selected_kps['right_shoulder'],
+        segment_area
+    )
+
+    if hip_kps == None or shoulder_kps == None:
+        #print("KEYPOINT ERRORS")
+        return None
+    
+    main_points = {}
+    main_points.update(hip_kps)
+    main_points.update(shoulder_kps)
+    main_points.update(
+        {
+            'left_ankle':selected_kps['left_ankle'],
+            'right_ankle':selected_kps['right_ankle']
+        }
+    )
+    
+    middle_hip = measure.find_middle_point(
+        main_points['left_hip'],
+        main_points['right_hip']
+    )
+       
+    scores, measures = SHA_score(
+        ls=main_points['left_shoulder'], 
+        rs=main_points['right_shoulder'],
+        lh=main_points['left_hip'],
+        rh=main_points['right_hip'],
+        la=main_points['left_ankle'],
+        ra=main_points['right_ankle']
+    )
+
+    return scores, measures
+
+
+def video_SHA_score(vid, device, font_dir,  segment_model, segment_transform, keypoints_model, keypoints_transform):
+    SHA_frames = []
+    SHA_scores = []
+    SHA_measures = []
+    
+    for index, frame in enumerate(vid):
+        image = to_pil_image(frame)
+        image = image.rotate(-90, expand=True)
+        selected_box, keypoints = infer.detect_main_target(
+            image, device, 0.8, keypoints_model, keypoints_transform
+        )
+
+        mask, mask_image, bool_mask = infer.segment_selected_target(
+            image, device, selected_box, 0.99, segment_model, segment_transform
+        )
+
+        selected_kps = process.keypoints_filter(
+             [
+                'left_ear', 'right_ear',
+                'left_shoulder','right_shoulder',
+                'left_wrist','right_wrist',
+                'left_hip', 'right_hip',
+                'left_ankle', 'right_ankle'
+            ], 
+            keypoints
+        )
+        
+        segment_area = process.segmentation_area(
+            image, 
+            bool_mask
+        )
+        
+        hip_kps = process.find_hip_points(
+            selected_kps['left_hip'], 
+            selected_kps['right_hip'],
+            selected_kps['left_wrist'],
+            selected_kps['right_wrist'],
+            segment_area
+        )
+        
+        shoulder_kps = process.find_shoulder_points(
+            selected_kps['left_shoulder'], 
+            selected_kps['right_shoulder'],
+            segment_area
+        )
+
+        if hip_kps == None or shoulder_kps == None:
+            #print("KEYPOINT ERRORS")
+            continue
+        
+        main_points = {}
+        main_points.update(hip_kps)
+        main_points.update(shoulder_kps)
+        main_points.update(
+            {
+                'left_ankle':selected_kps['left_ankle'],
+                'right_ankle':selected_kps['right_ankle']
+            }
+        )
+        
+        middle_hip = measure.find_middle_point(
+            main_points['left_hip'],
+            main_points['right_hip']
+        )
+
+        float_labeled_frame = image
+        for key, value in main_points.items():
+            #print(key, value)
+            float_labeled_frame = draw.floating_rectangle_label(
+                image = float_labeled_frame, 
+                longitude_coordinate = middle_hip[0], 
+                point=value, 
+                label_text=key, 
+                label_size=16, 
+                label_color="#ffffff", 
+                label_font=font_dir, 
+                background_color="#11114A"
+            )    
+
+       
+        score, measures = SHA_score(
+            ls=main_points['left_shoulder'], 
+            rs=main_points['right_shoulder'],
+            lh=main_points['left_hip'],
+            rh=main_points['right_hip'],
+            la=main_points['left_ankle'],
+            ra=main_points['right_ankle']
+        )
+        
+        SHA_frames.append(float_labeled_frame)
+
+        SHA_scores.append(score)
+        SHA_measures.append(measures)
+
+        print("##############################")
+        print("Finished Processing ", index, " with score ", score, "\nand measures ", measures)
+        print("##############################")
+        
+    return SHA_frames, SHA_scores, SHA_measures
+
+
+
+
