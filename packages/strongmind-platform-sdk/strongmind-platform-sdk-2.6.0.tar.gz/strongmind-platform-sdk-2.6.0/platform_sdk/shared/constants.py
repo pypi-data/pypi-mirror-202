608be170aa082634886324fbc4c4c94d1e92b632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:30 +0000

    Add package strongmind-platform-sdk/2.6.0/strongmind-platform-sdk-2.6.0.tar.gz

---
+++ packages/strongmind-platform-sdk/strongmind-platform-sdk-2.6.0.tar.gz/strongmind-platform-sdk-2.6.0/platform_sdk/shared/constants.py
@@ -0,0 +1,14 @@
+# Static OneRoster sourcedID Values
+GUID_ONEROSTER_CURRENT_GRADE_CATEGORY_ID: str = "4b9a9d38-ac45-447f-8733-51aeb6d988ad"
+GUID_ONEROSTER_FINAL_GRADE_CATEGORY_ID: str = "0cff2c81-f135-49dc-989b-6ce2dfe81e0a"
+
+# Dependency Names
+UNKNOWN = "Unknown"
+IDENTITY_SERVER = "IdentityServer"
+USER_CREATION = "UserCreation"
+EVENTS_API = "EventsApi"
+
+ONEROSTER_DATA_STORE = "OneRosterDataStore"
+
+# OneRoster Types
+ONEROSTER_SINGLE_TYPE_NAME_CLASS = "class"
