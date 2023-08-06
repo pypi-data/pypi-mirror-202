aaa9f0a0b9e6400397ed7eb60cdfed3adea1129e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:17 +0000

    Add package mypy_baseline/0.5.0/mypy_baseline-0.5.0.tar.gz

---
+++ packages/mypy_baseline/mypy_baseline-0.5.0.tar.gz/mypy_baseline-0.5.0/tests/test_commands/test_filter.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from io import StringIO
+
+from mypy_baseline import main
+
+from .helpers import LINE1, LINE2, NOTEBOOK_LINE1, run
+
+
+def test_filter():
+    stdin = StringIO()
+    stdin.write(LINE1)
+    stdin.write(LINE2)
+    stdin.seek(0)
+    stdout = StringIO()
+    code = main(['filter'], stdin, stdout)
+    assert code == 2
+    stdout.seek(0)
+    actual = stdout.read()
+    assert LINE1.strip() in actual
+    assert LINE2.strip() in actual
+    assert '  assignment  ' in actual
+    assert '  union-attr  ' in actual
+    assert '  unresolved' in actual
+    assert 'Your changes introduced' in actual
+
+
+def test_filter_notebook():
+    stdin = StringIO()
+    stdin.write(NOTEBOOK_LINE1)
+
+    stdin.seek(0)
+    stdout = StringIO()
+    code = main(['filter'], stdin, stdout)
+    assert code == 1
+    stdout.seek(0)
+    actual = stdout.read()
+    assert NOTEBOOK_LINE1 in actual
+    assert '  return-value  ' in actual
+    assert '  unresolved' in actual
+    assert 'Your changes introduced' in actual
+
+
+def test_filter__empty_stdin():
+    actual = run(['filter'])
+    assert actual == ''
