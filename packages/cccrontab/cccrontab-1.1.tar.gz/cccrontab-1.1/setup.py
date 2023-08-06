314a2f7e3cf652d5113b386acdd20cf2e6c995ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:52 +0000

    Add package cccrontab/1.1/cccrontab-1.1.tar.gz

---
+++ packages/cccrontab/cccrontab-1.1.tar.gz/cccrontab-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='cccrontab',
+      version='1.1',
+      description='CC-Crontab by python',
+      url='https://github.com/OpenFibers/CCCrontab',
+      author='OpenFibers',
+      author_email='openfibers@gmail.com',
+      license='MIT',
+      packages=['cccrontab'],
+      zip_safe=False)
