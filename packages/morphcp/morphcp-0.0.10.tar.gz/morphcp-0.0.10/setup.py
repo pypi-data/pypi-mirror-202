2c26bc0ac96d8e0a396d27ba83ba4cdec2a57b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:49 +0000

    Add package morphcp/0.0.10/morphcp-0.0.10.tar.gz

---
+++ packages/morphcp/morphcp-0.0.10.tar.gz/morphcp-0.0.10/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Jared Lutgen",
+    author_email="jlutgen@morpheusdata.com",
+    name='morphcp',
+    license="MIT",
+    description='Tool utilized to deploy content into a morpheus appliance',
+    version='v0.0.10',
+    long_description=README,
+    url='https://gitlab.com/jaredlutgen/morphcp',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['requests'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
