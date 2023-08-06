62ab8a937eda74053d643e56132d2f605dd7b6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:22 +0000

    Add package jiwan-mycalc0001/0.0.1/jiwan_mycalc0001-0.0.1-py3-none-any.whl

---
+++ packages/jiwan-mycalc0001/jiwan_mycalc0001-0.0.1-py3-none-any.whl/mycalc/calculator.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='mycalc0001',
+    version='0.0.1',
+    description='my calculator',
+    author='seojiwan',
+    autohor_email='gg812056@gmail.com',
+    url="https://github.com/sejin-chun/calc0001",
+    download_url="https://github.com/sejin-chun/calc0001/archive/refs/tags/v0.0.1.zip",
+    install_requires=['pytest'],
+    long_description='oss-dev calculator python module',
+    long_description_content_type='test/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
