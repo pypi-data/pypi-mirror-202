2818abff6e9c9d347f762fc9d58c6da96c993a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:49 +0000

    Add package girok/0.1.16/girok-0.1.16.tar.gz

---
+++ packages/girok/girok-0.1.16.tar.gz/girok-0.1.16/girok/api/auth.py
@@ -0,0 +1,54 @@
+import requests
+from girok.config import get_config
+import girok.utils.general as general_utils
+import girok.utils.display as display_utils
+
+cfg = get_config()
+
+base_url = cfg.base_url 
+
+def register(email, password):
+    resp = requests.post(base_url + "/register", json={
+        "email": email,
+        "password": password
+    })
+    return resp
+
+def verify_verification_code(email, verification_code):
+    resp = requests.post(base_url + "/register/verification_code", json={
+        "email": email,
+        "verification_code": verification_code
+    })
+    if resp.status_code == 200:
+        return True
+    elif resp.status_code == 401:
+        err_msg = general_utils.bytes2dict(resp.content)['detail']
+        # display_utils.center_print(err_msg, type="error")
+        return False
+    else:
+        err_msg = general_utils.bytes2dict(resp.content)['detail'][0]['msg']
+        # print(err_msg)
+        # display_utils.center_print(str(err_msg), type="error") 
+        return False
+    
+
+def login(email, password):
+    files = { 
+        "username": (None, email),
+        "password": (None, password)
+    }
+    
+    resp = requests.post(base_url + "/login", files=files)
+    return resp
+
+
+def validate_access_token(access_token):
+    options = {
+        "headers": {
+        "Authorization": "Bearer " + access_token,
+        }
+    }
+    
+
+    resp = requests.get(base_url + "/validate-access-token", headers=options['headers'])
+    return resp
