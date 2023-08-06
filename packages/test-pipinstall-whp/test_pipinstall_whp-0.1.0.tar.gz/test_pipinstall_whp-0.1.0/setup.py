da08601d2e5f28cc6ed39cfcaecef7eb10de6d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:35 +0000

    Add package test-pipinstall-whp/0.1.0/test_pipinstall_whp-0.1.0.tar.gz

---
+++ packages/test-pipinstall-whp/test_pipinstall_whp-0.1.0.tar.gz/test_pipinstall_whp-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='test_pipinstall_whp',
+      version='0.1.0',
+      description='test pip installation',
+      author='haopengwu',
+      author_email='wuhaopeng106@gmail.com',
+      requires= ['numpy','matplotlib'],
+      packages=find_packages(),
+      license="apache 3.0"
+      )
