24d358b1365138798996c3f5fc8f9a18bfdb5cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:20:01 +0000

    Add package napari-cellseg3d/0.0.2rc3/napari_cellseg3d-0.0.2rc3-py3-none-any.whl

---
+++ packages/napari-cellseg3d/napari_cellseg3d-0.0.2rc3-py3-none-any.whl/napari_cellseg3d/_tests/test_review.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from napari_cellseg3d.code_plugins import plugin_review as rev
+
+
+def test_launch_review(make_napari_viewer):
+
+    view = make_napari_viewer()
+    widget = rev.Reviewer(view)
+
+    # widget.filetype_choice.setCurrentIndex(0)
+
+    im_path = str(Path(__file__).resolve().parent / "res/test.tif")
+
+    widget.folder_choice.setChecked(True)
+    widget.image_filewidget.text_field = im_path
+    widget.labels_filewidget.text_field = im_path
+    widget.results_filewidget.text_field = str(
+        Path(__file__).resolve().parent / "res"
+    )
+
+    widget.run_review()
+    widget._viewer.close()
+
+    assert widget._viewer is not None
