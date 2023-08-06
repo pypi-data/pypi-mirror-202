f2cbf81e153da9e6da80956f570c7cbb40617f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:05 +0000

    Add package locuaz/0.2.0/locuaz-0.2.0.tar.gz

---
+++ packages/locuaz/locuaz-0.2.0.tar.gz/locuaz-0.2.0/locuaz/mutators.py
@@ -0,0 +1,22 @@
+from typing import Dict, Type
+import warnings
+
+from basemutator import BaseMutator
+from mutatorbiobb import MutatorBiobb
+from mutatorevoef2 import MutatorEvoEF2
+
+__all__ = ("mutators",)
+
+mutators: Dict[str, Type[BaseMutator]] = {
+    "biobb": MutatorBiobb,
+    "evoef2": MutatorEvoEF2,
+}
+
+try:
+    from mutatordlpr import MutatorDLPackerReconstruct
+    from mutatordlp import MutatorDLPacker
+
+    mutators["dlp"] = MutatorDLPacker
+    mutators["dlpr"] = MutatorDLPackerReconstruct
+except ModuleNotFoundError:
+    warnings.warn("Could not import DLPacker, dlp and dlpr mutators won't be available.")
---
+++ packages/locuaz/locuaz-0.2.0.tar.gz/locuaz-0.2.0/locuaz/scoringfunctions.py
@@ -0,0 +1,26 @@
+from typing import Dict, Type
+
+from abstractscoringfunction import AbstractScoringFunction
+from bach import Bach
+from bluues import Bluues
+from bluuesbmf import BluuesBmf
+from evoef2 import Evoef2
+from gmxmmpbsa import GmxMmpbsa
+from haddock import Haddock
+from pisa import Pisa
+from piepisa import PiePisa
+from rosetta import Rosetta
+from autodockvina import AutodockVina
+
+scoringfunctions: Dict[str, Type[AbstractScoringFunction]] = {
+    "bach": Bach,
+    "bluues": Bluues,
+    "bluuesbmf": BluuesBmf,
+    "evoef2": Evoef2,
+    "haddock": Haddock,
+    "pisa": Pisa,
+    "piepisa": PiePisa,
+    "rosetta": Rosetta,
+    "gmxmmpbsa": GmxMmpbsa,
+    "autodockvina": AutodockVina,
+}
