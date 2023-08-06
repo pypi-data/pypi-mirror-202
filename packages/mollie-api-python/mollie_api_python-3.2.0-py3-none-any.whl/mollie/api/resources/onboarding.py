dc58786ff32939e5129a20723c6d9265269747c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:28 +0000

    Add package mollie-api-python/3.2.0/mollie_api_python-3.2.0-py3-none-any.whl

---
+++ packages/mollie-api-python/mollie_api_python-3.2.0-py3-none-any.whl/mollie/api/resources/onboarding.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict
+
+from ..error import IdentifierError
+from ..objects.onboarding import Onboarding as OnboardingObject
+from .base import ResourceGetMixin
+
+__all__ = [
+    "Onboarding",
+]
+
+
+class Onboarding(ResourceGetMixin):
+    """Resource handler for the `/onboarding` endpoint."""
+
+    def get_resource_object(self, result: dict) -> OnboardingObject:
+        return OnboardingObject(result, self.client)
+
+    def get(self, resource_id: str, **params: Any) -> OnboardingObject:
+        if resource_id != "me":
+            raise IdentifierError(f"Invalid onboarding ID: '{resource_id}'. The onboarding ID should be 'me'.")
+        return super().get(resource_id, **params)
+
+    def create(self, data: Dict[str, Any], **params: Any) -> OnboardingObject:
+        resource_path = self.get_resource_path()
+        path = f"{resource_path}/me"
+        result = self.perform_api_call(self.REST_CREATE, path, data, params)
+        return self.get_resource_object(result)
