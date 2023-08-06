095d2ee67f275316eceec8258bf4294accb429f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:03 +0000

    Add package abuscom-libs/0.1.4/abuscom-libs-0.1.4.tar.gz

---
+++ packages/abuscom-libs/abuscom-libs-0.1.4.tar.gz/abuscom-libs-0.1.4/abuscom/connectors/compass.py
@@ -0,0 +1,41 @@
+from airflow.providers.jdbc.hooks.jdbc import JdbcHook
+import datetime
+
+#from java.sql import Timestamp
+
+class Compass:
+
+    def __init__(self, compass_conn_id):
+        self.__compass_conn_id = compass_conn_id
+
+    def __dataFrameToTuples(self, df, columns):
+        tuples= []
+        for i, row in df.iterrows():
+            tuple = ()
+
+            for idx, column in enumerate(columns):
+                try:
+                    timestamp = row[idx].getTime() / 1000
+                    tuple = tuple + (datetime.datetime.fromtimestamp(timestamp),)
+                except AttributeError:
+                    tuple = tuple + (row[idx],)
+            tuples.append(tuple)
+
+        return tuples
+
+
+    def load_to_dataframe(self, tableName, columns, schema):
+        cpHook = JdbcHook(log_sql=True, jdbc_conn_id=self.__compass_conn_id)
+        strColumns = ",".join(columns)
+        sql = f'select {strColumns} from {schema}.{tableName}'
+        # print(sql)
+        db = cpHook.get_pandas_df(sql=sql)
+        # print(db)
+        return db
+
+    def load_to_tuples(self, tableName, columns, schema):
+        df = self.load_to_dataframe(tableName=tableName, columns=columns, schema=schema)
+        tuples = self.__dataFrameToTuples(df=df, columns=columns)
+        # print(tuples)
+        return tuples
+        # return list(df.itertuples(index=False, name=None))
