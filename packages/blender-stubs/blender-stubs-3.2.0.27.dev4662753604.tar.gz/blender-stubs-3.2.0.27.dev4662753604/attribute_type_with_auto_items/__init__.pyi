c39408641597cf209d599bd935767c0b9a88bf99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:09 +0000

    Add package blender-stubs/3.2.0.27.dev4662753604/blender-stubs-3.2.0.27.dev4662753604.tar.gz

---
+++ packages/blender-stubs/blender-stubs-3.2.0.27.dev4662753604.tar.gz/blender-stubs-3.2.0.27.dev4662753604/attribute_type_with_auto_items/__init__.pyi
@@ -0,0 +1,59 @@
+"""
+
+
+Attribute Type With Auto Items
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:AUTO:            
+  Auto.
+
+:FLOAT:           
+  Float.
+
+  Floating-point value.
+
+:INT:             
+  Integer.
+
+  32-bit integer.
+
+:FLOAT_VECTOR:    
+  Vector.
+
+  3D vector with floating-point values.
+
+:FLOAT_COLOR:     
+  Color.
+
+  RGBA color with 32-bit floating-point values.
+
+:BYTE_COLOR:      
+  Byte Color.
+
+  RGBA color with 8-bit positive integer values.
+
+:STRING:          
+  String.
+
+  Text string.
+
+:BOOLEAN:         
+  Boolean.
+
+  True or false.
+
+:FLOAT2:          
+  2D Vector.
+
+  2D vector with floating-point values.
+
+:INT8:            
+  8-Bit Integer.
+
+  Smaller integer with a range from -128 to 127.
+
+.. _rna-enum-attribute-type-with-auto-items:
+
+"""
+
+import typing
