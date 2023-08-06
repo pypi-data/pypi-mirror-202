a2c1d01566c88608881fb5ff691764f87415d2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:10 +0000

    Add package rcg/0.0.10/rcg-0.0.10.tar.gz

---
+++ packages/rcg/rcg-0.0.10.tar.gz/rcg-0.0.10/setup.py
@@ -0,0 +1,36 @@
+import os
+import pip
+import codecs
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = "0.0.10"
+DESCRIPTION = "Rapid catchment generator - is a tool for rapid prototyping of a hydraulic model catchments that can be read and edited with SWMM."
+LONG_DESCRIPTION = "Tool for rapid prototyping of a hydraulic model that can be read and edited with SWMM. The generator was created using feature analysis and surface runoff research from the literature. Fuzzy logic controller rules were developed using parameterized categories of soil, slope,  and permeability. The catchment configuration procedure was simplified by mapping typical storage and Manning's coefficients. The use of fuzzy logic rules allows the system to be modified to adjust the categories to certain situations. The use of membership functions allows us to increase computation accuracy and customize the tool to diverse applications. Following alteration of the catchment in the SWMM GUI allows for accurate portrayal of the real condition of the catchment."
+
+# Setting up
+setup(
+    name="rcg",
+    version=VERSION,
+    author="Rafał Buczyński",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        "scikit-fuzzy",
+        "numpy",
+        "pandas",
+        "swmmio",
+        "pyswmm",
+    ],
+    project_urls={
+        "Homepage": "https://github.com/BuczynskiRafal/rapid-catchment-generator"
+    },
+)
+
+# python setup.py sdist
