ba8e0a7594380c5103d7f7252ea09cea4d8df1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:50 +0000

    Add package listless/0.1.4/listless-0.1.4-py3-none-any.whl

---
+++ packages/listless/listless-0.1.4-py3-none-any.whl/listless/_meta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+
+__title__ = "listless"
+__description__ = "generator utils; aka listless"
+__pkgroot__ = __file__.replace("_meta.py", "").rstrip("/\\")
+__version__ = "0.1.4"
