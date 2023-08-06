3d10a44a1f9f75400a7a6a935d3ab40c20bc7fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:58 +0000

    Add package pyclient2d/0.0.2/pyclient2d-0.0.2-py3-none-any.whl

---
+++ packages/pyclient2d/pyclient2d-0.0.2-py3-none-any.whl/pyclient2d/Client/Client.py
@@ -0,0 +1,7 @@
+from ..Shared import Engine
+
+
+class Client(Engine):
+
+    def __init__(self):
+        super().__init__()
---
+++ packages/pyclient2d/pyclient2d-0.0.2-py3-none-any.whl/pyclient2d/Server/Server.py
@@ -0,0 +1,7 @@
+from ..Shared import Engine
+
+
+class Server(Engine):
+
+    def __init__(self):
+        super().__init__()
