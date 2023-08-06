f29b8c781012871611bbf2a8d690a6d472f9de1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:22 +0000

    Add package substrafl/0.35.1/substrafl-0.35.1.tar.gz

---
+++ packages/substrafl/substrafl-0.35.1.tar.gz/substrafl-0.35.1/substrafl/remote/register/__init__.py
@@ -0,0 +1,7 @@
+from substrafl.remote.register.register import add_metric
+from substrafl.remote.register.register import register_function
+
+__all__ = [
+    "register_function",
+    "add_metric",
+]
