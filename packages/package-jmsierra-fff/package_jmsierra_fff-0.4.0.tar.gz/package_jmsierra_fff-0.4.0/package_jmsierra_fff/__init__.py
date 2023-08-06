eefd604256bf3a688805bdd6a08a00d0006fafa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:21 +0000

    Add package package-jmsierra-fff/0.4.0/package_jmsierra_fff-0.4.0.tar.gz

---
+++ packages/package-jmsierra-fff/package_jmsierra_fff-0.4.0.tar.gz/package_jmsierra_fff-0.4.0/package_jmsierra_fff/__init__.py
@@ -0,0 +1,12 @@
+#from ._sumar import add4
+from ._restar import add3
+from ._multiplicar import add2
+from ._dividir import add1
+
+
+__all__ = [
+    "add1",
+    "add2",
+    "add3"]
+    #,
+    #"add4"]
