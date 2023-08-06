64f7f6aee79bfdc36852bf9f44e2f6164b69d99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:46 +0000

    Add package ImgProcessZ/1.0.0/ImgProcessZ-1.0.0.tar.gz

---
+++ packages/ImgProcessZ/ImgProcessZ-1.0.0.tar.gz/ImgProcessZ-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ImgProcessZ',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://blog.csdn.net/qq_40672115/article/details/127012332?spm=1001.2014.3001.5502',
+    license='MIT',
+    author='Z',
+    author_email='1941981477@qq.com',
+    description='A simple module for image preprocess',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8'
+    ],
+    install_requires=[
+        'numpy',
+        'opencv-python'
+    ]
+)
