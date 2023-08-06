e558faee7af2df5c1888ba5d9683d5992dea7c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:47 +0000

    Add package WaBach/0.1.4/WaBach-0.1.4.tar.gz

---
+++ packages/WaBach/WaBach-0.1.4.tar.gz/WaBach-0.1.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+setuptools.setup(
+    name="WaBach",
+    version="0.1.4",
+    description="Implementation of the Weyl tensor and the Bach tensor into Gravipy",
+    url="",
+    author="Simon Knoska",
+    author_email="knoskas@gmail.com",
+    license="BSD",
+    packages=["wabach"],
+    include_package_data=True,
+    install_requires=["GraviPy == 0.2.0"],
+    platforms="any",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
