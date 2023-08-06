9ddc58f1c353d29e1a2a8506aad442fc8f04a80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:07 +0000

    Add package nwslpy/0.0.0.7000/nwslpy-0.0.0.7000.tar.gz

---
+++ packages/nwslpy/nwslpy-0.0.0.7000.tar.gz/nwslpy-0.0.0.7000/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='nwslpy',
+      version='0.0.0.7000',
+      description='Python wrapper around the nwslR library',
+      packages=['nwslpy'],
+      requires=[
+          "pandas",
+      ])
