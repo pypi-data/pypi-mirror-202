5672689b4d038bd776faea1ff0c23e85657b6a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:38 +0000

    Add package pyfakefs/5.2.1/pyfakefs-5.2.1.tar.gz

---
+++ packages/pyfakefs/pyfakefs-5.2.1.tar.gz/pyfakefs-5.2.1/pyfakefs/pytest_plugin.py
@@ -0,0 +1,66 @@
+"""A pytest plugin for using pyfakefs as a fixture
+
+When pyfakefs is installed, the "fs" fixture becomes available.
+
+:Usage:
+
+def my_fakefs_test(fs):
+    fs.create_file('/var/data/xx1.txt')
+    assert os.path.exists('/var/data/xx1.txt')
+"""
+import py
+import pytest
+
+from pyfakefs.fake_filesystem_unittest import Patcher
+
+Patcher.SKIPMODULES.add(py)
+Patcher.SKIPMODULES.add(pytest)
+
+
+@pytest.fixture
+def fs(request):
+    """Fake filesystem."""
+    if hasattr(request, "param"):
+        # pass optional parameters via @pytest.mark.parametrize
+        patcher = Patcher(*request.param)
+    else:
+        patcher = Patcher()
+    patcher.setUp()
+    yield patcher.fs
+    patcher.tearDown()
+
+
+@pytest.fixture(scope="class")
+def fs_class(request):
+    """Class-scoped fake filesystem fixture."""
+    if hasattr(request, "param"):
+        patcher = Patcher(*request.param)
+    else:
+        patcher = Patcher()
+    patcher.setUp()
+    yield patcher.fs
+    patcher.tearDown()
+
+
+@pytest.fixture(scope="module")
+def fs_module(request):
+    """Module-scoped fake filesystem fixture."""
+    if hasattr(request, "param"):
+        patcher = Patcher(*request.param)
+    else:
+        patcher = Patcher()
+    patcher.setUp()
+    yield patcher.fs
+    patcher.tearDown()
+
+
+@pytest.fixture(scope="session")
+def fs_session(request):
+    """Session-scoped fake filesystem fixture."""
+    if hasattr(request, "param"):
+        patcher = Patcher(*request.param)
+    else:
+        patcher = Patcher()
+    patcher.setUp()
+    yield patcher.fs
+    patcher.tearDown()
