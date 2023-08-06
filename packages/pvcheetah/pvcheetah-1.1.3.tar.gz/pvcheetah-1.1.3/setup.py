608276f80b81c9a19568ce6b520be2ca673eabcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:47 +0000

    Add package pvcheetah/1.1.3/pvcheetah-1.1.3.tar.gz

---
+++ packages/pvcheetah/pvcheetah-1.1.3.tar.gz/pvcheetah-1.1.3/setup.py
@@ -0,0 +1,73 @@
+#
+# Copyright 2022 Picovoice Inc.
+#
+# You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+# file accompanying this source.
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+
+import os
+import shutil
+
+import setuptools
+
+os.system('git clean -dfx')
+
+package_folder = os.path.join(os.path.dirname(__file__), 'pvcheetah')
+os.mkdir(package_folder)
+
+shutil.copy(os.path.join(os.path.dirname(__file__), '../../LICENSE'), package_folder)
+
+shutil.copy(os.path.join(os.path.dirname(__file__), '__init__.py'), os.path.join(package_folder, '__init__.py'))
+shutil.copy(os.path.join(os.path.dirname(__file__), '_cheetah.py'), os.path.join(package_folder, '_cheetah.py'))
+shutil.copy(os.path.join(os.path.dirname(__file__), '_factory.py'), os.path.join(package_folder, '_factory.py'))
+shutil.copy(os.path.join(os.path.dirname(__file__), '_util.py'), os.path.join(package_folder, '_util.py'))
+
+platforms = ('jetson', 'linux', 'mac', 'raspberry-pi', 'windows')
+
+os.mkdir(os.path.join(package_folder, 'lib'))
+for platform in ('common',) + platforms:
+    shutil.copytree(
+        os.path.join(os.path.dirname(__file__), '../../lib', platform),
+        os.path.join(package_folder, 'lib', platform))
+
+MANIFEST_IN = """
+include pvcheetah/LICENSE
+include pvcheetah/__init__.py
+include pvcheetah/_cheetah.py
+include pvcheetah/_factory.py
+include pvcheetah/_util.py
+recursive-include pvcheetah/lib/ *
+"""
+
+with open(os.path.join(os.path.dirname(__file__), 'MANIFEST.in'), 'w') as f:
+    f.write(MANIFEST_IN.strip('\n '))
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pvcheetah",
+    version="1.1.3",
+    author="Picovoice",
+    author_email="hello@picovoice.ai",
+    description="Cheetah Speech-to-Text Engine.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Picovoice/cheetah",
+    packages=["pvcheetah"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Sound/Audio :: Speech"
+    ],
+    python_requires='>=3.5',
+    keywords="Speech-to-Text, Speech Recognition, Voice Recognition, ASR, Automatic Speech Recognition",
+)
