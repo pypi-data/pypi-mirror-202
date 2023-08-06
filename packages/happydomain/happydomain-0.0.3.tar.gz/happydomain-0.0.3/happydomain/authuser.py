e167a7d8ba6f751c91674f0942d386a365447ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:15 +0000

    Add package happydomain/0.0.3/happydomain-0.0.3.tar.gz

---
+++ packages/happydomain/happydomain-0.0.3.tar.gz/happydomain-0.0.3/happydomain/authuser.py
@@ -0,0 +1,31 @@
+class AuthUser:
+
+    def __init__(self, _session, Id, Email, EmailVerification, Password, CreatedAt, LastLoggedIn, AllowCommercials):
+        self._session = _session
+
+        self.Id = Id
+        self.Email = Email
+        self.EmailVerification = EmailVerification
+        self.Password = Password
+        self.CreatedAt = CreatedAt
+        self.LastLoggedIn = LastLoggedIn
+        self.AllowCommercials = AllowCommercials
+
+    def Delete(self):
+        self._session.authuser_delete(self.Id)
+
+    def ResetPassword(self, *args, **kwargs):
+        r = self.session.post(
+            "http+unix://" + self.socket_path + "/api/auth/" + quote_plus(Id) + "/reset_password",
+            data=json.dumps({
+                "password": NewPassword,
+            })
+        )
+
+        if r.status_code != 200 and r.status_code != 406:
+            raise HappyError(r.status_code, **r.json())
+
+        return r.status_code == 200
+
+    def Update(self):
+        self._session.authuser_update(self.Id, self)
