545993a87846549dbdcd4cfcf69a2232aed4ea7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:51 +0000

    Add package simple-sharepoint/0.0.1/simple-sharepoint-0.0.1.tar.gz

---
+++ packages/simple-sharepoint/simple-sharepoint-0.0.1.tar.gz/simple-sharepoint-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simple-sharepoint",
+    version="0.0.1",
+    author="sprumin",
+    author_email="sprumin@wellbia.com",
+    description="Simple way to handle sharepoint with python",
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
