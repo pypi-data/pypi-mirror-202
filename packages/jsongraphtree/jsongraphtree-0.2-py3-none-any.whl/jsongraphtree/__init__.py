34588d5587b3e5eb3f986977e0e98a3e96ffaa17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:58 +0000

    Add package jsongraphtree/0.2/jsongraphtree-0.2-py3-none-any.whl

---
+++ packages/jsongraphtree/jsongraphtree-0.2-py3-none-any.whl/jsongraphtree/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from .jsongraphtree import *
+import argparse
+import json
+from graphviz import Digraph
+
+name="jsongraphtree"
+
+def output():
+     print("测试")
+
+def ok(a):
+     print("ok,测试成功"+str(a))
+
+
+
+     
