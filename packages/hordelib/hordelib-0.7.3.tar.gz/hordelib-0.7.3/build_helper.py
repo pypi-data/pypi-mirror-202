32d43cce88d28df012e98f948489bfd10f410984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:54 +0000

    Add package hordelib/0.7.3/hordelib-0.7.3.tar.gz

---
+++ packages/hordelib/hordelib-0.7.3.tar.gz/hordelib-0.7.3/build_helper.py
@@ -0,0 +1,67 @@
+# build_helper.py
+# This is just a build helper script to build the pypi package.
+import os
+import shutil
+import subprocess
+
+from hordelib import install_comfy
+from hordelib.consts import COMFYUI_VERSION
+
+
+def run(command):
+    result = subprocess.run(command, shell=True, text=True)
+    if result.returncode:
+        raise Exception(result.stderr)
+
+
+def patch_requirements(unpatch=False):
+    with open("requirements.txt") as infile:
+        data = infile.readlines()
+    newfile = []
+    for line in data:
+        if not unpatch and line.startswith("--"):
+            newfile.append(f"#{line}")
+        elif unpatch and line.startswith("#--"):
+            newfile.append(f"{line[1:]}")
+        else:
+            newfile.append(line)
+    with open("requirements.txt", "w") as outfile:
+        outfile.writelines(newfile)
+
+
+def patch_toml(unpatch=False):
+    with open("pyproject.toml") as infile:
+        data = infile.readlines()
+    newfile = []
+    for line in data:
+        if not unpatch and line.startswith('dynamic=["version"]'):
+            newfile.append(f"#{line}")
+        elif not unpatch and line.startswith('#dynamic=["version", "dependencies"]'):
+            newfile.append(f"{line[1:]}")
+        elif unpatch and line.startswith('#dynamic=["version"]'):
+            newfile.append(f"{line[1:]}")
+        elif unpatch and line.startswith('dynamic=["version", "dependencies"]'):
+            newfile.append(f"#{line}")
+        elif not unpatch and line.startswith("#write_to ="):
+            newfile.append(f"{line[1:]}")
+        elif unpatch and line.startswith("write_to ="):
+            newfile.append(f"#{line}")
+        else:
+            newfile.append(line)
+    with open("pyproject.toml", "w") as outfile:
+        outfile.writelines(newfile)
+
+
+def static_package_comfyui():
+    installer = install_comfy.Installer()
+    installer.install(COMFYUI_VERSION)
+
+    if not os.path.exists("ComfyUI"):
+        raise Exception("ComfyUI not found")
+    shutil.copytree("ComfyUI", "hordelib/_comfyui")
+
+
+if __name__ == "__main__":
+    static_package_comfyui()
+    patch_requirements()
+    patch_toml()
---
+++ packages/hordelib/hordelib-0.7.3.tar.gz/hordelib-0.7.3/hordelib/model_manager/safety_checker.py
@@ -0,0 +1,41 @@
+import typing
+
+import torch
+from diffusers.pipelines.stable_diffusion.safety_checker import (
+    StableDiffusionSafetyChecker,
+)
+from loguru import logger
+
+from hordelib.consts import MODEL_CATEGORY_NAMES, MODEL_DB_NAMES
+from hordelib.model_manager.base import BaseModelManager
+
+
+class SafetyCheckerModelManager(BaseModelManager):
+    def __init__(self, download_reference=False):
+        super().__init__(
+            models_db_name=MODEL_DB_NAMES[MODEL_CATEGORY_NAMES.safety_checker],
+            download_reference=download_reference,
+        )
+
+    def modelToRam(
+        self,
+        model_name,
+        half_precision=True,
+        gpu_id=0,
+        cpu_only=True,  # XXX # FIXME These extra parameters are never able to be called from `load`
+    ) -> dict[str, typing.Any]:
+        if not self.cuda_available:
+            cpu_only = True
+        if cpu_only:
+            device = torch.device("cpu")
+            half_precision = False
+        else:
+            device = torch.device(f"cuda:{gpu_id}" if self.cuda_available else "cpu")
+        logger.info(f"Loading model {model_name} on {device}")
+        logger.info(f"Model path: {self.modelFolderPath}")
+        model = StableDiffusionSafetyChecker.from_pretrained(self.modelFolderPath)
+        model = model.eval()
+        model.to(device)
+        if half_precision:
+            model = model.half()
+        return {"model": model, "device": device, "half_precision": half_precision}
