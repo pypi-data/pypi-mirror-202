ca43c71f2f428d1b1d18aee9497c3be12e274a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:46 +0000

    Add package ronaldosLibraries/1.0/ronaldosLibraries-1.0.tar.gz

---
+++ packages/ronaldosLibraries/ronaldosLibraries-1.0.tar.gz/ronaldosLibraries-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ronaldosLibraries',
+    version='1.0',
+    description='myLibrary',
+    author='Jordan Torske',
+    author_email='jordan.torske@ucalgary.ca',
+    packages=find_packages(),
+    install_requires=[],
+)
