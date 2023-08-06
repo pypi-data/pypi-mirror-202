43d51550c1295d943799481581b6c17a803ce989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:03 +0000

    Add package libgpiod/2.0.1/libgpiod-2.0.1.tar.gz

---
+++ packages/libgpiod/libgpiod-2.0.1.tar.gz/libgpiod-2.0.1/setup.py
@@ -0,0 +1,60 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2022 Bartosz Golaszewski <brgl@bgdev.pl>
+
+from os import environ, path
+from setuptools import setup, Extension, find_packages
+
+def src(filename):
+    return path.join(path.dirname(__file__), filename)
+
+gpiod_ext = Extension(
+    "gpiod._ext",
+    sources=[
+        src("gpiod/ext/chip.c"),
+        src("gpiod/ext/common.c"),
+        src("gpiod/ext/line-config.c"),
+        src("gpiod/ext/line-settings.c"),
+        src("gpiod/ext/module.c"),
+        src("gpiod/ext/request.c"),
+    ],
+    define_macros=[("_GNU_SOURCE", "1")],
+    libraries=["gpiod"],
+    extra_compile_args=["-Wall", "-Wextra"],
+)
+
+gpiosim_ext = Extension(
+    "tests.gpiosim._ext",
+    sources=[src("tests/gpiosim/ext.c")],
+    define_macros=[("_GNU_SOURCE", "1")],
+    libraries=["gpiosim"],
+    extra_compile_args=["-Wall", "-Wextra"],
+)
+
+procname_ext = Extension(
+    "tests.procname._ext",
+    sources=[src("tests/procname/ext.c")],
+    define_macros=[("_GNU_SOURCE", "1")],
+    extra_compile_args=["-Wall", "-Wextra"],
+)
+
+extensions = [gpiod_ext]
+if "GPIOD_WITH_TESTS" in environ and environ["GPIOD_WITH_TESTS"] == "1":
+    extensions.append(gpiosim_ext)
+    extensions.append(procname_ext)
+
+with open(src("gpiod/version.py"), "r") as fd:
+    exec(fd.read())
+
+setup(
+    name="libgpiod",
+    packages=find_packages(exclude=["tests"]),
+    ext_modules=extensions,
+    version=__version__,
+    author="Bartosz Golaszewski",
+    author_email="brgl@bgdev.pl",
+    description="Python bindings for libgpiod",
+    long_description="This is a package spun out of the main libgpiod repository containing " \
+                     "python bindings to the underlying C library.",
+    platforms=["linux"],
+    license="LGPLv2.1",
+)
