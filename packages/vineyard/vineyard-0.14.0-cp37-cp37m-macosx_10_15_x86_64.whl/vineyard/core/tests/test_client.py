c48defbbd52adea00cd5c3881fa496ca56feca48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:29 +0000

    Add package vineyard/0.14.0/vineyard-0.14.0-cp37-cp37m-macosx_10_15_x86_64.whl

---
+++ packages/vineyard/vineyard-0.14.0-cp37-cp37m-macosx_10_15_x86_64.whl/vineyard/core/tests/test_client.py
@@ -0,0 +1,97 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2023 Alibaba Group Holding Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from vineyard._C import ObjectMeta
+from vineyard.core import default_builder_context
+from vineyard.core import default_resolver_context
+from vineyard.data import register_builtin_types
+
+register_builtin_types(default_builder_context, default_resolver_context)
+
+
+def test_metadata(vineyard_client):
+    xid = vineyard_client.put(1.2345)
+    yid = vineyard_client.put(2.3456)
+    meta = ObjectMeta()
+    meta['typename'] = 'vineyard::Pair'
+    meta.add_member('first_', xid)
+    meta.add_member('second_', vineyard_client.get_meta(yid))
+    meta.set_global(False)
+    rmeta = vineyard_client.create_metadata(meta)
+    vineyard_client.persist(rmeta)
+
+    def go(meta):
+        for k, v in meta.items():
+            if isinstance(v, ObjectMeta):
+                go(v)
+            else:
+                print('k-v in meta: ', k, v)
+
+    meta = vineyard_client.get_meta(rmeta.id)
+    go(meta)
+    go(meta)
+    go(meta)
+    go(meta)
+
+
+def test_metadata_global(vineyard_client):
+    xid = vineyard_client.put(1.2345)
+    yid = vineyard_client.put(2.3456)
+    meta = ObjectMeta()
+    meta['typename'] = 'vineyard::Pair'
+    meta.add_member('first_', xid)
+    meta.add_member('second_', vineyard_client.get_meta(yid))
+    meta.set_global(True)
+    rmeta = vineyard_client.create_metadata(meta)
+    vineyard_client.persist(rmeta)
+
+    def go(meta):
+        for k, v in meta.items():
+            if isinstance(v, ObjectMeta):
+                go(v)
+            else:
+                print('k-v in meta: ', k, v)
+
+    meta = vineyard_client.get_meta(rmeta.id)
+    go(meta)
+    go(meta)
+    go(meta)
+    go(meta)
+
+
+def test_persist(vineyard_client):
+    xid = vineyard_client.put(1.2345)
+    yid = vineyard_client.put(2.3456)
+    meta = ObjectMeta()
+    meta['typename'] = 'vineyard::Pair'
+    meta.add_member('first_', xid)
+    meta.add_member('second_', yid)
+    meta.set_global(True)
+    rmeta = vineyard_client.create_metadata(meta)
+    vineyard_client.persist(rmeta)
+
+
+def test_persist_multiref(vineyard_client):
+    xid = vineyard_client.put(1.2345)
+    meta = ObjectMeta()
+    meta['typename'] = 'vineyard::Pair'
+    meta.add_member('first_', xid)
+    meta.add_member('second_', xid)
+    meta.set_global(True)
+    rmeta = vineyard_client.create_metadata(meta)
+    vineyard_client.persist(rmeta)
