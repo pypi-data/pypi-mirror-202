0fef6fa72b999f4d1ad177a9a2661523ccdd05af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:20 +0000

    Add package etica/0.0.1/etica-0.0.1.tar.gz

---
+++ packages/etica/etica-0.0.1.tar.gz/etica-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='etica',
+    version='0.0.1',
+    description='Etica ',
+    url='https://github.com/etica',
+    author='EticaProtocol',
+    author_email='info@eticaprotocol.org',
+    license='MIT',
+    packages=['etica'],
+    zip_safe=False
+)
