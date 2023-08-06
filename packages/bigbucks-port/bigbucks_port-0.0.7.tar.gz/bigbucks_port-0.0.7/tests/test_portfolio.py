f362f4bedad0a9bf62f0648bb0d9eff05c5d0f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:04 +0000

    Add package bigbucks-port/0.0.7/bigbucks_port-0.0.7.tar.gz

---
+++ packages/bigbucks-port/bigbucks_port-0.0.7.tar.gz/bigbucks_port-0.0.7/tests/test_portfolio.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+import sys
+sys.path.append('../src/bigbucks_port')
+import portfolio
+
+from bigbucks_db import *
+
+# # Example data
+# er = np.array([.05, .04, .03])
+# sd = np.array([.2, .1, .05])
+# corr = [[1,.5,0],
+#         [.5,1,.5],
+#         [0,.5,1]]
+# covar = np.diag(sd) @ corr @ np.diag(sd)
+
+
+objs_realtime = Buy_And_Sell(STOCK_API_KEYS)
+objs = Table_View(url, key)
+
+print(portfolio.frontier_json(objs,6,5))
