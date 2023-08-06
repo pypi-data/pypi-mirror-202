d09abfcb72c59e4f24c469ce7eae14a22d724f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:14 +0000

    Add package coso/1.0.0/coso-1.0.0.tar.gz

---
+++ packages/coso/coso-1.0.0.tar.gz/coso-1.0.0/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='coso',
+    version='1.0.0',    
+    url='https://github.com/PietroTotis/CoSo',
+    author='Pietro Totis',
+    author_email='pietro.totis@kuleuven.be',
+    license='GNU General Public License (GPL)',
+    packages=['src'],
+    install_requires=["portion",
+                      "clingo==5.5.1",
+                      "psutil",
+                      "yattag",
+                      "ply"],    
+    description = ("A solver for combinatorics math word problems."),
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+    keywords="combinatorics automated reasoning",
+)
