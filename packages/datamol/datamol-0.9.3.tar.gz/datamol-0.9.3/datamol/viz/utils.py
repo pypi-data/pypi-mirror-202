8a82ad63444f7223f96bc31e6377150a450a8204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:07 +0000

    Add package datamol/0.9.3/datamol-0.9.3.tar.gz

---
+++ packages/datamol/datamol-0.9.3.tar.gz/datamol-0.9.3/datamol/viz/utils.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from rdkit.Chem import Draw
+
+import datamol as dm
+
+
+def prepare_mol_for_drawing(mol: Optional[dm.Mol], kekulize: bool = True) -> Optional[dm.Mol]:
+    """Prepare the molecule before drawing to avoid any error due to unsanitized molecule
+    or incorrect valence or aromaticity.
+
+    Code is inspired from `rdkit.Chem.Draw._moltoimg`.
+
+    Args:
+        mol: A molecule to prepare. If set to None, the function will return None.
+        kekulize: Whether to kekulize the molecule.
+    """
+
+    if mol is None:
+        return None
+
+    try:
+        with dm.without_rdkit_log():
+            # Check for implicit and explicit valence
+            if mol.NeedsUpdatePropertyCache():  # type: ignore
+                mol.UpdatePropertyCache(False)  # type: ignore
+
+            # Check for aromaticity
+            if dm.is_lower_than_current_rdkit_version("2022.09"):
+                _kekulize = Draw._okToKekulizeMol(mol, kekulize)
+            else:
+                _kekulize = Draw.shouldKekulize(mol, kekulize)
+
+            # Run the rdkit preparation procedure
+            _mol = Draw.rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=_kekulize)
+
+    except ValueError:  # <- can happen on a kekulization failure
+        # Run the rdkit preparation procedure with kekulize set to `False`
+        _mol = Draw.rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=False)
+
+    return _mol
