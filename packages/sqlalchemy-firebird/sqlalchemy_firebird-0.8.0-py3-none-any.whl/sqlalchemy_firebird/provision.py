0fb2648e45d73226afcb7aef800f8bd635678b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:08 +0000

    Add package sqlalchemy-firebird/0.8.0/sqlalchemy_firebird-0.8.0-py3-none-any.whl

---
+++ packages/sqlalchemy-firebird/sqlalchemy_firebird-0.8.0-py3-none-any.whl/sqlalchemy_firebird/provision.py
@@ -0,0 +1,12 @@
+from sqlalchemy.testing.provision import temp_table_keyword_args
+
+
+@temp_table_keyword_args.for_db("firebird")
+def _firebird_temp_table_keyword_args(cfg, eng):
+    return {
+        "prefixes": ["GLOBAL TEMPORARY"],
+        "firebird_on_commit": "PRESERVE ROWS",
+    }
+
+
+# TODO:  try overriding the generate_db_urls from sqlalchemy here
