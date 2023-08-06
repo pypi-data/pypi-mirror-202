358dc4a5f8cd34c41d36c9ac163bbda9670a911b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:12 +0000

    Add package youte/2.1.0/youte-2.1.0-py3-none-any.whl

---
+++ packages/youte/youte-2.1.0-py3-none-any.whl/youte/exceptions.py
@@ -0,0 +1,30 @@
+class InvalidFileName(Exception):
+    pass
+    # def __str__(self):
+    #     return "Incorrect file extension or file does not exist."
+
+
+class InvalidRequest(Exception):
+    pass
+
+
+class InvalidRequestParameter(Exception):
+    pass
+    # def __str__(self):
+    #     return "Paramater fields are invalid for this request."
+
+
+class StopCollector(Exception):
+    pass
+
+
+class ValueAlreadyExists(Exception):
+    pass
+
+
+class APIError(Exception):
+    pass
+
+
+class CommentsDisabled(Exception):
+    pass
