e5f6219e451840d0a86f0b406805ff1b37496c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:50 +0000

    Add package dxsp/0.0.39/dxsp-0.0.39-py3-none-any.whl

---
+++ packages/dxsp/dxsp-0.0.39-py3-none-any.whl/dxsp/assets/blockchains.py
@@ -0,0 +1,162 @@
+blockchains = {
+    #  ETHEREUM
+    1: {
+        "url": "https://api.etherscan.io/api?",
+        "rpc": "https://rpc.ankr.com/eth",
+    },
+    # ETHEREUM Ropsten
+    3: {
+        "url": "https://api-Ropsten.etherscan.io/api?",
+        "rpc": "https://rpc.ankr.com/eth",
+    },
+    # ETHEREUM Rinkeby
+    4: {
+        "url": "https://api-Rinkeby.etherscan.io/api?",
+        "rpc": "https://rpc.ankr.com/eth",
+    },
+    # ETHEREUM Gorli
+    5: {
+        "url": "https://api-Goerli.etherscan.io/api?",
+        "rpc": "https://rpc.ankr.com/eth",
+    },
+    # Ethereum Classic Kotti
+    6: {
+        "url": "https://blockscout.com/etc/kotti/api?",
+        "rpc": "https://rpc.ankr.com/eth",
+    },
+    # Optimism
+    10: {
+        "url": "https://api-optimistic.etherscan.io/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # CRONOS
+    25: {
+        "url": "https://api.cronoscan.com/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # RSK
+    30: {
+        "url": "https://blockscout.com/rsk/mainnet/api?",
+        "rpc": "https://rpc.ankr.com/bsc"
+    },
+    # ETHEREUM Kovan
+    42: {
+        "url": "https://api-Kovan.etherscan.io/api?",
+        "rpc": "https://rpc.ankr.com/bsc"
+    },
+    # Binance Smart Chain
+    56: {
+        "url": "https://api.bscscan.com/api?",
+        "rpc": "https://rpc.ankr.com/bsc"
+    },
+    # Optimism kovan
+    69: {
+        "url": "https://api-kovan-optimistic.etherscan.io/api?",
+        "rpc": "https://rpc.ankr.com/bsc"
+    },
+    # Hoo Smart Chain
+    70: {
+        "url": "https://hooscan.com/api?",
+        "rpc": "https://rpc.ankr.com/bsc"
+    },
+    # Binance Smart Chain Testnet
+    97: {
+        "url": "https://api-testnet.bscscan.com/api?",
+        "rpc": "https://rpc.ankr.com/bsc"
+    },
+    # Gnosis
+    100: {
+        "url": "https://blockscout.com/xdai/mainnet/api?",
+        "rpc": "https://rpc.ankr.com/gnosis"
+    },
+    # Polygon
+    137: {
+        "url": "https://api.polygonscan.com/api?",
+        "rpc": "https://rpc.ankr.com/polygon",
+    },
+    # Fantom
+    250: {
+        "url": "https://api.ftmscan.com/api?",
+        "rpc": "https://rpc.ankr.com/fantom",
+    },
+    # Cronos Testnet
+    338: {
+        "url": "https://api-testnet.cronoscan.com/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    #  Moonbeam
+    1284: {
+        "url": "https://api-moonbeam.moonscan.io/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    #  Moonriver
+    1285: {
+        "url": "https://api-moonriver.moonscan.io/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    #  Moonbase
+    1287: {
+        "url": "https://api-moonbase.moonscan.io/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # Fantom Testnet
+    4002: {
+        "url": "https://api-testnet.ftmscan.com/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # Arbitrum One
+    42161: {
+        "url": "https://api.arbiscan.io/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # Celo
+    42220: {
+        "url": "https://api.celoscan.xyz/api?",
+        "rpc": "https://rpc.ankr.com/celo",
+    },
+    #  Avalanche Fuji Testnet
+    43113: {
+        "url": "https://api-testnet.snowtrace.io/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # Avalanche
+    43114: {
+        "url": "https://api.snowtrace.io/api?",
+        "rpc": "https://rpc.ankr.com/avalanche",
+    },
+    # Celo Alfajores
+    44787: {
+        "url": "https://api-alfajores.celoscan.xyz/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # Polygon Mumbai
+    80001: {
+        "url": "https://api-testnet.polygonscan.com/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # Artis sigma1
+    246529: {
+        "url": "https://blockscout.com/artis/sigma1/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # Arbitrum Rinkeby
+    421611: {
+        "url": "https://api-testnet.arbiscan.io/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # ETHEREUM Sepolia
+    11155111: {
+        "url": "https://api-Sepolia.etherscan.io/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # Aurora
+    1313161554: {
+        "url": "https://api.aurorascan.dev/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+    # Aurora Testnet
+    1313161555: {
+        "url": "https://api-testnet.aurorascan.dev/api?",
+        "rpc": "https://rpc.ankr.com/optimism",
+    },
+}
