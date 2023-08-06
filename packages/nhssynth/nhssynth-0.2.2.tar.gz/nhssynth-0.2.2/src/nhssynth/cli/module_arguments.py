92c99443cbcd2fc97ed544f2a42046dc32ead94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:04 +0000

    Add package nhssynth/0.2.2/nhssynth-0.2.2.tar.gz

---
+++ packages/nhssynth/nhssynth-0.2.2.tar.gz/nhssynth-0.2.2/src/nhssynth/cli/module_arguments.py
@@ -0,0 +1,213 @@
+import argparse
+
+from nhssynth.common.constants import *
+
+
+class customAction(argparse.Action):
+    """
+    Customized argparse action for defaulting to the full list of choices
+
+        1) If no `option_string` is supplied: set to default value (`self.default`)
+        2) If `option_string` is supplied:
+            A) If `values` are supplied:
+                set to list of values
+            B) If no `values` are supplied:
+                set to `self.const`, if `self.const` is not set, set to `self.default`
+    """
+
+    def __call__(self, parser, namespace, values=None, option_string=None):
+        if values:
+            setattr(namespace, self.dest, values)
+        elif option_string:
+            setattr(namespace, self.dest, self.const if self.const else self.default)
+        else:
+            setattr(namespace, self.dest, self.default)
+
+
+def add_dataloader_args(parser: argparse.ArgumentParser, group_title: str, overrides: bool = False) -> None:
+    """Adds arguments to an existing dataloader module sub-parser instance."""
+    group = parser.add_argument_group(title=group_title)
+    group.add_argument(
+        "--data-dir",
+        type=str,
+        default="./data",
+        help="the directory containing the chosen dataset",
+    )
+    group.add_argument(
+        "--index-col",
+        default=None,
+        choices=[None, 0],
+        help="indicate whether the csv file's 0th column is an index column, such that pandas can ignore it",
+    )
+    group.add_argument(
+        "--sdv-workflow",
+        action="store_true",
+        help="utilise the SDV synthesizer workflow for transformation and metadata, rather than a `HyperTransformer` from RDT",
+    )
+    group.add_argument(
+        "--allow-null-transformers",
+        action="store_true",
+        help="allow null / None transformers, i.e. leave some columns as they are",
+    )
+    group.add_argument(
+        "--collapse-yaml",
+        action="store_true",
+        help="use aliases and anchors in the output metadata yaml, this will make it much more compact",
+    )
+    # TODO might be good to have something like this, needs some thought in how to only apply to appropriate transformers, without overriding metadata
+    # group.add_argument(
+    #     "--imputation-strategy",
+    #     default=None,
+    #     help="imputation strategy for missing values, pick one of None, `mean`, `mode`, or a number",
+    # )
+    group.add_argument(
+        "--synthesizer",
+        type=str,
+        default="TVAE",
+        choices=list(SDV_SYNTHESIZER_CHOICES.keys()),
+        help="pick a synthesizer to use (note this can also be specified in the model module, these must match)",
+    )
+
+
+def add_structure_args(parser: argparse.ArgumentParser, group_title: str, overrides: bool = False) -> None:
+    pass
+
+
+def add_model_args(parser: argparse.ArgumentParser, group_title: str, overrides: bool = False) -> None:
+    """Adds arguments to an existing model module sub-parser instance."""
+    group = parser.add_argument_group(title=group_title)
+    group.add_argument(
+        "--model-file",
+        type=str,
+        default="_model",
+        help="specify the filename of the model to be saved in `experiments/<EXPERIMENT_NAME>/`, defaults to `<args.real_data>_model.pt`",
+    )
+    group.add_argument(
+        "--use-gpu",
+        action="store_true",
+        help="use the GPU for training",
+    )
+    group.add_argument(
+        "--non-private-training",
+        action="store_true",
+        help="train the model in a non-private way",
+    )
+    group.add_argument(
+        "--secure-rng",
+        action="store_true",
+        default=False,
+        help="Enable Secure RNG to have trustworthy privacy guarantees. Comes at a performance cost",
+    )
+    group.add_argument(
+        "--num-epochs",
+        type=int,
+        default=100,
+        help="number of epochs to train for",
+    )
+    group.add_argument(
+        "--tracked-metrics",
+        type=str,
+        nargs="+",
+        default=TRACKED_METRIC_CHOICES,
+        help="metrics to track during training of the DPVAE model",
+        choices=TRACKED_METRIC_CHOICES,
+    )
+    group.add_argument(
+        "--latent-dim",
+        type=int,
+        default=256,
+        help="the latent dimension of the model",
+    )
+    group.add_argument(
+        "--hidden-dim",
+        type=int,
+        default=256,
+        help="the hidden dimension of the model",
+    )
+    group.add_argument(
+        "--learning-rate",
+        type=float,
+        default=1e-3,
+        help="the learning rate for the model",
+    )
+    group.add_argument(
+        "--batch-size",
+        type=int,
+        default=32,
+        help="the batch size for the model",
+    )
+    group.add_argument(
+        "--patience",
+        type=int,
+        default=5,
+        help="how many epochs the model is allowed to train for without improvement",
+    )
+    group.add_argument(
+        "--delta",
+        type=int,
+        default=10,
+        help="the difference in successive ELBO values that register as an 'improvement'",
+    )
+    group.add_argument(
+        "--target-epsilon",
+        type=float,
+        default=1.0,
+        help="the target epsilon for differential privacy",
+    )
+    group.add_argument(
+        "--target-delta",
+        type=float,
+        default=1e-5,
+        help="the target delta for differential privacy",
+    )
+    group.add_argument(
+        "--max-grad-norm",
+        type=int,
+        default=10,
+        help="the clipping threshold for gradients (only relevant under differential privacy)",
+    )
+
+
+def generate_evaluation_arg(group, name):
+    group.add_argument(
+        f"--{'-'.join(name.split()).lower()}-metrics",
+        type=str,
+        default=None,
+        nargs="*",
+        action=customAction,
+        const=list(SDV_METRIC_CHOICES[name].keys()),
+        choices=list(SDV_METRIC_CHOICES[name].keys()),
+        help=f"run the {name.lower()} evaluation",
+    )
+
+
+def add_evaluation_args(parser: argparse.ArgumentParser, group_title: str, overrides: bool = False) -> None:
+    """Adds arguments to an existing evaluation module sub-parser instance."""
+    group = parser.add_argument_group(title=group_title)
+    group.add_argument(
+        "--diagnostic",
+        action="store_true",
+        help="run the diagnostic evaluation",
+    )
+    group.add_argument(
+        "--quality",
+        action="store_true",
+        help="run the quality evaluation",
+    )
+    for name in SDV_METRIC_CHOICES:
+        generate_evaluation_arg(group, name)
+
+
+def add_plotting_args(parser: argparse.ArgumentParser, group_title: str, overrides: bool = False) -> None:
+    """Adds arguments to an existing plotting module sub-parser instance."""
+    group = parser.add_argument_group(title=group_title)
+    group.add_argument(
+        "--plot-sdv-report",
+        action="store_true",
+        help="plot the SDV report",
+    )
+    group.add_argument(
+        "--plot-tsne",
+        action="store_true",
+        help="plot the t-SNE embeddings of the real and synthetic data",
+    )
---
+++ packages/nhssynth/nhssynth-0.2.2.tar.gz/nhssynth-0.2.2/src/nhssynth/modules/model/DPVAE.py
@@ -0,0 +1,268 @@
+import time
+import warnings
+
+import opacus
+import torch
+import torch.nn as nn
+from torch.distributions.normal import Normal
+from tqdm import tqdm
+
+
+def setup_device(use_gpu):
+    if use_gpu:
+        if torch.cuda.is_available():
+            return torch.device("cuda:0")
+        else:
+            warnings.warn("`use_gpu` was provided but no GPU is available, using CPU")
+    return torch.device("cpu")
+
+
+class Encoder(nn.Module):
+    """Encoder, takes in x
+    and outputs mu_z, sigma_z
+    (diagonal Gaussian variational posterior assumed)
+    """
+
+    def __init__(
+        self,
+        input_dim,
+        latent_dim,
+        hidden_dim=32,
+        activation=nn.Tanh,
+        use_gpu=False,
+    ):
+        super().__init__()
+
+        self.device = setup_device(use_gpu)
+        output_dim = 2 * latent_dim
+        self.latent_dim = latent_dim
+
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            activation(),
+            nn.Linear(hidden_dim, hidden_dim),
+            activation(),
+            nn.Linear(hidden_dim, output_dim),
+        )
+
+    def forward(self, x):
+        outs = self.net(x)
+        mu_z = outs[:, : self.latent_dim]
+        logsigma_z = outs[:, self.latent_dim :]
+        return mu_z, logsigma_z
+
+
+class Decoder(nn.Module):
+    """Decoder, takes in z and outputs reconstruction"""
+
+    def __init__(
+        self,
+        latent_dim,
+        onehots=[[]],
+        singles=[],
+        hidden_dim=32,
+        activation=nn.Tanh,
+        use_gpu=False,
+    ):
+        super().__init__()
+
+        self.device = setup_device(use_gpu)
+        output_dim = len(singles) + sum([len(x) for x in onehots])
+        self.singles = singles
+        self.onehots = onehots
+
+        self.net = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim),
+            activation(),
+            nn.Linear(hidden_dim, hidden_dim),
+            activation(),
+            nn.Linear(hidden_dim, output_dim),
+        )
+
+    def forward(self, z):
+        return self.net(z)
+
+
+class Noiser(nn.Module):
+    def __init__(self, num_singles):
+        super().__init__()
+        self.output_logsigma_fn = nn.Linear(num_singles, num_singles, bias=True)
+        torch.nn.init.zeros_(self.output_logsigma_fn.weight)
+        torch.nn.init.zeros_(self.output_logsigma_fn.bias)
+        self.output_logsigma_fn.weight.requires_grad = False
+
+    def forward(self, X):
+        return self.output_logsigma_fn(X)
+
+
+class VAE(nn.Module):
+    """Combines encoder and decoder into full VAE model"""
+
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder.to(encoder.device)
+        self.decoder = decoder.to(decoder.device)
+        self.device = encoder.device
+        self.onehots = self.decoder.onehots
+        self.singles = self.decoder.singles
+        self.noiser = Noiser(len(self.singles)).to(decoder.device)
+
+    def reconstruct(self, X):
+        mu_z, logsigma_z = self.encoder(X)
+
+        x_recon = self.decoder(mu_z)
+        return x_recon
+
+    def generate(self, N):
+        z_samples = torch.randn_like(torch.ones((N, self.encoder.latent_dim)), device=self.device)
+        x_gen = self.decoder(z_samples)
+        x_gen_ = torch.ones_like(x_gen, device=self.device)
+
+        for cat_idxs in self.onehots:
+            x_gen_[:, cat_idxs] = torch.distributions.one_hot_categorical.OneHotCategorical(
+                logits=x_gen[:, cat_idxs]
+            ).sample()
+
+        x_gen_[:, self.singles] = x_gen[:, self.singles] + torch.exp(
+            self.noiser(x_gen[:, self.singles])
+        ) * torch.randn_like(x_gen[:, self.singles])
+        if torch.cuda.is_available():
+            x_gen_ = x_gen_.cpu()
+        return x_gen_.detach()
+
+    def loss(self, X):
+        mu_z, logsigma_z = self.encoder(X)
+
+        p = Normal(torch.zeros_like(mu_z), torch.ones_like(mu_z))
+        q = Normal(mu_z, torch.exp(logsigma_z))
+
+        kld = torch.sum(torch.distributions.kl_divergence(q, p))
+
+        s = torch.randn_like(mu_z)
+        z_samples = mu_z + s * torch.exp(logsigma_z)
+
+        x_recon = self.decoder(z_samples)
+
+        categoric_loglik = 0
+        if len(self.onehots):
+            for cat_idxs in self.onehots:
+                categoric_loglik += -torch.nn.functional.cross_entropy(
+                    x_recon[:, cat_idxs],
+                    torch.max(X[:, cat_idxs], 1)[1],
+                ).sum()
+
+        gauss_loglik = 0
+        if len(self.singles):
+            gauss_loglik = (
+                Normal(
+                    loc=x_recon[:, self.singles],
+                    scale=torch.exp(self.noiser(x_recon[:, self.singles])),
+                )
+                .log_prob(X[:, self.singles])
+                .sum()
+            )
+
+        reconstruction_loss = -(categoric_loglik + gauss_loglik)
+
+        elbo = kld + reconstruction_loss
+
+        return {
+            "ELBO": elbo,
+            "ReconstructionLoss": reconstruction_loss,
+            "KLD": kld,
+            "CategoricalLoss": categoric_loglik,
+            "NumericalLoss": gauss_loglik,
+        }
+
+    def train(
+        self,
+        x_dataloader: torch.utils.data.DataLoader,
+        num_epochs: int,
+        tracked_metrics: list[str] = ["ELBO"],
+        privacy_engine: opacus.PrivacyEngine = None,
+        patience: int = 5,
+        delta: int = 10,
+    ):
+        print("")
+
+        self.start_time = time.time()
+
+        if privacy_engine is not None:
+            self.privacy_engine = privacy_engine
+            self.privacy_engine.attach(self.optimizer)
+        elif "Privacy" in tracked_metrics:
+            tracked_metrics.remove("Privacy")
+
+        min_elbo = 0.0  # For early stopping workflow
+        stop_counter = 0  # Counter for stops
+
+        metrics = {metric: [] for metric in tracked_metrics}
+        stats_bars = {
+            metric: tqdm(total=0, desc="", position=i, bar_format="{desc}", leave=True)
+            for i, metric in enumerate(tracked_metrics)
+        }
+        max_length = max(len(s) for s in tracked_metrics) + 1
+        epoch_bar = tqdm(range(num_epochs), desc="Epochs", position=len(stats_bars), leave=False)
+
+        for epoch in epoch_bar:
+            for key in metrics.keys():
+                if key != "Privacy":
+                    metrics[key].append(0.0)
+
+            for (Y_subset,) in tqdm(x_dataloader, desc="Batches", position=len(stats_bars) + 1, leave=False):
+                self.optimizer.zero_grad()
+                losses = self.loss(Y_subset.to(self.encoder.device))
+                losses["ELBO"].backward()
+                self.optimizer.step()
+
+                for key in metrics.keys():
+                    if key in losses:
+                        if losses[key]:
+                            metrics[key][-1] += losses[key].item()
+                        else:
+                            metrics[key][-1] += 0.0
+
+            for key, stats_bar in stats_bars.items():
+                if key == "Privacy" and privacy_engine is not None:
+                    epsilon_e = self.privacy_engine.get_privacy_spent()
+                    # epsilon_e = self.privacy_engine.accountant.get_epsilon()
+                    stats_bar.set_description_str(
+                        f"{(key + ':').ljust(max_length)}  \u03B5 = {epsilon_e[0]:.2f}\tbest \u03B1 = {epsilon_e[1]:.2f}"
+                    )
+                    metrics["Privacy"].append(epsilon_e)
+                else:
+                    stats_bar.set_description_str(f"{(key + ':').ljust(max_length)}  {metrics[key][-1]:.2f}")
+
+            if epoch == 0:
+                min_elbo = metrics["ELBO"][-1]
+
+            if metrics["ELBO"][-1] < (min_elbo - delta):
+                min_elbo = metrics["ELBO"][-1]
+                stop_counter = 0  # Set counter to zero
+            else:  # elbo has not improved
+                stop_counter += 1
+
+            if stop_counter == patience:
+                num_epochs = epoch + 1
+                break
+
+        for stats_bar in stats_bars.values():
+            stats_bar.close()
+        tqdm.write(f"Completed {num_epochs} epochs in {time.time() - self.start_time:.2f} seconds.")
+
+        return (num_epochs, metrics)
+
+    def get_privacy_spent(self, delta):
+        if hasattr(self, "privacy_engine"):
+            return self.privacy_engine.get_privacy_spent(delta)
+        else:
+            print(
+                """This VAE object does not a privacy_engine attribute.
+                Run diff_priv_train to create one."""
+            )
+
+    def save(self, filename):
+        torch.save(self.state_dict(), filename)
+
+    def load(self, filename):
+        self.load_state_dict(torch.load(filename))
