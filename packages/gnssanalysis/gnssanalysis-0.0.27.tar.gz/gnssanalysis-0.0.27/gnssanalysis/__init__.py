0f42dbba170127850694bbc960dcf5f26163a53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:05 +0000

    Add package gnssanalysis/0.0.27/gnssanalysis-0.0.27.tar.gz

---
+++ packages/gnssanalysis/gnssanalysis-0.0.27.tar.gz/gnssanalysis-0.0.27/gnssanalysis/__init__.py
@@ -0,0 +1,16 @@
+from . import (
+    gn_aux,
+    gn_combi,
+    gn_const,
+    gn_datetime,
+    gn_diffaux,
+    gn_download,
+    gn_frame,
+    gn_io,
+    gn_plot,
+    gn_transform,
+    gn_utils,
+)
+
+from . import _version
+__version__ = _version.get_versions()['version']
