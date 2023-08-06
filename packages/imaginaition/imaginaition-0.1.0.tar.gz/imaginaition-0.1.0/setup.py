1cdbfffe150986376a4b3760f883308f02856409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:47 +0000

    Add package imaginaition/0.1.0/imaginaition-0.1.0.tar.gz

---
+++ packages/imaginaition/imaginaition-0.1.0.tar.gz/imaginaition-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imaginaition',
+    version='0.1.0',
+    author='jtpotato',
+    author_email='',
+    description='Imagination and technology for a better world.',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'imaginaition=imaginaition.__main__:main',
+        ],
+    },
+    install_requires=["auto-editor", "rgbprint"],
+)
