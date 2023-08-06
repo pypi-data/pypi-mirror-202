e04bc553280f88fd21d860d0bca857bc0b279db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:07 +0000

    Add package flagsmith-flag-engine/3.4.0/flagsmith-flag-engine-3.4.0.tar.gz

---
+++ packages/flagsmith-flag-engine/flagsmith-flag-engine-3.4.0.tar.gz/flagsmith-flag-engine-3.4.0/flag_engine/projects/models.py
@@ -0,0 +1,15 @@
+import typing
+from dataclasses import dataclass, field
+
+from flag_engine.organisations.models import OrganisationModel
+from flag_engine.segments.models import SegmentModel
+
+
+@dataclass
+class ProjectModel:
+    id: int
+    name: str
+    organisation: OrganisationModel
+    hide_disabled_flags: bool
+    segments: typing.List[SegmentModel] = field(default_factory=list)
+    enable_realtime_updates: bool = False
