0cc94722de5f730101d17f6c71df49f4ca1e5433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:22 +0000

    Add package myLibrary-Rodmichael-Marcus/1.0/myLibrary-Rodmichael-Marcus-1.0.tar.gz

---
+++ packages/myLibrary-Rodmichael-Marcus/myLibrary-Rodmichael-Marcus-1.0.tar.gz/myLibrary-Rodmichael-Marcus-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'myLibrary-Rodmichael-Marcus',
+      version = '1.0',
+      description = 'Package containing Linear, nodes and trees',
+      author = 'Marcus Kittelson & Rodmichael Umapas',
+      packages = ['myLib', 'myLib.datastructures', 'myLib.datastructures.Linear', 'myLib.datastructures.nodes', 'myLib.datastructures.trees'],
+      authors_email= 'marcus.kittelson@ucalgary.ca'
+)
