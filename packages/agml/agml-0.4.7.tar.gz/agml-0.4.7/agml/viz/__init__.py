b429300a8a783ae406bfdc606700e783b1d932ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:36 +0000

    Add package agml/0.4.7/agml-0.4.7.tar.gz

---
+++ packages/agml/agml-0.4.7.tar.gz/agml-0.4.7/agml/viz/__init__.py
@@ -0,0 +1,44 @@
+# Copyright 2021 UC Davis Plant AI and Biophysics Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .tools import (
+    format_image,
+    set_colormap,
+    get_colormap,
+    convert_figure_to_image
+)
+from .masks import (
+    output_to_mask,
+    visualize_image_and_mask,
+    overlay_segmentation_masks,
+    visualize_overlaid_masks,
+    visualize_image_mask_and_predicted
+)
+from .boxes import (
+    annotate_bboxes_on_image,
+    visualize_image_and_boxes,
+    visualize_real_and_predicted_bboxes,
+    visualize_image_and_many_boxes
+)
+from .labels import (
+    visualize_images_with_labels
+)
+from .general import (
+    visualize_images,
+    show_sample
+)
+from .inspection import (
+    plot_synthetic_camera_positions,
+    visualize_all_views
+)
