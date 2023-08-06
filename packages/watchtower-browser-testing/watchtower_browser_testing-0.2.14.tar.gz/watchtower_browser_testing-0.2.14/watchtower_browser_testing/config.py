9f0488df59ba5c7b144ba5a15908430e6c9a5364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:26 +0000

    Add package watchtower-browser-testing/0.2.14/watchtower_browser_testing-0.2.14.tar.gz

---
+++ packages/watchtower-browser-testing/watchtower_browser_testing-0.2.14.tar.gz/watchtower_browser_testing-0.2.14/watchtower_browser_testing/config.py
@@ -0,0 +1,11 @@
+TEST_FILE_PREFIX = 'tests_'
+DEFAULT_PIPELINE_PATTERN = 'https?://pipeline[.].*'
+SCENARIO_METHOD_PREFIX = 'scenario_'
+VALIDATION_METHOD_PREFIX = 'validation_'
+DEFAULT_TEST_DESCRIPTION = ''
+DEFAULT_BROWSERS = ['chromium', 'firefox', 'webkit']
+HARVEST_USER_SS_COOKIE_NAME = '_harvest_ss_user'
+HARVEST_USER_WEB_COOKIE_NAME = '_harvest_web_user'
+OLD_HARVEST_USER_ID_KEY = 'oldHarvestUserId'
+GTM_WEB_PREVIEW_LINK_REGEX = r'^https://tagassistant[.]google[.]com/.*'
+DEFAULT_PATH_TO_TAG_ASSISTANT_EXTENSION = r'tag_assistant'
