7880c69ea258ddfa8896f5644cc51ad51d0913c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package oss-test/0.0.3/oss_test-0.0.3-py3-none-any.whl

---
+++ packages/oss-test/oss_test-0.0.3-py3-none-any.whl/mycalc/setup.py
@@ -0,0 +1,15 @@
+
+import setuptools
+
+setuptools.setup(
+  name = 'oss-test',
+  version='0.0.1',
+  description= 'oss-dev my calculator 0001',
+  author ='dohyeong',
+  url='https://github.com/Kim-DO-Hyeong/calc0001',
+  download_url='https://github.com/Kim-DO-Hyeong/calc0001/archive/refs/tags/v0.0.1.zip',
+  packages = ['mycalc'],
+  classifiers=[
+    "Programming Language :: Python :: 3"
+  ]
+)
