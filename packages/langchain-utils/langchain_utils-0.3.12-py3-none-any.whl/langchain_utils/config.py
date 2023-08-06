32b497d4484c3f2cfd1669104f9004070d7c54d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:35 +0000

    Add package langchain-utils/0.3.12/langchain_utils-0.3.12-py3-none-any.whl

---
+++ packages/langchain-utils/langchain_utils-0.3.12-py3-none-any.whl/langchain_utils/config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+DEFAULT_PDF_WHAT = 'the content of a PDF file'
+DEFAULT_URL_WHAT = 'the content of a webpage'
+DEFAULT_HTML_WHAT = 'the text content of a html file'
+DEFAULT_GENERAL_WHAT = 'the content of a document'
