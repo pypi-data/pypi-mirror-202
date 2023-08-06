0bd6a4d5ff51ed1dfa0d019bcd3e601d8b98869e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:01 +0000

    Add package vmngclient/0.8.2/vmngclient-0.8.2-py3-none-any.whl

---
+++ packages/vmngclient/vmngclient-0.8.2-py3-none-any.whl/vmngclient/exceptions.py
@@ -0,0 +1,65 @@
+class InvalidOperationError(Exception):
+    """The exception that is thrown when a method call is invalid for the object's current state."""
+
+    pass
+
+
+class RetrieveIntervalOutOfRange(Exception):
+    pass
+
+
+class VersionDeclarationError(Exception):
+    """The exception that is thrown in one of two below cases.
+    1. User passes software image and version, at the same time.
+    2. User doesn't passes any of them."""
+
+    pass
+
+
+class ImageNotInRepositoryError(Exception):
+    """The exception that is thrown, if image is not in vManage images Repository"""
+
+    pass
+
+
+class TemplateNotFoundError(Exception):
+    """Used when a template item is not found."""
+
+    def __init__(self, template):
+        self.message = f"No such template: '{template}'"
+
+
+class AttachedError(Exception):
+    """Used when delete attached template."""
+
+    def __init__(self, template):
+        self.message = f"Template: {template} is attached to device."
+
+
+class TemplateTypeError(Exception):
+    """Used when wrong type template."""
+
+    def __init__(self, name):
+        self.message = f"Template: {name} - wrong template type."
+
+
+class AlreadyExistsError(Exception):
+    """Raised when an entity that we attempted to create already exists."""
+
+    pass
+
+
+class EmptyTaskResponseError(Exception):
+    """Raised if task is registred by vManage, but reponse content is empty"""
+
+    pass
+
+
+class TaskNotRegisteredError(Exception):
+    """Raised if task_id is generated, but it's not registere in vManage"""
+
+    pass
+
+
+class MultiplePersonalityError(Exception):
+    """Raised if Device DataSequnce contains devices with multiples personalities"""
