4cdd4cb6ba164225a67c320456cdf2fd445fe267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:49 +0000

    Add package pycachera/4.5.0/pycachera-4.5.0.tar.gz

---
+++ packages/pycachera/pycachera-4.5.0.tar.gz/pycachera-4.5.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+files = ["pycachera/*"]
+setup(
+        name = 'pycachera',
+        packages = ['pycachera'],
+        package_data = {'pycachera' : files },
+        version = '4.5.0',
+        install_requires = ['numpy','pandas'],
+        description = 'A powerfull python caching tool',
+        author = 'Anto Idicherian Lonappan',
+        author_email = 'mail@antolonappan.me',
+        url = 'https://github.com/antolonappan/pycachera',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        )
