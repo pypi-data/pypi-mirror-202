3cfa47d27f63ddccbbf9bf158b3440433a26d6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:54 +0000

    Add package dgg-bot/0.14.0/dgg_bot-0.14.0-py3-none-any.whl

---
+++ packages/dgg-bot/dgg_bot-0.14.0-py3-none-any.whl/dggbot/live/message.py
@@ -0,0 +1,47 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class StreamInfo:
+    live: bool
+    game: str
+    preview: str
+    status_text: str
+    started_at: str
+    ended_at: str
+    duration: int
+    viewers: int
+    id: str
+
+    @classmethod
+    def from_json(cls, data: dict) -> "StreamInfo":
+        yt = data["data"]["streams"]["youtube"]
+        del yt["platform"]
+        del yt["type"]
+        return cls(**yt)
+
+
+@dataclasses.dataclass
+class YoutubeVideo:
+    id: str
+    title: str
+    mediumThumbnailUrl: str
+    highThumbnailUrl: str
+    streamStartTime: str
+    streamEndTime: str
+    url: str
+
+    @classmethod
+    def from_json(cls, data: dict) -> tuple["YoutubeVideo"]:
+        return tuple(
+            cls(
+                vid["id"],
+                vid["title"],
+                vid["mediumThumbnailUrl"],
+                vid["highThumbnailUrl"],
+                vid["streamStartTime"],
+                vid["streamEndTime"],
+                vid["url"],
+            )
+            for vid in data["data"]
+        )
