da17224b730c3f9eb9ec86ca3176677347ab3eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:30 +0000

    Add package dkist-fits-specifications/3.5.0/dkist_fits_specifications-3.5.0.tar.gz

---
+++ packages/dkist-fits-specifications/dkist_fits_specifications-3.5.0.tar.gz/dkist_fits_specifications-3.5.0/check_changelog_updated.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CURRENT_VERSION=`git describe --tags --match v* --exact-match`
+ERROR_STR="CHANGELOG has not been updated for ${CURRENT_VERSION}\\nSee the README for instructions"
+ERROR_PAD="***********"
+
+head -n 1 CHANGELOG.rst | grep -q $CURRENT_VERSION - || {
+  echo -e "\\n${ERROR_PAD}"
+  echo -e $ERROR_STR;
+  echo -e "${ERROR_PAD}"
+  exit 1; }
