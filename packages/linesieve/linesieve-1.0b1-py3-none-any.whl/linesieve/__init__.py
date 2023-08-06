8142997931b2c1cd31a0175909871b476540cc3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:37 +0000

    Add package linesieve/1.0b1/linesieve-1.0b1-py3-none-any.whl

---
+++ packages/linesieve/linesieve-1.0b1-py3-none-any.whl/linesieve/__init__.py
@@ -0,0 +1,40 @@
+"""
+An unholy blend of grep, sed, awk, and Python.
+
+Read standard input, process it, and write to standard output.
+
+Split input into sections, and output selected sections.
+Filter output lines, globally or per-section.
+Compress runs of blank lines.
+
+All patterns use the Python regular expression syntax.
+
+Example:
+
+\b
+    $ cat simple.txt
+    0
+    one:
+    1...
+    two:
+    2 (two)
+    three:
+    3, three
+    fail
+
+\b
+    $ cat simple.txt \\
+    | linesieve --section '(\\S+):$' --failure fail \\
+      show --ignore-case ^O \\
+      match --section one --only-matching '\\d+' \\
+      sub '([a-z])' '\\1\\1\\1'
+    one  # dim
+    1
+    ..  # dim
+    three  # dim
+    3, ttthhhrrreeeeee
+    fail  # red
+
+"""
+
+__version__ = '1.0b1'
