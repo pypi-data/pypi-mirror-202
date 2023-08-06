ecf89d0eaeb72713d5a298708ab9bce7f14af00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:36 +0000

    Add package snowflake-ice-pick/0.0.1/snowflake_ice_pick-0.0.1.tar.gz

---
+++ packages/snowflake-ice-pick/snowflake_ice_pick-0.0.1.tar.gz/snowflake_ice_pick-0.0.1/src/ice_pick/__init__.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env python3
+#
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+#
+
+"""
+Contains core classes of Ice Pick
+"""
+
+__all__ = [
+    "SchemaObject",
+    "SchemaObjectFilter",
+    "extend_session",
+    "auto_union_standalone"
+]
+
+
+from ice_pick.version import VERSION
+
+__version__ = ".".join(str(x) for x in VERSION if x is not None)
+
+
+from ice_pick.schema_object import SchemaObject
+from ice_pick.filter import SchemaObjectFilter
+from ice_pick.extension import extend_session
+from ice_pick.utils import auto_union_standalone
+
