6c13b35a16849187404f838ca3864922c0dfbbd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:49 +0000

    Add package crazydoc/0.1.6/crazydoc-0.1.6.tar.gz

---
+++ packages/crazydoc/crazydoc-0.1.6.tar.gz/crazydoc-0.1.6/crazydoc/__init__.py
@@ -0,0 +1,11 @@
+""" dna_sequencing_viewer/__init__.py """
+
+# __all__ = []
+
+from .conf import conf
+from .Observers import StyleObserver
+from .CrazydocParser import CrazydocParser
+from .crazydoc_writers import write_crazydoc, write_crazyseq, make_groups
+from .plots import CrazydocSketcher
+from .biotools import records_to_genbank
+from .version import __version__
