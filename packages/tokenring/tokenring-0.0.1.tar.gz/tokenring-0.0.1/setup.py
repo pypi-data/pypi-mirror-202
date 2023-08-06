2cf91cb92f6eeeeb847295dd9572ad49b30c703b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package tokenring/0.0.1/tokenring-0.0.1.tar.gz

---
+++ packages/tokenring/tokenring-0.0.1.tar.gz/tokenring-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tokenring',
+    version='0.0.1',
+    description='To Be Announced',
+    long_description='',
+    url='https://github.com/glyph/tokenring',
+    author='me',
+    author_email='code@glyph.im',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
