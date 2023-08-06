cc57923c95ead5a8ecb9255f84777ed5c2e51f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:31 +0000

    Add package hardbrake/0.0.7/hardbrake-0.0.7.tar.gz

---
+++ packages/hardbrake/hardbrake-0.0.7.tar.gz/hardbrake-0.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt", "r") as f:
+  required_packages = f.read().splitlines()
+
+
+setup(
+    name="hardbrake",
+    version="0.0.7",
+    author="Tahsin",
+    author_email="hello@tahsin.us",
+    description="A wrapper around HandBrake CLI for encoding multiple files with ease.",
+    packages=find_packages(),
+    install_requires=required_packages,
+    data_files=[("", ["requirements.txt"])],
+    entry_points={
+        'console_scripts': [
+            'hardbrake = src.app:cli',
+            'hb = src.app:cli'
+        ]
+    }
+)
