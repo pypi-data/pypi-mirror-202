e8d808600364f30fa094a0d0b13f17ff7eef215a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:16 +0000

    Add package xs-transformers/1.0.0/xs_transformers-1.0.0.tar.gz

---
+++ packages/xs-transformers/xs_transformers-1.0.0.tar.gz/xs_transformers-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='xs_transformers',
+version='1.0.0',
+description='xs transformers from transformers',
+python_requires='>=3',
+package_dir={"xs_transformers": "xs_transformers"},
+packages=['xs_transformers']
+)
