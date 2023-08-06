44cae9e1f89ec1272b845702caf6f357769a3dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:20 +0000

    Add package OpenGeode-core/13.4.6/OpenGeode_core-13.4.6-cp37-cp37m-win_amd64.whl

---
+++ packages/OpenGeode-core/OpenGeode_core-13.4.6-cp37-cp37m-win_amd64.whl/opengeode/opengeode.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2019 - 2023 Geode-solutions
+
+from opengeode_basic import *
+from opengeode_geometry import *
+from opengeode_image import *
+from opengeode_mesh import *
+from opengeode_model import *
