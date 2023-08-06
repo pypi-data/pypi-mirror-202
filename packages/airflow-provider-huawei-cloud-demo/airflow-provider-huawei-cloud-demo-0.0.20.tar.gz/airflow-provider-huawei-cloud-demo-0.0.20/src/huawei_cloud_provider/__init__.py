a2644c238b5661748f5a54bbca11831e4b14f1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:05 +0000

    Add package airflow-provider-huawei-cloud-demo/0.0.20/airflow-provider-huawei-cloud-demo-0.0.20.tar.gz

---
+++ packages/airflow-provider-huawei-cloud-demo/airflow-provider-huawei-cloud-demo-0.0.20.tar.gz/airflow-provider-huawei-cloud-demo-0.0.20/src/huawei_cloud_provider/__init__.py
@@ -0,0 +1,19 @@
+# from setuptools.config.setupcfg import read_configuration as c;
+
+# __name__ = (c("setup.cfg")["metadata"]["name"])
+# __version__ = (c("setup.cfg")["metadata"]["version"])
+
+
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-huawei-cloud-demo",  # Required
+        "name": "Huawei Cloud Apache Airflow Provider",
+        "description": "Huawei Cloud Apache Airflow Provider",
+        "connection-types": [
+            {
+                "connection-type": "huaweicloud",
+                "hook-class-name": "huawei_cloud_provider.hooks.base_huawei_cloud.HuaweiBaseHook",
+            }
+        ],
+        "versions": ["0.0.20"],
+    }
