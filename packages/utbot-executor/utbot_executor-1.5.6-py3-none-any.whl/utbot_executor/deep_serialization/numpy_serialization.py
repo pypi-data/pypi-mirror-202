8262570a02a477956e0ca716306ab2c7770a9e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:38 +0000

    Add package utbot-executor/1.5.6/utbot_executor-1.5.6-py3-none-any.whl

---
+++ packages/utbot-executor/utbot_executor-1.5.6-py3-none-any.whl/utbot_executor/deep_serialization/numpy_serialization.py
@@ -0,0 +1,9 @@
+def is_numpy_ndarray(obj: object) -> bool:
+    try:
+        if hasattr(type(obj), '__module__') and hasattr(type(obj), '__qualname__'):
+            module_check = type(obj).__module__ == 'numpy'
+            name_check = type(obj).__qualname__ == 'ndarray'
+            return module_check and name_check
+        return False
+    except Exception:
+        return False
