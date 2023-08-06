c6ef619b96e2aaaed805d64ce974ac5fd90d98a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:49 +0000

    Add package jupyterlite-repl-prerun/0.1.0/jupyterlite_repl_prerun-0.1.0.tar.gz

---
+++ packages/jupyterlite-repl-prerun/jupyterlite_repl_prerun-0.1.0.tar.gz/jupyterlite_repl_prerun-0.1.0/src/index.ts
@@ -0,0 +1,40 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IConsoleTracker } from '@jupyterlab/console';
+
+/**
+ * Initialization data for the jupyterlite-repl-prerun extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlite-repl-prerun:plugin',
+  autoStart: true,
+  optional: [IConsoleTracker],
+  activate: (app: JupyterFrontEnd, tracker: IConsoleTracker | null) => {
+    if (!tracker) {
+      return;
+    }
+    const search = window.location.search;
+    const urlParams = new URLSearchParams(search);
+    const prerun = urlParams.getAll('prerun');
+
+    tracker.widgetAdded.connect(async (_, widget) => {
+      const { console } = widget;
+
+      if (prerun[0]) {
+        await console.sessionContext.ready;
+        prerun.forEach(line =>
+          console.sessionContext.session?.kernel?.requestExecute({
+            code: line,
+            silent: true,
+            store_history: false
+          })
+        );
+      }
+    });
+  }
+};
+
+export default plugin;
