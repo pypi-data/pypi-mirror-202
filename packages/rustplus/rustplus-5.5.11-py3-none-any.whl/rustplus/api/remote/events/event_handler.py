3168080fc0bf5da74d4ae9d29f1c64da3c5bd691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:11 +0000

    Add package rustplus/5.5.11/rustplus-5.5.11-py3-none-any.whl

---
+++ packages/rustplus/rustplus-5.5.11-py3-none-any.whl/rustplus/api/remote/events/event_handler.py
@@ -0,0 +1,69 @@
+import asyncio
+import logging
+from asyncio.futures import Future
+from typing import Set
+
+from .events import EntityEvent, TeamEvent, ChatEvent, ProtobufEvent
+from .registered_listener import RegisteredListener
+from .event_loop_manager import EventLoopManager
+
+
+class EventHandler:
+    @staticmethod
+    def schedule_event(loop, coro, arg) -> None:
+        def callback(inner_future: Future):
+            if inner_future.exception() is not None:
+                logging.getLogger("rustplus.py").exception(inner_future.exception())
+
+        future: Future = asyncio.run_coroutine_threadsafe(coro(arg), loop)
+        future.add_done_callback(callback)
+
+    def run_entity_event(self, name, app_message, server_id) -> None:
+
+        handlers: Set[RegisteredListener] = EntityEvent.handlers.get_handlers(
+            server_id
+        ).get(str(name))
+
+        if handlers is None:
+            return
+
+        for handler in handlers.copy():
+            coro, event_type = handler.data
+
+            self.schedule_event(
+                EventLoopManager.get_loop(server_id),
+                coro,
+                EntityEvent(app_message, event_type),
+            )
+
+    def run_team_event(self, app_message, server_id) -> None:
+
+        handlers: Set[RegisteredListener] = TeamEvent.handlers.get_handlers(server_id)
+        for handler in handlers.copy():
+            coro = handler.data
+
+            self.schedule_event(
+                EventLoopManager.get_loop(server_id), coro, TeamEvent(app_message)
+            )
+
+    def run_chat_event(self, app_message, server_id) -> None:
+
+        handlers: Set[RegisteredListener] = ChatEvent.handlers.get_handlers(server_id)
+        for handler in handlers.copy():
+            coro = handler.data
+
+            self.schedule_event(
+                EventLoopManager.get_loop(server_id), coro, ChatEvent(app_message)
+            )
+
+    def run_proto_event(self, byte_data: bytes, server_id) -> None:
+
+        handlers: Set[RegisteredListener] = ProtobufEvent.handlers.get_handlers(
+            server_id
+        )
+        for handler in handlers.copy():
+            coro = handler.data
+
+            self.schedule_event(
+                EventLoopManager.get_loop(server_id), coro, ProtobufEvent(byte_data)
+            )
