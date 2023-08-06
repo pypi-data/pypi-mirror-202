a8dcbd467e16bb7fa390790465d02225db98b4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:51 +0000

    Add package mediaify/1.2.4/mediaify-1.2.4-py3-none-any.whl

---
+++ packages/mediaify/mediaify-1.2.4-py3-none-any.whl/mediaify/configs.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+from typing_extensions import TypeAlias
+
+ImageConfig: TypeAlias = """
+    PNGEncodeConfig |
+    JPEGEncodeConfig |
+    WEBPImageEncodeConfig |
+    UnencodedConfig |
+    ThumbnailConfig
+"""
+AnimationConfig: TypeAlias = """
+    GIFEncodeConfig |
+    WEBPAnimationEncodeConfig |
+    UnencodedConfig |
+    ThumbnailConfig
+"""
+VideoConfig: TypeAlias = """
+    AnimationSummaryConfig  |
+    UnencodedConfig |
+    ThumbnailConfig
+"""
+
+
+
+@dataclass
+class ResizeConfig:
+    crop: bool = False
+    width: int|None = None
+    height: int|None = None
+
+    min_width: int|None = None
+    min_height: int|None = None
+
+    max_width: int|None = None
+    max_height: int|None = None
+    pass
+
+
+@dataclass
+class UnencodedConfig:
+    pass
+
+
+@dataclass
+class WEBPImageEncodeConfig:
+    width: int
+    height: int
+    quality: int = 85
+    lossless: bool = False
+
+
+@dataclass
+class PNGEncodeConfig:
+    width: int
+    height: int
+
+
+@dataclass
+class JPEGEncodeConfig:
+    width: int
+    height: int
+    quality: int = 85
+    progressive: bool = True
+
+
+@dataclass
+class GIFEncodeConfig:
+    width: int
+    height: int
+
+
+@dataclass
+class WEBPAnimationEncodeConfig:
+    width: int
+    height: int
+    quality: int = 85
+    lossless: bool = False
+
+
+@dataclass
+class ThumbnailConfig:
+    encoding: ImageConfig
+    offset: float = 0.2
+
+
+@dataclass
+class AnimationSummaryConfig:
+    encoding: AnimationConfig
+    framerate: int = 15
+    frames: int = 45
