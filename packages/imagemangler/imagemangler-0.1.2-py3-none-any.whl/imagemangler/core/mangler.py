a5075a5b63b7ee892ec75710c55265e087ae731d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:33 +0000

    Add package imagemangler/0.1.2/imagemangler-0.1.2-py3-none-any.whl

---
+++ packages/imagemangler/imagemangler-0.1.2-py3-none-any.whl/imagemangler/core/mangler.py
@@ -0,0 +1,18 @@
+import io
+
+from PIL import Image
+
+
+def deteriorate(img: Image, optimize: bool = True, quality: int = 20) -> Image:
+    """Deteriorate the image quality for `iterations` number of times"""
+
+    compressed_buffer = io.BytesIO()
+    img.save(compressed_buffer, format="JPEG", optimize=optimize, quality=quality)
+
+    # Get the compressed image data as bytes
+    compressed_bytes = compressed_buffer.getvalue()
+
+    # Load the compressed image into a PIL.Image object
+    img = Image.open(io.BytesIO(compressed_bytes))
+
+    return img
