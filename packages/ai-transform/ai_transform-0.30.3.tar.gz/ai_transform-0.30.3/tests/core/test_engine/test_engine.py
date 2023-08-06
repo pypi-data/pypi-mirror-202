cdcd1d2be880b2a6160fb177487f392769220bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:03 +0000

    Add package ai-transform/0.30.3/ai_transform-0.30.3.tar.gz

---
+++ packages/ai-transform/ai_transform-0.30.3.tar.gz/ai_transform-0.30.3/tests/core/test_engine/test_engine.py
@@ -0,0 +1,46 @@
+from typing import Any
+
+from ai_transform.dataset.dataset import Dataset
+from ai_transform.operator.abstract_operator import AbstractOperator
+from ai_transform.engine.abstract_engine import AbstractEngine
+
+
+class TestAbstractEngine:
+    def test_engine(self, full_dataset: Dataset, test_operator: AbstractOperator):
+        class ExampleEngine(AbstractEngine):
+            def apply(self) -> Any:
+
+                iterator = self.iterate()
+                for chunk in iterator:
+                    new_batch = self.operator(chunk)
+                    self.update_chunk(new_batch)
+
+                return
+
+        engine = ExampleEngine(full_dataset, test_operator)
+
+        assert isinstance(engine.operator, AbstractOperator)
+        assert len(ExampleEngine.__abstractmethods__) == 0
+
+    def test_engine_abstract(
+        self, full_dataset: Dataset, test_operator: AbstractOperator
+    ):
+        class ExampleEngine(AbstractEngine):
+            pass
+
+        try:
+            engine = ExampleEngine(full_dataset, test_operator)
+        except:
+            assert True
+
+    def test_engine_select_fields(
+        self, full_dataset: Dataset, test_operator: AbstractOperator
+    ):
+        class ExampleEngine(AbstractEngine):
+            def apply(self) -> Any:
+                return
+
+        engine = ExampleEngine(
+            full_dataset, test_operator, select_fields=["_id", "_chunk_.label"]
+        )
+        assert "_chunk_" in engine._select_fields
