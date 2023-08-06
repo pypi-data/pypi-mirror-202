2a8e08bf8fc1fa62256735345c2eedd623ac9059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:55 +0000

    Add package qtpy-fluent-widgets/0.0.2/QtPy_Fluent_Widgets-0.0.2-py3-none-any.whl

---
+++ packages/qtpy-fluent-widgets/QtPy_Fluent_Widgets-0.0.2-py3-none-any.whl/qtfluentwidgets/__init__.py
@@ -0,0 +1,19 @@
+"""
+qtpy-Fluent-Widgets
+======================
+A fluent design widgets library based on qtpy.
+
+Documentation is available in the docstrings and
+online at https://pyqt-fluent-widgets.readthedocs.io.
+
+Examples are available at https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/qtpy/examples.
+
+:copyright: (c) 2023 by zhiyiYo.
+:license: LGPLv3, see LICENSE for more details.
+"""
+
+__version__ = "0.6.8"
+
+from .components import *
+from .common import *
+from ._rc import resource
