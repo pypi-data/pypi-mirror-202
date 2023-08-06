6c19918998a9f796e70870ef57785893cb0cc655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:20 +0000

    Add package tcfg/0.4.4/tcfg-0.4.4-py3-none-any.whl

---
+++ packages/tcfg/tcfg-0.4.4-py3-none-any.whl/tcfg/__init__.py
@@ -0,0 +1,27 @@
+from .config import cfg
+from .reserved import *
+
+__version__ = "0.4.4"
+
+def tcfg(
+    cls=None,
+    *,
+    path: str|None = None,
+):
+    """Decorator to convert a class to a configuration class object."""
+
+    def wrapper(wrap=object):
+        if path is not None:
+            class Config(cfg, wrap):
+                """Config class from the config base class and the wrapped class."""
+                _path_ = path
+            return Config
+        else:
+            class Config(cfg, wrap):
+                """Config class from the config base class and the wrapped class."""
+            return Config
+
+    if cls is None:
+        return wrapper
+
+    return wrapper(cls)
