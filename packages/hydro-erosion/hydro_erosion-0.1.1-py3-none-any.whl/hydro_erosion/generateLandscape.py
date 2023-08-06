8b106d637425bfd87412df7bdec9fb83298c5545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:25 +0000

    Add package hydro-erosion/0.1.1/hydro_erosion-0.1.1-py3-none-any.whl

---
+++ packages/hydro-erosion/hydro_erosion-0.1.1-py3-none-any.whl/hydro_erosion/generateLandscape.py
@@ -0,0 +1,26 @@
+import noise
+from utilities import writeCSV, normalizeNoiseMap
+
+def generateNoiseMap(length, width, scale=190, roughness=5, shift=2.0):
+    octaves = roughness
+    persistence = .5
+    lacunarity = shift
+    map = [[noise.pnoise2(i/scale, j/scale,
+        octaves=octaves,
+        persistence=persistence,
+        lacunarity=lacunarity,
+        repeatx=length,
+        repeaty=width,
+        base=0
+    ) for i in range(width)] for j in range(length)]
+    return map
+
+def generateTerrainMaps(x=150, y=150):
+    heightmap = normalizeNoiseMap(generateNoiseMap(x,y), 0, 1)
+    rockmap = normalizeNoiseMap(generateNoiseMap(x,y, roughness=8), 0, 1)
+    return (heightmap, rockmap)
+
+if __name__ == '__main__':
+    heightmap, rockmap = generateTerrainMaps(300, 200)
+    writeCSV(heightmap, './maps/raw/heightmap.csv')
+    writeCSV(rockmap, './maps/raw/rockmap.csv')
