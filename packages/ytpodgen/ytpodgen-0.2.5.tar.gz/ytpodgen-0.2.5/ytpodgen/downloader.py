e1439078ff51aaa732836e6bd8672e4e9ff1b079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:46 +0000

    Add package ytpodgen/0.2.5/ytpodgen-0.2.5.tar.gz

---
+++ packages/ytpodgen/ytpodgen-0.2.5.tar.gz/ytpodgen-0.2.5/ytpodgen/downloader.py
@@ -0,0 +1,47 @@
+import yt_dlp
+
+
+class Downloader:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def download(title, liveurl):
+        live_from_start = True if Downloader._is_live(liveurl) else False
+
+        # See help(yt_dlp.YoutubeDL) for a list of available options
+        # and public functions
+        # https://github.com/yt-dlp/yt-dlp/blob/216bcb66d7dce0762767d751dad10650cb57da9d/yt_dlp/YoutubeDL.py#L184
+        ydl_opts = {
+            # best audio-only format if available,
+            # and if not, fall back to best format that contains both video and audio
+            "format": "bestaudio/best",
+            "wait_for_video": (60, 60),
+            "outtmpl": f"{title}_%(epoch>%Y%m%d%H%M%S)s_%(id)s",
+            "live_from_start": live_from_start,
+            "postprocessors": [
+                {  # Extract audio using ffmpeg
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "64",
+                }
+            ],
+            "postprocessor_args": [
+                "-ac",
+                "1",  # audio channel mono
+            ],
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(liveurl)
+
+    @staticmethod
+    def _is_live(liveurl):
+        ydl_opts = {}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            try:
+                info = ydl.extract_info(liveurl, download=False)
+                live_status = info["live_status"].rstrip("\n")
+                return True if live_status == "is_live" else False
+            except yt_dlp.utils.DownloadError:
+                return False
