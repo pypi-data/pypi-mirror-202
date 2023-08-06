8decefb9e3f167c77fddb0311edf4a9350361722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:20 +0000

    Add package hkkang-utils/0.1.51/hkkang_utils-0.1.51.tar.gz

---
+++ packages/hkkang-utils/hkkang_utils-0.1.51.tar.gz/hkkang_utils-0.1.51/src/hkkang_utils/list.py
@@ -0,0 +1,14 @@
+import operator
+import functools
+
+
+from typing import List, Any
+def do_flatten_list(list_of_list: List[List[Any]])-> List[Any]:
+    """Flatten a list of list to a list
+
+    :param list_of_list: list to flatten
+    :type list_of_list: List[List[Any]]
+    :return: flatten list
+    :rtype: List[Any]
+    """
+    return functools.reduce(operator.iconcat, list_of_list, [])
