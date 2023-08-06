7c0b79938b4ab25b3a96f1892e813c284ad26302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:19 +0000

    Add package casatools/6.5.5.21/casatools-6.5.5.21-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/casatools/casatools-6.5.5.21-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/casatools/__casac__/__init__.py
@@ -0,0 +1,81 @@
+from __future__ import absolute_import
+__name__ = '__casac__'
+__all__ = [ 'synthesismaskhandler',
+            'utils',
+            'miriadfiller',
+            'coordsys',
+            'mstransformer',
+            'vpmanager',
+            'image',
+            'sidebandseparator',
+            'measures',
+            'sakura',
+            'atmosphere',
+            'quanta',
+            'vlafiller',
+            'synthesisdeconvolver',
+            'synthesisimstore',
+            'synthesisnormalizer',
+            'regionmanager',
+            'calanalysis',
+            'iterbotsink',
+            'singledishms',
+            'synthesisimager',
+            'linearmosaic',
+            'imager',
+            'simulator',
+            'componentlist',
+            'spectralline',
+            'msmetadata',
+            'imagepol',
+            'agentflagger',
+            'sdm',
+            'atcafiller',
+            'imagemetadata',
+            'calibrater',
+            'logsink',
+            'functional',
+            'tablerow',
+            'ms',
+            'synthesisutils',
+            'table' ]
+
+from casatools.__casac__.synthesismaskhandler import synthesismaskhandler
+from casatools.__casac__.utils import utils
+from casatools.__casac__.miriadfiller import miriadfiller
+from casatools.__casac__.coordsys import coordsys
+from casatools.__casac__.mstransformer import mstransformer
+from casatools.__casac__.vpmanager import vpmanager
+from casatools.__casac__.image import image
+from casatools.__casac__.sidebandseparator import sidebandseparator
+from casatools.__casac__.measures import measures
+from casatools.__casac__.sakura import sakura
+from casatools.__casac__.atmosphere import atmosphere
+from casatools.__casac__.quanta import quanta
+from casatools.__casac__.vlafiller import vlafiller
+from casatools.__casac__.synthesisdeconvolver import synthesisdeconvolver
+from casatools.__casac__.synthesisimstore import synthesisimstore
+from casatools.__casac__.synthesisnormalizer import synthesisnormalizer
+from casatools.__casac__.regionmanager import regionmanager
+from casatools.__casac__.calanalysis import calanalysis
+from casatools.__casac__.iterbotsink import iterbotsink
+from casatools.__casac__.singledishms import singledishms
+from casatools.__casac__.synthesisimager import synthesisimager
+from casatools.__casac__.linearmosaic import linearmosaic
+from casatools.__casac__.imager import imager
+from casatools.__casac__.simulator import simulator
+from casatools.__casac__.componentlist import componentlist
+from casatools.__casac__.spectralline import spectralline
+from casatools.__casac__.msmetadata import msmetadata
+from casatools.__casac__.imagepol import imagepol
+from casatools.__casac__.agentflagger import agentflagger
+from casatools.__casac__.sdm import sdm
+from casatools.__casac__.atcafiller import atcafiller
+from casatools.__casac__.imagemetadata import imagemetadata
+from casatools.__casac__.calibrater import calibrater
+from casatools.__casac__.logsink import logsink
+from casatools.__casac__.functional import functional
+from casatools.__casac__.tablerow import tablerow
+from casatools.__casac__.ms import ms
+from casatools.__casac__.synthesisutils import synthesisutils
+from casatools.__casac__.table import table
