2519d55fc4e66756135c5329fe154323f65a64ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:34 +0000

    Add package base16-colorlib/0.2.0/base16_colorlib-0.2.0-py3-none-any.whl

---
+++ packages/base16-colorlib/base16_colorlib-0.2.0-py3-none-any.whl/base16_colorlib/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# flake8: noqa
+from . import color_scheme
+from . import utils
+from .utils import Color
+from .color_scheme import (
+    aquarium,
+    ashes,
+    ayu_dark,
+    ayu_light,
+    bearded_arc,
+    blossom_light,
+    catppuccin_latte,
+    catppuccin_frappe,
+    catppuccin_macchiato,
+    catppuccin_mocha,
+    dracula,
+    decay,
+    everblush,
+    everforest_dark,
+    everforest_light,
+    falcon,
+    gruvbox_dark,
+    gruvbox_light,
+    kanagawa,
+    melange,
+    monokai,
+    monochrome,
+    mountain,
+    nord,
+    onedark,
+    onelight,
+    rosepine,
+    rosepine_moon,
+    rosepine_dawn,
+    rxyhn,
+    solarized,
+    sweetpastel,
+    tokyodark,
+    tokyonight,
+    yoru
+)
+from .version import __version__
